984c2cc3ff60b67f83f5b893087c10cd899d24bb
Miguel Covarrubias <mcovarr@broadinstitute.org>
2015-06-25 12:28:59 -0400

    A final-ish version of the DataAccess API for persistence to unblock implementation in Slick.

--- src/main/scala/cromwell/engine/db/LocalCallInfo.scala
+++
@@ -1,6 +0,0 @@
-package cromwell.engine.db
-
-import cromwell.binding.FullyQualifiedName
-
-case class LocalCallInfo(callFqn: FullyQualifiedName, status: CallStatus,
-                         processId: Int, command: String, resultCode: Int) extends CallInfo

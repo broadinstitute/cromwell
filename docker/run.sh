b0db9d398fab43cc66e22d0e5e7be4eb93eecc80
Khalid Shakir <kshakir@broadinstitute.org>
2015-07-08 02:06:11 -0300

    Updated run.sh to pass in the mysql key & trust stores. Also updated jenkins with file that appears in the dev /etc/cromwell.conf.

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,15 @@
 
 set -e
 
-java -jar $(find /cromwell | grep 'cromwell.*\.jar') server
+key_store_file=${CROMWELL_KEY_STORE_FILE-/etc/cromwell.keystore}
+key_store_pass=${CROMWELL_KEY_STORE_PASS-changeit}
+trust_store_file=${CROMWELL_TRUST_STORE_FILE-/etc/cromwell.truststore}
+trust_store_pass=${CROMWELL_TRUST_STORE_PASS-changeit}
+
+java \
+  -Djavax.net.ssl.keyStore=${key_store_file} \
+  -Djavax.net.ssl.keyStorePassword=${key_store_pass} \
+  -Djavax.net.ssl.trustStore=${trust_store_file} \
+  -Djavax.net.ssl.trustStorePassword=${trust_store_pass} \
+  -jar $(find /cromwell | grep 'cromwell.*\.jar') \
+  server

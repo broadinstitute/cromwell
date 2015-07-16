74a125f4f6ca9fd892fffbed586d3f3f0d31e30d
Miguel Covarrubias <mcovarr@broadinstitute.org>
2015-07-16 13:02:52 -0400

    Test at JDBC level.

--- src/main/scala/cromwell/engine/db/slick/RunMysql.scala
+++ src/main/scala/cromwell/engine/db/slick/RunMysql.scala
@@ -1,5 +1,7 @@
 package cromwell.engine.db.slick
 
+import java.sql.DriverManager
+
 import slick.jdbc.SimpleJdbcAction
 
 import scala.concurrent.Await
@@ -9,6 +11,20 @@ import scala.language.postfixOps
 object RunMysql {
 
   def main(args: Array[String]): Unit = {
+    Class.forName("com.mysql.jdbc.Driver").newInstance()
+    val config = DatabaseConfig.databaseConfig
+
+    val Seq(url, user, password) = Seq("url", "user", "password") map {
+      key => if (config.hasPath(key)) config.getString(key) else ""
+    }
+
+    val connectionString = s"$url?user=$user&password=$password"
+    println(s"Testing connection '$connectionString'")
+    val connection = DriverManager.getConnection(connectionString)
+    println("Connection valid? " + connection.isValid(5))
+  }
+
+  def slickMain(args: Array[String]): Unit = {
     val dataAccess = new SlickDataAccess()
 
     val connectionValidFuture = dataAccess.database.run(SimpleJdbcAction(_.connection.isValid(1)))

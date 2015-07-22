4d687f79a72b06d4e30ce609b83e2721d2930a0b
Andrew Teixeira <teixeira@broadinstitute.org>
2015-07-22 11:58:31 -0400

    Update run.sh to use JAVA_OPTS instead of hard-coded -D options

--- docker/run.sh
+++ docker/run.sh
@@ -2,16 +2,4 @@
 
 set -e
 
-key_store_file=${CROMWELL_KEY_STORE_FILE-/etc/cromwell.keystore}
-key_store_pass=${CROMWELL_KEY_STORE_PASS-changeit}
-trust_store_file=${CROMWELL_TRUST_STORE_FILE-/etc/cromwell.truststore}
-trust_store_pass=${CROMWELL_TRUST_STORE_PASS-changeit}
-
-java \
-  -Djavax.net.ssl.keyStore=${key_store_file} \
-  -Djavax.net.ssl.keyStorePassword=${key_store_pass} \
-  -Djavax.net.ssl.trustStore=${trust_store_file} \
-  -Djavax.net.ssl.trustStorePassword=${trust_store_pass} \
-  -Dconfig.file=/etc/cromwell.conf \
-  -jar $(find /cromwell | grep 'cromwell.*\.jar') \
-  server
+java $JAVA_OPTS -jar $(find /cromwell | grep 'cromwell.*\.jar') server

fc6164fff672490577829787c7a2ace948cca6f1
Miguel Covarrubias <mcovarr@broadinstitute.org>
2015-10-08 18:05:15 -0400

    Removing Await.results: 1 down 6 to go.

--- src/test/scala/cromwell/SimpleWorkflowActorSpec.scala
+++ src/test/scala/cromwell/SimpleWorkflowActorSpec.scala
@@ -2,10 +2,10 @@ package cromwell
 
 import java.util.UUID
 
-import akka.pattern.ask
+import akka.actor
+import akka.actor.ActorInitializationException
 import akka.testkit._
 import cromwell.binding._
-import cromwell.binding.values.WdlString
 import cromwell.engine._
 import cromwell.engine.backend.local.LocalBackend
 import cromwell.engine.workflow.WorkflowActor
@@ -14,65 +14,58 @@ import cromwell.parser.BackendType
 import cromwell.util.SampleWdl
 import cromwell.util.SampleWdl.HelloWorld.Addressee
 
-import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
 
 class SimpleWorkflowActorSpec extends CromwellTestkitSpec("SimpleWorkflowActorSpec") {
 
-  private def buildWorkflowFSMRef(sampleWdl: SampleWdl, rawInputsOverride: Option[WorkflowRawInputs] = None):
+  private def buildWorkflowFSMRef(sampleWdl: SampleWdl, rawInputsOverride: String):
   TestFSMRef[WorkflowState, WorkflowFailure, WorkflowActor] = {
-
-    val namespace = NamespaceWithWorkflow.load(sampleWdl.wdlSource(), BackendType.LOCAL)
-    val rawInputs = rawInputsOverride.getOrElse(sampleWdl.rawInputs)
-    val coercedInputs = namespace.coerceRawInputs(rawInputs).get
-    val workflowSources = WorkflowSourceFiles(sampleWdl.wdlSource(), sampleWdl.wdlJson, "{}")
+    val workflowSources = WorkflowSourceFiles(sampleWdl.wdlSource(), rawInputsOverride, "{}")
     val descriptor = WorkflowDescriptor(WorkflowId(UUID.randomUUID()), workflowSources)
     TestFSMRef(new WorkflowActor(descriptor, new LocalBackend))
   }
 
-  val TestExecutionTimeout = 5000 milliseconds
+  val TestExecutionTimeout = 5.seconds.dilated
 
   "A WorkflowActor" should {
     "start, run, succeed and die" in {
-      val fsm = buildWorkflowFSMRef(SampleWdl.HelloWorld)
-      val probe = TestProbe()
-      probe watch fsm
-      assert(fsm.stateName == WorkflowSubmitted)
       startingCallsFilter("hello.hello") {
-        fsm ! Start
+        val fsm = buildWorkflowFSMRef(SampleWdl.HelloWorld, SampleWdl.HelloWorld.wdlJson)
+        val probe = TestProbe()
+        probe watch fsm
         within(TestExecutionTimeout) {
-          awaitCond(fsm.stateName == WorkflowRunning)
-          awaitCond(fsm.stateName == WorkflowSucceeded)
+          waitForPattern("transitioning from Submitted to Running") {
+            waitForPattern("transitioning from Running to Succeeded") {
+              fsm ! Start
+            }
+          }
         }
-        probe.expectTerminated(fsm, 10 seconds)
+        probe.expectTerminated(fsm, 10.seconds.dilated)
       }
     }
 
     "fail to construct with missing inputs" in {
-      intercept[UnsatisfiedInputsException] {
-        buildWorkflowFSMRef(SampleWdl.HelloWorld, rawInputsOverride = Some(Map.empty))
+      intercept[UnsatisfiedInputsException]  {
+        buildWorkflowFSMRef(SampleWdl.HelloWorld, rawInputsOverride = "{}")
       }
     }
 
     "fail to construct with inputs of the wrong type" in {
       intercept[UnsatisfiedInputsException] {
-        buildWorkflowFSMRef(SampleWdl.HelloWorld, rawInputsOverride = Some(Map(Addressee -> 3)))
+        buildWorkflowFSMRef(SampleWdl.HelloWorld, rawInputsOverride = s""" { "$Addressee" : 3} """)
       }
     }
 
     "fail when a call fails" in {
-      val fsm = buildWorkflowFSMRef(SampleWdl.GoodbyeWorld)
-      assert(fsm.stateName == WorkflowSubmitted)
       startingCallsFilter("goodbye.goodbye") {
-        waitForPattern("persisting status of goodbye.goodbye to Starting.") {
-          waitForPattern("persisting status of goodbye.goodbye to Running.") {
-            waitForPattern("persisting status of goodbye.goodbye to Failed.") {
-              waitForPattern("WorkflowActor .+ transitioning from Running to Failed\\.") {
+        waitForPattern("WorkflowActor .+ transitioning from Submitted to Running\\.") {
+          waitForPattern("persisting status of goodbye.goodbye to Starting.") {
+            waitForPattern("persisting status of goodbye.goodbye to Running.") {
+              waitForPattern("persisting status of goodbye.goodbye to Failed.") {
+                val fsm = buildWorkflowFSMRef(SampleWdl.GoodbyeWorld, SampleWdl.GoodbyeWorld.wdlJson)
                 fsm ! Start
-                awaitCond(fsm.stateName == WorkflowRunning)
-                awaitCond(fsm.stateName == WorkflowFailed)
               }
             }
           }
@@ -81,10 +74,10 @@ class SimpleWorkflowActorSpec extends CromwellTestkitSpec("SimpleWorkflowActorSp
     }
 
     "gracefully handle malformed WDL" in {
-      val fsm = buildWorkflowFSMRef(SampleWdl.CoercionNotDefined)
-      assert(fsm.stateName == WorkflowSubmitted)
-      fsm ! Start
       within(TestExecutionTimeout) {
+        val fsm = buildWorkflowFSMRef(SampleWdl.CoercionNotDefined, SampleWdl.CoercionNotDefined.wdlJson)
+        fsm ! Start
+        awaitCond(fsm.stateName == WorkflowSubmitted)
         awaitCond(fsm.stateName == WorkflowFailed)
       }
     }

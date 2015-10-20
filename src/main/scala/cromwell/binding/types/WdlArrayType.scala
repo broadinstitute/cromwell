1785f1895c169671c1072ecb873f3a85b43f8a25
Scott Frazer <scott.d.frazer@gmail.com>
2015-10-20 09:22:07 -0400

    Adding coercion for Array[Array[_]]

--- src/main/scala/cromwell/binding/types/WdlArrayType.scala
+++ src/main/scala/cromwell/binding/types/WdlArrayType.scala
@@ -2,6 +2,7 @@ package cromwell.binding.types
 
 import cromwell.binding.values.{WdlValue, WdlArray, WdlFile, WdlString}
 import spray.json.JsArray
+import scala.util.Failure
 
 case class WdlArrayType(memberType: WdlType) extends WdlType {
   val toWdlString: String = s"Array[${memberType.toWdlString}]"
@@ -20,6 +21,15 @@ case class WdlArrayType(memberType: WdlType) extends WdlType {
       WdlArray(WdlArrayType(WdlFileType), wdlArray.value.map(str => WdlFile(str.asInstanceOf[WdlString].value)).toList)
     case wdlArray: WdlArray if wdlArray.wdlType.memberType == memberType => wdlArray
     case wdlArray: WdlArray if wdlArray.wdlType.memberType == WdlAnyType => coerceIterable(wdlArray.value)
+    case wdlArray: WdlArray if wdlArray.wdlType.memberType.isInstanceOf[WdlArrayType] && memberType.isInstanceOf[WdlArrayType] =>
+      val coercedElements = wdlArray.value.map(memberType.coerceRawValue)
+      val coercedArray = coercedElements.collect({case x: Failure[_] => x}) match {
+        case failures if failures.nonEmpty =>
+          throw new UnsupportedOperationException(failures.map(f => s"Could not coerce $wdlArray: ${f.exception.getMessage}").mkString("\n"))
+        case _ => coercedElements.map(_.get) // .get because they're all Success
+      }
+      WdlArray(WdlArrayType(memberType), coercedArray)
+
   }
 
   override def isCoerceableFrom(otherType: WdlType): Boolean = otherType match {

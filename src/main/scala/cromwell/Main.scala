2f7f3cc44a2cd0c7a260d064e322a5622313182b
Thibault Jeandet <tjeandet@broadinstitute.org>
2015-11-10 10:34:32 -0500

    Fix switch + Main bugs

--- src/main/scala/cromwell/Main.scala
+++ src/main/scala/cromwell/Main.scala
@@ -139,7 +139,7 @@ class Main private[cromwell](enableSysExit: Boolean, managerSystem: () => Workfl
     // Return the path for the arg index, or the default, but remove "-" paths.
     for {
       path <- args.lift(index) orElse defaultPath filterNot (_ == "-")
-    } yield Paths.get(args.head).resolveSibling(path)
+    } yield Paths.get(path)
   }
 
   private[this] def runWorkflow(workflowSourceFiles: WorkflowSourceFiles, metadataPath: Option[Path]): Int = {

627e19586d3f0977491ee0e6af1b0adbcf3a3475
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-01-11 18:54:52 -0500

    Add WDL Exception with error list support

--- src/main/scala/wdl4s/UnsatisfiedInputsException.scala
+++
@@ -1,5 +0,0 @@
-package wdl4s
-
-class UnsatisfiedInputsException(message: String, cause: Throwable) extends RuntimeException(message, cause) {
-  def this(message: String) = this(message, null)
-}
---
+++ src/test/scala/wdl4s/WdlExceptionsSpec.scala
@@ -0,0 +1,22 @@
+package wdl4s
+
+import org.scalatest.{Matchers, FlatSpec}
+
+import scalaz.NonEmptyList
+
+class WdlExceptionsSpec extends FlatSpec with Matchers {
+
+  "ThrowableWithErrors Exceptions" should "aggregate errors in getMessage method" in {
+    val exception = new Throwable with ThrowableWithErrors {
+      val message = "This is absolutely NOT working."
+      val errors = NonEmptyList("because of A", "and also B", "and maybe C")
+    }
+
+    exception.getMessage shouldBe
+      """This is absolutely NOT working.
+        |because of A
+        |and also B
+        |and maybe C""".stripMargin
+  }
+
+}

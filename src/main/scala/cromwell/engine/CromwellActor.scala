18410ec8d5949fcc7052df5a136075f30b77b254
dgtester <dtester@google.com>
2016-01-21 16:15:27 -0500

    Aborts jobs on shutdown.
    Added configuration option for abortJobsOnTerminate. Added support for server mode. SingleWorkflowRunnerActor now uses the internal state to determine whether the workflow is done (and not a separately maintained flag).
    
    Added configuration option for abortJobsOnTerminate. Added support for server mode. SingleWorkflowRunnerActor now uses the internal state to determine whether the workflow is done (and not a separately maintained flag).

--- src/main/scala/cromwell/engine/CromwellActor.scala
+++ src/main/scala/cromwell/engine/CromwellActor.scala
@@ -1,10 +1,26 @@
 package cromwell.engine
 
 import akka.util.Timeout
+import com.typesafe.config.{ConfigException, ConfigFactory}
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
 trait CromwellActor {
   protected implicit val timeout = Timeout(5 seconds)
+
+  /**
+   * Retrieves the configuration option that determines whether this actor should abort all jobs if it receives
+   * a shutdown hook.
+   * @return - The value of the configuration option, or 'false' if the option isn't specified.
+   */
+  def getAbortJobsOnTerminate: Boolean = {
+    val config=ConfigFactory.load.getConfig("backend")
+    try {
+      config.getBoolean("abortJobsOnTerminate")
+    } catch {
+      case _:ConfigException  => false
+    }
+  }
+
 }

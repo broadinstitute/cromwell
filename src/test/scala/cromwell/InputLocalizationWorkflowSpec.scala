2d0418140797a79a408aac3469ce908c0df05f03
Francisco Casares <francisco.m.casares@intel.com>
2016-02-10 10:56:47 -0800

    Fixed unit test case.

--- src/test/scala/cromwell/InputLocalizationWorkflowSpec.scala
+++ src/test/scala/cromwell/InputLocalizationWorkflowSpec.scala
@@ -11,7 +11,7 @@ class InputLocalizationWorkflowSpec extends CromwellTestkitSpec {
   "a workflow running on a SharedFileSystem" should {
 
     val expectedOutputs = Map(
-      "wf.fromDifferentDirectories.ls" -> WdlString("1"),
+      "wf.fromDifferentDirectories.ls" -> WdlString("5"),
       "wf.echo_int.out" -> WdlArray(WdlArrayType(WdlFileType), Seq(WdlFile("out"), WdlFile("out"))),
       "wf.fromSameDirectory.ls" -> WdlString("2")
     )

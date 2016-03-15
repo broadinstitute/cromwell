6a3311fbb6a8a6c6c35c840d554a5df84a3f7e1b
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-03-15 16:50:26 -0400

    PR comments

--- engine/src/main/scala/cromwell/engine/backend/ExecutionHandle.scala
+++ engine/src/main/scala/cromwell/engine/backend/ExecutionHandle.scala
@@ -3,39 +3,6 @@ package cromwell.engine.backend
 import cromwell.engine.{CallOutputs, ExecutionEventEntry, ExecutionHash}
 
 /**
- * Represents a Call that is intended to be run on a specific Backend.
- *
- * A BackendCall is created by calling `Backend.bindCall(c: Call)`.  The
- * combination of a Call and a Backend give enough context to be able to
- * instantiate the Call's Task's command line, and provide an implementation
- * of the WDL standard library functions as well as a lookup functions to
- * resolve identifiers in expressions.
- *
- * The BackendCall also includes a Map[String, WdlValue] (CallInputs) which
- * represents the locally-qualified input values for this call.
- *
- * BackendCall aims to package everything that a Call needs to run into one
- * object so a client can do a parameterless `.execute()` to run the job.
- *
- * To understand why one needs a Backend and locally-qualified inputs in order
- * to instantiate a command, consider this example:
- *
- * task test {
- *   Array[String] something
- *   command {
- *     ./analysis --something-list=${write_lines(something)}
- *   }
- * }
- *
- * Without the Backend and the locallyQualifiedInputs, the expression
- * `write_lines(something)` could not be evaluated because:
- *
- * 1) We need to resolve "something" in locallyQualifiedInputs
- * 2) We need a place to write the results of `write_lines()`, and that location
- *    is backend-specific
- */
-
-/**
  * Trait to encapsulate whether an execution is complete and if so provide a result.  Useful in conjunction
  * with the `poll` API to feed results of previous job status queries forward.
  */

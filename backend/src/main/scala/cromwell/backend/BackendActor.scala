78adbb2e77a277efa14128116d652538f81f15d3
Francisco Casares <francisco.m.casares@intel.com>
2016-03-23 16:52:50 -0700

    - Removed Backend and JobExecutor traits. - Created a hierachy of events

--- backend/src/main/scala/cromwell/backend/BackendActor.scala
+++ backend/src/main/scala/cromwell/backend/BackendActor.scala
@@ -1,29 +1,37 @@
 package cromwell.backend
 
-import akka.actor.{ActorRef, Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, ActorRef}
 import akka.event.LoggingReceive
-import cromwell.backend.BackendActor.{Validate, AfterAll, BeforeAll, CreateJobExecutor}
-import cromwell.backend.model.JobDescriptor
+import cromwell.backend.BackendActor._
+import cromwell.backend.model.{JobDescriptor, WorkflowDescriptor}
 
 object BackendActor {
 
   // Commands
   sealed trait BackendActorMessage
   case object BeforeAll extends BackendActorMessage
-  case class CreateJobExecutor(jobDescriptor: JobDescriptor) extends BackendActorMessage
+  case class CreateJobExecutorActor(jobDescriptor: JobDescriptor) extends BackendActorMessage
   case object AfterAll extends BackendActorMessage
   case object Validate extends BackendActorMessage
 
   // Events
   sealed trait BackendActorEvent
-  case object BeforeAllCompleted extends BackendActorEvent
-  case class BeforeAllFailed(throwable: Throwable) extends BackendActorEvent
-  case class JobExecutorCreated(backendJobExecutor: ActorRef) extends BackendActorEvent
-  case class JobExecutorCreationFailed(throwable: Throwable) extends BackendActorEvent
-  case object AfterAllCompleted extends BackendActorEvent
-  case class AfterAllFailed(throwable: Throwable) extends BackendActorEvent
-  case object ValidationCompleted extends BackendActorEvent
-  case class ValidationFailed(throwable: Throwable) extends BackendActorEvent
+
+  sealed trait BeforeAllEvent extends BackendActorEvent
+  case object BeforeAllSucceeded extends BeforeAllEvent
+  case class BeforeAllFailed(throwable: Throwable) extends BeforeAllEvent
+
+  sealed trait JobExecutorCreationEvent extends BackendActorEvent
+  case class JobExecutorCreationSucceeded(backendJobExecutorActor: ActorRef) extends JobExecutorCreationEvent
+  case class JobExecutorCreationFailed(throwable: Throwable) extends JobExecutorCreationEvent
+
+  sealed trait AfterAllEvent extends BackendActorEvent
+  case object AfterAllSucceeded extends AfterAllEvent
+  case class AfterAllFailed(throwable: Throwable) extends AfterAllEvent
+
+  sealed trait ValidationEvent extends BackendActorEvent
+  case object ValidationSucceeded extends ValidationEvent
+  case class ValidationFailed(throwable: Throwable) extends ValidationEvent
 
 }
 
@@ -31,11 +39,47 @@ object BackendActor {
   * Defines basic structure and functionality to initialize and make use of a backend through an Akka actor system.
   * Backend functions should be implemented by each custom backend.
   */
-trait BackendActor extends Backend with Actor with ActorLogging {
+trait BackendActor extends Actor with ActorLogging {
+  /**
+    * Defines needed data to be able to execute a workflow.
+    */
+  val workflowDescriptor: WorkflowDescriptor
+
   def receive: Receive = LoggingReceive {
-    case BeforeAll => beforeAll()
-    case CreateJobExecutor(jobDescriptor: JobDescriptor) => createJobExecutor(jobDescriptor)
-    case AfterAll => afterAll()
-    case Validate => validate() //TODO: to be discussed.
+    case BeforeAll =>
+      val sndr = sender()
+      sndr ! beforeAll()
+    case CreateJobExecutorActor(jobDescriptor: JobDescriptor) =>
+      val sndr = sender()
+      sndr ! createJobExecutorActor(jobDescriptor)
+    case AfterAll =>
+      val sndr = sender()
+      sndr ! afterAll()
+    case Validate => //TODO: to be discussed.
+      val sndr = sender()
+      sndr ! validate()
   }
+
+  /**
+    * Registers code to be executed before the backend is ready for executing jobs for the specific workflow.
+    */
+  def beforeAll(): BeforeAllEvent
+
+  /**
+    * Factory for creating Jobs in the specific backend.
+    *
+    * @param jobDescriptor All information needed to execute a task in the backend.
+    * @return A BackendJobExecutor with functionality to handle the life cycle management of the task.
+    */
+  def createJobExecutorActor(jobDescriptor: JobDescriptor): JobExecutorCreationEvent
+
+  /**
+    * Registers code to be executed after the backend finished executing all related tasks for the specific workflow.
+    */
+  def afterAll(): AfterAllEvent
+
+  /**
+    * Executes validation on workflow descriptor in order to see if the workflow can be executed by the backend.
+    */
+  def validate(): ValidationEvent
 }
--- backend/src/main/scala/cromwell/backend/BackendJobExecutorActor.scala
+++ backend/src/main/scala/cromwell/backend/BackendJobExecutorActor.scala
@@ -2,7 +2,8 @@ package cromwell.backend
 
 import akka.actor.{Actor, ActorLogging}
 import akka.event.LoggingReceive
-import cromwell.backend.BackendJobExecutorActor.{CleanUp, Execute, Prepare, Stop}
+import cromwell.backend.BackendJobExecutorActor._
+import cromwell.backend.model.JobDescriptor
 
 object BackendJobExecutorActor {
 
@@ -16,15 +17,22 @@ object BackendJobExecutorActor {
   //TODO: identify other generic events within job execution functionality.
   // Events
   sealed trait BackendJobExecutorActorEvent
-  case object PrepareCompleted extends BackendJobExecutorActorEvent
-  case class PrepareFailed(throwable: Throwable) extends BackendJobExecutorActorEvent
-  case object ExecutionStarted extends BackendJobExecutorActorEvent
-  case class ExecutionCompleted(result: String) extends BackendJobExecutorActorEvent
-  case class ExecutionFailed(throwable: Throwable) extends BackendJobExecutorActorEvent
-  case object StopCompleted extends BackendJobExecutorActorEvent
-  case class StopFailed(throwable: Throwable) extends BackendJobExecutorActorEvent
-  case object CleanUpCompleted extends BackendJobExecutorActorEvent
-  case class CleanUpFailed(throwable: Throwable) extends BackendJobExecutorActorEvent
+
+  sealed trait PrepareEvent extends BackendJobExecutorActorEvent
+  case object PrepareSucceeded extends PrepareEvent
+  case class PrepareFailed(throwable: Throwable) extends PrepareEvent
+
+  sealed trait ExecutionEvent extends BackendJobExecutorActorEvent
+  case class ExecutionSucceeded(result: String) extends ExecutionEvent
+  case class ExecutionFailed(throwable: Throwable) extends ExecutionEvent
+
+  sealed trait StopEvent extends BackendJobExecutorActorEvent
+  case object StopSucceeded extends StopEvent
+  case class StopFailed(throwable: Throwable) extends StopEvent
+
+  sealed trait CleanUpEvent extends BackendJobExecutorActorEvent
+  case object CleanUpSucceeded extends CleanUpEvent
+  case class CleanUpFailed(throwable: Throwable) extends CleanUpEvent
 
 }
 
@@ -32,11 +40,44 @@ object BackendJobExecutorActor {
   * Defines basic structure and functionality to execute a job in a backend through an Akka actor system.
   * BackendJobExecutor functions should be implemented by each custom backend.
   */
-trait BackendJobExecutorActor extends BackendJobExecutor with Actor with ActorLogging {
+trait BackendJobExecutorActor extends Actor with ActorLogging {
+  /**
+    * Defines needed data to be able to execute a job.
+    */
+  val jobDescriptor: JobDescriptor
+
   def receive: Receive = LoggingReceive {
-    case Prepare => prepare()
-    case Execute => execute()
-    case Stop => stop()
-    case CleanUp => cleanUp()
+    case Prepare =>
+      val sndr = sender()
+      sndr ! prepare()
+    case Execute =>
+      val sndr = sender()
+      sndr ! execute()
+    case Stop =>
+      val sndr = sender()
+      sndr ! stop()
+    case CleanUp =>
+      val sndr = sender()
+      sndr ! cleanUp()
   }
+
+  /**
+    * Prepare the task and context for execution.
+    */
+  def prepare(): PrepareEvent
+
+  /**
+    * Executes task in given context.
+    */
+  def execute(): ExecutionEvent
+
+  /**
+    * Stops a task execution.
+    */
+  def stop(): StopEvent
+
+  /**
+    * Performs a cleanUp after the task was executed.
+    */
+  def cleanUp(): CleanUpEvent
 }

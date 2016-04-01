ff7a4bc8554a5b0359a5d585fa48f8b87445ec34
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-04-01 10:40:02 -0400

    Update lenthall and wdl4s versions for realease

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  lazy val lenthallV = "0.16"
-  lazy val wdl4sV = "0.4-438b5bd-SNAPSHOT"
+  lazy val lenthallV = "0.17"
+  lazy val wdl4sV = "0.4"
   lazy val sprayV = "1.3.2"
   lazy val DowngradedSprayV = "1.3.1"
   lazy val akkaV = "2.3.12"

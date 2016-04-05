b1ff21c83f6312c530a0215c01a35cda746e103a
Chris Llanwarne <cjllanwarne@gmail.com>
2016-04-05 17:56:32 -0400

    blargh

--- engine/src/main/scala/cromwell/engine/analysis/BackendSelector.scala
+++ engine/src/main/scala/cromwell/engine/analysis/BackendSelector.scala
@@ -8,6 +8,6 @@ import scala.util.Try
 // This can be enhanced later...
 object BackendSelector {
   def selectBackend(defaultBackend: Option[Backend], call: Call): Try[Backend] =
-    // TODO: Probably want to enhance this somewhat:
+  // TODO: Probably want to enhance this somehow:
     Try(defaultBackend.get)
 }

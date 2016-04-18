da1f436b8aa2d74e99e77e47a334acc556907b69
Kristian Cibulskis <kcibul@broadinstitute.org>
2016-04-18 08:32:28 -0400

    updated docs and parallelism-throttling script

--- build.sbt
+++ build.sbt
@@ -8,6 +8,15 @@ val sprayV = "1.3.3"
 val downgradedSprayV = "1.3.2"
 val akkaV = "2.3.14"
 
+
+/***
+ * by default log buffering is set to true in sbt, which means
+ * that for tests executed in parallel you will not see the 
+ * output until the test suite completes.  Setting this to false
+ * will not buffer output, but it will be interleaved
+ */
+// logBuffered in Test := false
+
 libraryDependencies ++= Seq(
   "com.typesafe" % "config" % "1.3.0",
   "org.typelevel" %% "cats" % "0.4.1",
---
+++ run_tests_parallel.sh
@@ -0,0 +1,15 @@
+#
+# This approach allows for the running of scalatests that mixin the
+# trait 'ParallelTestExecution' to be executed in parallel with a
+# throttle to the number of threads.  Simply running 'sbt test' 
+# has that throttle hardcoded to be 2*cores
+#
+
+# optional parameter for parallelism, defaults to 3
+THREADS=${1:-3}
+
+echo "Running tests with ${THREADS}-way parallelism"
+
+sbt test:compile
+CP=$(sbt "export test:dependency-classpath" --error)
+java -cp $CP org.scalatest.tools.Runner -R target/scala-2.11/test-classes -oD -PS${THREADS}

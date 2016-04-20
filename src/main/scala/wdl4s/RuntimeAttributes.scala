de142995d19d3d4f4c86a400ffb3843cede28e92
Francisco Casares <francisco.m.casares@intel.com>
2016-04-20 15:10:04 -0700

    Removing memory validation from parser.

--- src/main/scala/wdl4s/RuntimeAttributes.scala
+++ src/main/scala/wdl4s/RuntimeAttributes.scala
@@ -13,79 +13,24 @@ import scala.collection.JavaConverters._
 import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
 
-case class RuntimeAttributes(attrs: Map[String, WdlExpression]) {
-  import RuntimeAttributes._
-
-  def evaluate(lookup: ScopedLookupFunction, functions: WdlFunctions[WdlValue]): Map[String, Try[WdlValue]] = {
-    attrs map { case (k, v) =>
-      k -> v.evaluate(lookup, functions).flatMap(validateRuntimeValue(k, _))
-    }
-  }
-
-  private def validateRuntimeValue(key: String, value: WdlValue): Try[WdlValue] = {
-    key match {
-      case "memory" => validateMemoryValue(value).map(m => WdlString(m.toString))
-      case _ => Success(value)
-    }
-  }
-}
+case class RuntimeAttributes(attrs: Map[String, WdlExpression])
 
 object RuntimeAttributes {
   def apply(ast: Ast, declarations: Seq[Declaration]): RuntimeAttributes = {
     val asts = ast.findAsts(AstNodeName.Runtime)
     if (asts.size > 1) throw new UnsupportedOperationException("Only one runtime block may be defined per task")
     val kvPairAsts = asts.headOption.map(_.getAttribute("map").asInstanceOf[AstList].asScala.toVector.map(_.asInstanceOf[Ast]))
-    val runtimeAttributeMap = kvPairAsts match {
+    val runtimeAttributeMap: Map[String, WdlExpression] = kvPairAsts match {
       case Some(vector) => vector.map(ast => processRuntimeAttribute(ast, declarations)).toMap
-      case None => Map.empty[String, Try[WdlExpression]]
+      case None => Map.empty[String, WdlExpression]
     }
 
-    // .get below to throw the exception if validation didn't pass
-    TryUtil.sequenceMap(runtimeAttributeMap).map(RuntimeAttributes(_)).get
+    RuntimeAttributes(runtimeAttributeMap)
   }
 
-  def validateMemoryValue(value: WdlValue): Try[MemorySize] = {
-    value match {
-      case i: WdlInteger => Success(MemorySize(i.value.toDouble, MemoryUnit.Bytes))
-      case s: WdlString => MemorySize.parse(s.valueString).map(m => m.to(MemoryUnit.GB))
-      case other => Failure(new UnsupportedOperationException("Valid memory values are either strings (e.g. '8 GB') or integers"))
-    }
-  }
-
-  private def processRuntimeAttribute(ast: Ast, declarations: Seq[Declaration]): (String, Try[WdlExpression]) = {
+  private def processRuntimeAttribute(ast: Ast, declarations: Seq[Declaration]): (String, WdlExpression) = {
     val key = ast.getAttribute("key").sourceString
     val expression = WdlExpression(ast.getAttribute("value"))
-    val validatedExpression = key match {
-      case "memory" => validateMemoryExpression(expression, declarations)
-      case _ => Success(expression)
-    }
-    key -> validatedExpression
-  }
-
-  private def validateMemoryExpression(expression: WdlExpression, declarations: Seq[Declaration]): Try[WdlExpression] = {
-    // .get below because lookup functions should throw exceptions if they could not lookup the variable
-    def lookupType(n: String) = declarations.find(_.name == n).map(_.wdlType).get
-    expression.evaluateType(lookupType, new WdlStandardLibraryFunctionsType) match {
-      case Success(wdlType) if !Seq(WdlIntegerType, WdlStringType).contains(wdlType) =>
-        Failure(new SyntaxError("Expecting the 'memory' runtime attribute to be either a String or an Int"))
-      case Success(wdlType) => validateStaticMemoryExpression(expression)
-      case Failure(ex) => Failure(ex)
-    }
-  }
-
-  /** If an expression can be evaluated without any lookup function or WDL function implementations,
-    * Then validate that the static value is properly formatted.  If the expression cannot be statically
-    * evaluated at compile time, then defer the validation until runtime (i.e. return Success).
-    *
-    * For example, the expression `"8" + "GB"` can be statically evaluated to `"8GB"`, which can be
-    * determined to be correctly formatted at compile time.  However, the expression `x + "GB` cannot
-    * be statically evaluated because we may not have a value for `x` yet, so return Success(expression)
-    * and check the value when the expression can be evaluated.
-    */
-  private def validateStaticMemoryExpression(expression: WdlExpression): Try[WdlExpression] = {
-    expression.evaluate(NoLookup, NoFunctions) match {
-      case Success(value) => validateMemoryValue(value).map(_ => expression)
-      case Failure(ex) => Success(expression)
-    }
+    key -> expression
   }
 }
--- src/test/scala/wdl4s/RuntimeAttributeSpec.scala
+++ src/test/scala/wdl4s/RuntimeAttributeSpec.scala
@@ -1,11 +1,6 @@
 package wdl4s
 
-import scala.util.{Failure, Success}
-import org.scalatest.{EitherValues, Matchers, FlatSpec}
-import RuntimeAttributeSpec._
-import wdl4s.expression.NoFunctions
-import wdl4s.util.AggregatedException
-import wdl4s.values.{WdlString, WdlInteger}
+import org.scalatest.{EitherValues, FlatSpec, Matchers}
 
 object RuntimeAttributeSpec {
   val WorkflowWithRuntime =
@@ -78,114 +73,17 @@ object RuntimeAttributeSpec {
       |}
     """.stripMargin
 
-  val WorkflowWithMessedUpMemory =
-    """
-    |task messed_up_memory {
-    |  command {
-    |      echo "YO"
-    |  }
-    |  runtime {
-    |    memory: "HI TY"
-    |  }
-    |}
-    |
-    |workflow great_googly_moogly {
-    |  call messed_up_memory
-    |}
-  """.stripMargin
-  val WorkflowWithMessedUpMemoryUnit =
-    """
-      |task messed_up_memory {
-      |  command {
-      |      echo "YO"
-      |  }
-      |  runtime {
-      |    memory: "5 TY"
-      |  }
-      |}
-      |
-      |workflow great_googly_moogly {
-      |  call messed_up_memory
-      |}
-    """.stripMargin
-  val WorkflowWithMemoryExpression =
-    """
-      |task memory_expression {
-      |  Int gb
-      |  command {
-      |      echo "YO"
-      |  }
-      |  runtime {
-      |    memory: gb + " GB"
-      |  }
-      |}
-      |
-      |workflow great_googly_moogly {
-      |  call memory_expression
-      |}
-    """.stripMargin
-  val WorkflowWithMemoryExpressionAndStringInterpolation =
-    """
-      |task memory_expression {
-      |  Int gb
-      |  command {
-      |      echo "YO"
-      |  }
-      |  runtime {
-      |    memory: "${gb} GB"
-      |  }
-      |}
-      |
-      |workflow great_googly_moogly {
-      |  call memory_expression
-      |}
-    """.stripMargin
-  }
+  class RuntimeAttributeSpec extends FlatSpec with Matchers with EitherValues {
+    val NamespaceWithRuntime = NamespaceWithWorkflow.load(WorkflowWithRuntime)
+    val NamespaceWithoutRuntime = NamespaceWithWorkflow.load(WorkflowWithoutRuntime)
 
-class RuntimeAttributeSpec extends FlatSpec with Matchers with EitherValues {
-  val NamespaceWithRuntime = NamespaceWithWorkflow.load(WorkflowWithRuntime)
-  val NamespaceWithoutRuntime = NamespaceWithWorkflow.load(WorkflowWithoutRuntime)
-
-  "WDL file with runtime attributes" should "have attribute maps" in {
-    NamespaceWithRuntime.tasks.forall(_.runtimeAttributes.attrs.nonEmpty) should be(true)
-  }
-
-  "WDL file without runtime attributes" should "not have attribute maps" in {
-    NamespaceWithoutRuntime.tasks.forall(_.runtimeAttributes.attrs.isEmpty) should be(true)
-  }
-
-  "WDL file with a 'memory' runtime attribute" should "throw an exception if a malformed static string is specified" in {
-    val ex = intercept[AggregatedException] {
-      NamespaceWithWorkflow.load(WorkflowWithMessedUpMemory)
+    "WDL file with runtime attributes" should "have attribute maps" in {
+      NamespaceWithRuntime.tasks.forall(_.runtimeAttributes.attrs.nonEmpty) should be(true)
     }
 
-    ex.getMessage should include ("should be of the form 'X Unit'")
-  }
-
-  it should "throw an exception if the static string contains an invalid memory unit" in {
-    val ex = intercept[AggregatedException] {
-      NamespaceWithWorkflow.load(WorkflowWithMessedUpMemoryUnit)
+    "WDL file without runtime attributes" should "not have attribute maps" in {
+      NamespaceWithoutRuntime.tasks.forall(_.runtimeAttributes.attrs.isEmpty) should be(true)
     }
-
-    ex.getMessage should include ("is an invalid memory unit")
   }
 
-  it should "accept a value that contains an expression that can't be statically evaluated (1)" in {
-    val ns = NamespaceWithWorkflow.load(WorkflowWithMemoryExpressionAndStringInterpolation)
-    val runtime = ns.findTask("memory_expression").get.runtimeAttributes.evaluate((s:String) => WdlInteger(10), NoFunctions)
-    runtime("memory") shouldBe a[Success[_]]
-    runtime("memory").get shouldEqual WdlString("10 GB")
-  }
-
-  it should "accept a value that contains an expression that can't be statically evaluated (2)" in {
-    val ns = NamespaceWithWorkflow.load(WorkflowWithMemoryExpression)
-    val runtime = ns.findTask("memory_expression").get.runtimeAttributes
-
-    val goodRuntime = runtime.evaluate((s:String) => WdlInteger(4), NoFunctions)
-    goodRuntime("memory") shouldBe a[Success[_]]
-    goodRuntime("memory").get shouldEqual WdlString("4 GB")
-
-    val badRuntime = runtime.evaluate((s:String) => WdlString("foobar"), NoFunctions)
-    badRuntime("memory") shouldBe a[Failure[_]]
-  }
 }

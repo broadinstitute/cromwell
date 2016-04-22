ba0730274d710d91847befd14525489e5a068d42
Gaurav <gaurav1.k.singh@intel.com>
2016-04-22 11:29:57 -0700

    Restoring ShadowExecution functionality from Chris's branch

--- engine/src/main/scala/cromwell/server/CromwellServer.scala
+++ engine/src/main/scala/cromwell/server/CromwellServer.scala
@@ -1,8 +1,6 @@
 package cromwell.server
 
 import akka.util.Timeout
-import com.typesafe.config.ConfigFactory
-import cromwell.engine.workflow.MaterializeWorkflowDescriptorActor
 import cromwell.webservice.CromwellApiServiceActor
 import lenthall.spray.SprayCanHttpService._
 
@@ -15,7 +13,6 @@ object CromwellServer extends WorkflowManagerSystem {
   implicit val timeout = Timeout(5.seconds)
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  val conf = ConfigFactory.load()
   val service = actorSystem.actorOf(CromwellApiServiceActor.props(workflowManagerActor, conf), "cromwell-service")
   val webserviceConf = conf.getConfig("webservice")
 

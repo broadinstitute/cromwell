1ee102401a026d56acf4f80645142c5b3d432a5e
Ruchi Munshi <rmunshi@broadinstitute.org>
2016-05-11 15:55:40 -0400

    configure restarting workflow using the WFMA

--- engine/src/main/scala/cromwell/engine/workflow/WorkflowManagerActor.scala
+++ engine/src/main/scala/cromwell/engine/workflow/WorkflowManagerActor.scala
@@ -78,9 +78,11 @@ class WorkflowManagerActor(config: Config)
 
   private val donePromise = Promise[Unit]()
 
+  private val conf = ConfigFactory.load
+
   override def preStart() {
     addShutdownHook()
-    restartIncompleteWorkflows()
+    if (conf.getBoolean("system.workflow-restart")) { restartIncompleteWorkflows() }
   }
 
   private def addShutdownHook(): Unit = {

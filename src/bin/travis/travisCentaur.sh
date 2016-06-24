b246001dbb43989df53c9320855550cf3c516bfb
Jeff Gentry <geoffjentry@gmail.com>
2016-06-24 18:30:42 -0400

    Run a round of Centaur during travis. It's cromwell's all the way down. Closes #1022 (#1050)

---
+++ src/bin/travis/travisCentaur.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+set -x
+
+removeCromwellJar() {
+    gsutil rm "${JAR_GCS_PATH}"
+}
+
+# Unpack our credentials and such
+openssl aes-256-cbc -K "$encrypted_fb828d0791bd_key" -iv "$encrypted_fb828d0791bd_iv" -in src/bin/travis/resources/jesConf.tar.enc -out jesConf.tar -d
+tar xvf jesConf.tar
+
+# Do a bunch of crap to enable gsutil. It's possible this is overkill but it doesn't take long anyways
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1397BC53640DB551
+CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get install google-cloud-sdk
+export PYTHONPATH="/usr/lib/python2.7/site-packages:/usr/local/lib/python2.7/site-packages:/usr/local/lib/python2.7/dist-packages"
+export CONFIGURE_OPTS="--enable-unicode=ucs4"
+pyenv install 2.7.10
+pyenv global 2.7.10
+sudo pip install --upgrade pip
+sudo pip install pyopenssl ndg-httpsclient pyasn1 --upgrade
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
+gcloud auth activate-service-account --key-file=broad-dsde-cromwell-dev-d9c443bb4a94.json "$CROMWELL_SERVICE_ACCOUNT"
+
+echo "RUNNING TRAVIS CENTAUR"
+sbt assembly
+
+# Update the inputs file with stuff specific to this run
+sed -i "s/BRANCH/${TRAVIS_BRANCH}/g" src/bin/travis/resources/centaur.inputs
+CROMWELL_JAR=cromwell_${TRAVIS_BUILD_ID}.jar
+sed -i "s/CROMWELL_JAR/${CROMWELL_JAR}/g" src/bin/travis/resources/centaur.inputs
+
+# Upload the built Cromwell jar to GCS so we can use it in our centaur test. Set an exit trap to clean it up on failure
+JAR_GCS_PATH=gs://cloud-cromwell-dev/travis-centaur/${CROMWELL_JAR}
+gsutil cp target/scala-2.11/cromwell-*.jar "${JAR_GCS_PATH}"
+trap "removeCromwellJar" EXIT
+
+java -Dconfig.file=./jes.conf -jar target/scala-2.11/cromwell-*.jar run src/bin/travis/resources/centaur.wdl src/bin/travis/resources/centaur.inputs

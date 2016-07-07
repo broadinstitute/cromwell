7432a1b7f956bb1f7ae7ba1222d17939fe461331
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-07-07 15:41:44 -0400

    Use genomicsAuth for auth file upload

--- supportedBackends/jes/src/main/scala/cromwell/backend/impl/jes/JesWorkflowPaths.scala
+++ supportedBackends/jes/src/main/scala/cromwell/backend/impl/jes/JesWorkflowPaths.scala
@@ -4,7 +4,11 @@ import java.nio.file.Path
 
 import cromwell.backend.BackendWorkflowDescriptor
 import cromwell.backend.impl.jes.io._
-import cromwell.filesystems.gcs.GcsFileSystem
+import cromwell.core.WorkflowOptions
+import cromwell.filesystems.gcs.GoogleAuthMode.GoogleAuthOptions
+import cromwell.filesystems.gcs.{GoogleAuthMode, GcsFileSystemProvider, GcsFileSystem}
+
+import scala.util.Try
 
 object JesWorkflowPaths {
   private val GcsRootOptionKey = "jes_gcs_root"
@@ -13,10 +17,16 @@ object JesWorkflowPaths {
   def apply(workflowDescriptor: BackendWorkflowDescriptor, jesConfiguration: JesConfiguration, gcsFileSystem: GcsFileSystem) = {
     new JesWorkflowPaths(workflowDescriptor, jesConfiguration, gcsFileSystem)
   }
+
+  implicit class GoogleAuthWorkflowOptions(val workflowOptions: WorkflowOptions) extends AnyVal {
+    def toGoogleAuthOptions: GoogleAuthMode.GoogleAuthOptions = new GoogleAuthOptions {
+      override def get(key: String): Try[String] = workflowOptions.get(key)
+    }
+  }
 }
 
 class JesWorkflowPaths(workflowDescriptor: BackendWorkflowDescriptor, jesConfiguration: JesConfiguration, gcsFileSystem: GcsFileSystem) {
-
+import JesWorkflowPaths._
   val rootPath: Path =
     gcsFileSystem.getPath(workflowDescriptor.workflowOptions.getOrElse(JesWorkflowPaths.GcsRootOptionKey, jesConfiguration.root))
 
@@ -29,6 +39,11 @@ class JesWorkflowPaths(workflowDescriptor: BackendWorkflowDescriptor, jesConfigu
      * unlike everywhere else where the filesystem used is built from gcsFileSystemAuth
      */
     val bucket = workflowDescriptor.workflowOptions.get(JesWorkflowPaths.AuthFilePathOptionKey) getOrElse workflowRootPath.toString
-    gcsFileSystem.getPath(bucket).resolve(s"${workflowDescriptor.id}_auth.json")
+
+    val storage = jesConfiguration.jesAttributes.genomicsAuth.buildStorage(
+      workflowDescriptor.workflowOptions.toGoogleAuthOptions, jesConfiguration.googleConfig)
+    val fs = GcsFileSystem(GcsFileSystemProvider(storage))
+
+    fs.getPath(bucket).resolve(s"${workflowDescriptor.id}_auth.json")
   }
 }

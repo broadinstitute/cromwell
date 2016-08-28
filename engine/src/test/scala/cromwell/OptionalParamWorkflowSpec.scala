4f71b8efdde9a27fd4c2bd242dec266d2a574574
geoffjentry <geoffjentry@gmail.com>
2016-08-28 16:54:06 -0400

    Cleaning out more tests which have moved to Centaur

--- engine/src/test/scala/cromwell/OptionalParamWorkflowSpec.scala
+++ engine/src/test/scala/cromwell/OptionalParamWorkflowSpec.scala
@@ -1,45 +1,13 @@
 package cromwell
 
-import akka.testkit._
-import cromwell.core.Tags.DockerTest
-import cromwell.util.SampleWdl
+import org.scalatest.{Matchers, WordSpecLike}
 import wdl4s.WdlNamespace
 import wdl4s.expression.NoFunctions
 import wdl4s.values.{WdlFile, WdlString}
 
 import scala.language.postfixOps
 
-class OptionalParamWorkflowSpec extends CromwellTestkitSpec {
-  val outputs = Map(
-    "optional.hello.greeting" -> WdlString("hello john"),
-    "optional.hello2.greeting" -> WdlString("hello"),
-    "optional.hello_person.greeting" -> WdlString("hello world")
-  )
-
-  "A workflow with optional parameters" should {
-    "accept optional parameters" in {
-      runWdlAndAssertOutputs(
-        sampleWdl = SampleWdl.OptionalParamWorkflow,
-        eventFilter = EventFilter.info(pattern =
-          "Starting calls: optional.hello:NA:1, optional.hello2:NA:1, optional.hello_person:NA:1", occurrences = 1),
-        expectedOutputs = outputs
-      )
-    }
-    "accept optional parameters in a Docker environment" taggedAs DockerTest in {
-      runWdlAndAssertOutputs(
-        sampleWdl = SampleWdl.OptionalParamWorkflow,
-        eventFilter = EventFilter.info(pattern =
-          "Starting calls: optional.hello:NA:1, optional.hello2:NA:1, optional.hello_person:NA:1", occurrences = 1),
-        runtime =
-          """runtime {
-            |  docker: "ubuntu:latest"
-            |}
-          """.stripMargin,
-        expectedOutputs = outputs
-      )
-    }
-  }
-
+class OptionalParamWorkflowSpec extends Matchers with WordSpecLike {
   "A workflow with an optional parameter that has a prefix inside the tag" should {
     "not include that prefix if no value is specified" in {
       val wf = """

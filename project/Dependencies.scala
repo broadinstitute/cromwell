31fa405401cae5bf5068a9e186dfd5de77589345
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-09-22 16:07:44 -0400

    update dependencies prior to release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  lazy val lenthallV = "0.18-fca8809-SNAPSHOT"
-  lazy val wdl4sV = "0.5-7f31fef-SNAPSHOT"
+  lazy val lenthallV = "0.18"
+  lazy val wdl4sV = "0.5"
   lazy val sprayV = "1.3.3"
   /*
   spray-json is an independent project from the "spray suite"

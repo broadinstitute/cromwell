5ceeee5c63b9f85b44102a86fcd3d26ea4a95d5f
Ruchi <rmunshi@broadinstitute.org>
2016-10-12 11:14:44 -0400

    fix gotc build (#1557)

--- docker/install.sh
+++ docker/install.sh
@@ -4,7 +4,7 @@ set -e
 
 CROMWELL_DIR=$1
 cd $CROMWELL_DIR
-sbt 'set test in assembly := {}' notests:assembly
+sbt assembly
 CROMWELL_JAR=$(find target | grep 'cromwell.*\.jar')
 mv $CROMWELL_JAR ./cromwell.jar
 sbt clean

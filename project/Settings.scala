f7f761ffe55d50718eafa822794de5b488eb6a9f
Matt Bemis <mbemis@broadinstitute.org>
2016-10-21 13:53:39 -0400

    port sbt docker fixes back to cromwell 0.21

--- project/Settings.scala
+++ project/Settings.scala
@@ -57,7 +57,13 @@ object Settings {
         from("openjdk:8")
         expose(8000)
         add(artifact, artifactTargetPath)
-        entryPoint("java", "-jar", artifactTargetPath)
+        runRaw(s"ln -s $artifactTargetPath /app/cromwell.jar")
+
+        // If you use the 'exec' form for an entry point, shell processing is not performed and 
+        // environment variable substitution does not occur.  Thus we have to /bin/bash here
+        // and pass along any subsequent command line arguments
+        // See https://docs.docker.com/engine/reference/builder/#/entrypoint
+        entryPoint("/bin/bash", "-c", "java ${JAVA_OPTS} -jar /app/cromwell.jar ${CROMWELL_ARGS} ${*}", "--")
       }
     },
     buildOptions in docker := BuildOptions(

61cc0d07d4416887eeb6385b70a6ffed9c9c1b0a
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-12-05 13:16:37 -0500

    pull ubuntu:latest before starting local centaur tests

--- src/bin/travis/testCentaurLocal.sh
+++ src/bin/travis/testCentaurLocal.sh
@@ -36,4 +36,8 @@ git clone https://github.com/broadinstitute/centaur.git
 cd centaur
 git checkout ${CENTAUR_BRANCH}
 cd ..
+# All tests use ubuntu:latest - make sure it's there before starting the tests 
+# because pulling the image during some of the tests would cause them to fail 
+# (specifically output_redirection which expects a specific value in stderr)
+docker pull ubuntu:latest
 centaur/test_cromwell.sh -j"${CROMWELL_JAR}" -c${LOCAL_CONF}

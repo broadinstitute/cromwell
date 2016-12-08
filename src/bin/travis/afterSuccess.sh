e2dee7e9903d0e1f45789a2eb6ba7190cffa56e9
Khalid Shakir <kshakir@broadinstitute.org>
2016-12-07 23:24:25 -0500

    Publish develop to docker hub.

--- src/bin/travis/afterSuccess.sh
+++ src/bin/travis/afterSuccess.sh
@@ -9,7 +9,12 @@ echo "TRAVIS_PULL_REQUEST='$TRAVIS_PULL_REQUEST'"
 if [ "$BUILD_TYPE" == "sbt" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     if [ "$TRAVIS_BRANCH" == "develop" ]; then
-        sbt 'set test in Test := {}' publish
+        docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+        sbt \
+          'set test in Test := {}' \
+          'set imageNames in docker := Seq(ImageName("broadinstitute/cromwell:'${TRAVIS_BRANCH}'"))' \
+          publish \
+          dockerBuildAndPush
 
     elif [[ "$TRAVIS_BRANCH" =~ ^[0-9\.]+_hotfix$ ]]; then
         docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"

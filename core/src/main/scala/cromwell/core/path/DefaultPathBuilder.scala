8dd7f9f0537f99abe45090709778cb16a00a5313
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-12-14 11:11:20 -0500

    fix default path builder

--- core/src/main/scala/cromwell/core/path/DefaultPathBuilder.scala
+++ core/src/main/scala/cromwell/core/path/DefaultPathBuilder.scala
@@ -1,6 +1,6 @@
 package cromwell.core.path
 
-import java.net.URI
+import java.net.{URI, URLEncoder}
 import java.nio.file.{FileSystems, Path}
 
 import scala.util.Try
@@ -10,13 +10,10 @@ import scala.util.Try
   */
 case object DefaultPathBuilder extends PathBuilder {
   override def name = "Default"
-
   override def build(pathAsString: String): Try[Path] = Try {
-    val uri = URI.create(pathAsString)
-    val host = Option(uri.getHost) getOrElse ""
-    val path = Option(uri.getPath) getOrElse ""
+    val uri = URI.create(URLEncoder.encode(pathAsString, "UTF-8"))
     Option(uri.getScheme) match {
-      case Some("file") | None => FileSystems.getDefault.getPath(host, path)
+      case Some("file") | None => FileSystems.getDefault.getPath(pathAsString)
       case _ => throw new RuntimeException(s"Cannot build a local path from $pathAsString")
     }
   }

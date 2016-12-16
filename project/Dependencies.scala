2b82fb4a313338402b0684f22684dd579b4fa9fc
Thib <Horneth@users.noreply.github.com>
2016-12-16 14:19:50 -0500

    update wdl4s (#1788)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val lenthallV = "0.20-7f708c7-SNAP"
-  lazy val wdl4sV = "0.8-4e236a7-SNAP"
+  lazy val wdl4sV = "0.8-2861696-SNAP"
   lazy val sprayV = "1.3.3"
   /*
   spray-json is an independent project from the "spray suite"

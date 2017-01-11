d76753ffcd9d93232dabace339d742a7b21fad21
Thibault Jeandet <tjeandet@broadinstitute.org>
2017-01-11 14:10:59 -0500

    clean up execution success

--- backend/src/main/scala/cromwell/backend/async/KnownJobFailureException.scala
+++ backend/src/main/scala/cromwell/backend/async/KnownJobFailureException.scala
@@ -11,6 +11,15 @@ final case class WrongReturnCode(jobTag: String, returnCode: Int, stderrPath: Pa
   override def getMessage = s"Job $jobTag exited with return code $returnCode which has not been declared as a valid return code. See 'continueOnReturnCode' runtime attribute for more details."
 }
 
+final case class ReturnCodeIsNotAnInt(jobTag: String, returnCode: String, stderrPath: Path) extends KnownJobFailureException {
+  override def getMessage = {
+    if (returnCode.isEmpty)
+      s"The return code file for job $jobTag was empty."
+    else
+      s"Job $jobTag exited with return code $returnCode which couldn't be converted to an Integer."
+  }
+}
+
 final case class StderrNonEmpty(jobTag: String, stderrLength: Long, stderrPath: Path) extends KnownJobFailureException {
   override def getMessage = s"stderr for job $jobTag has length $stderrLength and 'failOnStderr' runtime attribute was true."
 }

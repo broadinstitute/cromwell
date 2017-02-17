2cf23b8437c92de66caa9c951d8ce5cdd418cebf
Thibault Jeandet <tjeandet@broadinstitute.org>
2017-02-17 11:02:57 -0500

    PR comment

--- supportedBackends/sfs/src/main/scala/cromwell/backend/sfs/SharedFileSystemCacheHitCopyingActor.scala
+++ supportedBackends/sfs/src/main/scala/cromwell/backend/sfs/SharedFileSystemCacheHitCopyingActor.scala
@@ -6,7 +6,6 @@ import cromwell.core.path.Path
 class SharedFileSystemCacheHitCopyingActor(standardParams: StandardCacheHitCopyingActorParams)
   extends StandardCacheHitCopyingActor(standardParams) with SharedFileSystemJobCachingActorHelper {
   override protected def duplicate(source: Path, destination: Path): Unit = {
-    // -Ywarn-value-discard
     sharedFileSystem.cacheCopy(source, destination).get
   }
 }

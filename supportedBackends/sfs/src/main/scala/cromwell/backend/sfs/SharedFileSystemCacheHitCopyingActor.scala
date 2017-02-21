74727a7160a8db682cc28af3e17280f3c32e3f25
Thib <Horneth@users.noreply.github.com>
2017-02-21 13:28:44 -0500

    Throw if cache hit copying fails on SFS (#2003)
    * throw if cache hit copying fails on SFS
    
    * PR comment

--- supportedBackends/sfs/src/main/scala/cromwell/backend/sfs/SharedFileSystemCacheHitCopyingActor.scala
+++ supportedBackends/sfs/src/main/scala/cromwell/backend/sfs/SharedFileSystemCacheHitCopyingActor.scala
@@ -6,8 +6,6 @@ import cromwell.core.path.Path
 class SharedFileSystemCacheHitCopyingActor(standardParams: StandardCacheHitCopyingActorParams)
   extends StandardCacheHitCopyingActor(standardParams) with SharedFileSystemJobCachingActorHelper {
   override protected def duplicate(source: Path, destination: Path): Unit = {
-    // -Ywarn-value-discard
-    sharedFileSystem.cacheCopy(source, destination)
-    ()
+    sharedFileSystem.cacheCopy(source, destination).get
   }
 }

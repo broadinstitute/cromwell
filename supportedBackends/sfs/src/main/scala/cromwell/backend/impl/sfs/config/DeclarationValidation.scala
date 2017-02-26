ba56af3b4ac8e0dff1239fb838e59065bd9bcf92
Khalid Shakir <kshakir@broadinstitute.org>
2017-02-25 22:48:21 -0500

    Updates for memory validations. Fixed SGE memory parameter in reference.conf. Split `MemoryDeclarationValidation` out of `DeclarationValidation`. Added tests for `MemoryDeclarationValidation`. `OptionalRuntimeAttributesValidation` were not returning the defaults from the underlying `RuntimeAttributesValidation`.

--- supportedBackends/sfs/src/main/scala/cromwell/backend/impl/sfs/config/DeclarationValidation.scala
+++ supportedBackends/sfs/src/main/scala/cromwell/backend/impl/sfs/config/DeclarationValidation.scala
@@ -1,12 +1,9 @@
 package cromwell.backend.impl.sfs.config
 
-import cromwell.backend.MemorySize
-import cromwell.backend.impl.sfs.config.ConfigConstants._
 import cromwell.backend.validation._
 import wdl4s.expression.NoFunctions
-import wdl4s.parser.MemoryUnit
 import wdl4s.types._
-import wdl4s.values.{WdlFloat, WdlInteger, WdlValue}
+import wdl4s.values.WdlValue
 import wdl4s.{Declaration, NoLookup, WdlExpression}
 
 /**
@@ -105,101 +102,5 @@ class DeclarationValidation(declaration: Declaration, instanceValidation: Runtim
     RuntimeAttributesValidation.extractOption(instanceValidation, validatedRuntimeAttributes) map {
       declaration.wdlType.coerceRawValue(_).get
     }
-  }
-}
-
-/**
-  * Maps declarations of memory in WDL runtime attributes to the commands used to submit.
-  *
-  * The wdl runtime attributes for memory specified as strings such as:
-  *
-  * {{{
-  *   runtime {
-  *     memory: "500 MB"
-  *   }
-  * }}}
-  *
-  * However, the backend configuration only supports specifying memory in amounts in Float or Int. To specify the unit
-  * of the amount, the string "memory_" is suffixed with the unit, for example "memory_mb" or "memory_gb", or even
-  * "memory_ki".
-  *
-  * This class and companion object will do the conversion. The backend configuration should use the runtime attribute
-  * "Float? memory_gb", meaning that "memory" is now an optional runtime attribute, and will be converted to GB.
-  *
-  * Just like the runtime attribute, when no units are specified the config, the default unit is bytes.
-  *
-  * @param declaration The declaration used to create this memory validation.
-  */
-class MemoryDeclarationValidation(declaration: Declaration)
-  extends DeclarationValidation(declaration, MemoryValidation.instance) {
-
-  import MemoryDeclarationValidation._
-
-  /**
-    * Converts the validation to a version with the default from the memory expression.
-    *
-    * If the backend configuration contains a runtime attribute such as "Float memory_gb = 1.0", then the default will
-    * be set to 1 GB of memory when the attribute is not set.
-    *
-    * @param validation    The validation to set the default for.
-    * @param wdlExpression The declaration expression to retrieve the default.
-    * @return The new validation.
-    */
-  override protected def default(validation: RuntimeAttributesValidation[_],
-                                 wdlExpression: WdlExpression): RuntimeAttributesValidation[_] = {
-    val wdlValue = declaration.expression.get.evaluate(NoLookup, NoFunctions).get
-    val amount: Double = wdlValue match {
-      case WdlInteger(value) => value.toDouble
-      case WdlFloat(value) => value
-      case other => throw new RuntimeException(s"Unsupported memory default: $other")
-    }
-    val memorySize = MemorySize(amount, declarationMemoryUnit)
-    validation.withDefault(WdlInteger(memorySize.bytes.toInt))
-  }
-
-  private lazy val declarationMemoryUnit: MemoryUnit = {
-    val suffix = memoryUnitSuffix(declaration.unqualifiedName)
-    val memoryUnitOption = MemoryUnit.values.find(_.suffixes.map(_.toLowerCase).contains(suffix.toLowerCase))
-    memoryUnitOption match {
-      case Some(memoryUnit) => memoryUnit
-      case None => throw new IllegalArgumentException(s"MemoryUnit with suffix $suffix was not found.")
-    }
-  }
-
-  /**
-    * Converts the memory value from a `MemorySize` to a `Float` or `Int` based on the units.
-    *
-    * @param validatedRuntimeAttributes The validated attributes.
-    * @return The value from the collection wrapped in `Some`, or `None` if the value wasn't found.
-    */
-  override def extractWdlValueOption(validatedRuntimeAttributes: ValidatedRuntimeAttributes): Option[WdlValue] = {
-    RuntimeAttributesValidation.extractOption(MemoryValidation.instance, validatedRuntimeAttributes) map { value =>
-      declaration.wdlType match {
-        case WdlIntegerType => WdlInteger(value.to(declarationMemoryUnit).amount.toInt)
-        case WdlFloatType => WdlFloat(value.to(declarationMemoryUnit).amount)
-        case other => throw new RuntimeException(s"Unsupported wdl type for memory: $other")
-      }
-    }
-  }
-}
-
-object MemoryDeclarationValidation {
-  def isMemoryDeclaration(name: String): Boolean = {
-    name match {
-      case MemoryRuntimeAttribute => true
-      case prefixed if prefixed.startsWith(MemoryRuntimeAttributePrefix) =>
-        val suffix = memoryUnitSuffix(name)
-        MemoryUnit.values exists {
-          _.suffixes.map(_.toLowerCase).contains(suffix)
-        }
-      case _ => false
-    }
-  }
-
-  private def memoryUnitSuffix(name: String) = {
-    if (name == MemoryRuntimeAttribute)
-      MemoryUnit.Bytes.suffixes.head
-    else
-      name.substring(MemoryRuntimeAttributePrefix.length)
   }
 }

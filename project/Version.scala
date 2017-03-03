e7791bee565a39cb00e82dba513abd6652532bb1
Ruchi Munshi <rmunshi@broadinstitute.org>
2017-03-03 17:11:40 -0500

    Update Cromwell develop to the next version

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ import sbt._
 
 object Version {
   // Upcoming release, or current if we're on a master / hotfix branch
-  val cromwellVersion = "25"
+  val cromwellVersion = "26"
 
   // Adapted from SbtGit.versionWithGit
   def cromwellVersionWithGit: Seq[Setting[_]] =

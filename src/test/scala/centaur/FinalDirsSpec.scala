13b67c19f59bf36719113a0cc8b9f75efddf3054
Dan Billings <danb@broadinstitute.org>
2017-05-16 15:40:56 -0400

    limit final dir tests to run on supported backends

--- src/test/scala/centaur/FinalDirsSpec.scala
+++ src/test/scala/centaur/FinalDirsSpec.scala
@@ -1,12 +1,14 @@
 package centaur
-import centaur.test.CheckFiles
-
-import java.nio.file.{Paths, Path}
-
+import centaur.test.{CheckFiles, JesCheckFiles, LocalCheckFiles, TestOptions, Test}
+import java.nio.file.{Path, Paths}
 import java.io.File
-import centaur.test.{LocalCheckFiles, JesCheckFiles}
+import cats._, cats.implicits._
+
 import cats.data.Validated.{Invalid, Valid}
+import centaur.api.CentaurCromwellClient
 import centaur.test.formulas.TestFormulas
+import centaur.test.standard.StandardTestCase
+import centaur.test.standard.StandardTestFormat.WorkflowSuccessTest
 import centaur.test.workflow.Workflow
 import org.scalatest.{FlatSpec, Matchers, ParallelTestExecution}
 import com.google.cloud.storage._
@@ -28,29 +30,38 @@ object FinalDirsSpec {
 class FinalDirsSpec extends FlatSpec with Matchers with ParallelTestExecution {
   import FinalDirsSpec._
 
-  "final Outputs on local backend" should "place files in output dir" in 
-    testFinalOutputs(OutputsDirLocalTest, "final_workflow_outputs_dir", LocalCheckFiles())
+  case class Backend(id: String, checkFiles: CheckFiles)
+  val Jes = Backend("Jes", JesCheckFiles(StorageOptions.getDefaultInstance.getService))
+  val Local = Backend("Local", LocalCheckFiles())
+
+  val cromwellBackends = CentaurCromwellClient.backends.get.supportedBackends.map(_.toLowerCase)
+
+  "final Outputs on local backend" should "place files in output dir" in
+    testFinalOutputs(OutputsDirLocalTest, "final_workflow_outputs_dir", Local)
 
   "final log dirs on local backend" should "place log files in output dir when requested" in 
-    testFinalOutputs(LogDirLocalTest, "final_workflow_log_dir", LocalCheckFiles())
+    testFinalOutputs(LogDirLocalTest, "final_workflow_log_dir", Local)
 
   "final call logs dir in local" should "place call files in output dir when requested" in 
-    testFinalOutputs(CallLogsDirLocalTest, "final_call_logs_dir", LocalCheckFiles())
+    testFinalOutputs(CallLogsDirLocalTest, "final_call_logs_dir", Local)
 
   "final Logs dir on jes backend" should "place log files in output dir when requested" in 
-    testFinalOutputs(LogDirJesTest, "final_workflow_log_dir", JesCheckFiles(StorageOptions.getDefaultInstance.getService))
+    testFinalOutputs(LogDirJesTest, "final_workflow_log_dir", Jes)
 
   "final outputs on jes backend" should "place files in output dir" in 
-    testFinalOutputs(OutputsDirJesTest, "final_workflow_outputs_dir", JesCheckFiles(StorageOptions.getDefaultInstance.getService))
+    testFinalOutputs(OutputsDirJesTest, "final_workflow_outputs_dir", Jes)
 
   "final Call logs dir in Jes" should "place call files in GCS dir when requested" in 
-    testFinalOutputs(CallLogsDirJesTest, "final_call_logs_dir", JesCheckFiles(StorageOptions.getDefaultInstance.getService))
+    testFinalOutputs(CallLogsDirJesTest, "final_call_logs_dir", Jes)
 
-  def testFinalOutputs(path: Path, option: String, backend: CheckFiles) = 
-    Workflow.fromPath(path) match {
-      case Valid(w) => TestFormulas.runFinalDirsWorkflow(w, option, backend).run.get
-      case Invalid(e) => fail(s"Could not read logs test:\n -${e.toList.mkString("\n-")}")
+  def testFinalOutputs(path: Path, option: String, backend: Backend) =
+    (Workflow.fromPath(path), cromwellBackends.contains(backend.id)) match {
+
+      case (Valid(w), true) =>
+        TestFormulas.runFinalDirsWorkflow(w, option, backend.checkFiles).run.get
+
+      case (Invalid(e), _) => fail(s"Could not read logs test:\n -${e.toList.mkString("\n-")}")
+
+      case (Valid(_), _) => Monad[Test].pure(()) //valid but not proper backend, ignore
     }
-
-
 }

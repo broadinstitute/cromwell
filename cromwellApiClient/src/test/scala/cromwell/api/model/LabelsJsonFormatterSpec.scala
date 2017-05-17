ba25e78f79465e1b08043132f2bc6183858ed543
Ruchi Munshi <rmunshi@broadinstitute.org>
2017-05-17 16:06:21 -0400

    final edits to test

--- cromwellApiClient/src/test/scala/cromwell/api/model/LabelsJsonFormatterSpec.scala
+++ cromwellApiClient/src/test/scala/cromwell/api/model/LabelsJsonFormatterSpec.scala
@@ -1,6 +1,5 @@
-package cromwell.api
+package cromwell.api.model
 
-import cromwell.api.model._
 import spray.json._
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
@@ -9,6 +8,13 @@ class LabelsJsonFormatterSpec extends FlatSpec with Matchers {
   import cromwell.api.model.LabelsJsonFormatter._
 
   behavior of "WdlValueJsonFormat"
+
+  val sampleLabels = List(Label("key-1", "value-1"), Label("key-2", "value-2"), Label("key-3", "value-3"))
+  val sampleJson =  """|{
+                       |  "key-1":"value-1",
+                       |  "key-2":"value-2",
+                       |  "key-3":"value-3"
+                       |}""".stripMargin.parseJson.asJsObject
 
   it should "write a Label as a structured JsObject" in {
     val label = List(Label("test-key", "test-value"))
@@ -21,32 +27,10 @@ class LabelsJsonFormatterSpec extends FlatSpec with Matchers {
   }
 
   it should "write an optional list of labels as a structured JsObject" in {
-    val labels =  Option(List(Label("key-1", "value-1"),
-                              Label("key-2", "value-2"),
-                              Label("key-3", "value-3")))
-
-    val expectedJson: JsObject =
-      """|{
-        |  "key-1":"value-1",
-        |  "key-2":"value-2",
-        |  "key-3":"value-3"
-        |}""".stripMargin.parseJson.asJsObject
-
-    labels.toJson shouldEqual expectedJson
+    Option(sampleLabels).toJson shouldEqual sampleJson
   }
 
   it should "read a list of labels as a structured JsObject" in {
-    val expectedLabels =  List(Label("key-1", "value-1"),
-      Label("key-2", "value-2"),
-      Label("key-3", "value-3"))
-
-    val labelsJson: JsObject =
-      """|{
-        |  "key-1":"value-1",
-        |  "key-2":"value-2",
-        |  "key-3":"value-3"
-        |}""".stripMargin.parseJson.asJsObject
-
-    labelsJson.convertTo[List[Label]] shouldBe expectedLabels
+    sampleJson.convertTo[List[Label]] shouldBe sampleLabels
   }
 }

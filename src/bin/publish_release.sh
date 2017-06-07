6fbb17e7dc7cdec0fede16cbc495f8545f393c64
Miguel Covarrubias <mcovarr@broadinstitute.org>
2017-06-07 16:30:34 -0400

    hopping over to jfrog

--- src/bin/publish_release.sh
+++ src/bin/publish_release.sh
@@ -9,6 +9,6 @@ sbt \
     "set version := \"${TRAVIS_TAG}\"" \
     'set isSnapshot := false' \
     'set resolvers += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
-    'set publishTo := Option("artifactory-publish" at "https://artifactory.broadinstitute.org/artifactory/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
-    "set credentials += Credentials(\"Artifactory Realm\", \"artifactory.broadinstitute.org\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
+    'set publishTo := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
+    "set credentials += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
     "+ publish" 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'

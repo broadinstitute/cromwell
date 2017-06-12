f4212efafcad02e4270e47ec1bba69373e3d5a93
Thibault Jeandet <tjeandet@broadinstitute.org>
2017-06-12 18:08:42 -0400

    fail properly on wrong google conf

--- engine/src/main/scala/cromwell/engine/EngineFilesystems.scala
+++ engine/src/main/scala/cromwell/engine/EngineFilesystems.scala
@@ -11,29 +11,41 @@ import net.ceedubs.ficus.Ficus._
 import cats.instances.future._
 import cats.instances.list._
 import cats.syntax.traverse._
+import cromwell.filesystems.gcs.auth.GoogleAuthMode
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 object EngineFilesystems {
   private val config = ConfigFactory.load
-  private val googleConf: GoogleConfiguration = GoogleConfiguration(config)
-  private val googleAuthMode = config.as[Option[String]]("engine.filesystems.gcs.auth") map { confMode =>
-    googleConf.auth(confMode) match {
-      case Valid(mode) => mode
-      case Invalid(errors) => throw new RuntimeException() with MessageAggregation {
-        override def exceptionContext: String = s"Failed to create authentication mode for $confMode"
-        override def errorMessages: Traversable[String] = errors.toList
+  private val googleConf = Try(GoogleConfiguration(config))
+  private val googleAuthMode: Try[Option[GoogleAuthMode]] = config.as[Option[String]]("engine.filesystems.gcs.auth") match {
+    case Some(confMode) =>
+      googleConf map { conf => conf.auth(confMode) match {
+        case Valid(mode) => Option(mode)
+        case Invalid(errors) => throw new RuntimeException() with MessageAggregation {
+          override def exceptionContext: String = s"Failed to create authentication mode for $confMode"
+
+          override def errorMessages: Traversable[String] = errors.toList
+        }
       }
-    }
+      }
+    case None => Success(None)
   }
 
-  private val gcsPathBuilderFactory = googleAuthMode map { mode =>
-    GcsPathBuilderFactory(mode, googleConf.applicationName)
-  }
+  private val gcsPathBuilderFactory: Try[Option[GcsPathBuilderFactory]] =
+    for {
+      conf <- googleConf
+      mode <- googleAuthMode
+    } yield mode map { GcsPathBuilderFactory(_, conf.applicationName) }
+
 
   private val defaultFileSystem =
     Option(DefaultPathBuilder).filter(_ => config.as[Boolean]("engine.filesystems.local.enabled"))
 
-  def pathBuildersForWorkflow(workflowOptions: WorkflowOptions)(implicit as: ActorSystem, ec: ExecutionContext): Future[List[PathBuilder]] =
-    gcsPathBuilderFactory.toList.traverse(_.withOptions(workflowOptions)).map(_ ++ defaultFileSystem)
+  def pathBuildersForWorkflow(workflowOptions: WorkflowOptions)(implicit as: ActorSystem, ec: ExecutionContext): Future[List[PathBuilder]] = gcsPathBuilderFactory match {
+    case Success(maybeBuilderFactory) => maybeBuilderFactory.toList.traverse(_.withOptions(workflowOptions)).map(_ ++ defaultFileSystem)
+    case Failure(failure) => Future.failed(failure)
+  }
+
 }

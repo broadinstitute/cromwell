ffed2fc87ce15bcff92013debd86f5c671355527
Miguel Covarrubias <mcovarr@broadinstitute.org>
2017-06-22 18:27:06 -0400

    explicit install of google-compute-engine

--- src/bin/travis/testCentaurJes.sh
+++ src/bin/travis/testCentaurJes.sh
@@ -56,6 +56,7 @@ CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
+sudo -H pip install google-compute-engine
 # sudo gcloud components update
 #
 # You cannot perform this action because this Cloud SDK installation is

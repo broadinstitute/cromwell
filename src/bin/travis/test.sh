ac726af2106528b928bf579435abd882906941d3
jsotobroad <jsotobroad@broadinstitute.org>
2017-06-23 17:02:47 -0400

    try some magic

--- src/bin/travis/test.sh
+++ src/bin/travis/test.sh
@@ -4,32 +4,47 @@ set -e
 
 SCRIPT_DIR=src/bin/travis
 
-if [ "$TRAVIS_EVENT_TYPE" = "push" ] || [ "$TRAVIS_EVENT_TYPE" = "pull_request" ] || [ "$TRAVIS_EVENT_TYPE" = "api" ]; then
-    # BUILD_TYPE is coming in from the Travis build matrix
-    if [ "$BUILD_TYPE" = "centaurJes" ]; then
-        "${SCRIPT_DIR}"/testCentaurJes.sh
-    elif [ "$BUILD_TYPE" = "centaurTes" ]; then
-        "${SCRIPT_DIR}"/testCentaurTes.sh
-    elif [ "$BUILD_TYPE" = "centaurLocal" ]; then
-        "${SCRIPT_DIR}"/testCentaurLocal.sh
-    elif [ "$BUILD_TYPE" = "sbt" ]; then
-        "${SCRIPT_DIR}"/testSbt.sh
-    elif [ "$BUILD_TYPE" = "checkPublish" ]; then
-        "${SCRIPT_DIR}"/testCheckPublish.sh
-    else
-        echo "Unknown BUILD_TYPE: '$BUILD_TYPE'"
-        exit 1
-    fi
-fi
-
 # $TRAVIS_EVENT_TYPE will be cron if this build was initiated by a cron job
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
-    if [ "$BUILD_TYPE" = "centaurJes" ]; then
-        "${SCRIPT_DIR}"/testCentaurJes.sh -i
-    elif [ "$BUILD_TYPE" = "centaurTes" ] || [ "$BUILD_TYPE" = "centaurLocal" ] || [ "$BUILD_TYPE" = "sbt" ] || [ "$BUILD_TYPE" = "checkPublish" ]; then
-        exit 0
-    else
-        echo "Unknown BUILD_TYPE: '$BUILD_TYPE'"
-        exit 1
-    fi
-fi
+case "$TRAVIS_EVENT_TYPE" in
+    push|pull_request|api)
+    # BUILD_TYPE is coming in from the Travis build matrix
+        case "$BUILD_TYPE" in
+        centaurJes)
+            "${SCRIPT_DIR}"/testCentaurJes.sh -i
+            ;;
+        centaurTes)
+            "${SCRIPT_DIR}"/testCentaurJes.sh
+            ;;
+        centaurLocal)
+            "${SCRIPT_DIR}"/testCentaurLocal.sh
+            ;;
+        sbt)
+            "${SCRIPT_DIR}"/testSbt.sh
+            ;;
+        checkPublish)
+            "${SCRIPT_DIR}"/testCheckPublish.sh
+            ;;
+
+        *)
+            echo "Unknown BUILD_TYPE: '$BUILD_TYPE'"
+            exit 1
+            ;;
+        esac
+        ;;
+    cron)
+        case "$BUILD_TYPE" in
+        centaurJes)
+            "${SCRIPT_DIR}"/testCentaurJes.sh -i
+            ;;
+        centaurTes|centaurLocal|sbt|checkPublish)
+            exit 0
+            ;;
+        *)
+            echo "Unknown BUILD_TYPE: '$BUILD_TYPE'"
+            exit 1
+            ;;
+        esac
+        ;;
+    *)
+        echo "Unknown TRAVIS_EVENT_TYPE: '$TRAVIS_EVENT_TYPE'"
+    esac

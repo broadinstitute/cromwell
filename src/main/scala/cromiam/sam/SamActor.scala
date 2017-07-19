c9d082dfdb20e1ae4def11926c794a66778aa99e
geoffjentry <geoffjentry@gmail.com>
2017-07-19 10:40:52 -0400

    It is called sam, not samiam: https://github.com/broadinstitute/sam

--- src/main/scala/cromiam/sam/SamActor.scala
+++ src/main/scala/cromiam/sam/SamActor.scala
@@ -1,25 +1,25 @@
-package cromiam.samiam
+package cromiam.sam
 
 import akka.actor.{Actor, ActorLogging, Props}
-import cromiam.samiam.SamIamActor._
+import cromiam.sam.SamActor._
 
 import scala.concurrent.{ExecutionContext, Promise}
 import scala.util.{Failure, Random, Success}
 import scala.concurrent.duration._
 
-final class SamIamActor(userIdHeader: String, allowedUsers: List[String]) extends Actor with ActorLogging {
+final class SamActor(userIdHeader: String, allowedUsers: List[String]) extends Actor with ActorLogging {
 
   implicit val ec: ExecutionContext = context.dispatcher
 
   override def receive: Receive = {
-    case r: SamIamActorRequest => handleRequest(r)
+    case r: SamActorRequest => handleRequest(r)
   }
 
-  private def handleRequest: Function[SamIamActorRequest, Unit] = {
+  private def handleRequest: Function[SamActorRequest, Unit] = {
     case RequestAuthorization(ar, onComplete) =>
       // TODO: Replace with a real IAM lookup:
       val delay = (Random.nextInt(500) + 100).milliseconds
-      val message = if (allowedUsers.contains(ar.userIdToken)) Success(()) else Failure(SamIamDenialException)
+      val message = if (allowedUsers.contains(ar.userIdToken)) Success(()) else Failure(SamDenialException)
       context.system.scheduler.scheduleOnce(delay) { onComplete.complete(message) }
       ()
     case RegisterWorkflow(u, w, onComplete) =>
@@ -32,14 +32,14 @@ final class SamIamActor(userIdHeader: String, allowedUsers: List[String]) extend
   }
 }
 
-object SamIamActor {
-  def props(userIdHeader: String, allowedUsers: List[String]): Props = Props(new SamIamActor(userIdHeader, allowedUsers))
+object SamActor {
+  def props(userIdHeader: String, allowedUsers: List[String]): Props = Props(new SamActor(userIdHeader, allowedUsers))
 
-  sealed trait SamIamActorRequest
-  final case class RequestAuthorization(authorizationRequest: AuthorizationRequest, onComplete: Promise[Unit]) extends SamIamActorRequest
-  final case class RegisterWorkflow(user: String, workflowId: String, onComplete: Promise[Unit]) extends SamIamActorRequest
+  sealed trait SamActorRequest
+  final case class RequestAuthorization(authorizationRequest: AuthorizationRequest, onComplete: Promise[Unit]) extends SamActorRequest
+  final case class RegisterWorkflow(user: String, workflowId: String, onComplete: Promise[Unit]) extends SamActorRequest
 
-  case object SamIamDenialException extends Exception("Access Denied")
+  case object SamDenialException extends Exception("Access Denied")
 
   sealed trait AuthorizationRequest {
     def userIdToken: String
--- src/main/scala/cromiam/sam/SamClient.scala
+++ src/main/scala/cromiam/sam/SamClient.scala
@@ -1,25 +1,25 @@
-package cromiam.samiam
+package cromiam.sam
 
 import akka.actor.{ActorRef, ActorRefFactory}
-import cromiam.samiam.SamIamActor.{AuthorizationRequest, RegisterWorkflow, RequestAuthorization}
+import cromiam.sam.SamActor.{AuthorizationRequest, RegisterWorkflow, RequestAuthorization}
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import cats.implicits._
 
-trait SamIamClient {
-  protected def samIamActor: ActorRef
+trait SamClient {
+  protected def samActor: ActorRef
   protected def actorRefFactory: ActorRefFactory
 
   protected def requestAuth(authorizationRequest: AuthorizationRequest): Future[Unit] = {
     val completeMe: Promise[Unit] = Promise()
-    samIamActor.tell(RequestAuthorization(authorizationRequest, completeMe), ActorRef.noSender)
+    samActor.tell(RequestAuthorization(authorizationRequest, completeMe), ActorRef.noSender)
     completeMe.future
   }
 
   protected def registerCreation(user: String, workflowIds: List[String])(implicit ec: ExecutionContext): Future[Unit] = {
     (workflowIds traverse { workflowId =>
       val completeMe: Promise[Unit] = Promise()
-      samIamActor.tell(RegisterWorkflow(user, workflowId, completeMe), ActorRef.noSender)
+      samActor.tell(RegisterWorkflow(user, workflowId, completeMe), ActorRef.noSender)
       completeMe.future
     }).void
   }

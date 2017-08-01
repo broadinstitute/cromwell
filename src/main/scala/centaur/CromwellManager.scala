14252ff809340121e22dd3ead37b7d244a05427b
Thib <Horneth@users.noreply.github.com>
2017-08-01 10:59:27 -0400

    yes restart (#224)
    * yes restart
    
    * Switch to Java process to trigger shutdown hook

--- src/main/scala/centaur/CromwellManager.scala
+++ src/main/scala/centaur/CromwellManager.scala
@@ -1,11 +1,12 @@
 package centaur
 
+import java.lang.ProcessBuilder.Redirect
+
 import better.files.File
 import centaur.api.CentaurCromwellClient
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.sys.process._
 
 /**
   * This is not thread-safe, and hence only works if cromwell is started / stopped from only 1 thread at a time.
@@ -15,7 +16,6 @@ object CromwellManager {
   private val timeout = 30 seconds
   private val interval = 1 second
   private var cromwellProcess: Option[Process] = None
-  private var processLogger: FileProcessLogger = _
   private var _ready: Boolean = false
   private var _isManaged: Boolean = false
   
@@ -33,15 +33,15 @@ object CromwellManager {
     _isManaged = true
     
     if (!isAlive) {
-      val cromwellCommand = s"java -Dconfig.file=${cromwell.conf} -Dwebservice.port=$ManagedCromwellPort -jar ${cromwell.jar} server"
-      
-      // Redirect stdout and stderr to the log file
       val logFile: File = File(cromwell.logFile)
-      processLogger = ProcessLogger(logFile.toJava)
+      
+      val processBuilder = new java.lang.ProcessBuilder()
+      processBuilder.command("java", s"-Dconfig.file=${cromwell.conf}", s"-Dwebservice.port=$ManagedCromwellPort", "-jar", cromwell.jar, "server")
+      processBuilder.redirectOutput(Redirect.appendTo(logFile.toJava))
         
       // Start the cromwell process
       println("Starting Cromwell...")
-      val process = cromwellCommand.run(processLogger)
+      val process = processBuilder.start()
       cromwellProcess = Option(process)
 
       var waitedFor = Duration.Zero
@@ -67,10 +67,9 @@ object CromwellManager {
     _ready = false
     println("Stopping Cromwell...")
     cromwellProcess foreach { process =>
-      processLogger.flush()
-      processLogger.close()
+      process.getOutputStream.flush()
       process.destroy()
-      process.exitValue()
+      process.waitFor()
     }
     
     cromwellProcess = None

4d3ea9c95abc2a2c94b497ea2b1baad38357ff43
Khalid Shakir <kshakir@broadinstitute.org>
2017-08-02 20:04:41 -0400

    Only try to inject a refresh_token when asked.

--- cromwellApiClient/src/test/scala/cromwell/api/CromwellClientSpec.scala
+++ cromwellApiClient/src/test/scala/cromwell/api/CromwellClientSpec.scala
@@ -10,6 +10,8 @@ class CromwellClientSpec extends FlatSpec with Matchers with TableDrivenProperty
   val table = Table(
     ("description", "optionsOption", "refreshTokenOption", "expected"),
     ("ignore bad json when refresh token not provided", Option("{"), None, Option("{")),
+    ("ignore bad json when refresh token provided but not used", Option("{"), Option("myToken"),
+      Option("{")),
     ("not format json when refresh token key not found", Option("{   }"), Option("myToken"), Option("{   }")),
     ("replace token when found", Option("""{"refresh_token" : "replace_me"}"""), Option("myToken"),
       Option("""{"refresh_token":"myToken"}""")),
@@ -22,15 +24,15 @@ class CromwellClientSpec extends FlatSpec with Matchers with TableDrivenProperty
     }
   }
 
-  it should "throw an exception when inserting a refresh token into bad json" in {
-    val optionsOption = Option("{")
+  it should "throw an exception when inserting a refresh token into bad json using the token" in {
+    val optionsOption = Option("""{"refresh_token" : "replace_me"""")
     val refreshTokenOption = Option("myToken")
     val actual = intercept[ParsingException](CromwellClient.insertSecrets(optionsOption, refreshTokenOption))
-    actual.summary should be("""Unexpected end-of-input at input index 1 (line 1, position 2), expected '"'""")
+    actual.summary should be("""Unexpected end-of-input at input index 31 (line 1, position 32), expected '}'""")
     actual.detail should be(
       """|
-         |{
-         | ^
+         |{"refresh_token" : "replace_me"
+         |                               ^
          |""".stripMargin)
   }
 }

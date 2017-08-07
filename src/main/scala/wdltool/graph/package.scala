8ef3e74a15fb9845ed2dfa16e6320093af731bd0
Chris Llanwarne <cjllanwarne@gmail.com>
2017-08-07 16:03:27 -0400

    For our own benefit, a first attempt at drawing pictures of WOM graphs

---
+++ src/main/scala/wdltool/graph/package.scala
@@ -0,0 +1,42 @@
+package wdltool
+
+import wdl4s.wom.graph.GraphNodePort.{InputPort, OutputPort}
+import wdl4s.wom.graph._
+
+package object graph {
+
+  private[graph] def dotSafe(s: String) = s""""${s.replaceAllLiterally("\"", "\\\"")}""""
+
+  private[graph] implicit class GraphNodeGraphics(val graphNode: GraphNode) extends AnyVal {
+    def graphFillColor = graphNode match {
+      case _: GraphInputNode => "lightskyblue1"
+      case _: GraphOutputNode => "palegreen"
+      case _ => "white"
+    }
+
+    def graphName: String = dotSafe(graphNode match {
+      case c: CallNode =>
+        s"call ${c.name}"
+      case s: ScatterNode =>
+        s"scatter ${s.scatterVariableMapping.graphInputNode.name}"
+      case gin: GraphInputNode =>
+        s"${gin.womType.toWdlString} ${gin.name}"
+      case gon: GraphOutputNode =>
+        s"${gon.womType.toWdlString} ${gon.name}"
+      case other =>
+        throw new Exception(s"Can't name $other")
+    })
+
+    def graphId: String = dotSafe("NODE" + graphNode.hashCode().toString)
+  }
+
+  private[graph] implicit class GraphNodePortGraphics(val graphNodePort: GraphNodePort) extends AnyVal {
+    def graphShape = graphNodePort match {
+      case _: InputPort => "oval"
+      case _: OutputPort => "hexagon"
+    }
+
+    def graphName: String = dotSafe(graphNodePort.name)
+    def graphId: String = dotSafe("PORT" + graphNodePort.hashCode().toString)
+  }
+}

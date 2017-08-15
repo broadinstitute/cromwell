b8b871a0e98ead744d09d6e3f4cc1758df9a2aef
Dan Billings <danb@broadinstitute.org>
2017-08-15 11:37:39 -0400

    distribute SBT settings to subprojects in release script

--- src/bin/publish_release.sh
+++ src/bin/publish_release.sh
@@ -6,9 +6,9 @@ echo "TRAVIS_TAG='$TRAVIS_TAG'"
 
 sbt \
     'set test in Test := {}' \
-    "set version := \"${TRAVIS_TAG}\"" \
-    'set isSnapshot := false' \
-    'set resolvers += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
-    'set publishTo := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
-    "set credentials += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
+    "set version in ThisBuild := \"${TRAVIS_TAG}\"" \
+    'set isSnapshot in ThisBuild := false' \
+    'set resolvers in ThisBuild += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
+    'set publishTo in ThisBuild := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
+    "set credentials in ThisBuild += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
     "+ publish" 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'

82feec8ed09ce0bf835bc6238fa6bfe89c7ceb21
Thibault Jeandet <tjeandet@broadinstitute.org>
2017-08-15 16:20:40 -0400

    validate workflowId for call cache diff endpoint

--- engine/src/main/scala/cromwell/engine/workflow/lifecycle/execution/callcaching/CallCacheDiffQueryParameter.scala
+++ engine/src/main/scala/cromwell/engine/workflow/lifecycle/execution/callcaching/CallCacheDiffQueryParameter.scala
@@ -2,19 +2,21 @@ package cromwell.engine.workflow.lifecycle.execution.callcaching
 
 import cats.data.{NonEmptyList, Validated}
 import cats.implicits._
+import cromwell.core.WorkflowId
+import lenthall.validation.ErrorOr.ShortCircuitingFlatMap
 import cromwell.engine.workflow.lifecycle.execution.callcaching.CallCacheDiffQueryParameter.CallCacheDiffQueryCall
 
 import scala.util.{Failure, Success, Try}
 
 object CallCacheDiffQueryParameter {
-  case class CallCacheDiffQueryCall(workflowId: String, callFqn: String, jobIndex: Option[Int])
+  case class CallCacheDiffQueryCall(workflowId: WorkflowId, callFqn: String, jobIndex: Option[Int])
 
   private def missingWorkflowError(attribute: String) = s"missing $attribute query parameter".invalidNel
 
   def fromParameters(parameters: Seq[(String, String)]): Validated[NonEmptyList[String], CallCacheDiffQueryParameter] = {
     def extractIndex(parameter: String): Validated[NonEmptyList[String], Option[Int]] = {
       parameters.find(_._1 == parameter) match {
-        case Some((_, value)) => Try(value.toInt) match {
+        case Some((_, value)) => Try(value.trim.toInt) match {
           case Success(index) => Option(index).validNel
           case Failure(f) => f.getMessage.invalidNel
         }
@@ -29,11 +31,19 @@ object CallCacheDiffQueryParameter {
       }
     }
     
-    val workflowAValidation = extractAttribute("workflowA")
-    val workflowBValidation = extractAttribute("workflowB")
+    def validateWorkflowId(parameter: String) = for {
+      workflowIdString <- extractAttribute(parameter)
+      workflowId <- Try(WorkflowId.fromString(workflowIdString.trim)) match {
+        case Success(id) => id.validNel
+        case Failure(f) => f.getMessage.invalidNel
+      }
+    } yield workflowId
+    
+    val workflowAValidation = validateWorkflowId("workflowA")
+    val workflowBValidation = validateWorkflowId("workflowB")
 
-    val callAValidation = extractAttribute("callA")
-    val callBValidation = extractAttribute("callB")
+    val callAValidation = extractAttribute("callA").map(_.trim)
+    val callBValidation = extractAttribute("callB").map(_.trim)
 
     val indexAValidation = extractIndex("indexA")
     val indexBValidation = extractIndex("indexB")

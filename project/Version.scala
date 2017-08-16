3a02ddeb185ba15df83f51c5b0bd6df944a60933
Dan Billings <danb@broadinstitute.org>
2017-08-16 10:32:46 -0400

    Update cromwell version from 29 to 30

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ import sbt._
 
 object Version {
   // Upcoming release, or current if we're on a master / hotfix branch
-  val cromwellVersion = "29"
+  val cromwellVersion = "30"
 
   // Adapted from SbtGit.versionWithGit
   def cromwellVersionWithGit: Seq[Setting[_]] =

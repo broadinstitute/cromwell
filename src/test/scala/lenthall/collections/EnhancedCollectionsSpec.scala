91b03b13098638657050fa170cce45d2f9be38d3
Chris Llanwarne <cjllanwarne@gmail.com>
2017-08-24 11:15:27 -0400

    Add a `filterByType` method to TraversableLikes

---
+++ src/test/scala/lenthall/collections/EnhancedCollectionsSpec.scala
@@ -0,0 +1,22 @@
+package lenthall.collections
+
+import org.scalatest.{FlatSpec, Matchers}
+import lenthall.collections.EnhancedCollections._
+
+class EnhancedCollectionsSpec extends FlatSpec with Matchers {
+  behavior of "EnhancedCollections"
+
+  it should "filter a List by type and return a List" in {
+    val objectList = List("hello", 3, None, "world")
+    val stringList = objectList.filterByType[String]
+
+    stringList should be(List("hello", "world"))
+  }
+
+  it should "filter a Set by type and return a Set" in {
+    val objectSet = Set("hello", 3, None, "world")
+    val intSet: Set[Int] = objectSet.filterByType[Int]
+
+    intSet should be(Set(3))
+  }
+}

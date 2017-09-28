50022c01545c9d112627a65dbac0efdab21e8471
Khalid Shakir <kshakir@broadinstitute.org>
2017-09-28 15:05:40 -0400

    Fixed snapshot check from old sbt setting to a sys.prop.

--- src/bin/after_success.sh
+++ src/bin/after_success.sh
@@ -10,21 +10,19 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "develop" ]; then
         sbt \
             'set test in Test := {}' \
-            'set isSnapshot in ThisBuild := false' \
             'set resolvers in ThisBuild += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
             'set publishTo in ThisBuild := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
             "set credentials in ThisBuild += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
-            "+ publish" 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'
+            +publish 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'
 
     elif [[ "$TRAVIS_BRANCH" =~ ^[0-9\.]+_hotfix$ ]]; then
         sbt \
             'set test in Test := {}' \
-            'set isSnapshot in ThisBuild := false' \
             'set git.gitUncommittedChanges in ThisBuild := false' \
             'set resolvers in ThisBuild += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
             'set publishTo in ThisBuild := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
             "set credentials in ThisBuild += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
-            "+ publish" 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'
+            -Dproject.isSnapshot=false +publish 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'
 
     fi
 fi
--- src/bin/publish_release.sh
+++ src/bin/publish_release.sh
@@ -7,8 +7,7 @@ echo "TRAVIS_TAG='$TRAVIS_TAG'"
 sbt \
     'set test in Test := {}' \
     "set version in ThisBuild := \"${TRAVIS_TAG}\"" \
-    'set isSnapshot in ThisBuild := false' \
     'set resolvers in ThisBuild += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
     'set publishTo in ThisBuild := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
     "set credentials in ThisBuild += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
-    "+ publish" 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'
+    -Dproject.isSnapshot=false +publish 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'

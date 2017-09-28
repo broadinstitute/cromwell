f5a7f3c4f77ec02bcb34df9254129b33545c4ce7
Khalid Shakir <kshakir@broadinstitute.org>
2017-09-28 13:53:56 -0400

    Fixes for after_success multi project publishing.

--- src/bin/after_success.sh
+++ src/bin/after_success.sh
@@ -10,20 +10,20 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "develop" ]; then
         sbt \
             'set test in Test := {}' \
-            'set isSnapshot := false' \
-            'set resolvers += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
-            'set publishTo := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
-            "set credentials += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
+            'set isSnapshot in ThisBuild := false' \
+            'set resolvers in ThisBuild += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
+            'set publishTo in ThisBuild := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
+            "set credentials in ThisBuild += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
             "+ publish" 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'
 
     elif [[ "$TRAVIS_BRANCH" =~ ^[0-9\.]+_hotfix$ ]]; then
         sbt \
             'set test in Test := {}' \
-            'set isSnapshot := false' \
-            'set git.gitUncommittedChanges := false' \
-            'set resolvers += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
-            'set publishTo := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
-            "set credentials += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
+            'set isSnapshot in ThisBuild := false' \
+            'set git.gitUncommittedChanges in ThisBuild := false' \
+            'set resolvers in ThisBuild += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)' \
+            'set publishTo in ThisBuild := Option("artifactory-publish" at "https://broadinstitute.jfrog.io/broadinstitute/libs-release-local;build.timestamp=" + new java.util.Date().getTime)' \
+            "set credentials in ThisBuild += Credentials(\"Artifactory Realm\", \"broadinstitute.jfrog.io\", \"${ARTIFACTORY_USERNAME}\", \"${ARTIFACTORY_PASSWORD}\")" \
             "+ publish" 2>&1 | sed 's/.*set credentials.*/REDACTED LOG LINE/'
 
     fi

d60f144cc047a8b9be0b6be4fbb4fba12971e2dd
geoffjentry <geoffjentry@gmail.com>
2017-10-27 12:20:29 -0400

    Update swagger yaml, upgrade swagger-ui, metadata still woefully broken

--- engine/src/main/scala/cromwell/webservice/SwaggerService.scala
+++ engine/src/main/scala/cromwell/webservice/SwaggerService.scala
@@ -3,5 +3,5 @@ package cromwell.webservice
 trait SwaggerService extends SwaggerUiResourceHttpService {
   override def swaggerServiceName = "cromwell"
 
-  override def swaggerUiVersion = "2.1.1"
+  override def swaggerUiVersion = "3.2.2"
 }

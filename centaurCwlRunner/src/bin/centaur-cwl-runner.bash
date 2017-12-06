4d713388729884102f8e2e596b64a475b44a0387
Dan Billings <danb@broadinstitute.org>
2017-12-06 14:19:24 -0500

    those derned quotes

--- centaurCwlRunner/src/bin/centaur-cwl-runner.bash
+++ centaurCwlRunner/src/bin/centaur-cwl-runner.bash
@@ -3,4 +3,4 @@
 # `sbt assembly` must have already been run.
 CENTAUR_CWL_JAR="${CENTAUR_CWL_JAR:-"$( find "$( dirname "${BASH_SOURCE[0]}" )/../../target/scala-2.12" -name 'centaur-cwl-runner-*.jar' )"}"
 
-java -jar "${CENTAUR_CWL_JAR}" $@
+java -jar ${CENTAUR_CWL_JAR} $@

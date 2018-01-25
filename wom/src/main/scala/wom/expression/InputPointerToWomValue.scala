f543933b2dbb4e2211bb3b9d2cfbc24c9865389c
Thibault Jeandet <tjeandet@broadinstitute.org>
2018-01-25 17:54:50 -0500

    update InputPointerToWomValue

--- wom/src/main/scala/wom/expression/InputPointerToWomValue.scala
+++ wom/src/main/scala/wom/expression/InputPointerToWomValue.scala
@@ -1,39 +1,37 @@
-package cromwell.engine.workflow.lifecycle.execution.job.preparation
+package wom.expression
 
 import cats.data.Validated.Valid
 import cats.syntax.validated._
-import cromwell.core.ExecutionIndex.ExecutionIndex
 import common.validation.ErrorOr.ErrorOr
-import cromwell.engine.workflow.lifecycle.execution.stores.ValueStore
 import shapeless.Poly1
 import wom.callable.Callable.{InputDefinition, InputDefinitionWithDefault, OptionalInputDefinition}
-import wom.expression.{IoFunctionSet, WomExpression}
 import wom.graph.GraphNodePort.OutputPort
 import wom.values.WomValue
 
-object InputPointerToWdlValue extends Poly1 {
+object InputPointerToWomValue extends Poly1 {
+  type OutputPortLookup = OutputPort => ErrorOr[WomValue]
   // Function that can transform any of the coproduct types to an ErrorOr[WomValue]
-  type ToWdlValueFn = (Map[String, WomValue], IoFunctionSet, ValueStore, ExecutionIndex, InputDefinition) => ErrorOr[WomValue]
+  type ToWdlValueFn = (Map[String, WomValue], IoFunctionSet, OutputPortLookup, InputDefinition) => ErrorOr[WomValue]
 
   implicit def fromWomValue: Case.Aux[WomValue, ToWdlValueFn] = at[WomValue] {
-    womValue => (_: Map[String, WomValue], _: IoFunctionSet, _: ValueStore, _: ExecutionIndex, _: InputDefinition) =>
-      womValue.validNel: ErrorOr[WomValue]
+    womValue => (_: Map[String, WomValue], ioFunctions: IoFunctionSet, _: OutputPortLookup, inputDefinition: InputDefinition) =>
+      inputDefinition.valueMapper(ioFunctions)(womValue).validNel: ErrorOr[WomValue]
   }
 
   implicit def fromOutputPort: Case.Aux[OutputPort, ToWdlValueFn] = at[OutputPort] {
-    port => (knownValues: Map[String, WomValue], ioFunctions: IoFunctionSet, valueStore: ValueStore, index: ExecutionIndex, inputDefinition: InputDefinition) =>
-      (valueStore.resolve(index)(port), inputDefinition) match {
-        case (v: Valid[WomValue], _) => v
-        case (_, InputDefinitionWithDefault(_, _, defaultExpression)) =>
-          evaluate(defaultExpression, knownValues, ioFunctions)
-        case (_, OptionalInputDefinition(_, optionalType)) => optionalType.none.validNel
-        case _ => s"Failed to lookup input value for required input ${port.name} at index $index in value store $valueStore".invalidNel
+    port => (knownValues: Map[String, WomValue], ioFunctions: IoFunctionSet, outputPortLookup: OutputPortLookup, inputDefinition: InputDefinition) =>
+      (outputPortLookup(port), inputDefinition) match {
+        case (v: Valid[WomValue], _) => v.map(inputDefinition.valueMapper(ioFunctions))
+        case (_, InputDefinitionWithDefault(_, _, defaultExpression, valueMapper)) =>
+          evaluate(defaultExpression, knownValues, ioFunctions).map(valueMapper(ioFunctions))
+        case (_, OptionalInputDefinition(_, optionalType, valueMapper)) => valueMapper(ioFunctions)(optionalType.none).validNel
+        case _ => s"Failed to lookup input value for required input ${port.name}".invalidNel
       }
   }
 
-  implicit def fromWomExpression: Case.Aux[WomExpression, ToWdlValueFn] = at[WomExpression] { 
-    womExpression => (knownValues: Map[String, WomValue], ioFunctions: IoFunctionSet, _: ValueStore, _ : ExecutionIndex, _: InputDefinition) =>
-      womExpression.evaluateValue(knownValues, ioFunctions): ErrorOr[WomValue]
+  implicit def fromWomExpression: Case.Aux[WomExpression, ToWdlValueFn] = at[WomExpression] {
+    womExpression => (knownValues: Map[String, WomValue], ioFunctions: IoFunctionSet, _: OutputPortLookup, inputDefinition: InputDefinition) =>
+      womExpression.evaluateValue(knownValues, ioFunctions).map(inputDefinition.valueMapper(ioFunctions)): ErrorOr[WomValue]
   }
 
   def evaluate(womExpression: WomExpression, knownValues: Map[String, WomValue], ioFunctions: IoFunctionSet): ErrorOr[WomValue] =

ecd90ee158777af4b642fb386cfd53a29010f9e0
Chris Llanwarne <cjllanwarne@users.noreply.github.com>
2018-01-25 13:22:58 -0500

    IWDR: Allow inputs to be relative (#3146)
    IWDR: Allow local input paths to be relative to PWD

--- backend/src/main/scala/cromwell/backend/wdl/WriteFunctions.scala
+++ backend/src/main/scala/cromwell/backend/wdl/WriteFunctions.scala
@@ -1,16 +1,19 @@
 package cromwell.backend.wdl
 
+import java.io.File
+
 import better.files.File.OpenOptions
 import cats.instances.future._
 import cats.syntax.functor._
 import cromwell.core.io.AsyncIoFunctions
-import cromwell.core.path.Path
+import cromwell.core.path.{Path, PathFactory}
 import wom.expression.IoFunctionSet
 import wom.values.WomSingleFile
 
 import scala.concurrent.Future
 
-trait WriteFunctions extends IoFunctionSet with AsyncIoFunctions {
+
+trait WriteFunctions extends PathFactory with IoFunctionSet with AsyncIoFunctions {
 
   /**
     * Directory that will be used to write files.
@@ -27,10 +30,19 @@ trait WriteFunctions extends IoFunctionSet with AsyncIoFunctions {
     }
   }
 
+  private val relativeToLocal = {
+    val initial = System.getProperty("user.dir")
+    if (initial.endsWith("/")) initial else initial + "/"
+  }
+
+  def relativeToAbsolutePath(pathFrom: String): String = if (new File(pathFrom).isAbsolute) pathFrom else relativeToLocal + pathFrom
+
   override def copyFile(pathFrom: String, targetName: String): Future[WomSingleFile] = {
-    val source = _writeDirectory.root / pathFrom
+    val source = buildPath(relativeToAbsolutePath(pathFrom))
     val destination = _writeDirectory / targetName
 
-    asyncIo.copyAsync(source, destination).as(WomSingleFile(destination.pathAsString))
+    asyncIo.copyAsync(source, destination).as(WomSingleFile(destination.pathAsString)) recoverWith {
+      case e => Future.failed(new Exception(s"Could not copy ${source.toAbsolutePath} to ${destination.toAbsolutePath}", e))
+    }
   }
 }

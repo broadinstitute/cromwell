ceda81752e25684f95029a88a868bf401b1526f3
Thibault Jeandet <tjeandet@broadinstitute.org>
2018-03-07 16:17:45 -0500

    small update to memory load

--- services/src/main/scala/cromwell/services/loadcontroller/impl/MemoryLoadControllerActor.scala
+++ services/src/main/scala/cromwell/services/loadcontroller/impl/MemoryLoadControllerActor.scala
@@ -22,6 +22,7 @@ object MemoryLoadControllerActor {
 class MemoryLoadControllerActor(thresholdInMB: Long,
                                 frequency: FiniteDuration,
                                 override val serviceRegistryActor: ActorRef) extends Actor with Timers with CromwellInstrumentationActor {
+  private var amortizedLoad: LoadLevel = NormalLoad
   private val nbRecordings = 10
   private var index: Int = 0
   private val loadRecordings = Array.fill[LoadLevel](nbRecordings) { NormalLoad }
@@ -30,23 +31,26 @@ class MemoryLoadControllerActor(thresholdInMB: Long,
     timers.startSingleTimer(MemoryLoadControlTimerKey, MemoryLoadControlTimerAction, frequency)
     super.preStart()
   }
-  
+
   override def receive = {
     case MemoryLoadControlTimerAction => updateAndCheckMemory()
   }
-  
+
   private [impl] def getFreeMemory = freeMemoryInMB
-  
+
   private def updateAndCheckMemory() = {
     val currentFreeMemory = getFreeMemory
     val currentLoad = if (currentFreeMemory < thresholdInMB) HighLoad else NormalLoad
     // Update
     loadRecordings.update(index, currentLoad)
     index = (index + 1) % nbRecordings
-    
-    // Check
-    val amortizedLoad = if (loadRecordings.forall(_ == HighLoad)) HighLoad
-    else NormalLoad
+
+    // Check if the load has been homogeneous for the past 10 checks
+    loadRecordings.toSet.size match {
+      // If yes and it's different from the current amortized load, update it
+      case 1 if loadRecordings.head != amortizedLoad => amortizedLoad = loadRecordings.head
+      case _ =>
+    }
 
     serviceRegistryActor ! LoadMetric("Memory", amortizedLoad)
     sendGauge(FreeMemoryInstrumentationPath, currentFreeMemory)

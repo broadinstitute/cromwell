d84be2e1c0fd75fcda3e18cb6e7b0ddb1827235b
Chris Llanwarne <cjllanwarne@users.noreply.github.com>
2018-03-07 12:17:21 -0500

    Type evaluators (#3364)
    Type evaluators

---
+++ wdl/transforms/draft3/src/test/scala/wdl/draft3/transforms/expression/MemberAccessTypeEvaluatorSpec.scala
@@ -0,0 +1,92 @@
+package wdl.draft3.transforms.expression
+
+import cats.data.NonEmptyList
+import common.assertion.ErrorOrAssertions._
+import org.scalatest.{FlatSpec, Matchers}
+import wdl.draft3.transforms.linking.expression.types.expressionTypeEvaluator
+import wdl.model.draft3.elements.ExpressionElement
+import wdl.model.draft3.elements.ExpressionElement._
+import wdl.model.draft3.graph._
+import wdl.model.draft3.graph.expression.TypeEvaluator.ops._
+import wom.types._
+import wom.values.WomInteger
+
+class MemberAccessTypeEvaluatorSpec extends FlatSpec with Matchers{
+
+  behavior of "member access type evaluator"
+
+  val fiveLiteral = PrimitiveLiteralExpressionElement(WomInteger(5))
+  val sixLiteral = PrimitiveLiteralExpressionElement(WomInteger(6))
+
+  it should "find the left and right hand sides of a pair expression" in {
+    val pair = PairLiteral(fiveLiteral, sixLiteral)
+    val leftExpression: ExpressionElement = ExpressionMemberAccess(pair, NonEmptyList("left", List.empty))
+    leftExpression.evaluateType(Map.empty) shouldBeValid WomIntegerType
+
+    val rightExpression: ExpressionElement = ExpressionMemberAccess(pair, NonEmptyList("right", List.empty))
+    rightExpression.evaluateType(Map.empty) shouldBeValid WomIntegerType
+  }
+
+  it should "find the appropriate value in a deeply nested Pair" in {
+    val nestedPairLookup: ExpressionElement = ExpressionMemberAccess(
+      expression = PairLiteral(
+        left = PairLiteral(
+          left = fiveLiteral,
+          right = PairLiteral(
+            left = fiveLiteral,
+            right = PairLiteral(
+              left = PairLiteral(fiveLiteral, sixLiteral), // This pair is the value we should be selecting
+              right = fiveLiteral
+            )
+          )
+        ),
+        right = fiveLiteral
+      ),
+      memberAccessTail = NonEmptyList("left", List("right", "right", "left"))
+    )
+     nestedPairLookup.evaluateType(Map.empty) shouldBeValid WomPairType(WomIntegerType, WomIntegerType)
+  }
+
+  it should "evaluate a nested member access on a call output" in {
+    val callOutputLookup: ExpressionElement = IdentifierMemberAccess("t", "out", List("left", "right"))
+    val linkedValues = Map[UnlinkedConsumedValueHook, GeneratedValueHandle](
+      UnlinkedCallOutputOrIdentifierAndMemberAccessHook("t", "out") -> GeneratedCallOutputValueHandle("t", "out", WomPairType(WomPairType(WomIntegerType, WomPairType(WomIntegerType, WomIntegerType)), WomStringType))
+    )
+
+    callOutputLookup.evaluateType(linkedValues) shouldBeValid WomPairType(WomIntegerType, WomIntegerType)
+  }
+
+  it should "evaluate a nested member access on a struct" in {
+    val objectLookup: ExpressionElement = IdentifierMemberAccess("t", "out", List("left", "right"))
+    val linkedValues = Map[UnlinkedConsumedValueHook, GeneratedValueHandle](
+      UnlinkedCallOutputOrIdentifierAndMemberAccessHook("t", "out") -> GeneratedIdentifierValueHandle(
+        linkableName = "t",
+        womType = WomCompositeType(Map("out" -> WomPairType(WomPairType(WomIntegerType, WomPairType(WomIntegerType, WomIntegerType)), WomIntegerType)))
+      )
+    )
+
+    objectLookup.evaluateType(linkedValues) shouldBeValid WomPairType(WomIntegerType, WomIntegerType)
+  }
+
+  it should "evaluate a nested member access type on an object" in {
+    val objectLookup: ExpressionElement = IdentifierMemberAccess("t", "out", List("left", "right"))
+    val linkedValues = Map[UnlinkedConsumedValueHook, GeneratedValueHandle](
+      UnlinkedCallOutputOrIdentifierAndMemberAccessHook("t", "out") -> GeneratedIdentifierValueHandle(
+        linkableName = "t",
+        womType = WomObjectType
+      )
+    )
+
+    objectLookup.evaluateType(linkedValues) shouldBeValid WomAnyType
+  }
+
+  it should "evaluate an identifier lookup" in {
+    val identifierLookup: ExpressionElement = IdentifierLookup("foo")
+    val linkedValues = Map[UnlinkedConsumedValueHook, GeneratedValueHandle](
+      UnlinkedIdentifierHook("foo") -> GeneratedIdentifierValueHandle(linkableName = "foo", womType = WomPairType(WomStringType, WomStringType))
+    )
+
+    identifierLookup.evaluateType(linkedValues) shouldBeValid WomPairType(WomStringType, WomStringType)
+  }
+
+}
--- wdl/transforms/draft3/src/test/scala/wdl/draft3/transforms/expression/UnaryAndBinaryOperatorsEvaluatorSpec.scala
+++ wdl/transforms/draft3/src/test/scala/wdl/draft3/transforms/expression/UnaryAndBinaryOperatorsEvaluatorSpec.scala
@@ -3,10 +3,13 @@ package wdl.draft3.transforms.expression
 import common.assertion.ErrorOrAssertions._
 import org.scalatest.{FlatSpec, Matchers}
 import wdl.draft3.transforms.linking.expression.values.expressionEvaluator
+import wdl.draft3.transforms.linking.expression.types.expressionTypeEvaluator
 import wdl.model.draft3.elements.ExpressionElement
 import wdl.model.draft3.elements.ExpressionElement._
 import wdl.model.draft3.graph.expression.ValueEvaluator.ops._
+import wdl.model.draft3.graph.expression.TypeEvaluator.ops._
 import wom.expression.NoIoFunctionSet
+import wom.types.{WomBooleanType, WomIntegerType, WomType}
 import wom.values.{WomBoolean, WomInteger, WomValue}
 
 /**
@@ -26,40 +29,44 @@ class UnaryAndBinaryOperatorsEvaluatorSpec extends FlatSpec with Matchers{
   val womSix = WomInteger(6)
 
   // Format:
-  // Test name, input expression, output value.
-  val expressionTests: List[(String, ExpressionElement, WomValue)] = List(
-    ("+5", UnaryPlus(fiveLiteral), womFive),
-    ("-5", UnaryNegation(fiveLiteral), WomInteger(-5)),
-    ("-(-5)", UnaryNegation(UnaryNegation(fiveLiteral)), womFive),
-    ("-(+(-5))", UnaryNegation(UnaryPlus(UnaryNegation(fiveLiteral))), WomInteger(-5)),
-    ("!true", LogicalNot(trueLiteral), WomBoolean(false)),
-    ("!(!true)", LogicalNot(LogicalNot(trueLiteral)), WomBoolean(true)),
+  // Test name, input expression, output value, output type.
+  val expressionTests: List[(String, ExpressionElement, WomValue, WomType)] = List(
+    ("+5", UnaryPlus(fiveLiteral), womFive, WomIntegerType),
+    ("-5", UnaryNegation(fiveLiteral), WomInteger(-5), WomIntegerType),
+    ("-(-5)", UnaryNegation(UnaryNegation(fiveLiteral)), womFive, WomIntegerType),
+    ("-(+(-5))", UnaryNegation(UnaryPlus(UnaryNegation(fiveLiteral))), WomInteger(-5), WomIntegerType),
+    ("!true", LogicalNot(trueLiteral), WomBoolean(false), WomBooleanType),
+    ("!(!true)", LogicalNot(LogicalNot(trueLiteral)), WomBoolean(true), WomBooleanType),
 
-    ("true || false", LogicalOr(trueLiteral, falseLiteral), WomBoolean(true)),
-    ("true && false", LogicalAnd(trueLiteral, falseLiteral), WomBoolean(false)),
-    ("true == false", Equals(trueLiteral, falseLiteral), WomBoolean(false)),
-    ("true != false", NotEquals(trueLiteral, falseLiteral), WomBoolean(true)),
+    ("true || false", LogicalOr(trueLiteral, falseLiteral), WomBoolean(true), WomBooleanType),
+    ("true && false", LogicalAnd(trueLiteral, falseLiteral), WomBoolean(false), WomBooleanType),
+    ("true == false", Equals(trueLiteral, falseLiteral), WomBoolean(false), WomBooleanType),
+    ("true != false", NotEquals(trueLiteral, falseLiteral), WomBoolean(true), WomBooleanType),
 
-    ("5 < 6", LessThan(fiveLiteral, sixLiteral), WomBoolean(true)),
-    ("5 < 5", LessThan(fiveLiteral, fiveLiteral), WomBoolean(false)),
-    ("5 <= 6", LessThanOrEquals(fiveLiteral, sixLiteral), WomBoolean(true)),
-    ("5 <= 5", LessThanOrEquals(fiveLiteral, fiveLiteral), WomBoolean(true)),
+    ("5 < 6", LessThan(fiveLiteral, sixLiteral), WomBoolean(true), WomBooleanType),
+    ("5 < 5", LessThan(fiveLiteral, fiveLiteral), WomBoolean(false), WomBooleanType),
+    ("5 <= 6", LessThanOrEquals(fiveLiteral, sixLiteral), WomBoolean(true), WomBooleanType),
+    ("5 <= 5", LessThanOrEquals(fiveLiteral, fiveLiteral), WomBoolean(true), WomBooleanType),
 
-    ("6 > 5", GreaterThan(sixLiteral, fiveLiteral), WomBoolean(true)),
-    ("5 > 5", GreaterThan(fiveLiteral, fiveLiteral), WomBoolean(false)),
-    ("6 >= 6", GreaterThanOrEquals(sixLiteral, fiveLiteral), WomBoolean(true)),
-    ("5 >= 5", GreaterThanOrEquals(fiveLiteral, fiveLiteral), WomBoolean(true)),
+    ("6 > 5", GreaterThan(sixLiteral, fiveLiteral), WomBoolean(true), WomBooleanType),
+    ("5 > 5", GreaterThan(fiveLiteral, fiveLiteral), WomBoolean(false), WomBooleanType),
+    ("6 >= 6", GreaterThanOrEquals(sixLiteral, fiveLiteral), WomBoolean(true), WomBooleanType),
+    ("5 >= 5", GreaterThanOrEquals(fiveLiteral, fiveLiteral), WomBoolean(true), WomBooleanType),
 
-    ("5 + 5", Add(fiveLiteral, fiveLiteral), WomInteger(10)),
-    ("5 - 5", Subtract(fiveLiteral, fiveLiteral), WomInteger(0)),
-    ("5 * 5", Multiply(fiveLiteral, fiveLiteral), WomInteger(25)),
-    ("25 * 5", Divide(PrimitiveLiteralExpressionElement(WomInteger(25)), fiveLiteral), WomInteger(5)),
-    ("27 % 5", Remainder(PrimitiveLiteralExpressionElement(WomInteger(27)), fiveLiteral), WomInteger(2))
+    ("5 + 5", Add(fiveLiteral, fiveLiteral), WomInteger(10), WomIntegerType),
+    ("5 - 5", Subtract(fiveLiteral, fiveLiteral), WomInteger(0), WomIntegerType),
+    ("5 * 5", Multiply(fiveLiteral, fiveLiteral), WomInteger(25), WomIntegerType),
+    ("25 * 5", Divide(PrimitiveLiteralExpressionElement(WomInteger(25)), fiveLiteral), WomInteger(5), WomIntegerType),
+    ("27 % 5", Remainder(PrimitiveLiteralExpressionElement(WomInteger(27)), fiveLiteral), WomInteger(2), WomIntegerType)
   )
 
-  expressionTests foreach { case (name, expression, expected) =>
+  expressionTests foreach { case (name, expression, expectedValue, expectedType) =>
     it should s"evaluate the expression '$name'" in {
-      expression.evaluateValue(Map.empty, NoIoFunctionSet) shouldBeValid expected
+      expression.evaluateValue(Map.empty, NoIoFunctionSet) shouldBeValid expectedValue
+    }
+
+    it should s"evaluate the type of the expression '$name'" in {
+      expression.evaluateType(Map.empty) shouldBeValid expectedType
     }
   }
 }

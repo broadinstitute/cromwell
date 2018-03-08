a5145c5a4e347ccebb8b1a42dee1a664bbbd0028
Thibault Jeandet <tjeandet@broadinstitute.org>
2018-03-07 20:37:10 -0500

    Update abort and token return

--- supportedBackends/jes/src/main/scala/cromwell/backend/impl/jes/statuspolling/JesAbortClient.scala
+++ supportedBackends/jes/src/main/scala/cromwell/backend/impl/jes/statuspolling/JesAbortClient.scala
@@ -2,7 +2,7 @@ package cromwell.backend.impl.jes.statuspolling
 
 import akka.actor.{Actor, ActorLogging, ActorRef}
 import cromwell.backend.impl.jes.statuspolling.JesApiQueryManager.JesApiAbortQueryFailed
-import cromwell.backend.impl.jes.statuspolling.RunAbort.{PAPIAbortRequestSuccessful, PAPIOperationAlreadyCancelled}
+import cromwell.backend.impl.jes.statuspolling.RunAbort.{PAPIAbortRequestSuccessful, PAPIOperationAlreadyCancelled, PAPIOperationHasAlreadyFinished}
 import cromwell.core.logging.JobLogging
 
 trait JesAbortClient { this: Actor with ActorLogging with JobLogging =>
@@ -14,6 +14,9 @@ trait JesAbortClient { this: Actor with ActorLogging with JobLogging =>
     // In this case we could immediately return an aborted handle and spare ourselves a d round of polling
     case PAPIOperationAlreadyCancelled(jobId) =>
       jobLogger.info(s"Operation $jobId was already cancelled")
+    // In this case we could immediately return an aborted handle and spare ourselves a d round of polling
+    case PAPIOperationHasAlreadyFinished(jobId) =>
+      jobLogger.info(s"Operation $jobId has already finished")
     case JesApiAbortQueryFailed(jobId, e) =>
       jobLogger.error(s"Could not request cancellation of job $jobId", e)
   }

4200bc649e46730fcf4b2c7a638654ab5401865d
Dan Billings <danbills2live.com>
2018-03-08 21:13:06 -0500

    couple things from giab

--- wom/src/main/scala/wom/types/WomLongType.scala
+++ wom/src/main/scala/wom/types/WomLongType.scala
@@ -9,10 +9,11 @@ case object WomLongType extends WomPrimitiveType {
   override protected def coercion = {
     case i: Long => WomLong(i)
     case i: Integer => WomLong(i.toLong)
-    case n: JsNumber if n.value.isValidLong => WomInteger(n.value.intValue())
-    case i: WomInteger => i
-    case s: WomString => WomInteger(s.value.toInt)
-    case s: String => WomInteger(s.toInt)
-    case s: JsString => WomInteger(s.value.toInt)
+    case n: JsNumber if n.value.isValidLong => WomLong(n.value.longValue())
+    case WomInteger(i) => WomLong(i.toLong)
+    case i: WomLong => i
+    case s: WomString => WomLong(s.value.toLong)
+    case s: String => WomLong(s.toLong)
+    case s: JsString => WomLong(s.value.toLong)
   }
 }

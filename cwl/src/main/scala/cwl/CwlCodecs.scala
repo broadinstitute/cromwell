2eddeb04b251ed483caa1bd052d7eaad793abe3b
Adam Nichols <anichols@broadinstitute.org>
2018-04-18 16:15:40 -0400

    Maybe fix

--- cwl/src/main/scala/cwl/CwlCodecs.scala
+++ cwl/src/main/scala/cwl/CwlCodecs.scala
@@ -29,9 +29,9 @@ object CwlCodecs {
 
   //According to automatic derivation, these instances should not be required.  But
   //removing these breaks decodeCwl, so...
-  implicit def wfD : Decoder[Workflow] = implicitly[Decoder[Workflow]]
-  implicit def cltD: Decoder[CommandLineTool] = implicitly[Decoder[CommandLineTool]]
-  implicit def etD : Decoder[ExpressionTool] = implicitly[Decoder[ExpressionTool]]
+  implicit lazy val wfD : Decoder[Workflow] = implicitly[Decoder[Workflow]]
+  implicit lazy val cltD: Decoder[CommandLineTool] = implicitly[Decoder[CommandLineTool]]
+  implicit lazy val etD : Decoder[ExpressionTool] = implicitly[Decoder[ExpressionTool]]
 
   def decodeCwl(json: Json): Checked[Cwl] = {
     findClass(json) match {

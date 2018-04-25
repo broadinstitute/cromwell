ccc76c5be9d7fdc01a3d69ecca46d4593a6220ee
Chris Llanwarne <cjllanwarne@users.noreply.github.com>
2018-04-25 16:03:22 -0400

    Test struct localization and delocalization (#3547)
    Struct localization and delocalization

--- wdl/model/shared/src/main/scala/wdl/shared/model/expression/FileEvaluatorUtil.scala
+++ wdl/model/shared/src/main/scala/wdl/shared/model/expression/FileEvaluatorUtil.scala
@@ -1,7 +1,7 @@
 package wdl.shared.model.expression
 
 import wom.types.WomType
-import wom.values.{WomArray, WomFile, WomMap, WomOptionalValue, WomPair, WomValue}
+import wom.values.{WomArray, WomFile, WomMap, WomObject, WomOptionalValue, WomPair, WomValue}
 
 import scala.util.Success
 
@@ -16,6 +16,7 @@ object FileEvaluatorUtil {
         (m.value flatMap { case (k, v) => Seq(k, v) } flatMap(findFilesToDelocalize(_, coerceTo, coerce=false))).toSeq
       case Success(WomOptionalValue(_, Some(v))) => findFilesToDelocalize(v, coerceTo, coerce=false)
       case Success(WomPair(l, r)) => findFilesToDelocalize(l, coerceTo, coerce = false) ++ findFilesToDelocalize(r, coerceTo, coerce = false)
+      case Success(o: WomObject) => o.values.values.flatMap(inner => findFilesToDelocalize(inner, inner.womType, coerce = false)).toSeq
       case _ => Seq.empty[WomFile]
     }
   }

e998096b0f563e21da8d9fb75fa6bd2be34fbcbc
Thibault Jeandet <tjeandet@broadinstitute.org>
2018-06-13 20:13:49 -0400

    only change for v2

--- supportedBackends/google/pipelines/v2alpha1/src/main/scala/cromwell/backend/google/pipelines/v2alpha1/PipelinesUtilityConversions.scala
+++ supportedBackends/google/pipelines/v2alpha1/src/main/scala/cromwell/backend/google/pipelines/v2alpha1/PipelinesUtilityConversions.scala
@@ -4,10 +4,11 @@ import java.time.OffsetDateTime
 
 import com.google.api.services.genomics.v2alpha1.model.{Accelerator, Disk, Event, Mount}
 import cromwell.backend.google.pipelines.common.api.PipelinesApiRequestFactory.CreatePipelineParameters
-import cromwell.backend.google.pipelines.common.io.PipelinesApiAttachedDisk
+import cromwell.backend.google.pipelines.common.io.{DiskType, PipelinesApiAttachedDisk}
 import cromwell.backend.google.pipelines.common.{GpuResource, PipelinesApiRuntimeAttributes}
 import cromwell.core.ExecutionEvent
 import cromwell.core.logging.JobLogger
+import mouse.all._
 
 trait PipelinesUtilityConversions {
   def toAccelerator(gpuResource: GpuResource) = new Accelerator().setCount(gpuResource.gpuCount.value.toLong).setType(gpuResource.gpuType.toString)
@@ -20,6 +21,12 @@ trait PipelinesUtilityConversions {
   def toDisk(disk: PipelinesApiAttachedDisk) = new Disk()
     .setName(disk.name)
     .setSizeGb(disk.sizeGb)
-    .setType(disk.diskType.googleTypeName)
+    .setType(disk.diskType |> toV2DiskType)
   def toExecutionEvent(event: Event) : ExecutionEvent = ExecutionEvent(event.getDescription, OffsetDateTime.parse(event.getTimestamp))
+
+  private def toV2DiskType(diskType: DiskType) = diskType match {
+    case DiskType.HDD => "pd-standard"
+    case DiskType.SSD => "pd-ssd"
+    case DiskType.LOCAL => "local-ssd"
+  }
 }

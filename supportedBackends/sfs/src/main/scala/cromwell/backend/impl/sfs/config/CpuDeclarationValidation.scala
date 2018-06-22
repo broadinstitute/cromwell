53200ada22e1c829bb154c275fcc960ee1bcd499
Thibault Jeandet <tjeandet@broadinstitute.org>
2018-06-22 17:07:36 -0400

    with test

---
+++ supportedBackends/sfs/src/main/scala/cromwell/backend/impl/sfs/config/CpuDeclarationValidation.scala
@@ -0,0 +1,15 @@
+package cromwell.backend.impl.sfs.config
+
+import cromwell.backend.validation.{RuntimeAttributesValidation, ValidatedRuntimeAttributes}
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.Positive
+import wdl.draft2.model.Declaration
+import wom.values.WomValue
+
+class CpuDeclarationValidation(declaration: Declaration, instanceValidation: RuntimeAttributesValidation[Int Refined Positive]) extends DeclarationValidation(declaration, instanceValidation) {
+  override def extractWdlValueOption(validatedRuntimeAttributes: ValidatedRuntimeAttributes): Option[WomValue] = {
+    RuntimeAttributesValidation.extractOption(instanceValidation, validatedRuntimeAttributes) map { refined =>
+      declaration.womType.coerceRawValue(refined.value).get
+    }
+  }
+}
--- supportedBackends/sfs/src/main/scala/cromwell/backend/impl/sfs/config/DeclarationValidation.scala
+++ supportedBackends/sfs/src/main/scala/cromwell/backend/impl/sfs/config/DeclarationValidation.scala
@@ -2,7 +2,6 @@ package cromwell.backend.impl.sfs.config
 
 import cromwell.backend.impl.sfs.config.ConfigConstants._
 import cromwell.backend.validation._
-import eu.timepit.refined.api.Refined
 import wdl.draft2.model.expression.NoFunctions
 import wdl.draft2.model.{Declaration, NoLookup, WdlExpression}
 import wom.RuntimeAttributesKeys
@@ -28,9 +27,9 @@ object DeclarationValidation {
       // Docker and CPU are special keys understood by cromwell.
       case name if name == DockerValidation.instance.key =>
         new DeclarationValidation(declaration, DockerValidation.instance)
-      case RuntimeAttributesKeys.CpuKey => new DeclarationValidation(declaration, CpuValidation.instance)
-      case RuntimeAttributesKeys.CpuMinKey => new DeclarationValidation(declaration, CpuValidation.instanceMin)
-      case RuntimeAttributesKeys.CpuMaxKey => new DeclarationValidation(declaration, CpuValidation.instanceMax)
+      case RuntimeAttributesKeys.CpuKey => new CpuDeclarationValidation(declaration, CpuValidation.instance)
+      case RuntimeAttributesKeys.CpuMinKey => new CpuDeclarationValidation(declaration, CpuValidation.instanceMin)
+      case RuntimeAttributesKeys.CpuMaxKey => new CpuDeclarationValidation(declaration, CpuValidation.instanceMax)
       // See MemoryDeclarationValidation for more info
       case name if MemoryDeclarationValidation.isMemoryDeclaration(name, MemoryRuntimeAttribute, MemoryRuntimeAttributePrefix) =>
         new MemoryDeclarationValidation(declaration, MemoryRuntimeAttribute, MemoryRuntimeAttributePrefix)
@@ -111,8 +110,7 @@ class DeclarationValidation(declaration: Declaration, instanceValidation: Runtim
     */
   def extractWdlValueOption(validatedRuntimeAttributes: ValidatedRuntimeAttributes): Option[WomValue] = {
     RuntimeAttributesValidation.extractOption(instanceValidation, validatedRuntimeAttributes) map {
-      case refined: Refined[_, _] => declaration.womType.coerceRawValue(refined.value).get
-      case other => declaration.womType.coerceRawValue(other).get
+      declaration.womType.coerceRawValue(_).get
     }
   }
 }

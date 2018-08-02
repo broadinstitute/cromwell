8033db11b6dc3c0916719dd057ef5afe1139dab1
Garret Kelly <Garret.Kelly@gmail.com>
2018-08-02 16:09:03 -0400

    Add NCBI Sequence Read Archive support via fusera (#3950)
    * Parse Sequence Read Archive (SRA) paths
    
    Add an SRA path factory and path class for capturing an accession and
    path underneath it as a single object.
    
    * Support inputs hosted in SRA via fusera
    
    This change adds support to the pipelines v2alpha1 backend for inputs
    which reside in the Sequence Read Archive.

---
+++ filesystems/sra/src/main/scala/cromwell/filesystems/sra/SraPathBuilder.scala
@@ -0,0 +1,32 @@
+package cromwell.filesystems.sra
+
+import cromwell.core.path.{NioPath, Path, PathBuilder}
+
+import scala.util.{Failure, Success, Try}
+
+class SraPathBuilder extends PathBuilder {
+  override def name: String = "NIH Sequence Read Archive"
+
+  def build(path: String): Try[SraPath] = {
+    if (!path.startsWith(SraPath.Scheme)) {
+      return Failure(new IllegalArgumentException(s"$path is not a SRA path"))
+    }
+    val parts = path.stripPrefix(SraPath.Scheme).split("/", 2)
+    if (parts.length != 2) {
+      return Failure(new IllegalArgumentException(s"$path does not have enough components"))
+    }
+    Success(new SraPath(parts(0), parts(1)))
+  }
+}
+
+object SraPath {
+  val Scheme = "sra://"
+}
+
+case class SraPath(accession: String, path: String) extends Path {
+  override def pathAsString: String = SraPath.Scheme + pathWithoutScheme
+  override def pathWithoutScheme: String = accession + "/" + path
+
+  protected def newPath(nioPath: NioPath): Path = throw new NotImplementedError("'newPath' not implemented for SraPath")
+  protected def nioPath: NioPath = throw new NotImplementedError("'nioPath' not implemented for SraPath")
+}
---
+++ filesystems/sra/src/test/scala/cromwell/filesystems/sra/SraPathBuilderSpec.scala
@@ -0,0 +1,88 @@
+package cromwell.filesystems.sra
+
+import cromwell.core.path._
+import cromwell.core.TestKitSuite
+import org.scalatest.{FlatSpecLike, Matchers}
+
+class SraPathBuilderSpec extends TestKitSuite with FlatSpecLike with Matchers with PathBuilderSpecUtils {
+  behavior of "SraPathBuilder"
+
+  goodPaths foreach {
+    good => it should behave like buildGood(good)
+  }
+
+  badPaths foreach {
+    bad => it should behave like buildBad(bad)
+  }
+
+  private def buildGood(good: Good): Unit = {
+    behavior of s"Building ${good.description}"
+
+    val built = pathBuilder.build(good.in)
+    it should "successfully construct an SraPath" in {
+      assert(built.isSuccess)
+    }
+
+    val path = built.get
+    val sraPath = path.asInstanceOf[SraPath]
+
+    it should "match expected accession" in {
+      sraPath.accession should be (good.accession)
+    }
+
+    it should "match expected path" in {
+      sraPath.path should be (good.path)
+    }
+  }
+
+  private def buildBad(bad: Bad): Unit = {
+    behavior of s"Building ${bad.description}"
+
+    it should "fail to build an SraPath" in {
+      pathBuilder.build(bad.in).isSuccess should be (false)
+    }
+  }
+
+  private lazy val pathBuilder = new SraPathBuilder
+
+  private case class Good(description: String,
+                          in: String,
+                          accession: String,
+                          path: String)
+  private def goodPaths = Seq(
+    Good(
+      description = "well-formed path",
+      in = "sra://SXP000001/asdf.bam",
+      accession = "SXP000001",
+      path = "asdf.bam",
+    ),
+    Good(
+      description = "nested path",
+      in = "sra://SRA42424242/first/second/third.bam.bai",
+      accession = "SRA42424242",
+      path = "first/second/third.bam.bai",
+    ),
+    Good(
+      description = "path with spaces",
+      in = "sra://SXP111111/top level/nested level/file name.bz2",
+      accession = "SXP111111",
+      path = "top level/nested level/file name.bz2",
+    ),
+  )
+
+  private case class Bad(description: String, in: String)
+  private def badPaths = Seq(
+    Bad(
+      description = "not an SRA path",
+      in = "gcs://some/gcs/path/thing.txt",
+    ),
+    Bad(
+      description = "missing accession",
+      in = "sra://",
+    ),
+    Bad(
+      description = "missing path within accession",
+      in = "sra://SRA00001",
+    ),
+  )
+}

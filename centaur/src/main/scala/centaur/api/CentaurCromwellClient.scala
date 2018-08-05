1aceef880a6e83f10a13274cc4d4d8988c797570
geoffjentry <geoffjentry@gmail.com>
2018-08-05 17:40:44 -0400

    blarg

--- centaur/src/main/scala/centaur/api/CentaurCromwellClient.scala
+++ centaur/src/main/scala/centaur/api/CentaurCromwellClient.scala
@@ -70,8 +70,8 @@ object CentaurCromwellClient {
 
   def metadata(id: WorkflowId): IO[WorkflowMetadata] = {
     sendReceiveFutureCompletion(() => cromwellClient.metadata(id)) map { m =>
-      println("HAS SHARD INDEX: " + m.value.contains("shardIndex"))
-      println("HAS COMMAND LINE: " + m.value.contains("commandLine"))
+      println(id + " HAS SHARD INDEX: " + m.value.contains("shardIndex"))
+      println(id + "HAS COMMAND LINE: " + m.value.contains("commandLine"))
       WorkflowMetadata.fromMetadataJson(m.value).toOption.get
     }
   }

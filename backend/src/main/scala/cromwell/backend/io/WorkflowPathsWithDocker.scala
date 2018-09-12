de3da8d7bccfc206f765d323340fa60f7ddde20d
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-09-12 13:17:38 +0200

    allow dockerRoot to be settable in config

--- backend/src/main/scala/cromwell/backend/io/WorkflowPathsWithDocker.scala
+++ backend/src/main/scala/cromwell/backend/io/WorkflowPathsWithDocker.scala
@@ -3,13 +3,18 @@ package cromwell.backend.io
 import com.typesafe.config.Config
 import cromwell.backend.{BackendJobDescriptorKey, BackendWorkflowDescriptor}
 import cromwell.core.path.{DefaultPathBuilder, Path, PathBuilder}
+import net.ceedubs.ficus.Ficus._
 
 object WorkflowPathsWithDocker {
   val DockerRoot: Path = DefaultPathBuilder.get("/cromwell-executions")
 }
 
 final case class WorkflowPathsWithDocker(workflowDescriptor: BackendWorkflowDescriptor, config: Config, pathBuilders: List[PathBuilder] = WorkflowPaths.DefaultPathBuilders) extends WorkflowPaths {
-  val dockerWorkflowRoot: Path = workflowPathBuilder(WorkflowPathsWithDocker.DockerRoot)
+  val dockerRoot: Path = config.as[Option[String]]("dockerRoot") match {
+    case Some(pathString) => DefaultPathBuilder.get(pathString)
+    case _ => WorkflowPathsWithDocker.DockerRoot
+  }
+  val dockerWorkflowRoot: Path = workflowPathBuilder(dockerRoot)
 
   override def toJobPaths(workflowPaths: WorkflowPaths, jobKey: BackendJobDescriptorKey): JobPathsWithDocker = {
     new JobPathsWithDocker(workflowPaths.asInstanceOf[WorkflowPathsWithDocker], jobKey)

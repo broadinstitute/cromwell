cf17b636bb04e86237005c692ec81c0c1f038ce9
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-09-17 13:20:15 +0200

    start on reusable testfunctions

--- backend/src/test/scala/cromwell/backend/io/WorkflowPathsSpec.scala
+++ backend/src/test/scala/cromwell/backend/io/WorkflowPathsSpec.scala
@@ -11,11 +11,28 @@ import wom.graph.WomIdentifier
 
 class WorkflowPathsSpec extends FlatSpec with Matchers with BackendSpec {
 
-  val backendConfig = mock[Config]
+  def createBackendConfig(values: Map[String, String]): Config = {
+    val backendConfig = mock[Config]
+    values.foreach {
+      case (key: String, value: String) => {
+        when(backendConfig.hasPath(key)).thenReturn(true)
+        when(backendConfig.getString(key)).thenReturn(value)
+      }
+    }
+    backendConfig
+  }
+
+  def rootConfig(root: Option[String], dockerRoot: Option[String]) = {
+    val values: Map[String,String] = root.map("root" -> _).toMap ++ dockerRoot.map("dockerRoot" -> _).toMap
+    createBackendConfig(values)
+  }
+
+  def testWorkflowPaths(root: Option[String], dockerRoot: Option[String]): Unit = {
+
+  }
 
   "WorkflowPaths" should "provide correct paths for a workflow" in {
-    when(backendConfig.hasPath("root")).thenReturn(true)
-    when(backendConfig.getString("root")).thenReturn("local-cromwell-executions") // This is the folder defined in the config as the execution root dir
+    val backendConfig = createBackendConfig(Map("root" -> "local-cromwell-executions"))
     val wd = buildWdlWorkflowDescriptor(TestWorkflows.HelloWorld)
     val workflowPaths = new WorkflowPathsWithDocker(wd, backendConfig)
     val id = wd.id
@@ -26,6 +43,7 @@ class WorkflowPathsSpec extends FlatSpec with Matchers with BackendSpec {
   }
 
   "WorkflowPaths" should "provide correct paths for a sub workflow" in {
+    val backendConfig = createBackendConfig(Map("root" -> "local-cromwell-executions"))
     when(backendConfig.hasPath("root")).thenReturn(true)
     when(backendConfig.getString("root")).thenReturn("local-cromwell-executions") // This is the folder defined in the config as the execution root dir
     val rootWd = mock[BackendWorkflowDescriptor]

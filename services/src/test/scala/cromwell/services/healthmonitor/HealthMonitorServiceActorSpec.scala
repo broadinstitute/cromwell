3b3fe89e3c5e0bae1768b8b45975d02c82224298
Thib <Horneth@users.noreply.github.com>
2018-10-29 10:08:03 -0400

    Attempt to fix "fail if any subsystems fail but correctly bin them" in jenkins  (#4325)
    * health monitor

--- services/src/test/scala/cromwell/services/healthmonitor/HealthMonitorServiceActorSpec.scala
+++ services/src/test/scala/cromwell/services/healthmonitor/HealthMonitorServiceActorSpec.scala
@@ -74,9 +74,10 @@ class HealthMonitorServiceActorSpec extends TestKitSuite with FlatSpecLike with 
       /*
       Unlike success-statuses, failure-statuses retry a number of times before storing their values.
       While the failures-status is retrying, the success-status ends up becoming "stale", and flips to "unknown".
-      So, increase the timeout for marking the success-status as stale.
+      So, increase the timeout for marking the success-status as stale and set the failureRetryCount to 0
        */
       override lazy val staleThreshold = scaled(10.seconds)
+      override lazy val failureRetryCount = 0
     })
     eventualStatus(hm, ok = false, (SuccessSubsystem, OkStatus), (FailureSubsystem, FailedStatus))
   }

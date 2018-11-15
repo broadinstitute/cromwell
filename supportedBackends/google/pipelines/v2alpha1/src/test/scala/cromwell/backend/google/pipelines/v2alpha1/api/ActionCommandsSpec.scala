6a89a8a5817907af205e5af35d48562f7f212493
Thibault Jeandet <tjeandet@broadinstitute.org>
2018-11-15 13:22:56 -0500

    Print gsutil errors on PAPI2

--- supportedBackends/google/pipelines/v2alpha1/src/test/scala/cromwell/backend/google/pipelines/v2alpha1/api/ActionCommandsSpec.scala
+++ supportedBackends/google/pipelines/v2alpha1/src/test/scala/cromwell/backend/google/pipelines/v2alpha1/api/ActionCommandsSpec.scala
@@ -18,11 +18,42 @@ class ActionCommandsSpec extends FlatSpec with Matchers with Mockito {
       s"flag is $flag"
     }
     
-    recovered shouldBe "flag is  2> gsutil_output.txt; RC_GSUTIL=$?; if [ \"$RC_GSUTIL\" = \"1\" ]; then\n grep \"Bucket is requester pays bucket but no user project provided.\" gsutil_output.txt && echo \"Retrying with user project\"; flag is -u my-project; fi "
+    recovered shouldBe """flag is  2> gsutil_output.txt
+                         |# Record the exit code of the gsutil command without project flag
+                         |RC_GSUTIL=$?
+                         |if [ "$RC_GSUTIL" != "0" ]; then
+                         |  echo "gsutil command failed"
+                         |  # Print the reason of the failure
+                         |  cat gsutil_output.txt
+                         |  
+                         |  # Check if it matches the BucketIsRequesterPaysErrorMessage
+                         |  grep "Bucket is requester pays bucket but no user project provided." gsutil_output.txt
+                         |  
+                         |  # If it does, grep will return 0
+                         |  IS_RP_FAILURE=$?
+                         |  if [ "$IS_RP_FAILURE" = "0" ]; then
+                         |    echo "Retrying with user project"
+                         |    flag is -u my-project
+                         |  else
+                         |    exit "$RC_GSUTIL"
+                         |  fi
+                         |else
+                         |  exit 0
+                         |fi""".stripMargin
   }
   
   it should "use LocalizationConfiguration to set the number of localization retries" in {
     implicit val localizationConfiguration = LocalizationConfiguration(refineMV(31380))
-    retry("I'm very flaky") shouldBe "retry() { for i in `seq 31380`; do I'm very flaky; RC=$?; if [ \"$RC\" = \"0\" ]; then break; fi; sleep 5; done; return \"$RC\"; }; retry"
+    retry("I'm very flaky") shouldBe """for i in `seq 31380`; do
+                                       |  (
+                                       |    I'm very flaky
+                                       |  )
+                                       |  RC=$?
+                                       |  if [ "$RC" = "0" ]; then
+                                       |    break
+                                       |  fi
+                                       |  sleep 5
+                                       |done
+                                       |exit "$RC"""".stripMargin
   }
 }

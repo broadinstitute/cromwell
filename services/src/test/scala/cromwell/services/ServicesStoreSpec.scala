69521b0b32163e5f27bab4a66cbbeab2dc7a48cf
Khalid Shakir <kshakir@broadinstitute.org>
2018-11-15 14:55:58 -0500

    Added debugging for flaky ServicesStoreSpec. Also: make database init a bit lazier so that tests don't accidentally create the db.

--- services/src/test/scala/cromwell/services/ServicesStoreSpec.scala
+++ services/src/test/scala/cromwell/services/ServicesStoreSpec.scala
@@ -6,6 +6,7 @@ import java.time.OffsetDateTime
 
 import better.files._
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.Logger
 import cromwell.core.Tags._
 import cromwell.core.WorkflowId
 import cromwell.database.migration.liquibase.LiquibaseUtils
@@ -23,12 +24,13 @@ import org.scalactic.StringNormalizations
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FlatSpec, Matchers}
+import org.slf4j.LoggerFactory
 import slick.jdbc.JdbcProfile
 import slick.jdbc.meta._
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 import scala.xml._
 
 class ServicesStoreSpec extends FlatSpec with Matchers with ScalaFutures with StringNormalizations {
@@ -42,6 +44,7 @@ class ServicesStoreSpec extends FlatSpec with Matchers with ScalaFutures with St
   behavior of "ServicesStore"
 
   it should "not deadlock" in {
+    val logger = Logger(LoggerFactory.getLogger(getClass.getName))
     // Test based on https://github.com/kwark/slick-deadlock/blob/82525fc/src/main/scala/SlickDeadlock.scala
     val databaseConfig = ConfigFactory.parseString(
       s"""|db.url = "jdbc:hsqldb:mem:$${uniqueSchema};shutdown=false;hsqldb.tx=mvcc"
@@ -51,11 +54,13 @@ class ServicesStoreSpec extends FlatSpec with Matchers with ScalaFutures with St
           |profile = "slick.jdbc.HsqldbProfile$$"
           |""".stripMargin)
     import ServicesStore.EnhancedSqlDatabase
+    logger.info("Initializing deadlock-test database")
     for {
       database <- new EngineSlickDatabase(databaseConfig)
         .initialized(EngineServicesStore.EngineLiquibaseSettings).autoClosed
     } {
-      val futures = 1 to 20 map { _ =>
+      logger.info(s"Initialized deadlock-test database: ${database.connectionDescription}")
+      val futures = 1 to 20 map { count =>
         val workflowUuid = WorkflowId.randomId().toString
         val callFqn = "call.fqn"
         val jobIndex = 1
@@ -63,14 +68,29 @@ class ServicesStoreSpec extends FlatSpec with Matchers with ScalaFutures with St
         val jobSuccessful = false
         val jobStoreEntry = JobStoreEntry(workflowUuid, callFqn, jobIndex, jobAttempt, jobSuccessful, None, None, None)
         val jobStoreJoins = Seq(JobStoreJoin(jobStoreEntry, Seq()))
+
         // NOTE: This test just needs to repeatedly read/write from a table that acts as a PK for a FK.
-        for {
+        logger.info(s"Creating deadlock-test future #$count with wf id $workflowUuid")
+        val future = for {
+          _ <- Future(logger.info(s"Starting build deadlock-test future #$count"))
           _ <- database.addJobStores(jobStoreJoins, 10)
+          _ = logger.info(s"Added job stores for deadlock-test future #$count")
           queried <- database.queryJobStores(workflowUuid, callFqn, jobIndex, jobAttempt)
+          _ = logger.info(s"Queried job stores for deadlock-test future #$count")
           _ = queried.get.jobStoreEntry.workflowExecutionUuid should be(workflowUuid)
+          _ = logger.info(s"Successful check of deadlock-test future #$count")
         } yield ()
+        logger.info(s"Created deadlock-test future #$count")
+        future
       }
-      Future.sequence(futures).futureValue(Timeout(scaled(30.seconds)))
+
+      logger.info("Futures created for deadlock-test. Waiting for sequence to complete…")
+      Try(Future.sequence(futures).futureValue(Timeout(scaled(30.seconds)))) match {
+        case Success(_) => logger.info("Passed deadlock-test")
+        case Failure(throwable) =>
+          logger.error("Failed deadlock-test")
+          throw throwable
+      }
     }
   }
 

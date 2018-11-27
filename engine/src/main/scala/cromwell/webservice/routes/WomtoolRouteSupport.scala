812871f613a85695fdc22483d4e54fa60c582ff5
Adam Nichols <anichols@broadinstitute.org>
2018-11-27 15:45:50 -0500

    Actually return JSON

--- engine/src/main/scala/cromwell/webservice/routes/WomtoolRouteSupport.scala
+++ engine/src/main/scala/cromwell/webservice/routes/WomtoolRouteSupport.scala
@@ -1,8 +1,9 @@
 package cromwell.webservice.routes
 
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import spray.json.{JsObject, JsString}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -14,7 +15,10 @@ trait WomtoolRouteSupport extends {
     path("womtool" / Segment / "describe") { _ =>
       post {
         entity(as[Multipart.FormData]) { _: Multipart.FormData =>
-          complete(Future(ToResponseMarshallable("A")))
+
+          // After much discussion, it was resolved that returning Future(something) is the right way to do
+          // things in Akka HTTP, when no legacy or migration constraints funnel one into using PerRequest
+          complete(Future(JsObject(Map("a" -> JsString("b")))))
         }
       }
     }

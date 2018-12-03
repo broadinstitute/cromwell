c40e1c5f717734b6b317060401c0775c28500832
Adam Nichols <anichols@broadinstitute.org>
2018-12-03 18:11:43 -0500

    Inputs validation works

--- engine/src/main/scala/cromwell/webservice/routes/WomtoolRouteSupport.scala
+++ engine/src/main/scala/cromwell/webservice/routes/WomtoolRouteSupport.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorRef
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
@@ -30,54 +31,58 @@ trait WomtoolRouteSupport extends WebServiceUtils {
         entity(as[Multipart.FormData]) { formData: Multipart.FormData =>
           onComplete(materializeFormData(formData)) {
             case Success(data) =>
-
-              // TODO: move constants to WebServiceUtils, adopt in PartialWorkflowSources
-              val workflowSource = data.get("workflowSource").map(_.utf8String)
-              val workflowUrl = data.get("workflowUrl").map(_.utf8String)
-              val workflowInputs = data.get("workflowInputs").map(_.utf8String)
-              val workflowType = data.get("workflowType").map(_.utf8String)
-              val workflowVersion = data.get("workflowTypeVersion").map(_.utf8String)
-
-              val wsfc = WorkflowSourceFilesCollection(
-                workflowSource,
-                workflowUrl,
-                workflowRoot = None,
-                workflowType,
-                workflowVersion,
-                workflowInputs.getOrElse(""),
-                workflowOptionsJson = "",
-                labelsJson = "",
-                importsFile = None,
-                workflowOnHold = false,
-                warnings = Seq.empty
-              )
-
-              // TODO: would be nice to reuse findWorkflowSource
-              (workflowSource, workflowUrl) match {
-                case (Some(_), Some(_)) =>
-                  new IllegalArgumentException("Must submit exactly one of workflow source, workflow URL; received both").failRequest(StatusCodes.BadRequest)
-                case (None, Some(_)) =>
-                  new Exception("URL submissions not yet supported").failRequest(StatusCodes.NotImplemented)
-                case (Some(source), None) =>
-                  complete {
-                    // !!! The following is no longer true after service registry integration !!!
-                    // !!! Perhaps another discussions is required? !!!
-
-                    // After much discussion, it was resolved that returning Future(something) is the right way to do
-                    // things in Akka HTTP, when no legacy or migration constraints funnel one into using PerRequest
-
-                    serviceRegistryActor.ask(DescribeRequest(source, wsfc)) map {
-                      case result: DescribeResponse =>
-                        result
-                    }
-                  }
-                case (None, None) =>
-                  new IllegalArgumentException("Must submit exactly one of workflow source, workflow URL; received neither").failRequest(StatusCodes.BadRequest)
-              }
-            case Failure(e) => e.failRequest(StatusCodes.InternalServerError)
+              validateAndSubmitRequest(data)
+            case Failure(e) =>
+              e.failRequest(StatusCodes.InternalServerError)
           }
         }
       }
     }
+
+  private def validateAndSubmitRequest(data: MaterializedFormData): Route = {
+    // TODO: move constants to WebServiceUtils, adopt in PartialWorkflowSources
+    val workflowSource = data.get("workflowSource").map(_.utf8String)
+    val workflowUrl = data.get("workflowUrl").map(_.utf8String)
+    val workflowInputs = data.get("workflowInputs").map(_.utf8String)
+    val workflowType = data.get("workflowType").map(_.utf8String)
+    val workflowVersion = data.get("workflowTypeVersion").map(_.utf8String)
+
+    val wsfc = WorkflowSourceFilesCollection(
+      workflowSource,
+      workflowUrl,
+      workflowRoot = None,
+      workflowType,
+      workflowVersion,
+      workflowInputs.getOrElse(""),
+      workflowOptionsJson = "",
+      labelsJson = "",
+      importsFile = None,
+      workflowOnHold = false,
+      warnings = Seq.empty
+    )
+
+    // TODO: would be nice to reuse findWorkflowSource
+    (workflowSource, workflowUrl) match {
+      case (Some(_), Some(_)) =>
+        new IllegalArgumentException("Expected either workflow source or workflow URL, received both.").failRequest(StatusCodes.BadRequest)
+      case (None, Some(_)) =>
+        new Exception("URL submissions are not yet supported.").failRequest(StatusCodes.NotImplemented)
+      case (Some(source), None) =>
+        complete {
+          // !!! The following is no longer true after service registry integration !!!
+          // !!! Perhaps another discussions is required? !!!
+
+          // After much discussion, it was resolved that returning Future(something) is the right way to do
+          // things in Akka HTTP, when no legacy or migration constraints funnel one into using PerRequest
+
+          serviceRegistryActor.ask(DescribeRequest(source, wsfc)) map {
+            case result: DescribeResponse =>
+              result
+          }
+        }
+      case (None, None) =>
+        new IllegalArgumentException("Expected either workflow source or workflow URL, received neither.").failRequest(StatusCodes.BadRequest)
+    }
+  }
 
 }
--- services/src/main/scala/cromwell/services/womtool/impl/WomtoolServiceInCromwellActor.scala
+++ services/src/main/scala/cromwell/services/womtool/impl/WomtoolServiceInCromwellActor.scala
@@ -3,13 +3,17 @@ package cromwell.services.womtool.impl
 import akka.actor.ActorRef
 import cats.data.Validated.{Invalid, Valid}
 import com.typesafe.config.Config
+import common.Checked
 import cromwell.core.WorkflowSourceFilesCollection
-import cromwell.languages.LanguageFactory
+import cromwell.languages.{LanguageFactory, ValidatedWomNamespace}
 import cromwell.languages.util.LanguageFactoryUtil
 import cromwell.services.womtool.WomtoolServiceActor
 import cromwell.services.womtool.WomtoolServiceMessages.{DescribeRequest, DescribeResponse}
 import wom.core.WorkflowSource
+import wom.executable.WomBundle
+import wom.expression.NoIoFunctionSet
 
+// TODO: I think this new actor is screwing up Cromwell's shutdown routine, need to look at this.
 class WomtoolServiceInCromwellActor(serviceConfig: Config, globalConfig: Config, serviceRegistryActor: ActorRef) extends WomtoolServiceActor {
 
   override def receive: Receive = {
@@ -20,14 +24,34 @@ class WomtoolServiceInCromwellActor(serviceConfig: Config, globalConfig: Config,
   def describeWorkflow(workflow: WorkflowSource, workflowSourceFilesCollection: WorkflowSourceFilesCollection): DescribeResponse = {
     LanguageFactoryUtil.chooseFactory(workflow, workflowSourceFilesCollection) match {
       case Valid(factory: LanguageFactory) =>
-        // Why do we pass in the rest of the language factories here? I cannot figure out what we ever use them for.
-        factory.getWomBundle(workflow, "{}", List.empty, List.empty) match {
-          case Right(_) => DescribeResponse(valid = true, List.empty)
-          case Left(errors) => DescribeResponse(valid = false, errors.toList)
-        }
+        describeWorkflowInner(factory, workflow, workflowSourceFilesCollection)
       case Invalid(e) =>
         throw new Exception(e.toList.mkString(", "))
     }
+  }
+
+  private def describeWorkflowInner(factory: LanguageFactory, workflow: WorkflowSource, workflowSourceFilesCollection: WorkflowSourceFilesCollection): DescribeResponse = {
+
+    def createResponse(checked: Checked[_]): DescribeResponse = {
+      checked match {
+        case Right(_) => DescribeResponse(valid = true, List.empty)
+        case Left(errors) => DescribeResponse(valid = false, errors.toList)
+      }
+    }
+
+    // Mirror of womtool.validate.Validate
+    if (workflowSourceFilesCollection.inputsJson.isEmpty) {
+      // Why do we pass in the rest of the language factories here? I cannot figure out what we ever use them for.
+      createResponse(factory.getWomBundle(workflow, "{}", List.empty, List.empty))
+    } else {
+      factory.getWomBundle(workflow, "{}", List.empty, List.empty) map { bundle: WomBundle =>
+        val maybeExecutable: Checked[ValidatedWomNamespace] =
+          factory.createExecutable(bundle, workflowSourceFilesCollection.inputsJson, NoIoFunctionSet)
+
+        createResponse(maybeExecutable)
+      }
+    }.right.get // TODO: non-stupid error handling
+
   }
 
 }

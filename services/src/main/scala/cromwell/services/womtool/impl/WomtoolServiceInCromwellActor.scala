d4f17d6fa6ba9d6c5ee00ef5f878f083c920c898
Adam Nichols <anichols@broadinstitute.org>
2018-12-12 14:34:25 -0500

    Simplistic async to keep Actor responsive to messages

--- services/src/main/scala/cromwell/services/womtool/impl/WomtoolServiceInCromwellActor.scala
+++ services/src/main/scala/cromwell/services/womtool/impl/WomtoolServiceInCromwellActor.scala
@@ -1,8 +1,10 @@
 package cromwell.services.womtool.impl
 
 import akka.actor.{ActorRef, Props}
+import akka.pattern.pipe
 import cats.data.Validated.{Invalid, Valid}
 import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
 import cromwell.core.Dispatcher.ServiceDispatcher
 import cromwell.core.WorkflowSourceFilesCollection
 import cromwell.languages.util.ImportResolver.HttpResolver
@@ -14,11 +16,21 @@ import cromwell.util.GracefulShutdownHelper.ShutdownCommand
 import wom.core.WorkflowSource
 import wom.expression.NoIoFunctionSet
 
-class WomtoolServiceInCromwellActor(serviceConfig: Config, globalConfig: Config, serviceRegistryActor: ActorRef) extends WomtoolServiceActor {
+import scala.concurrent.{ExecutionContext, Future}
+
+class WomtoolServiceInCromwellActor(serviceConfig: Config, globalConfig: Config, serviceRegistryActor: ActorRef) extends WomtoolServiceActor with LazyLogging {
+
+  implicit val ec: ExecutionContext = context.dispatcher
 
   override def receive: Receive = {
     case DescribeRequest(filesCollection) =>
-      sender ! describeWorkflow(filesCollection)
+
+      // We are consciously wrapping a Future around the Await.result way down in the HTTP import resolver until we can update the whole call hierarchy to async
+      // https://doc.akka.io/docs/akka/2.5.16/actors.html?language=scala#ask-send-and-receive-future
+      Future {
+        describeWorkflow(filesCollection)
+      } pipeTo sender()
+      ()
     case ShutdownCommand =>
       // This service doesn't do any work on shutdown but the service registry pattern requires it (see #2575)
       context.stop(self)

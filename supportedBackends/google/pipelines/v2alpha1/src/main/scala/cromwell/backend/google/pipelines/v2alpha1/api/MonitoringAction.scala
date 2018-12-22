857c8b888404bbf482c61308b9a5942ca12c883f
Denis Loginov <denis@broadinstitute.org>
2018-12-22 17:39:03 -0500

    Pass task call index and attempt to MonitoringAction

--- supportedBackends/google/pipelines/v2alpha1/src/main/scala/cromwell/backend/google/pipelines/v2alpha1/api/MonitoringAction.scala
+++ supportedBackends/google/pipelines/v2alpha1/src/main/scala/cromwell/backend/google/pipelines/v2alpha1/api/MonitoringAction.scala
@@ -10,6 +10,8 @@ trait MonitoringAction {
       */
     val WorkflowId = "WORKFLOW_ID"
     val TaskCallName = "TASK_CALL_NAME"
+    val TaskCallIndex = "TASK_CALL_INDEX"
+    val TaskCallAttempt = "TASK_CALL_ATTEMPT"
     val DiskMounts = "DISK_MOUNTS"
   }
 
@@ -19,6 +21,8 @@ trait MonitoringAction {
     val environment = Map(
       Env.WorkflowId -> job.workflowDescriptor.id.toString,
       Env.TaskCallName -> job.taskCall.localName,
+      Env.TaskCallIndex -> (job.key.index map { _.toString } getOrElse "NA"),
+      Env.TaskCallAttempt -> job.key.attempt.toString,
       Env.DiskMounts -> mounts.map(_.getPath).mkString(" "),
     )
 

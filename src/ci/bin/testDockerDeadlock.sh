422d8b8b2f985015b7439c5002de562967eb08c6
Kristian Cibulskis <kcibul@broadinstitute.org>
2018-12-29 07:08:11 -0500

    Things Changed - added a cromwell-master, which refreshes status metadata.  There can be only one. - created a pool of cromwell_norefresh, which has status refresh turned off, these can scale - found race condition when multiple cromwells try to create the liquibase lock table at once, configured to have master go first - updated scripts/compose to handle the above two kinds of cromwell - increased to 100 batches of 10 workflow each - changed timeout script to show number of completed workflows and break when done - delete database at start of run, so the above works - ran heartbeats in auto-commit mode rather than in a single transaction - dump out logs at end of run for debugging
    Things I'd like to share
     - Lock Ordering in SELECT...FOR UPDATE no es bueno, there are great feature in MYSQL v8 (SKIP LOCKED) but we can't use those yet
     - how to configure mysql for query logging, and what it shows
     - heartbeat batches were never a batched update, just a big transaction
     - slick terminology can give give the wrong intuition
     - impact of cleaning db before each run
     - No deadlocks!

--- src/ci/bin/testDockerDeadlock.sh
+++ src/ci/bin/testDockerDeadlock.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#clean up local mysql database
+rm -rf scripts/docker-compose-mysql/compose/mysql/data
+
 set -o errexit -o nounset -o pipefail
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
@@ -28,7 +31,7 @@ CROMWELL_TAG="${TEST_CROMWELL_TAG}" \
 docker-compose \
   -f scripts/docker-compose-mysql/docker-compose-deadlock.yml \
   up \
-  --scale cromwell=3 \
+  --scale cromwell_norefresh=2 \
   --exit-code-from deadlocker
 
 exit_code=$?
@@ -36,8 +39,12 @@ set -o errexit
 
 echo "docker-compose exit code was ${exit_code}"
 
-# Tear everything down
+# Tear everything down, but dump out the logs first
 CROMWELL_TAG="${TEST_CROMWELL_TAG}" \
+docker-compose \
+  -f scripts/docker-compose-mysql/docker-compose-deadlock.yml \
+  logs --no-color > deadlock.logs \
+
 docker-compose \
   -f scripts/docker-compose-mysql/docker-compose-deadlock.yml \
   down \

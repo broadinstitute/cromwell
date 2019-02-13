4b04cdfc8ca18ea902ddc062c2d23ddaf90828f3
Adam Nichols <anichols@broadinstitute.org>
2019-02-13 11:30:43 -0500

    @JsonCodec

--- services/src/main/scala/cromwell/services/womtool/models/InputDescription.scala
+++ services/src/main/scala/cromwell/services/womtool/models/InputDescription.scala
@@ -1,14 +1,10 @@
 package cromwell.services.womtool.models
 
-import io.circe.Encoder
-import io.circe.generic.semiauto._
-import wom.types.WomType
 import cromwell.services.womtool.models.WomTypeJsonSupport.womTypeEncoder // IntelliJ is lying
 import cromwell.services.womtool.models.WomExpressionJsonSupport.womExpressionEncoder
+import io.circe.generic.JsonCodec
 import wom.expression.WomExpression
+import wom.types.WomType
 
+@JsonCodec(encodeOnly=true)
 case class InputDescription(name: String, valueType: WomType, typeDisplayName: String, optional: Boolean, default: Option[WomExpression])
-
-object InputDescription {
-  implicit val inputDescriptionEncoder: Encoder[InputDescription] = deriveEncoder[InputDescription]
-}
--- services/src/main/scala/cromwell/services/womtool/models/OutputDescription.scala
+++ services/src/main/scala/cromwell/services/womtool/models/OutputDescription.scala
@@ -1,12 +1,8 @@
 package cromwell.services.womtool.models
 
-import io.circe.Encoder
-import io.circe.generic.semiauto.deriveEncoder
-import wom.types.WomType
 import cromwell.services.womtool.models.WomTypeJsonSupport.womTypeEncoder // IntelliJ is lying
+import io.circe.generic.JsonCodec
+import wom.types.WomType
 
+@JsonCodec(encodeOnly=true)
 case class OutputDescription(name: String, valueType: WomType, typeDisplayName: String)
-
-object OutputDescription {
-  implicit val outputDescriptionEncoder: Encoder[OutputDescription] = deriveEncoder[OutputDescription]
-}

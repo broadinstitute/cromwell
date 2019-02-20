2726b697e5ceec9ff9e4d129cd2972e48dca62a4
Miguel Covarrubias <mcovarr@broadinstitute.org>
2019-02-20 14:44:28 -0500

    Engine upgrade script now uses the same major version for the previous version as the current version if on a hotfix branch.

--- src/ci/bin/testCentaurEngineUpgradeLocal.sh
+++ src/ci/bin/testCentaurEngineUpgradeLocal.sh
@@ -6,11 +6,15 @@ export CROMWELL_BUILD_OPTIONAL_SECURE=true
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
 
-cromwell::build::setup_common_environment
+if [ "${CROMWELL_BUILD_PROVIDER}" = "${CROMWELL_BUILD_PROVIDER_TRAVIS}" ] && [ -n "${TRAVIS_PULL_REQUEST_BRANCH}" ]; then
 
-cromwell::build::setup_centaur_environment
+  cromwell::build::setup_common_environment
 
-cromwell::build::assemble_jars
+  cromwell::build::setup_centaur_environment
 
-cromwell::build::run_centaur \
-    -s "centaur.EngineUpgradeTestCaseSpec"
+  cromwell::build::assemble_jars
+
+  cromwell::build::run_centaur \
+      -s "centaur.EngineUpgradeTestCaseSpec"
+
+fi

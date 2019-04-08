ded980c6e20b30a1ba8c31572078effe5552efb8
Miguel Covarrubias <mcovarr@broadinstitute.org>
2019-04-08 11:54:09 -0400

    not even a start really

---
+++ src/ci/bin/testCentaurKubernetesLocal.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+export CROMWELL_BUILD_REQUIRES_SECURE=true
+# import in shellcheck / CI / IntelliJ compatible ways
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
+
+# Setting these variables should cause the associated config values to be rendered into centaur_application_horicromtal.conf
+# There should probably be more indirections in CI scripts but that can wait.
+export TEST_CROMWELL_TAG=just-testing-horicromtal
+export TEST_CROMWELL_CONF=horicromtal_application.conf
+export CROMWELL_BUILD_MYSQL_USERNAME=travis
+
+cromwell::build::setup_common_environment
+
+cromwell::build::setup_centaur_environment
+
+cromwell::build::assemble_jars
+
+GOOGLE_AUTH_MODE="service-account"
+GOOGLE_REFRESH_TOKEN_PATH="${CROMWELL_BUILD_RESOURCES_DIRECTORY}/papi_refresh_token.txt"
+
+# Export variables used in conf files
+export GOOGLE_AUTH_MODE
+export GOOGLE_REFRESH_TOKEN_PATH
+export TEST_CROMWELL_COMPOSE_FILE="${CROMWELL_BUILD_ROOT_DIRECTORY}/scripts/docker-compose-mysql/docker-compose-horicromtal.yml"
+
+# Copy rendered files
+mkdir -p "${CROMWELL_BUILD_CENTAUR_TEST_RENDERED}"
+cp \
+    "${CROMWELL_BUILD_RESOURCES_DIRECTORY}/private_docker_papi_v2_usa.options" \
+    "${TEST_CROMWELL_COMPOSE_FILE}" \
+    "${CROMWELL_BUILD_CENTAUR_TEST_RENDERED}"
+
+docker image ls -q broadinstitute/cromwell:"${TEST_CROMWELL_TAG}" | grep . || \
+CROMWELL_SBT_DOCKER_TAGS="${TEST_CROMWELL_TAG}" sbt server/docker
+
+cromwell::build::run_centaur \
+    -p 100 \
+    -e localdockertest \
+    "${CROMWELL_BUILD_CENTAUR_TEST_ADDITIONAL_PARAMETERS:-""}" \
+    -d "${CROMWELL_BUILD_CENTAUR_TEST_DIRECTORY}"
+
+cromwell::build::generate_code_coverage

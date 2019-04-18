3fd5a056f4a40fc49d04786af3e1d83f8693ab79
Michael Milton <ttmigueltt@gmail.com>
2019-04-18 14:47:27 -0400

    More elegant temp file name

--- filesystems/s3/src/main/java/org/lerch/s3fs/S3Channel.java
+++ filesystems/s3/src/main/java/org/lerch/s3fs/S3Channel.java
@@ -6,6 +6,6 @@ import java.nio.file.Path;
 
 public interface S3Channel {
     default Path createTempFile(S3Path path) throws IOException {
-        return Files.createTempFile("", "");
+        return Files.createTempFile(path.getFileName().toString(), "");
     }
 }

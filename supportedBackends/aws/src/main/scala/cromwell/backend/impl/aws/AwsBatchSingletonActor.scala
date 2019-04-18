3600182226bbb1703bc1ecae552b63c90c223e81
Chris Llanwarne <cjllanwarne@gmail.com>
2019-04-18 17:02:54 -0400

    Go slow actor for job submits

--- supportedBackends/aws/src/main/scala/cromwell/backend/impl/aws/AwsBatchSingletonActor.scala
+++ supportedBackends/aws/src/main/scala/cromwell/backend/impl/aws/AwsBatchSingletonActor.scala
@@ -1,17 +1,21 @@
 package cromwell.backend.impl.aws
 
 import akka.actor.{Actor, ActorLogging, Props}
+import cromwell.backend.impl.aws.GoSlowJobSubmitActor.GoSlowSubmitActorMessage
 import cromwell.backend.impl.aws.OccasionalStatusPollingActor.OccasionalStatusPollingActorMessage
 import cromwell.core.Mailbox
 import software.amazon.awssdk.regions.Region
 
 class AwsBatchSingletonActor(configRegion: Option[Region]) extends Actor with ActorLogging {
   val awsOccasionalStatusPoller = context.actorOf(OccasionalStatusPollingActor.props(configRegion).withMailbox(Mailbox.PriorityMailbox), "AWSOccasionalStatusPoller")
+  val awsGoSlowSubmitActor = context.actorOf(GoSlowJobSubmitActor.props(configRegion).withMailbox(Mailbox.PriorityMailbox), "AWSGoSlowSubmitter")
 
   override def receive = {
 
     case statusQuery: OccasionalStatusPollingActorMessage =>
       awsOccasionalStatusPoller.forward(statusQuery)
+    case goSlowRequest: GoSlowSubmitActorMessage =>
+      awsGoSlowSubmitActor.forward(goSlowRequest)
     case other =>
       log.error(s"Unknown message to AwsBatchSingletonActor: ${other.getClass.getSimpleName}. Dropping it.")
   }

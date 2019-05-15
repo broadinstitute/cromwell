074d55cd778df7038622d268497f8806d11f239f
Miguel Covarrubias <mcovarr@broadinstitute.org>
2019-05-15 17:19:40 -0400

    File evaluation fixes.

--- wdl/transforms/new-base/src/main/scala/wdl/transforms/base/linking/expression/files/EngineFunctionEvaluators.scala
+++ wdl/transforms/new-base/src/main/scala/wdl/transforms/base/linking/expression/files/EngineFunctionEvaluators.scala
@@ -17,16 +17,22 @@ import wdl.transforms.base.wdlom2wdl.WdlWriterImpl.expressionElementWriter
 
 object EngineFunctionEvaluators {
 
-  private def evaluateToFile(forFunction: String, a: ExpressionElement, inputs: Map[String, WomValue], ioFunctionSet: IoFunctionSet)
+  private def evaluateToFile(forFunction: String, outermostElement: ExpressionElement, inputs: Map[String, WomValue], ioFunctionSet: IoFunctionSet, nestedElement: Option[ExpressionElement] = None)
                             (implicit expressionValueEvaluator: ValueEvaluator[ExpressionElement]): ErrorOr[Set[WomFile]] = {
-    (a match {
-      case _: IdentifierLookup | _: IdentifierMemberAccess | _: IndexAccess => Set.empty[WomFile].validNel
+    // `IndexAccess` `ExpressionElement`s require recursion to look for contained `IdentifierLookup` or `IdentifierMemberAccess`.
+    // After all `IndexAccess` elements have been traversed the original `outermostElement` should be evaluated for files.
+    val elementToExamine = nestedElement getOrElse outermostElement
+    (elementToExamine match {
+      case IndexAccess(expressionElement, _) => evaluateToFile(forFunction, outermostElement, inputs, ioFunctionSet, Option(expressionElement))
+      // If the specified identifier is not among the inputs there are no files to delocalize from this expression.
+      case IdentifierLookup(identifier) if !inputs.contains(identifier) => Set.empty[WomFile].validNel
+      case IdentifierMemberAccess(first, _, _) if !inputs.contains(first) => Set.empty[WomFile].validNel
       case _ =>
-        a.evaluateValue(inputs, ioFunctionSet, None) flatMap {
+        outermostElement.evaluateValue(inputs, ioFunctionSet, None) flatMap {
           case EvaluatedValue(p: WomPrimitive, _) => Set[WomFile](WomSingleFile(p.valueString)).validNel
           case other => s"Expected a primitive but got ${other.getClass.getSimpleName}".invalidNel
         }
-    }).contextualizeErrors(s"predict files needed to de-localize from '${a.toWdlV1}' for $forFunction")
+    }).contextualizeErrors(s"predict files needed to de-localize from '${outermostElement.toWdlV1}' for $forFunction")
   }
 
   def singleParameterPassthroughFileEvaluator[A <: OneParamFunctionCallElement]: FileEvaluator[A] = new FileEvaluator[A] {

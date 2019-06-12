305e6640709122d7f36151f2da4c4db75a2f744e
Khalid Shakir <kshakir@broadinstitute.org>
2019-06-12 09:25:03 -0400

    Running dockers for postgres and mariadb during tests - Switched from running Travis' mysql/postgresql instances to dockerized versions - Added a couple of centaur and some unit tests for mariadb, using mysql liquibase/drivers for now - NOTE: No postgresql tests enabled in this commit - Changed mysql CI login from travis: to cromwell:test - DRYed out more of the CI scripts into test.inc.sh - Switched CI broadinstitute/dsde-toolbox from :latest to :dev - Don't fail with false negatives when MetadataDatabaseAccessSpec is re-run against persistent RDBMS

--- src/ci/bin/testCentaurPapiUpgradePapiV1.sh
+++ src/ci/bin/testCentaurPapiUpgradePapiV1.sh
@@ -2,20 +2,17 @@
 
 set -o errexit -o nounset -o pipefail
 export CROMWELL_BUILD_REQUIRES_SECURE=true
+export CROMWELL_BUILD_REQUIRES_PULL_REQUEST=true
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
 
-if [ "${CROMWELL_BUILD_PROVIDER}" = "${CROMWELL_BUILD_PROVIDER_TRAVIS}" ] && [ -n "${TRAVIS_PULL_REQUEST_BRANCH}" ]; then
+cromwell::build::setup_common_environment
 
-  cromwell::build::setup_common_environment
+cromwell::build::setup_centaur_environment
 
-  cromwell::build::setup_centaur_environment
+cromwell::build::assemble_jars
 
-  cromwell::build::assemble_jars
-
-  cromwell::build::run_centaur \
-      -s "centaur.PapiUpgradeTestCaseSpec" \
-      -e localdockertest \
-
-fi
+cromwell::build::run_centaur \
+  -s "centaur.PapiUpgradeTestCaseSpec" \
+  -e localdockertest \

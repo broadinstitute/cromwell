05c18c468719c8c76441f4235860037ffcfc5ac9
Chris Llanwarne <cjllanwarne@gmail.com>
2019-06-24 15:09:44 -0400

    [Pausing as WIP]

---
+++ scripts/perf/run_centaur_blocking/run_on_jenkins.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+source scripts/perf/helper.inc.sh
+
+if [ -n "${TEST_CASE_DIRECTORY}" ]
+then
+    # Must be a directory
+    TEST_WORKFLOW_DIR=${TEST_WORKFLOW_ROOT}/${TEST_CASE_DIRECTORY}
+
+    # If it contains a custom cromwell configuration, use that
+    if [ -f "${TEST_WORKFLOW_DIR}/cromwell.conf" ]
+    then
+        addVar CROMWELL_CONF_DIR=${TEST_WORKFLOW_DIR}re
+        # copy the default one next to it so we can include it
+        cp ${PERF_ROOT}/vm_scripts/cromwell/cromwell.conf ${CROMWELL_CONF_DIR}/cromwell_default.conf
+    else
+        # Otherwise use the default one
+        addVar CROMWELL_CONF_DIR=${PERF_ROOT}/vm_scripts/cromwell
+    fi
+
+    # If it contains a custom centaur configuration, use that
+    if [ -f "${TEST_WORKFLOW_DIR}/centaur.conf" ]
+    then
+        addVar CENTAUR_CONF_DIR=${TEST_WORKFLOW_DIR}
+        # copy the default one next to it so we can include it
+        cp ${PERF_ROOT}/vm_scripts/centaur/centaur.conf ${CENTAUR_CONF_DIR}/centaur_default.conf
+    else
+        # Otherwise use the default one
+        addVar CENTAUR_CONF_DIR=${PERF_ROOT}/vm_scripts/centaur
+    fi
+
+    addVar CENTAUR_TEST_FILE=$(ls ${TEST_WORKFLOW_DIR}/*.test | head)
+    sed -i "s/\$BRANCH/${CROMWELL_BRANCH}/" ${CENTAUR_TEST_FILE}
+fi
+
+if [ -n "${CENTAUR_TEST_FILE}" ]
+then
+    wait_for_cromwell
+    run_test
+else
+    echo "No workflow provided, shutting down"
+fi

ded0ae9bd376eb37b5262ccc88d428df40f60790
Chris Llanwarne <cjllanwarne@gmail.com>
2019-07-02 14:32:34 -0400

    Use tag 'cromwell-perf'

--- scripts/perf/run_centaur_blocking/create_runner_instance.sh
+++ scripts/perf/run_centaur_blocking/create_runner_instance.sh
@@ -24,7 +24,7 @@ metadata=(
   "TEST_CASE_DIRECTORY=${TEST_CASE_DIRECTORY}"
 )
 
-# NB: The 'cromwell-perf-centaur-runner' tag is required to allow us through cromwell's firewall
+# NB: The 'cromwell-perf' tag is required to allow us through cromwell's firewall
 
 gcloud_run_as_service_account "perf_centaur_test_runner_${BUILD_NUMBER}" \
   "gcloud \
@@ -35,7 +35,7 @@ gcloud_run_as_service_account "perf_centaur_test_runner_${BUILD_NUMBER}" \
     create ${BUILD_TAG} \
     --zone us-central1-c \
     --source-instance-template ${INSTANCE_TEMPLATE} \
-    --tags cromwell-perf-centaur-runner \
+    --tags cromwell-perf \
     --metadata-from-file startup-script=${DOCKER_ETC_PATH}/test_runner_script.sh \
     --metadata \
         $(join ${metadata[@]})"  | tee startupResult.txt

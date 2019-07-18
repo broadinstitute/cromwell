8fcfc6c7912dbd3e608ef73b591b31a8f41b8ff0
Chris Llanwarne <cjllanwarne@gmail.com>
2019-07-18 05:50:41 -0400

    Little bit dryer

--- scripts/perf/delete_horicromtal/delete_horicromtal.sh
+++ scripts/perf/delete_horicromtal/delete_horicromtal.sh
@@ -8,14 +8,9 @@ mkdir -p mnt
 # Read the service account credentials from vault:
 read_service_account_from_vault
 
-gcloud_delete_instance \
-  "perf_delete_gcloud_instance_${BUILD_NUMBER}-runner101" \
-  "${CROMWELL_INSTANCE_NAME}-runner101"
-
-gcloud_delete_instance \
-  "perf_delete_gcloud_instance_${BUILD_NUMBER}-runner102" \
-  "${CROMWELL_INSTANCE_NAME}-runner102"
-
-gcloud_delete_instance \
-  "perf_delete_gcloud_instance_${BUILD_NUMBER}-reader101" \
-  "${CROMWELL_INSTANCE_NAME}-reader101"
+for instance in runner-101 runner102 reader101
+do
+    gcloud_delete_instance \
+        "perf_delete_gcloud_instance_${BUILD_NUMBER}-${instance}" \
+        "${CROMWELL_INSTANCE_NAME}-${instance}"
+done

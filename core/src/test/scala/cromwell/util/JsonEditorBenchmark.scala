7aabb0fdf21b431e8a95a27cd08fc5e836d70bb8
Ivan Chang <ichang@broadinstitute.org>
2019-08-01 14:12:37 -0400

    Added include keys benchmark test

--- core/src/test/scala/cromwell/util/JsonEditorBenchmark.scala
+++ core/src/test/scala/cromwell/util/JsonEditorBenchmark.scala
@@ -15,6 +15,7 @@ object JsonEditorBenchmark extends Bench[Double] {
   val jsonPool = Map(14 -> "bad.json", 32 -> "bec.json")
   val jsonTestParse = Gen.enumeration("Parse: json size MB")(14, 32)
   val jsonTestExcludeKeys = Gen.enumeration("Exclude keys traversal: json size MB")(14, 32)
+  val jsonTestIncludeKeys = Gen.enumeration("Include keys traversal: json size MB")(14, 32)
   val jsonTestAugmentLabels = Gen.enumeration("Augment labels traversal: json size MB")(14, 32)
   val excludeKeys = Map(14 -> "mt_", 32 -> "status")
 
@@ -31,6 +32,8 @@ object JsonEditorBenchmark extends Bench[Double] {
   lazy val reporter = new LoggingReporter[Double]
   lazy val persistor = Persistor.None
 
+  // Increase or decrease exec.benchRuns to repeat the same test numerous times.
+  // The avaerage measured clock time will be the output.
   performance of "JsonEditor with circe" in {
     // Measures how fast the JsonEditor can exclude keys using circe.
     measure method "parse" in {
@@ -53,6 +56,15 @@ object JsonEditorBenchmark extends Bench[Double] {
           tree <- jsonTree(sz) // already parsed
           newTree = includeExcludeJson(tree, None, Some(NonEmptyList.of(excludeKeys(sz)))) // traversal
         } yield newTree
+      }
+    }
+
+    measure method "includeJson(_, NonEmptyList.one(\"message\"))" in {
+      using(jsonTestIncludeKeys) config(
+        exec.maxWarmupRuns -> 1,
+        exec.benchRuns -> 1
+      ) in { sz =>
+        jsonTree(sz).map(includeJson(_, NonEmptyList.one("message"))).right.get // traversal
       }
     }
 

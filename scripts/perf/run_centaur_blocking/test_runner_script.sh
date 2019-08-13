6986a9e61e5f4964560b47ff9c6539fa5a40bd62
Chris Llanwarne <cjllanwarne@users.noreply.github.com>
2019-08-13 15:16:01 -0400

    Refactor perf tests and support horicromtal [BA-4842] (#5077)

--- scripts/perf/run_centaur_blocking/test_runner_script.sh
+++ scripts/perf/run_centaur_blocking/test_runner_script.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-set -x
+# Deliberately don't set these options. The script must continue to run (and clean itself up) even if the centaur tests
+# fail.
+#set -euo pipefail
+set +x
 
 ### /!\ This script assumes docker and docker compose are already installed on the host
 
@@ -19,32 +22,28 @@ addVar() {
 # Make sure ip forwarding is enabled by default so that docker doesn't loses connectivity
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
 
-# Set up env variables
-addVar CROMWELL_BRANCH=$(extract_metadata CROMWELL_PERF_SCRIPTS_BRANCH)
+# Extract env variables from instance metadata:
 addVar TEST_CASE_DIRECTORY=$(extract_metadata TEST_CASE_DIRECTORY)
-addVar CLOUD_SQL_DB_USER=$(extract_metadata CROMWELL_DB_USER)
-addVar CLOUD_SQL_DB_PASSWORD=$(extract_metadata CROMWELL_DB_PASS)
-addVar CLOUD_SQL_INSTANCE=$(extract_metadata CLOUD_SQL_INSTANCE)
-addVar CROMWELL_DOCKER_IMAGE=$(extract_metadata CROMWELL_DOCKER_IMAGE)
+addVar CROMWELL_UNDER_TEST=$(extract_metadata CROMWELL_UNDER_TEST)
 addVar CROMWELL_PROJECT=$(extract_metadata CROMWELL_PROJECT)
-addVar CROMWELL_EXECUTION_ROOT=$(extract_metadata CROMWELL_BUCKET)
 addVar GCS_REPORT_BUCKET=$(extract_metadata GCS_REPORT_BUCKET)
 addVar GCS_REPORT_PATH=$(extract_metadata GCS_REPORT_PATH)
 addVar BUILD_ID=$(extract_metadata BUILD_TAG)
-addVar CLEAN_UP=$(extract_metadata CLEAN_UP)
-
-addVar REPORT_BUCKET=cromwell-perf-test-reporting
+addVar CROMWELL_PERF_SCRIPTS_BRANCH=$(extract_metadata CROMWELL_PERF_SCRIPTS_BRANCH)
 
 addVar CROMWELL_ROOT=/app
 addVar PERF_ROOT=${CROMWELL_ROOT}/scripts/perf
-addVar TEST_WORKFLOW_ROOT=${PERF_ROOT}/test_cases
-
-addVar CROMWELL_UNDER_TEST="localhost"
 
 # Clone cromwell to get the perf scripts. Use https to avoid ssh fingerprint prompt when the script runs
-git clone -b ${CROMWELL_BRANCH} --depth 1 --single-branch https://github.com/broadinstitute/cromwell.git ${CROMWELL_ROOT}
+git clone -b ${CROMWELL_PERF_SCRIPTS_BRANCH} --depth 1 --single-branch https://github.com/broadinstitute/cromwell.git ${CROMWELL_ROOT}
 
 source ${PERF_ROOT}/helper.inc.sh
+
+addVar CLEAN_UP=true
+
+addVar REPORT_BUCKET="cromwell-perf-test-reporting"
+addVar TEST_WORKFLOW_ROOT="${PERF_ROOT}/test_cases"
+addVar REPORT_URL="$(strip_trailing_slash "gs://${GCS_REPORT_BUCKET}/${GCS_REPORT_PATH}")"
 
 if [ -n "${TEST_CASE_DIRECTORY}" ]
 then
@@ -74,13 +73,10 @@ then
     fi
     
     addVar CENTAUR_TEST_FILE=$(ls ${TEST_WORKFLOW_DIR}/*.test | head)
-    sed -i "s/\$BRANCH/${CROMWELL_BRANCH}/" ${CENTAUR_TEST_FILE}
+    sed -i "s/\$BRANCH/${CROMWELL_PERF_SCRIPTS_BRANCH}/" ${CENTAUR_TEST_FILE}
 fi
 
-# Start cromwell and cloud sql proxy
-docker-compose -f ${PERF_ROOT}/vm_scripts/docker-compose.yml up -d
-
-if [ -n "$CENTAUR_TEST_FILE" ]
+if [ -n "${CENTAUR_TEST_FILE}" ]
 then
     wait_for_cromwell
     run_test
@@ -88,4 +84,5 @@ else
     echo "No workflow provided, shutting down"
 fi
 
-shutdown
+export_centaur_logs
+self_destruct_instance

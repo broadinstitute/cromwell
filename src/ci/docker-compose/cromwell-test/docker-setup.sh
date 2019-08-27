3e92adcc71c5798ddc63c9bf0de2b126475f1b60
Khalid Shakir <kshakir@broadinstitute.org>
2019-08-27 13:38:47 -0400

    Test temp DOS/DRS images in GCR on Papi

--- src/ci/docker-compose/cromwell-test/docker-setup.sh
+++ src/ci/docker-compose/cromwell-test/docker-setup.sh
@@ -13,21 +13,12 @@ apt-get install -y \
     curl \
     gnupg \
     gnupg2 \
+    jq \
     mysql-client \
     postgresql-client \
     python-dev \
     software-properties-common \
     sudo \
-
-# install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-
-cat <<CONF >/etc/init/docker-chown.conf
-start on startup
-task
-exec chown root:docker /var/run/docker.sock
-CONF
 
 # install sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
@@ -40,8 +31,15 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install --upgrade --force-reinstall pyopenssl
 
+# install gcloud
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
+    | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+    | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt-get update
+apt-get install -y google-cloud-sdk
+
 useradd hoggett
-usermod -aG docker hoggett
 echo "hoggett ALL=NOPASSWD: ALL" >> /etc/sudoers
 mkdir -p /home/hoggett
 chown hoggett:hoggett /home/hoggett

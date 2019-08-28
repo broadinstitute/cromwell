b9bd23a748f52ccc3530389fea50e8f47c410595
Adam Nichols <aednichols@gmail.com>
2019-08-28 12:06:55 -0400

    Initial commit

---
+++ src/ci/bin/testSingleWorkflowRunner.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+# import in shellcheck / CI / IntelliJ compatible ways
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
+
+cromwell::build::setup_common_environment
+
+cromwell::build::setup_conformance_environment
+
+cromwell::build::assemble_jars
+
+echo "Hello world!"

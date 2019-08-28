f8cde60d4837d20ee3e0eb7b3cfb7114db3b98ad
Adam Nichols <aednichols@gmail.com>
2019-08-28 14:17:20 -0400

    Test [force ci]

--- src/ci/bin/testSingleWorkflowRunner.sh
+++ src/ci/bin/testSingleWorkflowRunner.sh
@@ -9,11 +9,8 @@ cromwell::build::setup_common_environment
 
 cromwell::build::assemble_jars
 
-cat /proc/cpuinfo
-
-java -version
-
 java -jar $CROMWELL_BUILD_CROMWELL_JAR run ./centaur/src/main/resources/standardTestCases/hello/hello.wdl --inputs ./centaur/src/main/resources/standardTestCases/hello/hello.inputs > console_output.txt
 
-grep -q "terminal state: WorkflowSucceededState" console_output.txt
-grep -q "\"wf_hello.hello.salutation\": \"Hello m'Lord!\"" console_output.txt
+grep "terminal state: WorkflowSucceededState" console_output.txt
+grep "\"wf_hello.hello.salutation\": \"Hello m'Lord!\"" console_output.txt
+grep "does not exist" console_output.txt

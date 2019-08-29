95e43119e6fd508653c9099718c4333287945cb6
Chris Llanwarne <cjllanwarne@users.noreply.github.com>
2019-08-29 13:45:15 -0400

    Refactor WDL parsing in draft-2 and re-use in WDL 1.0, 2.0 [BA-5664] (#5134)

--- languageFactories/wdl-draft2/src/main/scala/languages/wdl/draft2/WdlDraft2LanguageFactory.scala
+++ languageFactories/wdl-draft2/src/main/scala/languages/wdl/draft2/WdlDraft2LanguageFactory.scala
@@ -1,6 +1,5 @@
 package languages.wdl.draft2
 
-import java.security.MessageDigest
 import java.util.concurrent.Callable
 
 import cats.data.EitherT.fromEither
@@ -9,8 +8,8 @@ import cats.instances.either._
 import cats.instances.list._
 import cats.syntax.functor._
 import cats.syntax.traverse._
-import com.google.common.cache.{Cache, CacheBuilder}
 import com.typesafe.config.Config
+import com.typesafe.scalalogging.StrictLogging
 import common.Checked
 import common.validation.Checked._
 import common.validation.ErrorOr._
@@ -18,13 +17,14 @@ import common.validation.IOChecked.IOChecked
 import common.validation.Validation._
 import cromwell.core._
 import cromwell.languages.util.ImportResolver.{ImportResolutionRequest, ImportResolver}
-import cromwell.languages.util.{ImportResolver, LanguageFactoryUtil}
+import cromwell.languages.util.ParserCache.ParserCacheInputs
+import cromwell.languages.util.{ImportResolver, LanguageFactoryUtil, ParserCache}
 import cromwell.languages.{LanguageFactory, ValidatedWomNamespace}
 import languages.wdl.draft2.WdlDraft2LanguageFactory._
 import mouse.all._
 import net.ceedubs.ficus.Ficus._
 import wdl.draft2.Draft2ResolvedImportBundle
-import wdl.draft2.model.{Draft2ImportResolver, WdlNamespace, WdlNamespaceWithWorkflow}
+import wdl.draft2.model.{Draft2ImportResolver, WdlNamespace, WdlNamespaceWithWorkflow, WdlNamespaceWithoutWorkflow}
 import wdl.shared.transforms.wdlom2wom.WdlSharedInputParsing
 import wdl.transforms.draft2.wdlom2wom.WdlDraft2WomBundleMakers._
 import wdl.transforms.draft2.wdlom2wom.WdlDraft2WomExecutableMakers._
@@ -40,7 +40,7 @@ import wom.values._
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-class WdlDraft2LanguageFactory(override val config: Config) extends LanguageFactory {
+class WdlDraft2LanguageFactory(override val config: Config) extends LanguageFactory with ParserCache[WdlNamespace] with StrictLogging {
 
   override val languageName: String = "WDL"
   override val languageVersionName: String = "draft-2"
@@ -53,28 +53,30 @@ class WdlDraft2LanguageFactory(override val config: Config) extends LanguageFact
                                  ioFunctions: IoFunctionSet,
                                  importResolvers: List[ImportResolver]): IOChecked[ValidatedWomNamespace] = {
 
-    def checkTypes(namespace: WdlNamespaceWithWorkflow, inputs: Map[OutputPort, WomValue]): Checked[Unit] = {
-      val allDeclarations = namespace.workflow.declarations ++ namespace.workflow.calls.flatMap(_.declarations)
-      val list: List[Checked[Unit]] = inputs.map({ case (k, v) =>
-        allDeclarations.find(_.fullyQualifiedName == k) match {
-          case Some(decl) if decl.womType.coerceRawValue(v).isFailure =>
-            s"Invalid right-side type of '$k'.  Expecting ${decl.womType.stableName}, got ${v.womType.stableName}".invalidNelCheck[Unit]
-          case _ => ().validNelCheck
-        }
-      }).toList
+    def checkTypes(namespace: WdlNamespace, inputs: Map[OutputPort, WomValue]): Checked[Unit] = namespace match {
 
-      list.sequence[Checked, Unit].void
+      case namespaceWithWorkflow: WdlNamespaceWithWorkflow =>
+        val allDeclarations = namespaceWithWorkflow.workflow.declarations ++ namespaceWithWorkflow.workflow.calls.flatMap(_.declarations)
+        val list: List[Checked[Unit]] = inputs.map({ case (k, v) =>
+          allDeclarations.find(_.fullyQualifiedName == k) match {
+            case Some(decl) if decl.womType.coerceRawValue(v).isFailure =>
+              s"Invalid right-side type of '$k'.  Expecting ${decl.womType.stableName}, got ${v.womType.stableName}".invalidNelCheck[Unit]
+            case _ => ().validNelCheck
+          }
+        }).toList
+
+        list.sequence[Checked, Unit].void
+
+      case _: WdlNamespaceWithoutWorkflow =>
+        logger.error("Programmer Error: validateNamespace should never get called on WdlNamespaceWithoutWorkflow")
+        "Cannot execute this WDL: no primary workflow provided".invalidNelCheck
     }
 
-    def workflowHashKey: String = {
-      workflowSource.md5Sum + (source.importsZipFileOption map { bytes => new String(MessageDigest.getInstance("MD5").digest(bytes)) }).getOrElse("")
+    def validationCallable = new Callable[ErrorOr[WdlNamespace]] {
+      def call: ErrorOr[WdlNamespace] = WdlNamespaceWithWorkflow.load(workflowSource, importResolvers map resolverConverter).toErrorOr
     }
 
-    def validationCallable = new Callable[ErrorOr[WdlNamespaceWithWorkflow]] {
-      def call: ErrorOr[WdlNamespaceWithWorkflow] = WdlNamespaceWithWorkflow.load(workflowSource, importResolvers map resolverConverter).toErrorOr
-    }
-
-    lazy val wdlNamespaceValidation: ErrorOr[WdlNamespaceWithWorkflow] = namespaceCache.map(_.get(workflowHashKey, validationCallable)).getOrElse(validationCallable.call)
+    lazy val wdlNamespaceValidation: ErrorOr[WdlNamespace] = retrieveOrCalculate(ParserCacheInputs(Option(workflowSource), None, None, importResolvers), validationCallable)
 
     def evaluateImports(wdlNamespace: WdlNamespace): Map[String, String] = {
       // Descend the namespace looking for imports and construct `MetadataEvent`s for them.
@@ -104,7 +106,7 @@ class WdlDraft2LanguageFactory(override val config: Config) extends LanguageFact
     fromEither[IO](checked)
   }
 
-  private def validateWorkflowNameLengths(namespace: WdlNamespaceWithWorkflow): Checked[Unit] = {
+  private def validateWorkflowNameLengths(namespace: WdlNamespace): Checked[Unit] = {
     import common.validation.Checked._
     def allWorkflowNames(n: WdlNamespace): Seq[String] = n.workflows.map(_.unqualifiedName) ++ n.namespaces.flatMap(allWorkflowNames)
     val tooLong = allWorkflowNames(namespace).filter(_.length >= 100)
@@ -115,16 +117,21 @@ class WdlDraft2LanguageFactory(override val config: Config) extends LanguageFact
     }
   }
 
-
   override def getWomBundle(workflowSource: WorkflowSource,
                             workflowSourceOrigin: Option[ResolvedImportRecord],
                             workflowOptionsJson: WorkflowOptionsJson,
                             importResolvers: List[ImportResolver],
                             languageFactories: List[LanguageFactory],
                             convertNestedScatterToSubworkflow : Boolean = true): Checked[WomBundle] = {
+    lazy val validationCallable = new Callable[ErrorOr[WdlNamespace]] {
+      def call: ErrorOr[WdlNamespace] = WdlNamespace.loadUsingSource(workflowSource, None, Some(importResolvers map resolverConverter)).toErrorOr
+    }
+
+    lazy val parserCacheInputs = ParserCacheInputs(Option(workflowSource), workflowSourceOrigin.map(_.importPath), None, importResolvers)
+
     for {
       _ <- enabledCheck
-      namespace <- WdlNamespace.loadUsingSource(workflowSource, None, Some(importResolvers map resolverConverter)).toChecked
+      namespace <- retrieveOrCalculate(parserCacheInputs, validationCallable).toEither
       womBundle <- namespace.toWomBundle
     } yield womBundle.copyResolvedImportRecord(womBundle, workflowSourceOrigin)
   }
@@ -145,14 +152,6 @@ class WdlDraft2LanguageFactory(override val config: Config) extends LanguageFact
       caching <- config.as[Option[Config]]("caching")
       cc <- CacheConfig.optionalConfig(caching, defaultConcurrency = 2, defaultSize = 1000L, defaultTtl = 20 minutes)
     } yield cc
-  }
-
-  private[draft2] lazy val namespaceCache: Option[Cache[String, ErrorOr[WdlNamespaceWithWorkflow]]] = cacheConfig map { c =>
-    CacheBuilder.newBuilder()
-      .concurrencyLevel(c.concurrency)
-      .expireAfterAccess(c.ttl.length, c.ttl.unit)
-      .maximumSize(c.size)
-      .build[WorkflowSource, ErrorOr[WdlNamespaceWithWorkflow]]()
   }
 }
 

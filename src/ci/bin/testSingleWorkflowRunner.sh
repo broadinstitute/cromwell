1864bf658f309d20d1868ec05d2ac22061219d5d
Adam Nichols <aednichols@gmail.com>
2019-08-29 17:14:43 -0400

    Miguel suggestion

--- src/ci/bin/testSingleWorkflowRunner.sh
+++ src/ci/bin/testSingleWorkflowRunner.sh
@@ -15,8 +15,16 @@ java -jar $CROMWELL_BUILD_CROMWELL_JAR run ./centaur/src/main/resources/standard
 grep "terminal state: WorkflowSucceededState" console_output.txt
 grep "\"wf_hello.hello.salutation\": \"Hello m'Lord!\"" console_output.txt
 
-grep "\"actualWorkflowLanguageVersion\": \"draft-2\"" run_mode_metadata.json
-grep "\"actualWorkflowLanguage\": \"WDL\"" run_mode_metadata.json
-grep "\"wf_hello.hello.salutation\": \"Hello m'Lord!\"" run_mode_metadata.json
-grep "\"wf_hello.hello.addressee\": \"m'Lord\"" run_mode_metadata.json
-grep "\"dockerImageUsed\": \"ubuntu@sha256:71cd81252a3563a03ad8daee81047b62ab5d892ebbfbf71cf53415f29c130950\"" run_mode_metadata.json
+cat > expected.json <<FIN
+{
+  "actualWorkflowLanguage": "WDL",
+  "actualWorkflowLanguageVersion": "draft-2",
+  "dockerImageUsed": "ubuntu@sha256:71cd81252a3563a03ad8daee81047b62ab5d892ebbfbf71cf53415f29c130950",
+  "addressee": "m'Lord",
+  "salutation": "Hello m'Lord!"
+}
+FIN
+
+jq '{actualWorkflowLanguage,actualWorkflowLanguageVersion,dockerImageUsed:.calls["wf_hello.hello"][0].dockerImageUsed,addressee:.inputs["wf_hello.hello.addressee"],salutation:.outputs["wf_hello.hello.salutation"]}' run_mode_metadata.json > actual.json
+
+cmp <(jq -cS . actual.json) <(jq -cS . expected.json)

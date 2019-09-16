3aa0fe4d8c297fe8582828f77de97dfe5faeaed4
Miguel Covarrubias <mcovarr@broadinstitute.org>
2019-09-16 13:38:01 -0400

    PR feedback

--- cloud-nio/cloud-nio-impl-drs/src/main/scala/cloud/nio/impl/drs/DrsPathResolver.scala
+++ cloud-nio/cloud-nio-impl-drs/src/main/scala/cloud/nio/impl/drs/DrsPathResolver.scala
@@ -22,6 +22,10 @@ case class DrsPathResolver(drsConfig: DrsConfig, httpClientBuilder: HttpClientBu
   implicit lazy val dataObjectDecoder: Decoder[DrsDataObject] = deriveDecoder
   implicit lazy val drsObjectDecoder: Decoder[DrsObject] = deriveDecoder
   implicit lazy val saDataObjectDecoder: Decoder[SADataObject] = deriveDecoder
+  // Martha is still returning objects keyed by the obsolete "dos" terminology rather than the current term "drs".
+  // In order to avoid having Cromwell's case classes use the obsolete terminology that would arise from a derived
+  // decoder, this `forProduct2` construct instructs Circe to take the value keyed by `dos` and pass that as the
+  // first argument to `MarthaResponse.apply`, which happens to be the constructor parameter formally named `drs`.
   implicit lazy val marthaResponseDecoder: Decoder[MarthaResponse] = Decoder.forProduct2("dos", "googleServiceAccount")(MarthaResponse.apply)
 
   private val DrsPathToken = "${drsPath}"
--- cromwell-drs-localizer/src/main/scala/drs/localizer/MarthaResponse.scala
+++ cromwell-drs-localizer/src/main/scala/drs/localizer/MarthaResponse.scala
@@ -9,6 +9,10 @@ object MarthaResponseJsonSupport {
   implicit val dataObject: Decoder[DrsDataObject] = deriveDecoder
   implicit val drsObjectFormat: Decoder[DrsObject] = deriveDecoder
   implicit val googleServiceAccountFormat: Decoder[GoogleServiceAccount] = deriveDecoder
+  // Martha is still returning objects keyed by the obsolete "dos" terminology rather than the current term "drs".
+  // In order to avoid having Cromwell's case classes use the obsolete terminology that would arise from a derived
+  // decoder, this `forProduct2` construct instructs Circe to take the value keyed by `dos` and pass that as the
+  // first argument to `MarthaResponse.apply`, which happens to be the constructor parameter formally named `drs`.
   implicit val marthaResponseFormat: Decoder[MarthaResponse] = Decoder.forProduct2("dos", "googleServiceAccount")(MarthaResponse.apply)
 
   implicit val samErrorResponseFormat: Decoder[SamErrorResponse] = deriveDecoder

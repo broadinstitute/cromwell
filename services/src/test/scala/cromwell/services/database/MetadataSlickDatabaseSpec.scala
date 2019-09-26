1f2fa6e908e400e7d7d624132e9608645ab0ed49
Adam Nichols <aednichols@gmail.com>
2019-09-26 13:32:23 -0400

    Set up DB inserts

--- services/src/test/scala/cromwell/services/database/MetadataSlickDatabaseSpec.scala
+++ services/src/test/scala/cromwell/services/database/MetadataSlickDatabaseSpec.scala
@@ -1,6 +1,10 @@
 package cromwell.services.database
 
+import java.time.OffsetDateTime
+
 import cromwell.core.Tags.DbmsTest
+import cromwell.database.slick.MetadataSlickDatabase
+import cromwell.database.sql.tables.MetadataEntry
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FlatSpec, Matchers}
@@ -16,11 +20,17 @@ class MetadataSlickDatabaseSpec extends FlatSpec with Matchers with ScalaFutures
 
     behavior of s"MetadataSlickDatabase on ${databaseSystem.name}"
 
-    lazy val dataAccess = DatabaseTestKit.initializedDatabaseFromSystem(MetadataDatabaseType, databaseSystem)
+    lazy val database: MetadataSlickDatabase with TestSlickDatabase = DatabaseTestKit.initializedDatabaseFromSystem(MetadataDatabaseType, databaseSystem)
+    import database.dataAccess.driver.api._
+
+    import cromwell.database.migration.metadata.table.symbol.MetadataStatement.OffsetDateTimeToSystemTimestamp
+    database.runTestTransaction(
+      database.dataAccess.metadataEntries ++= Seq(MetadataEntry("wasd", None, None, None, "key", None, None, OffsetDateTime.now().toSystemTimestamp, None))
+    )
 
     // attempt deleting root workflow that does not exist
     it should "error when deleting a root workflow that does not exist" taggedAs DbmsTest in {
-      val delete = dataAccess.deleteNonLabelMetadataForWorkflow("asdf")
+      val delete = database.deleteNonLabelMetadataForWorkflow("asdf")
 
       delete.failed.futureValue(Timeout(10.seconds)).getMessage should be(s"Programmer error: attempted to delete metadata rows for non-existent root workflow asdf")
     }
@@ -36,7 +46,7 @@ class MetadataSlickDatabaseSpec extends FlatSpec with Matchers with ScalaFutures
     // attempt deleting root workflow id with subworkflows that have subworkflows. should delete expected row count
 
     it should "close the database" taggedAs DbmsTest in {
-      dataAccess.close()
+      database.close()
     }
   }
 

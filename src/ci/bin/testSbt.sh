e678f5778a47187465b5630ffd31a87a03fd77ec
Dan Billings <danb@broadinstitute.org>
2019-10-03 15:50:14 -0400

    add moar memory

--- src/ci/bin/testSbt.sh
+++ src/ci/bin/testSbt.sh
@@ -17,7 +17,8 @@ cromwell::build::unit::setup_exclude_tags
 CROMWELL_SBT_TEST_EXCLUDE_TAGS="${CROMWELL_BUILD_UNIT_EXCLUDE_TAGS}" \
 CROMWELL_SBT_TEST_SPAN_SCALE_FACTOR="${CROMWELL_BUILD_UNIT_SPAN_SCALE_FACTOR}" \
 sbt \
-    -J-XX:MaxMetaspaceSize=1024M \
+    -J-XX:Xmx6000M \
+    -J-XX:MaxMetaspaceSize=2048M \
     -Dakka.test.timefactor=${CROMWELL_BUILD_UNIT_SPAN_SCALE_FACTOR} \
     -Dbackend.providers.Local.config.filesystems.local.localization.0=copy \
     coverage test

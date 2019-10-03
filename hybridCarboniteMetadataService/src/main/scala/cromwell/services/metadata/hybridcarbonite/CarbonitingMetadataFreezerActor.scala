d4f5e026d7a59b8dcf0aa6237b41268355583683
Chris Llanwarne <cjllanwarne@gmail.com>
2019-10-03 16:05:31 -0400

    Pretty print

--- hybridCarboniteMetadataService/src/main/scala/cromwell/services/metadata/hybridcarbonite/CarbonitingMetadataFreezerActor.scala
+++ hybridCarboniteMetadataService/src/main/scala/cromwell/services/metadata/hybridcarbonite/CarbonitingMetadataFreezerActor.scala
@@ -48,7 +48,7 @@ class CarbonitingMetadataFreezerActor(carboniterConfig: HybridCarboniteConfig,
 
   when(Fetching) {
     case Event(BuiltMetadataResponse(_, responseJson), FetchingData(workflowId)) =>
-      asyncIo.writeAsync(carboniterConfig.makePath(workflowId), responseJson.compactPrint, Seq(StandardOpenOption.CREATE)) onComplete {
+      asyncIo.writeAsync(carboniterConfig.makePath(workflowId), responseJson.prettyPrint, Seq(StandardOpenOption.CREATE)) onComplete {
         result => self ! CarbonitingFreezeResult(result)
       }
       goto(Freezing) using FreezingData(workflowId)

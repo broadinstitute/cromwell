f6146a4fd3f4d4da60b5d63bbbe9e9569d3939cd
Chris Llanwarne <cjllanwarne@gmail.com>
2019-10-15 16:24:42 -0400

    Worth an un-try?

--- src/ci/bin/testSbt.sh
+++ src/ci/bin/testSbt.sh
@@ -19,7 +19,6 @@ CROMWELL_SBT_TEST_SPAN_SCALE_FACTOR="${CROMWELL_BUILD_UNIT_SPAN_SCALE_FACTOR}" \
 sbt \
     --warn \
     -J-Xmx6000M \
-    -Dsbt.classloader.close=false \
     -Dakka.test.timefactor=${CROMWELL_BUILD_UNIT_SPAN_SCALE_FACTOR} \
     -Dbackend.providers.Local.config.filesystems.local.localization.0=copy \
     coverage test

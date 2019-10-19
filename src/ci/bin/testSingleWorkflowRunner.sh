c71335806cdd412c8602cf2872983e960c84c0f1
Grigoriy Sterin <gsterin@broadinstitute.org>
2019-10-19 01:25:43 -0400

    Fixed testSingleWorkflowRunner.sh tests and added new test to check if application exists properly when parameter validation fails [BA-4061]

--- src/ci/bin/testSingleWorkflowRunner.sh
+++ src/ci/bin/testSingleWorkflowRunner.sh
@@ -68,3 +68,13 @@ jq '{
 
 cmp <(jq -cS . actual.json) <(jq -cS . expected.json)
 popd > /dev/null
+
+# Test 3: program should exit with error in case if validation of command line arguments failed
+java -jar "${CROMWELL_BUILD_CROMWELL_JAR}" run nonexistent.wdl &
+pid=$!
+sleep 10
+if kill -0 $pid > /dev/null 2>&1; then
+  echo "ERROR: Process still exists"
+  kill $pid
+  exit 1
+fi

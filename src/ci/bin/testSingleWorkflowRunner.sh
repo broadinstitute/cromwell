f8396f3666afa352269c97d8281818c113776a94
Grigoriy Sterin <gsterin@broadinstitute.org>
2019-10-21 09:39:07 -0400

    Added test for the invalid command-line args case into testSingleWorkflowRunner.sh [BA-3094]

--- src/ci/bin/testSingleWorkflowRunner.sh
+++ src/ci/bin/testSingleWorkflowRunner.sh
@@ -78,3 +78,14 @@ if kill -0 $pid > /dev/null 2>&1; then
   kill $pid
   exit 1
 fi
+
+# Test 4: application should return non-zero exit code when parsing of command-line arguments fails
+java \
+    -jar "${CROMWELL_BUILD_CROMWELL_JAR}" \
+    run ./centaur/src/main/resources/standardTestCases/hello/hello.wdl \
+    ./centaur/src/main/resources/standardTestCases/hello/hello.inputs
+retVal=$?
+if [ $retVal -eq 0 ]; then
+    echo "ERROR: application exited with exit code 0 when invalid command-line arguments were provided"
+    exit 1
+fi

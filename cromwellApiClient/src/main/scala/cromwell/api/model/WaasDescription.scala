8287f391a12c19afd51c5abbae7f7959c68b7300
Chris Llanwarne <cjllanwarne@gmail.com>
2019-10-28 17:01:25 -0400

    Fix cromwell client expected /describe response shape [force ci]

--- cromwellApiClient/src/main/scala/cromwell/api/model/WaasDescription.scala
+++ cromwellApiClient/src/main/scala/cromwell/api/model/WaasDescription.scala
@@ -1,29 +1,38 @@
 package cromwell.api.model
 
-import spray.json.{DefaultJsonProtocol, JsValue}
+import spray.json.{DefaultJsonProtocol, JsObject, JsValue}
 
 object WorkflowDescriptionJsonSupport extends DefaultJsonProtocol {
   implicit val WaasWorkflowDescriptorTypeFormat = jsonFormat2(WaasWorkflowDescriptorType)
   implicit val WaasDescriptionWomTypeFormat = jsonFormat1(WaasDescriptionWomType)
-  implicit val WaasDescriptionParameterFormat = jsonFormat5(WaasDescriptionParameter)
+  implicit val WaasDescriptionInputDefinitionFormat = jsonFormat5(WaasDescriptionInputDefinition)
+  implicit val WaasDescriptionOutputDefinitionFormat = jsonFormat3(WaasDescriptionOutputDefinition)
 
-  implicit val WorkflowDescriptionFormat = jsonFormat8(WaasDescription)
+  implicit val WorkflowDescriptionFormat = jsonFormat12(WaasDescription)
 }
 
 final case class WaasDescription(valid: Boolean,
                                  validWorkflow: Boolean,
                                  errors: List[String],
                                  name: String,
-                                 inputs: List[WaasDescriptionParameter],
-                                 outputs: List[WaasDescriptionParameter],
+                                 inputs: List[WaasDescriptionInputDefinition],
+                                 outputs: List[WaasDescriptionOutputDefinition],
+                                 images: List[String],
                                  submittedDescriptorType: WaasWorkflowDescriptorType,
+                                 importedDescriptorTypes: List[WaasWorkflowDescriptorType],
+                                 meta: JsObject,
+                                 parameterMeta: JsObject,
                                  isRunnableWorkflow: Boolean)
 
-final case class WaasDescriptionParameter(name: String,
-                                          valueType: WaasDescriptionWomType,
-                                          optional: Boolean,
-                                          default: Option[JsValue],
-                                          typeDisplayName: String)
+final case class WaasDescriptionInputDefinition(name: String,
+                                                valueType: WaasDescriptionWomType,
+                                                optional: Option[Boolean],
+                                                default: Option[JsValue],
+                                                typeDisplayName: String)
+
+final case class WaasDescriptionOutputDefinition(name: String,
+                                                 valueType: WaasDescriptionWomType,
+                                                 typeDisplayName: String)
 
 final case class WaasDescriptionWomType(typeName: String)
 final case class WaasWorkflowDescriptorType(descriptorType: String, descriptorTypeVersion: String)

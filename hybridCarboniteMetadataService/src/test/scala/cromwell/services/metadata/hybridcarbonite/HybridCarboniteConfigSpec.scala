55c393428263efcb48357ff1c2a5a850c8aeac29
Miguel Covarrubias <mcovarr@broadinstitute.org>
2019-11-08 15:03:23 -0500

    Configurable Carbonite freeze scanning parameters [BA-6109]

--- hybridCarboniteMetadataService/src/test/scala/cromwell/services/metadata/hybridcarbonite/HybridCarboniteConfigSpec.scala
+++ hybridCarboniteMetadataService/src/test/scala/cromwell/services/metadata/hybridcarbonite/HybridCarboniteConfigSpec.scala
@@ -5,6 +5,9 @@ import com.typesafe.config.ConfigFactory
 import cromwell.core.TestKitSuite
 import org.scalatest.{FlatSpecLike, Matchers}
 
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
 class HybridCarboniteConfigSpec extends TestKitSuite("HybridCarboniteConfigSpec") with FlatSpecLike with Matchers {
 
   behavior of "HybridCarboniteConfig"
@@ -29,11 +32,17 @@ class HybridCarboniteConfigSpec extends TestKitSuite("HybridCarboniteConfigSpec"
 
     carboniteConfig match {
       case Left(e) => fail(s"Expected to parse correctly but got failure. Reason: $e")
-      case Right(c) => {
+      case Right(c) =>
         c.enabled shouldBe true
         c.bucket shouldBe "my_test_bucket"
         c.pathBuilders.head.name shouldBe "Google Cloud Storage"
-      }
+        //noinspection RedundantDefaultArgument
+        val defaultFreezeScanConfig = HybridCarboniteFreezeScanConfig(
+          initialInterval = 5 seconds,
+          maxInterval = 5 minutes,
+          multiplier = 1.1
+        )
+        c.freezeScanConfig shouldBe defaultFreezeScanConfig
     }
   }
 
@@ -54,11 +63,10 @@ class HybridCarboniteConfigSpec extends TestKitSuite("HybridCarboniteConfigSpec"
 
     carboniteConfig match {
       case Left(e) => fail(s"Expected to parse correctly but got failure. Reason: $e")
-      case Right(c) => {
+      case Right(c) =>
         c.enabled shouldBe false
         c.bucket shouldBe "my_test_bucket"
         c.pathBuilders.head.name shouldBe "Google Cloud Storage"
-      }
     }
   }
 
@@ -114,6 +122,95 @@ class HybridCarboniteConfigSpec extends TestKitSuite("HybridCarboniteConfigSpec"
 
     carboniteConfig match {
       case Left(e) => e.head shouldBe "No configuration setting found for key 'auth'"
+      case Right(_) => fail(s"Expected to fail but the config was parsed correctly!")
+    }
+  }
+
+  it should "respect valid freeze-scan config settings" in {
+    val config = ConfigFactory.parseString(
+      """{
+        |   enabled = true
+        |   bucket = "my_test_bucket"
+        |   filesystems {
+        |     gcs {
+        |       auth = "application-default"
+        |     }
+        |   }
+        |   freeze-scan {
+        |     initial-interval = 1 second
+        |     max-interval = 5 seconds
+        |     multiplier = 1.2
+        |   }
+        |}
+      """.stripMargin
+    )
+
+    val carboniteConfig = HybridCarboniteConfig.parseConfig(config)
+
+    carboniteConfig match {
+      case Left(e) => fail(s"Expected to parse correctly but got failure. Reason: $e")
+      case Right(c) =>
+        val expectedConfig = HybridCarboniteFreezeScanConfig(
+          initialInterval = 1 second,
+          maxInterval = 5 seconds,
+          multiplier = 1.2
+        )
+        c.freezeScanConfig shouldBe expectedConfig
+    }
+  }
+
+  it should "reject max interval < initial interval in freeze-scan config settings" in {
+    val config = ConfigFactory.parseString(
+      """{
+        |   enabled = true
+        |   bucket = "my_test_bucket"
+        |   filesystems {
+        |     gcs {
+        |       auth = "application-default"
+        |     }
+        |   }
+        |   freeze-scan {
+        |     max-interval = 1 second
+        |     initial-interval = 5 seconds
+        |     multiplier = 1.2
+        |   }
+        |}
+      """.stripMargin
+    )
+
+    val carboniteConfig = HybridCarboniteConfig.parseConfig(config)
+
+    carboniteConfig match {
+      case Left(e) =>
+        e.head shouldBe "max-interval must be greater than or equal to initial-interval in Carboniter 'freeze-scan' stanza"
+      case Right(_) => fail(s"Expected to fail but the config was parsed correctly!")
+    }
+  }
+
+  it should "reject intervals that are not durations in freeze-scan config settings" in {
+    val config = ConfigFactory.parseString(
+      """{
+        |   enabled = true
+        |   bucket = "my_test_bucket"
+        |   filesystems {
+        |     gcs {
+        |       auth = "application-default"
+        |     }
+        |   }
+        |   freeze-scan {
+        |     initial-interval = I
+        |     max-interval = like
+        |     multiplier = turtles
+        |   }
+        |}
+      """.stripMargin
+    )
+
+    val carboniteConfig = HybridCarboniteConfig.parseConfig(config)
+
+    carboniteConfig match {
+      case Left(e) =>
+        e.head shouldBe "Failed to parse Carboniter 'freeze-scan' stanza (reason 1 of 1): String: 10: Invalid value at 'initial-interval': No number in duration value 'I'"
       case Right(_) => fail(s"Expected to fail but the config was parsed correctly!")
     }
   }

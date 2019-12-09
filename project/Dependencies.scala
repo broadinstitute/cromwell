a42492537f482a334fac1f32b5b0c9e4f83d1d2b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-12-09 09:20:13 +0100

    update to better-files 3.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   private val apacheCommonNetV = "3.6"
   private val apacheHttpClientV = "4.5.7"
   private val awsSdkV = "2.3.9"
-  private val betterFilesV = "2.17.1"
+  private val betterFilesV = "3.8.0"
   private val catsEffectV = "2.0.0"
   private val catsV = "2.0.0"
   private val circeOpticsV = "0.12.0"

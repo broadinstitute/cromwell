31762294d799511ad80437e599e31138a6ad8462
Chris Llanwarne <cjllanwarne@gmail.com>
2019-12-20 12:55:06 -0500

    Script to make external PR clone branches with salt

---
+++ scripts/make_pr_clone_branch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export PR_TO_CLONE=$1
+
+git fetch -f origin pull/${PR_TO_CLONE}/head:${PR_TO_CLONE}_pr_clone 
+git checkout ${PR_TO_CLONE}_pr_clone
+
+cat "PR for hash $(rev-parse --verify HEAD)" > pr_salt
+git add pr_salt
+git commit -m "Salting PR branch"
+
+echo "Created (or updated) pr-able branch for PR $1."
+echo "You can now force push this branch to run the CI suite for the changes in that PR.
+
+

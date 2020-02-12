7e7ee2f87a866553ba1d9098e47f89d5bd6fc38d
Miguel Covarrubias <mcovarr@users.noreply.github.com>
2020-02-11 22:50:24 -0500

    Unify RP error check so it also applies to singleton files [BA-6235] (#5406)

--- supportedBackends/google/pipelines/v2beta/src/main/resources/gcs_transfer.sh
+++ supportedBackends/google/pipelines/v2beta/src/main/resources/gcs_transfer.sh
@@ -137,9 +137,14 @@ private::determine_requester_pays() {
     fi
   done
 
-  if [[ ${attempt} -gt ${max_attempts} ]]; then
+  # The initial value of the USE_REQUESTER_PAYS variable is "error". If this function was able to run either the
+  # NO_REQUESTER_PAYS_COMMAND or REQUESTER_PAYS_COMMAND successfully then USE_REQUESTER_PAYS should have been reassigned
+  # a value of "false" or "true" as appropriate. But if this function was not able to execute either of those commands
+  # successfully then the value of USE_REQUESTER_PAYS will not be "false" or "true", so exit with an error.
+  if [[ "$USE_REQUESTER_PAYS" != "false" ]] && [[ "$USE_REQUESTER_PAYS" != "true" ]]; then
     echo "Error attempting to localize file with command: '$command'"
     cat ${gsutil_log}
+    exit 1
   fi
 }
 
@@ -167,11 +172,8 @@ localize_files() {
 
   if [[ ${USE_REQUESTER_PAYS} = true ]]; then
     rpflag="-u $project"
-  elif [[ ${USE_REQUESTER_PAYS} = false ]]; then
-    rpflag=""
   else
-    # error
-    exit 1
+    rpflag=""
   fi
 
 
@@ -246,10 +248,8 @@ localize_directories() {
 
   if [[ ${USE_REQUESTER_PAYS} = true ]]; then
     rpflag="-u $project"
-  elif [[ ${USE_REQUESTER_PAYS} = false ]]; then
-    rpflag=""
   else
-    exit 1
+    rpflag=""
   fi
 
   while [[ $# -gt 0 ]]; do

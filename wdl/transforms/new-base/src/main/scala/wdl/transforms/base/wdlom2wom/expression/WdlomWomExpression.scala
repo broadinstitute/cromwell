72d5b0c4add19fb538a68fab410ff2bfb24f4fea
Chris Llanwarne <cjllanwarne@gmail.com>
2020-03-16 19:55:10 -0400

    [force ci] Test 1 (for real this time)

--- wdl/transforms/new-base/src/main/scala/wdl/transforms/base/wdlom2wom/expression/WdlomWomExpression.scala
+++ wdl/transforms/new-base/src/main/scala/wdl/transforms/base/wdlom2wom/expression/WdlomWomExpression.scala
@@ -28,7 +28,7 @@ final case class WdlomWomExpression private (expressionElement: ExpressionElemen
 
   def evaluateValueForPlaceholder(inputValues: Map[String, WomValue], ioFunctionSet: IoFunctionSet, forCommandInstantiationOptions: ForCommandInstantiationOptions): ErrorOr[EvaluatedValue[_]] = {
     val preMappedInputs = inputValues.map { case (key, value) => key -> forCommandInstantiationOptions.valueMapper.apply(value) }
-    expressionElement.evaluateValue(preMappedInputs, ioFunctionSet, Option(forCommandInstantiationOptions))
+    expressionElement.evaluateValue(preMappedInputs, ioFunctionSet, None)
   }
 
   override def evaluateValue(inputValues: Map[String, WomValue], ioFunctionSet: IoFunctionSet): ErrorOr[WomValue] =

d269e5481033f6f925695d3d12375f20f755b272
Miguel Covarrubias <mcovarr@broadinstitute.org>
2020-03-26 18:11:16 -0400

    cleanup

--- scripts/perf/stop_database/stop_database.sh
+++ scripts/perf/stop_database/stop_database.sh
@@ -9,4 +9,4 @@ mkdir -p mnt
 read_service_account_from_vault
 
 # Start up the CloudSQL DB. There is an --async on `patch` but it doesn't return a waitable ID...
-gcloud_run_as_service_account "perf_sql_stop_gcloud_${BUILD_NUMBER}" "gcloud -q --project broad-dsde-cromwell-perf sql instances patch ${CLOUD_SQL_INSTANCE_TO_STOP} --activation-policy NEVER"
+gcloud_run_as_service_account "perf_sql_stop_gcloud_${BUILD_NUMBER}" "gcloud -q --project broad-dsde-cromwell-perf sql instances patch ${CLOUD_SQL_INSTANCE} --activation-policy NEVER"

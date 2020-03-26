1a104062e6b3662ffe8b30065eff55f8a1ef2ed7
Miguel Covarrubias <mcovarr@broadinstitute.org>
2020-03-26 13:27:05 -0400

    sync waiting

--- scripts/perf/start_database/start_database.sh
+++ scripts/perf/start_database/start_database.sh
@@ -8,7 +8,5 @@ mkdir -p mnt
 # Read the service account credentials from vault:
 read_service_account_from_vault
 
-# Start up the CloudSQL DB
-SQL_OPERATION=$(gcloud_run_as_service_account "perf_sql_start_gcloud_${BUILD_NUMBER}" "gcloud -q --project broad-dsde-cromwell-perf sql instances patch --async ${CLOUD_SQL_INSTANCE_TO_START} --activation-policy ALWAYS")
-
-gcloud_run_as_service_account "perf_sql_await_gcloud_${BUILD_NUMBER}" "gcloud beta sql operations wait --timeout=900 --project broad-dsde-cromwell-perf ${SQL_OPERATION}"
+# Start up the CloudSQL DB. There is an --async on `patch` but it doesn't return a waitable ID...
+gcloud_run_as_service_account "perf_sql_start_gcloud_${BUILD_NUMBER}" "gcloud -q --project broad-dsde-cromwell-perf sql instances patch ${CLOUD_SQL_INSTANCE_TO_START} --activation-policy ALWAYS"

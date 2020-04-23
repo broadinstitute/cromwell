10509c9cb8fdffa881aa8397d685b9c9d1a404d9
Chris Llanwarne <cjllanwarne@gmail.com>
2020-04-23 15:36:20 -0400

    And also papi v1 and v2beta

--- scripts/metadata_comparison/metadata_comparison/lib/papi/papi_clients.py
+++ scripts/metadata_comparison/metadata_comparison/lib/papi/papi_clients.py
@@ -23,10 +23,19 @@ class PapiClients:
 
 
     def make_client(self, api_version):
-        if 'api_version' in [ 'v1alpha2', 'v2alpha1']:
+        if api_version in [ 'v1alpha2', 'v2alpha1']:
             return google_client_build('genomics', api_version, credentials = self.credentials)
+        elif api_version == 'v2beta':
+            return google_client_build('lifesciences', api_version, credentials = self.credentials)
         else:
             raise Exception(f'Unsupported client api_version: "{api_version}"')
+
+
+    def read_papi_v1_operation_metadata(self, operation_id, genomics_v1_client):
+        """Reads the operations metadata for a pipelines API v1 job ID. Returns a python dict"""
+        logger.info(f'Reading PAPI v1 operation metadata for {operation_id}...')
+        result = genomics_v1_client.operations().get(name=operation_id).execute()
+        return result
 
 
     def read_papi_v2alpha1_operation_metadata(self, operation_id, genomics_v2alpha1_client):
@@ -36,11 +45,25 @@ class PapiClients:
         return result
 
 
+    def read_papi_v2beta_operation_metadata(self, operation_id, genomics_v2beta_client):
+        """Reads the operations metadata for a pipelines API v2beta job ID. Returns a python dict"""
+        logger.info(f'Reading PAPI v2beta operation metadata for {operation_id}...')
+        result = genomics_v2beta_client.projects().locations().operations().get(name=operation_id).execute()
+        return result
+
+
     def request_operation_metadata(self, operation_id):
+        """
+        Reads the operations metadata for any supported pipelines API version.
+        Returns a python dict
+        """
         api_version = operation_id_to_api_version(operation_id)
         client = self.get_client(api_version)
-        if api_version == 'v2alpha1':
+        if api_version == 'v1alpha2':
+            return self.read_papi_v1_operation_metadata(operation_id, client)
+        elif api_version == 'v2alpha1':
             return self.read_papi_v2alpha1_operation_metadata(operation_id, client)
+        elif api_version == 'v2beta':
+            return self.read_papi_v2beta_operation_metadata(operation_id, client)
         else:
             raise Exception(f'Unsupported client api_version in request_operation_metadata: "{api_version}"')
-

474f7fe915db7d04b0b22d503f6646749b4de6e2
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-04-25 22:44:02 -0400

    unused import

--- supportedBackends/google/pipelines/v2beta/src/main/scala/cromwell/backend/google/pipelines/v2beta/PipelinesUtilityConversions.scala
+++ supportedBackends/google/pipelines/v2beta/src/main/scala/cromwell/backend/google/pipelines/v2beta/PipelinesUtilityConversions.scala
@@ -12,7 +12,6 @@ import mouse.all._
 import PipelinesUtilityConversions._
 
 import scala.language.postfixOps
-import scala.util.Try
 
 trait PipelinesUtilityConversions {
   def toAccelerator(gpuResource: GpuResource) = new Accelerator().setCount(gpuResource.gpuCount.value.toLong).setType(gpuResource.gpuType.toString)

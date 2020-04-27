d26dc607f6101c0da5ef93ce0f2c8f38aa9cf470
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-04-27 15:31:51 -0400

    Update scripts/metadata_comparison/test/lib/helper_functions.py
    Co-Authored-By: Miguel Covarrubias <mcovarr@users.noreply.github.com>

--- scripts/metadata_comparison/test/lib/helper_functions.py
+++ scripts/metadata_comparison/test/lib/helper_functions.py
@@ -6,4 +6,4 @@ from pathlib import Path
 Can be used to read files from the resources directory, like:
  (RESOURCES / filename).read_text()
 """
-RESOURCES = Path(__file__).parent.parent / Path("resources")
+RESOURCES = Path(__file__).parent.parent / "resources"

047e7b53338dfd493e638492c9e90fea669f78d5
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-04-29 15:10:44 -0400

    debug [force ci]

--- src/ci/bin/testSbt.sh
+++ src/ci/bin/testSbt.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset -o pipefail
-export CROMWELL_BUILD_OPTIONAL_SECURE=true
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh

643d44284e183ad3d1921fc42bbde3ffef92ffa4
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-04-29 15:06:05 -0400

    debug [force ci]

--- src/ci/bin/testMetadataComparisonPython.sh
+++ src/ci/bin/testMetadataComparisonPython.sh
@@ -8,9 +8,14 @@ source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test_unit.inc.sh" || source test_unit.inc.sh
 
+echo 1
 cromwell::build::setup_common_environment
+echo 2
 cromwell::build::install_python_scripts_dependencies
+echo 3
 
 python3 -m unittest discover -v ${BASH_SOURCE%/*}/metadata_comparison
+echo 4
 
 cromwell::build::publish_artifacts
+echo 5

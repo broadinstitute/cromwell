ec7d70417624a014ae3538f63133c24e1b374dad
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-04-29 18:13:55 -0400

    pip [force ci]

--- src/ci/bin/testMetadataComparisonPython.sh
+++ src/ci/bin/testMetadataComparisonPython.sh
@@ -9,13 +9,13 @@ source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
 cromwell::build::setup_common_environment
 
 build_root="$( dirname "${BASH_SOURCE[0]}" )/../../.."
-docker run -it --rm -v "${build_root}/scripts/metadata_comparison:/metadata_comparison" python:3.6 /bin/bash -c "\
-pip install --upgrade requests\
-pip install --upgrade google-api-python-client\
-pip install --upgrade google-cloud\
-pip install --upgrade google-cloud-storage\
-pip install --upgrade pandas\
-python -m unittest discover -v /metadata_comparison\
+docker run -it --rm -v "${build_root}/scripts/metadata_comparison:/metadata_comparison" python:3.6 /bin/bash -c "
+pip install --upgrade requests
+pip install --upgrade google-api-python-client
+pip install --upgrade google-cloud
+pip install --upgrade google-cloud-storage
+pip install --upgrade pandas
+python -m unittest discover -v /metadata_comparison
 "
 
 cromwell::build::publish_artifacts

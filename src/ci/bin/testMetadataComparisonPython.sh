5b649b0c34a03c7f841f8138aa5c8983d5aeaa06
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-04-29 16:53:01 -0400

    pip [force ci]

--- src/ci/bin/testMetadataComparisonPython.sh
+++ src/ci/bin/testMetadataComparisonPython.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset -o pipefail
-export CROMWELL_BUILD_OPTIONAL_SECURE=true
+
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
-# shellcheck source=/dev/null
-source "${BASH_SOURCE%/*}/test_unit.inc.sh" || source test_unit.inc.sh
 
-echo 1
 cromwell::build::setup_common_environment
-echo 2
 cromwell::build::install_python_scripts_dependencies
-echo 3
 
-python3 -m unittest discover -v ${BASH_SOURCE%/*}/metadata_comparison
-echo 4
+build_root="$( dirname "${BASH_SOURCE[0]}" )/../../.."
+python3 -m unittest discover -v "${build_root}/scripts/metadata_comparison"
 
 cromwell::build::publish_artifacts
-echo 5

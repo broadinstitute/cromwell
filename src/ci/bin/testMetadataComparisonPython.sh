e05b2fac6bf90820ee22b36bdb269ddae52198ad
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-04-29 18:31:54 -0400

    fix

--- src/ci/bin/testMetadataComparisonPython.sh
+++ src/ci/bin/testMetadataComparisonPython.sh
@@ -9,6 +9,10 @@ source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
 cromwell::build::setup_common_environment
 
 build_root="$( dirname "${BASH_SOURCE[0]}" )/../../.."
+
+# Out Python scripts can only be run by Python version >= 3.6, because we use string interpolation which was introduced
+# in Python 3.6. But Travis environment is Ubuntu 16.04 Xenial LTS, which supports only Python 3.5.
+# Thus we have to run Python tests in Docker container.
 docker run -it --rm -v "${build_root}/scripts/metadata_comparison:/metadata_comparison" python:3.6 /bin/bash -c "
 pip install --upgrade requests
 pip install --upgrade google-api-python-client

0f87078475342f3ec72c61bf37e02edaf978a7ec
Miguel Covarrubias <mcovarr@broadinstitute.org>
2020-05-10 08:01:53 -0400

    interim broken

--- backend/src/main/scala/cromwell/backend/standard/callcaching/BlacklistCache.scala
+++ backend/src/main/scala/cromwell/backend/standard/callcaching/BlacklistCache.scala
@@ -2,10 +2,11 @@ package cromwell.backend.standard.callcaching
 
 import com.google.common.cache.{CacheBuilder, CacheLoader}
 import cromwell.core.CacheConfig
+import cromwell.services.CallCaching.CallCachingEntryId
 
 case class BlacklistCache(config: CacheConfig) {
-  val cache = {
-    // Queries to the blacklist cache return false by default (i.e. not blacklisted).
+  val bucketCache = {
+    // Queries to the bucket blacklist cache return false by default (i.e. not blacklisted).
     val falseLoader = new CacheLoader[String, java.lang.Boolean]() {
       override def load(key: String): java.lang.Boolean = false
     }
@@ -18,7 +19,25 @@ case class BlacklistCache(config: CacheConfig) {
       build[String, java.lang.Boolean](falseLoader)
   }
 
-  def isBlacklisted(bucket: String): Boolean = cache.get(bucket)
+  val hitCache = {
+    // Queries to the hit blacklist cache return false by default (i.e. not blacklisted).
+    val falseLoader = new CacheLoader[CallCachingEntryId, java.lang.Boolean]() {
+      override def load(key: CallCachingEntryId): java.lang.Boolean = false
+    }
 
-  def blacklist(bucket: String): Unit = cache.put(bucket, true)
+    CacheBuilder.
+      newBuilder().
+      concurrencyLevel(config.concurrency).
+      maximumSize(config.size).
+      expireAfterWrite(config.ttl.length, config.ttl.unit).
+      build[CallCachingEntryId, java.lang.Boolean](falseLoader)
+  }
+
+  def isBlacklisted(bucket: String): Boolean = bucketCache.get(bucket)
+
+  def isBlacklisted(hit: CallCachingEntryId): Boolean = hitCache.get(hit)
+
+  def blacklist(bucket: String): Unit = bucketCache.put(bucket, true)
+
+  def blacklist(hit: CallCachingEntryId): Unit = hitCache.put(hit, true)
 }

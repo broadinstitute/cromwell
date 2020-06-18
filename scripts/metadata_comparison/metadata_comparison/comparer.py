d1e4b1e47baee4a86dfd1b446c171e62abb3cfae
Miguel Covarrubias <mcovarr@broadinstitute.org>
2020-06-17 21:33:30 -0400

    interim

--- scripts/metadata_comparison/metadata_comparison/comparer.py
+++ scripts/metadata_comparison/metadata_comparison/comparer.py
@@ -8,7 +8,6 @@
 #
 # Python Prereqs (at least, the ones which I needed to manually install... YMMV):
 #
-#   * pip3 install --upgrade pandas
 #   * pip3 install --upgrade google-api-python-client
 #   * pip3 install --upgrade google-cloud-storage
 #
@@ -18,14 +17,11 @@
 from typing import AnyStr, List, Tuple
 import argparse
 import json
-import pandas
-import google.auth
 from google.cloud import storage
 import logging
 from metadata_comparison.lib.comparison_paths import ComparisonPath, validate_path
 from metadata_comparison.lib.logging import set_log_verbosity, quieten_chatty_imports
-from metadata_comparison.lib.storage import upload_blob
-from metadata_comparison.lib.argument_regex import gcs_path_regex_validator
+from metadata_comparison.lib.operation_ids import JsonObject
 
 logger = logging.getLogger('metadata_comparison.comparer')
 
@@ -42,63 +38,61 @@ def read_digester_jsons(path_strings: List[AnyStr],
     return result
 
 
-def compare_jsons(_workflow_ids_and_jsons: List[Tuple[str, dict]]) -> pandas.DataFrame:
-    """
-    Uses pandas library to convert JSONs into dataframes, and concatenate those dataframes into a single one.
-    Performs sanity check, producing exception, if at least one of the JSONs doesn't have matching subset of keys.
-    """
-    column_to_compare_name_ending = ".cromwellTotalTimeSeconds"
-    version_column_name = "version"
-    result = pandas.DataFrame()
-    last_cols = []
-    for workflow_id_and_json in _workflow_ids_and_jsons:
-        df = pandas.json_normalize(workflow_id_and_json[1])
-        cols = [c for c in df.columns if c.endswith(column_to_compare_name_ending)]
-        cols.sort()
-        cols.insert(0, version_column_name)
+def compare_jsons() -> List[List[AnyStr]]:
+    columns = [
+        {'PAPI Total Time': '.papiTotalTimeSeconds'}
+    ]
+    return [["foo"]]
 
-        if last_cols and last_cols != cols:
-            at = _workflow_ids_and_jsons[0]
-            msg = f"JSON data at {at} doesn't have matching subset of columns. Expected: {last_cols} but got {cols}"
-            raise Exception(msg)
 
-        last_cols = cols
-        df.index = [workflow_id_and_json[0]]
-        result = pandas.concat([result, df[cols]])
+def check_digest_versions():
+    version_1, version_2 = [j.get('version') for j in [json_1, json_2]]
 
-    rename_version_column_to = "digester format version"
-    result.rename(columns={version_column_name: rename_version_column_to}, inplace=True)
-    result.index.name = "workflow id"
+    if version_1 != version_2:
+        msg = f"Inconsistent digest versions: First JSON digest is {version_1} but second is {version_2}"
+        raise ValueError(msg)
 
-    return result
+    call_keys_1, call_keys_2 = [j.get('calls').keys() for j in [json_1, json_2]]
+
+    if call_keys_1 != call_keys_2:
+        raise ValueError("Digest files do not have the same call keys, not the same workflow and/or sample.")
+
+
+def json_from_path_string(path_string: AnyStr) -> JsonObject:
+    path = ComparisonPath.create(path_string)
+    return json.loads(path.read_text())
+
+
+def csv_string_from_data(data: List[List[AnyStr]]) -> AnyStr:
+    rows = [','.join(row) for row in data]
+    return '\n'.join(rows)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Compare performance metadata JSONs and produce CSV result')
+    parser = argparse.ArgumentParser(description='Compare performance digest JSONs and produce CSV result')
     parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('output_path', metavar='OUTPUT_PATH', type=validate_path, nargs=1,
-                        help='Path to output CSV file.')
-    parser.add_argument('digest_1', nargs=1, metavar='REQUIRED_DIGEST_1', type=validate_path,
-                        help='First required digest path to compare.')
-    parser.add_argument('digest_2', nargs=1, metavar='REQUIRED_DIGEST_2', type=validate_path,
-                        help='Second required digest path to compare.')
-    parser.add_argument('digests_more', nargs='*', metavar='OPTIONAL_MORE_DIGESTS', type=validate_path,
-                        help='Any additional digest paths to compare.')
+    parser.add_argument('--name1', nargs=1, metavar='NAME_FOR_DIGEST_1', type=str,
+                        required=True, help='Name to use for the first digest')
+    parser.add_argument('--name2', nargs=1, metavar='NAME_FOR_DIGEST_2', type=str,
+                        required=True, help='Name to use for the second digest')
+    parser.add_argument('--digest1', nargs=1, metavar='PATH_TO_DIGEST_1', type=validate_path,
+                        required=True, help='First digest path to compare.')
+    parser.add_argument('--digest2', nargs=1, metavar='PATH_TO_DIGEST_2', type=validate_path,
+                        required=True, help='Second digest path to compare.')
+    parser.add_argument('--output-path', metavar='OUTPUT_PATH', type=validate_path, nargs=1,
+                        required=True, help='Path to output CSV file.')
 
     args = parser.parse_args()
     set_log_verbosity(args.verbose)
     quieten_chatty_imports()
     logger.info("Starting Comparer operation.")
 
-    credentials, project_id = google.auth.default()
-    storage_client = storage.Client(credentials=credentials)
+    json_1, json_2 = [json_from_path_string(p[0]) for p in [args.digest1, args.digest2]]
+    name_1, name_2 = args.name1, args.name2
+    check_digest_versions()
 
-    paths = args.digest_1 + args.digest_2 + args.digests_more
-    workflow_ids_and_jsons = read_digester_jsons(paths, storage_client)
-    comparison_result_df = compare_jsons(workflow_ids_and_jsons)
-    result_csv_string = comparison_result_df.to_csv()
-
+    comparison_data = compare_jsons()
     out = ComparisonPath.create(args.output_path[0])
-    out.write_text(result_csv_string)
+    out.write_text(csv_string_from_data(comparison_data))
 
     logger.info('Comparer operation completed successfully.')

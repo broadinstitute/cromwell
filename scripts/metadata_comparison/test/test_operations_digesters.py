89b4e7c74d63afa95d8a31458bd14bea442cbf49
Miguel Covarrubias <mcovarr@broadinstitute.org>
2020-06-25 17:11:27 -0400

    dedup

--- scripts/metadata_comparison/test/test_operations_digesters.py
+++ scripts/metadata_comparison/test/test_operations_digesters.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-import json
-import os
-import unittest
-from typing import AnyStr
-from metadata_comparison.lib.comparison_paths import ComparisonPath
-from pathlib import Path
-import logging
-from metadata_comparison.lib.logging import quieten_chatty_imports, set_log_verbosity
-from metadata_comparison.lib.operations_digesters import OperationDigester
 import google.auth
 from google.cloud import storage
+import json
+import logging
+from metadata_comparison.lib.comparison_paths import ComparisonPath
+from metadata_comparison.lib.logging import quieten_chatty_imports, set_log_verbosity
+from metadata_comparison.lib.operations_digesters import OperationDigester
+import os
+import unittest
+from test.lib.test_digester_helper import download_metadata_from_gcs_if_needed,\
+    gcs_parent, subdir_for_papi_version, VERSION_PAPI_V1, VERSION_PAPI_V2
 
 
 class OperationsDigesterTestMethods(unittest.TestCase):
@@ -61,48 +61,6 @@ class OperationsDigesterTestMethods(unittest.TestCase):
                             self.assertEqual(method_to_call(), value, f'{key} was not {value}')
 
 
-def read_resource(filename: AnyStr) -> AnyStr:
-    path = Path('test/resources') / filename
-    with open(path, 'r') as file:
-        data = file.read()
-    return data
-
-
-def download_metadata_from_gcs(bucket: storage.Bucket, local_sample_path: ComparisonPath) -> None:
-    (local_sample_path / "operations").mkdir_p()
-
-    prefix = str(local_sample_path)
-    blobs = bucket.list_blobs(prefix=prefix)
-    for blob in blobs:
-        if not blob.name.endswith('/digest.json'):
-            logging.info(f'Downloading blob: {blob.name}')
-            blob.download_to_filename(blob.name)
-
-
-def subdir_for_papi_version(papi_version: AnyStr) -> AnyStr:
-    if papi_version == VERSION_PAPI_V1:
-        path_element = 'PAPIv1'
-    elif papi_version == VERSION_PAPI_V2:
-        path_element = 'PAPIv2_alpha1/v1_style_machine_types'
-    else:
-        raise ValueError(f'Unrecognized PAPI version {papi_version}')
-    return f'exome_germline_single_sample_v1.3/{path_element}'
-
-
-def download_metadata_from_gcs_if_needed(
-        sample_name: AnyStr, local_parent: ComparisonPath, bucket: storage.Bucket) -> None:
-    """
-    Copy down workflow and PAPI operations metadata from GCS if needed to test Local.
-    """
-    local_sample_path = local_parent / sample_name
-    if not local_sample_path.exists():
-        logging.info(f"Local sample directory '{local_sample_path}' does not exist, downloading from GCS.")
-        download_metadata_from_gcs(bucket, local_sample_path)
-
-
-VERSION_PAPI_V1 = 'PAPIv1'
-VERSION_PAPI_V2 = 'PAPIv2_alpha1'
-
 EXPECTATIONS = {
     'dev_C1963.CHMI_CHMI3_Nex1': {
         'PAPIv1': {
@@ -133,16 +91,6 @@ EXPECTATIONS = {
     # 'dev_D5327.NA12892',
     # 'dev_RP-1535.NA17-308'
 }
-
-
-def gcs_parent(subdir: AnyStr, gcs_comparison_path_by_subdir: dict) -> ComparisonPath:
-    """
-    GcsComparisonPaths are somewhat expensive to create so cache them.
-    """
-    if subdir not in gcs_comparison_path_by_subdir:
-        path = ComparisonPath.create(f'gs://papi-performance-analysis/{subdir}')
-        gcs_comparison_path_by_subdir[subdir] = path
-    return gcs_comparison_path_by_subdir[subdir]
 
 
 if __name__ == '__main__':

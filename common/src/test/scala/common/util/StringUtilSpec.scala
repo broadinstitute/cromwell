a6593d8a32778ba429fbf64abd8b7322b6c92065
Chris Llanwarne <cjllanwarne@gmail.com>
2020-07-27 14:05:34 -0400

    Prove that pretty elided string generation is lazy

--- common/src/test/scala/common/util/StringUtilSpec.scala
+++ common/src/test/scala/common/util/StringUtilSpec.scala
@@ -2,16 +2,19 @@ package common.util
 
 import org.scalatest.{FlatSpec, Matchers}
 import common.util.StringUtil.EnhancedToStringable
-import common.util.StringUtilSpec.Foo
+import common.util.StringUtilSpec._
 
 class StringUtilSpec extends FlatSpec with Matchers {
 
   it should "correctly truncate a case class with a really long list" in {
+    val fooOfBars = Foo("long long list", 0.until(100).toList.map(i => new Bar(i)))
 
-    def longLine(i: Int) = s"blah$i" * 100
-    val longList = Foo("long long list", (0.until(100)).map(i => longLine(i)).toList)
+    // If we try the naive toString, we get an exception when we toString the later elements:
+    assertThrows[BarException](fooOfBars.toString)
 
-    longList.toPrettyElidedString(1000) should be("""Foo(
+    // With the elided string, we stop processing early and are able to produce a nice, short string without ever
+    // touching the later elements:
+    fooOfBars.toPrettyElidedString(1000) should be("""Foo(
                                                     |  "long long list",
                                                     |  List(
                                                     |    "blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0blah0",
@@ -21,5 +24,18 @@ class StringUtilSpec extends FlatSpec with Matchers {
 }
 
 object StringUtilSpec {
-  final case class Foo(bar: String, list: List[String])
+  final case class Foo(bar: String, list: List[Bar])
+
+  final class Bar(index: Int) {
+    private def longLine(i: Int) = '"' +  s"blah$i" * 100 + '"'
+    override def toString: String = if (index < 2) {
+      longLine(index)
+    } else {
+      throw BarException(s"Don't look at index $index!")
+    }
+  }
+
+  final case class BarException(msg: String) extends Exception {
+    override def getMessage: String = msg
+  }
 }

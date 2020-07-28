f778ecc914e5d4fb715868ca9d35c9d7bfe8f691
Rebecca Asch <rasch@broadinstitute.org>
2020-07-28 12:08:06 -0400

    additional tests for file hash extraction from Martha response

--- cloud-nio/cloud-nio-impl-drs/src/test/scala/cloud/nio/impl/drs/DrsCloudNioRegularFileAttributesSpec.scala
+++ cloud-nio/cloud-nio-impl-drs/src/test/scala/cloud/nio/impl/drs/DrsCloudNioRegularFileAttributesSpec.scala
@@ -8,20 +8,35 @@ import scala.concurrent.duration.Duration
 
 class DrsCloudNioRegularFileAttributesSpec extends FlatSpecLike with Matchers {
 
-  val fakeDrsConfig = DrsConfig("http://martha-url", "{}")
-  val fakeCredentials = NoCredentials.getInstance
-  val httpClientBuilder = HttpClientBuilder.create()
-  val accessTokenAcceptableTTL: Duration = Duration.Inf
+  private val fakeDrsConfig = DrsConfig("http://martha-url", "{}")
+  private val fakeCredentials = NoCredentials.getInstance
+  private val httpClientBuilder = HttpClientBuilder.create()
+  private val accessTokenAcceptableTTL: Duration = Duration.Inf
 
 //  private def drsReadInterpreter(marthaResponse: MarthaResponse): IO[ReadableByteChannel] =
 //    throw new UnsupportedOperationException("Currently DrsResolverSpec doesn't need to use drs read interpreter.")
 
-  val mockDrsPathResolver = new MockEngineDrsPathResolver(fakeDrsConfig, httpClientBuilder, fakeCredentials, accessTokenAcceptableTTL)
-  val mockDrsFileAttributes = new MockDrsCloudNioRegularFileAttributes(MockDrsPaths.drsPathResolvingGcsPath, mockDrsPathResolver)
+  private val mockDrsPathResolver = new MockEngineDrsPathResolver(fakeDrsConfig, httpClientBuilder, fakeCredentials, accessTokenAcceptableTTL)
+  private val mockDrsFileAttributesWithAllHashes = new MockDrsCloudNioRegularFileAttributes(MockDrsPaths.drsPathResolvingGcsPathWithAllHashes, mockDrsPathResolver)
+  private val mockDrsFileAttributesWithMd5Hash = new MockDrsCloudNioRegularFileAttributes(MockDrsPaths.drsPathResolvingGcsPathWithMd5Hash, mockDrsPathResolver)
+  private val mockDrsFileAttributesWithShaHash = new MockDrsCloudNioRegularFileAttributes(MockDrsPaths.drsPathResolvingGcsPathWithShaHash, mockDrsPathResolver)
+  private val mockDrsFileAttributesWithNoPreferredHash = new MockDrsCloudNioRegularFileAttributes(MockDrsPaths.drsPathResolvingGcsPathWithNoPreferredHash, mockDrsPathResolver)
 
   behavior of "fileHash()"
 
-  it should "return proper hash from `hashes` in Martha response" in {
-    mockDrsFileAttributes.fileHash shouldBe Option("8a366443")
+  it should "return crc32c hash from `hashes` in Martha response when there is a crc32c" in {
+    mockDrsFileAttributesWithAllHashes.fileHash shouldBe Option("8a366443")
+  }
+
+  it should "return md5 hash from `hashes` in Martha response when there is no crc32c" in {
+    mockDrsFileAttributesWithMd5Hash.fileHash shouldBe Option("336ea55913bc261b72875bd259753046")
+  }
+
+  it should "return sha256 hash from `hashes` in Martha response when there is only a sha256" in {
+    mockDrsFileAttributesWithShaHash.fileHash shouldBe Option("f76877f8e86ec3932fd2ae04239fbabb8c90199dab0019ae55fa42b31c314c44")
+  }
+
+  it should "return first (alphabetized by type) hash from `hashes` in Martha response when there are no preferred hash types" in {
+    mockDrsFileAttributesWithNoPreferredHash.fileHash shouldBe Option("xrd")
   }
 }
--- cloud-nio/cloud-nio-impl-drs/src/test/scala/cloud/nio/impl/drs/MockEngineDrsPathResolver.scala
+++ cloud-nio/cloud-nio-impl-drs/src/test/scala/cloud/nio/impl/drs/MockEngineDrsPathResolver.scala
@@ -17,20 +17,43 @@ class MockEngineDrsPathResolver(drsConfig: DrsConfig,
 
   private lazy val mockMarthaUri = drsConfig.marthaUri
 
-  val marthaObjWithGcsPath: IO[MarthaResponse] = createMarthaResponse(Option(s"gs://${MockDrsPaths.gcsRelativePath}"))
+  val completeHashesObj = Map(
+    "betty" -> "abc123",
+    "charles" -> "456",
+    "alfred" -> "xrd",
+    "sha256" -> "f76877f8e86ec3932fd2ae04239fbabb8c90199dab0019ae55fa42b31c314c44",
+    "crc32c" -> "8a366443",
+    "md5" -> "336ea55913bc261b72875bd259753046",
+  )
 
-  val marthaObjWithNoGcsPath: IO[MarthaResponse] = createMarthaResponse(None)
+  val missingCRCHashesObj = Map(
+    "alfred" -> "xrd",
+    "sha256" -> "f76877f8e86ec3932fd2ae04239fbabb8c90199dab0019ae55fa42b31c314c44",
+    "betty" -> "abc123",
+    "md5" -> "336ea55913bc261b72875bd259753046",
+    "charles" -> "456",
+  )
 
-  private def createMarthaResponse(url: Option[String]): IO[MarthaResponse] =  {
-    val hashesObj = Map(
-      "sha256" -> "f76877f8e86ec3932fd2ae04239fbabb8c90199dab0019ae55fa42b31c314c44",
-      "crc32c" -> "8a366443",
-      "md5" -> "336ea55913bc261b72875bd259753046",
-//      "bec" -> "abc123",
-//      "saloni" -> "456",
-//      "alfred" -> "xrd",
-    )
+  val onlySHAHashesObj = Map(
+    "betty" -> "abc123",
+    "charles" -> "456",
+    "alfred" -> "xrd",
+    "sha256" -> "f76877f8e86ec3932fd2ae04239fbabb8c90199dab0019ae55fa42b31c314c44",
+  )
 
+  val noPreferredHashesObj = Map(
+    "alfred" -> "xrd",
+    "betty" -> "abc123",
+    "charles" -> "456",
+  )
+
+  val marthaObjWithGcsPathAndMissingCRCHash: IO[MarthaResponse] = createMarthaResponse(Option(s"gs://${MockDrsPaths.gcsRelativePath}"), missingCRCHashesObj)
+  val marthaObjWithGcsPathAndOnlySHAHash: IO[MarthaResponse] = createMarthaResponse(Option(s"gs://${MockDrsPaths.gcsRelativePath}"), onlySHAHashesObj)
+  val marthaObjWithGcsPathAndOnlyNoPreferredHash: IO[MarthaResponse] = createMarthaResponse(Option(s"gs://${MockDrsPaths.gcsRelativePath}"), noPreferredHashesObj)
+  val marthaObjWithGcsPathAndAllHashes: IO[MarthaResponse] = createMarthaResponse(Option(s"gs://${MockDrsPaths.gcsRelativePath}"), completeHashesObj)
+  val marthaObjWithNoGcsPathAndAllHashes: IO[MarthaResponse] = createMarthaResponse(None, completeHashesObj)
+
+  private def createMarthaResponse(url: Option[String], hashesObj: Map[String, String]): IO[MarthaResponse] =  {
     val drsResponse = MarthaResponse(
       size = Option(156018255),
       timeUpdated = Option("2020-04-27T15:56:09.696Z"),
@@ -46,8 +69,11 @@ class MockEngineDrsPathResolver(drsConfig: DrsConfig,
 
   override def resolveDrsThroughMartha(drsPath: String): IO[MarthaResponse] = {
     drsPath match {
-      case MockDrsPaths.drsPathResolvingGcsPath => marthaObjWithGcsPath
-      case MockDrsPaths.drsPathResolvingToNoGcsPath => marthaObjWithNoGcsPath
+      case MockDrsPaths.drsPathResolvingGcsPathWithAllHashes => marthaObjWithGcsPathAndAllHashes
+      case MockDrsPaths.drsPathResolvingGcsPathWithMd5Hash => marthaObjWithGcsPathAndMissingCRCHash
+      case MockDrsPaths.drsPathResolvingGcsPathWithShaHash => marthaObjWithGcsPathAndOnlySHAHash
+      case MockDrsPaths.drsPathResolvingToNoGcsPath => marthaObjWithNoGcsPathAndAllHashes
+      case MockDrsPaths.drsPathResolvingGcsPathWithNoPreferredHash => marthaObjWithGcsPathAndOnlyNoPreferredHash
       case MockDrsPaths.drsPathNotExistingInMartha => throw new RuntimeException(s"Unexpected response resolving ${MockDrsPaths.drsPathNotExistingInMartha} through Martha url $mockMarthaUri. Error: 404 Not Found.")
       case _ => throw new RuntimeException(s"Unexpected response resolving $drsPath through Martha url $mockMarthaUri. Error: 500 Internal Server Error.")
     }
@@ -57,21 +83,20 @@ class MockEngineDrsPathResolver(drsConfig: DrsConfig,
 
 object MockDrsPaths {
   private val drsPathPrefix = "drs://drs-host/"
-
   val gcsRelativePath = "my-bucket/dd3c716a-852f-4d74-9073-9920e835ec8a/f3b148ac-1802-4acc-a0b9-610ea266fb61"
-
-  val drsPathResolvingGcsPath = s"$drsPathPrefix/4d427aa3-5640-4f00-81ae-c33443f84acf"
-
+  val drsPathResolvingGcsPathWithAllHashes = s"$drsPathPrefix/4d427aa3-5640-4f00-81ae-c33443f84acf"
+  val drsPathResolvingGcsPathWithMd5Hash = s"$drsPathPrefix/4d427aa3-5640-4f00-81ae-aecfe7697"
+  val drsPathResolvingGcsPathWithShaHash = s"$drsPathPrefix/4d427aa3-5640-4f00-81ae-fabefa2039"
+  val drsPathResolvingGcsPathWithNoPreferredHash= s"$drsPathPrefix/4d427aa3-5640-4f00-81ae-bbfea33952"
   val drsPathResolvingToNoGcsPath = s"$drsPathPrefix/226686cf-22c9-4472-9f79-7a0b0044f253"
-
   val drsPathNotExistingInMartha = s"$drsPathPrefix/5e21b8c3-8eda-48d5-9a04-2b32e1571765"
 }
 
 
-class MockDrsCloudNioFileSystemProvider(config: Config,
-                                        credentials: OAuth2Credentials,
-                                        httpClientBuilder: HttpClientBuilder,
-                                        drsReadInterpreter: MarthaResponse => IO[ReadableByteChannel]) extends DrsCloudNioFileSystemProvider(config, credentials, httpClientBuilder, drsReadInterpreter) {
+abstract class MockDrsCloudNioFileSystemProvider(config: Config,
+                                                 credentials: OAuth2Credentials,
+                                                 httpClientBuilder: HttpClientBuilder,
+                                                 drsReadInterpreter: MarthaResponse => IO[ReadableByteChannel]) extends DrsCloudNioFileSystemProvider(config, credentials, httpClientBuilder, drsReadInterpreter) {
 
   private lazy val fakeDrsConfig = DrsConfig("http://martha-url", "{}")
 
@@ -94,3 +119,8 @@ class MockDrsCloudNioRegularFileAttributes(drsPath: String,
                                            drsPathResolver: EngineDrsPathResolver) extends DrsCloudNioRegularFileAttributes(drsPath, drsPathResolver) {
 
 }
+
+class MockDrsResolver (drsConfig: DrsConfig,
+                       httpClientBuilder: HttpClientBuilder,
+                       accessTokenAcceptableTTL: Duration,
+                       authCredentials: OAuth2Credentials) extends EngineDrsPathResolver (drsConfig, httpClientBuilder, accessTokenAcceptableTTL, authCredentials)

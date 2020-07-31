757d542b241114eaf531cce0eb97149bba8f8241
Scala Steward <me@scala-steward.org>
2020-08-01 00:16:38 +0200

    Update slick-cats to 0.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -88,7 +88,7 @@ object Dependencies {
   private val shapelessV = "2.3.3"
   private val simulacrumV = "0.15.0"
   private val slf4jV = "1.7.25"
-  private val slickCatsV = "0.9.0"
+  private val slickCatsV = "0.9.1"
   private val testContainersScalaV = "0.35.2"
 
   /* If you're about to update our Slick version:

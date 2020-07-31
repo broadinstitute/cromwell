88bf24c1a2a31d8cdecbb75918156323f2fdead0
Scala Steward <me@scala-steward.org>
2020-08-01 00:20:14 +0200

    Update circe-optics to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   private val betterFilesV = "3.9.1"
   private val catsEffectV = "2.0.0"
   private val catsV = "2.0.0"
-  private val circeOpticsV = "0.12.0"
+  private val circeOpticsV = "0.13.0"
   private val circeV = "0.12.3"
   private val circeYamlV = "0.11.0-M1"
   private val commonsCodecV = "1.11"

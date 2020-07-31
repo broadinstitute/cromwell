caaafa660a4ed197fb73060579377a51de0c447d
Scala Steward <me@scala-steward.org>
2020-08-01 00:24:34 +0200

    Update workbench-util to 0.6-27810079-SNAP

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -110,7 +110,7 @@ object Dependencies {
   private val typesafeConfigV = "1.3.3"
   private val workbenchGoogleV = "0.15-2fc79a3"
   private val workbenchModelV = "0.10-6800f3a"
-  private val workbenchUtilV = "0.3-f3ce961"
+  private val workbenchUtilV = "0.6-27810079-SNAP"
 
   private val slf4jFacadeDependencies = List(
     "org.slf4j" % "slf4j-api" % slf4jV,

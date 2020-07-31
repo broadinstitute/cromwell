6c291746e6fde5cb94888f39fee5a5812b3b9e35
Scala Steward <me@scala-steward.org>
2020-08-01 00:21:21 +0200

    Update grpc-core to 1.30.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   private val googleOauth2V = "0.21.0"
   private val googleOauthClientV = "1.31.0"
   private val googleCloudResourceManagerV = "0.87.0-alpha"
-  private val grpcV = "1.30.1"
+  private val grpcV = "1.30.2"
   private val guavaV = "27.0.1-jre"
   private val heterodonV = "1.0.0-beta3"
   private val hsqldbV = "2.4.1"

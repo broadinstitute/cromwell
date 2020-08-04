06d012914fb7aff2ccef6e405696ebd7120ef63f
Scala Steward <me@scala-steward.org>
2020-08-04 22:09:29 +0200

    Update ammonite-ops to 1.6.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   private val aliyunCoreV = "4.3.2"
   private val aliyunCrV = "3.0.0"
   private val aliyunOssV = "3.4.2"
-  private val ammoniteOpsV = "1.6.3"
+  private val ammoniteOpsV = "1.6.9"
   private val apacheCommonNetV = "3.6"
   private val apacheHttpClientV = "4.5.7"
   private val awsSdkV = "2.10.71"

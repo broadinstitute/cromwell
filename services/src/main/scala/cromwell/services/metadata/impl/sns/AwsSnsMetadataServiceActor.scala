6e1a06da6f7e522bcc1d89946616e1a4e97656a8
Mark Schreiber <markjschreiber@gmail.com>
2020-08-07 15:55:26 -0400

    working implementation
    Signed-off-by: markjschreiber <markjschreiber@gmail.com>

--- services/src/main/scala/cromwell/services/metadata/impl/sns/AwsSnsMetadataServiceActor.scala
+++ services/src/main/scala/cromwell/services/metadata/impl/sns/AwsSnsMetadataServiceActor.scala
@@ -1,3 +1,34 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ *  BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ *  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package cromwell.services.metadata.impl.sns
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
@@ -16,15 +47,19 @@ import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.util.{Failure, Success}
 
 
-
+/**
+  * An actor that publishes metadata events to AWS SNS
+  * @param serviceConfig the source of service config information
+  * @param globalConfig the source of global config information
+  * @param serviceRegistryActor the actor for registering services
+  */
 class AwsSnsMetadataServiceActor(serviceConfig: Config, globalConfig: Config, serviceRegistryActor: ActorRef) extends Actor with ActorLogging {
   implicit val ec: ExecutionContextExecutor = context.dispatcher
 
   //setup sns client
-  val topicArn = "cromwell-metadata" //todo get this from serviceConfig
+  val topicArn = "arn:aws:sns:us-east-1:251159210224:cromwell-metadata" //todo get this from serviceConfig
 
-  //todo, should this be service config?
-  val awsConfig: AwsConfiguration = AwsConfiguration(serviceConfig)
+  val awsConfig: AwsConfiguration = AwsConfiguration(globalConfig)
   val credentialsProviderChain: AwsCredentialsProviderChain =
     AwsCredentialsProviderChain.of(awsConfig.authsByName.values.map(_.provider()).toSeq :_*)
 
@@ -37,11 +72,13 @@ class AwsSnsMetadataServiceActor(serviceConfig: Config, globalConfig: Config, se
     import AwsSnsMetadataServiceActor.EnhancedMetadataEvents
 
     val eventsJson = events.toJson
-    log.debug("Publishing to " + topicArn + ": " + eventsJson)
+    //if there are no events then don't publish anything
+    if( eventsJson.length < 1) { return Future(())}
+    log.info("Publishing to " + topicArn + ": " + eventsJson)
 
     def publish(): Unit = {
       snsClient.publish(PublishRequest.builder()
-        .message(eventsJson.mkString("\n"))
+        .message("[" + eventsJson.mkString(",") + "]")
         .topicArn(topicArn)
         .subject("cromwell-metadata-event")
         .build())

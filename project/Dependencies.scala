485ad6c0069a2fd6f2754ef93f40e3ea173ced6b
Scala Steward <me@scala-steward.org>
2020-08-12 00:20:00 +0200

    Scala-Steward: Update google-cloud-bigquery from 1.116.7 to 1.116.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   private val fs2V = "2.0.1"
   private val fs2VStatsDProxy = "1.0.5"
   private val googleApiClientV = "1.30.10"
-  private val googleCloudBigQueryV = "1.116.7"
+  private val googleCloudBigQueryV = "1.116.10"
   private val googleCloudKmsV = "v1-rev20200609-1.30.10"
   private val googleCloudMonitoringV = "1.100.1"
   private val googleCloudNioV = "0.61.0-alpha" // scala-steward:off

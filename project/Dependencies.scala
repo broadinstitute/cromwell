fcb88b6f31cebfc2051d5e3162f6e4a4d8514b75
Scala Steward <me@scala-steward.org>
2020-08-18 00:10:02 +0200

    Scala-Steward: Update scalactic from 3.0.8 to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -94,7 +94,7 @@ object Dependencies {
   private val scalaLoggingV = "3.9.2"
   private val scalaPoolV = "0.4.3"
   private val scalacheckV = "1.14.0"
-  private val scalacticV = "3.0.8"
+  private val scalacticV = "3.2.1"
   private val scalameterV = "0.19"
   private val scalamockV = "4.4.0"
   private val scalatestV = "3.0.5"

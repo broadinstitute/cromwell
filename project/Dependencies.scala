78a83bd8ac15cbfced3d6e3ccd429b1f652f2df8
Scala Steward <me@scala-steward.org>
2020-08-18 12:13:22 +0200

    Scala-Steward: Update aliyun-java-sdk-core from 4.3.9 to 4.5.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val akkaHttpV = "10.1.12"
   private val akkaV = "2.5.31"
   private val aliyunBcsV = "6.2.4"
-  private val aliyunCoreV = "4.3.9"
+  private val aliyunCoreV = "4.5.7"
   private val aliyunCrV = "3.0.1"
   private val aliyunOssV = "3.4.2"
   private val ammoniteOpsV = "1.6.9"

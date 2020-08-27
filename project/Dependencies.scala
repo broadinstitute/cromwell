498ce24c1305bfd843e2f60776212811771a658d
Scala Steward <me@scala-steward.org>
2020-08-27 22:09:05 +0200

    Scala-Steward: Update awssdk:batch, ... from 2.14.3 to 2.14.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   private val ammoniteOpsV = "2.2.0"
   private val apacheCommonNetV = "3.7"
   private val apacheHttpClientV = "4.5.12"
-  private val awsSdkV = "2.14.3"
+  private val awsSdkV = "2.14.6"
   private val betterFilesV = "3.9.1"
   private val catsEffectV = "2.1.4"
   private val catsV = "2.1.1"

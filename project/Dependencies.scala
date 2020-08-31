a2773d6598adc7ca087b1e4b4241eb2c0838da4b
Scala Steward <me@scala-steward.org>
2020-08-31 12:10:34 +0200

    Scala-Steward: Update aliyun-java-sdk-core from 4.5.7 to 4.5.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val akkaHttpV = "10.1.12"
   val akkaV = "2.6.8" // When updating: Do we still need the merge "fix" from https://github.com/broadinstitute/cromwell/pull/5776 (not private because used in Merging.scala)
   private val aliyunBcsV = "6.2.4"
-  private val aliyunCoreV = "4.5.7"
+  private val aliyunCoreV = "4.5.8"
   private val aliyunCrV = "4.1.1"
   private val aliyunOssV = "3.10.2"
   private val ammoniteOpsV = "2.2.0"

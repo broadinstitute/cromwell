029dc272c60967828f23427f1039871054521230
Adam Nichols <aednichols@gmail.com>
2020-09-01 15:52:10 -0400

    Just fix it as simply as possible
    We don't have time to figure out all timezone problems. At least we added test coverage.

--- cloud-nio/cloud-nio-impl-drs/src/main/scala/cloud/nio/impl/drs/DrsPathResolver.scala
+++ cloud-nio/cloud-nio-impl-drs/src/main/scala/cloud/nio/impl/drs/DrsPathResolver.scala
@@ -1,7 +1,5 @@
 package cloud.nio.impl.drs
 
-import java.time.{LocalDateTime, OffsetDateTime}
-
 import cats.effect.{IO, Resource}
 import cats.instances.option._
 import cats.instances.string._
@@ -120,15 +118,7 @@ object MarthaResponseSupport {
   def convertMarthaResponseV2ToV3(response: MarthaV2Response): MarthaResponse = {
     val dataObject = response.dos.data_object
     val size = dataObject.size
-
-    val timeUpdated: Option[String] = dataObject.updated map { time: String =>
-      if (time.endsWith("Z")) {
-        OffsetDateTime.parse(time).toString
-      } else {
-        LocalDateTime.parse(time).toString
-      }
-    }
-
+    val timeUpdated = dataObject.updated
     val hashesMap = dataObject.checksums.map(convertChecksumsToHashesMap)
     val gcsUrl = dataObject.urls.find(_.url.startsWith(GcsScheme)).map(_.url)
     val (bucketName, fileName) = getGcsBucketAndName(gcsUrl)

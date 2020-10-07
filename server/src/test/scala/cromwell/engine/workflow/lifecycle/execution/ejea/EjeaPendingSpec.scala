68f5c16b903b2970f20d85cea5f77803a0cca256
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-10-07 17:29:03 -0400

    Allow overriding system-level `hog-factor` setting in individual backend configurations [BA-6584] (#5911)

--- server/src/test/scala/cromwell/engine/workflow/lifecycle/execution/ejea/EjeaPendingSpec.scala
+++ server/src/test/scala/cromwell/engine/workflow/lifecycle/execution/ejea/EjeaPendingSpec.scala
@@ -1,5 +1,8 @@
 package cromwell.engine.workflow.lifecycle.execution.ejea
 
+import com.typesafe.config.ConfigFactory
+import cromwell.backend.BackendConfigurationDescriptor
+import cromwell.backend.TestConfig.globalConfig
 import cromwell.engine.workflow.lifecycle.execution.job.EngineJobExecutionActor._
 import cromwell.engine.workflow.tokens.JobExecutionTokenDispenserActor.JobExecutionTokenRequest
 import org.scalatest.concurrent.Eventually
@@ -20,6 +23,26 @@ class EjeaPendingSpec extends EngineJobExecutionActorSpec with CanValidateJobSto
         helper.jobPreparationProbe.msgAvailable should be(false)
         helper.jobStoreProbe.msgAvailable should be(false)
         ejea.stateName should be(RequestingExecutionToken)
+      }
+
+      s"should use system-level hog-factor setting in token request, if hog-factor is not overridden in backend configuration (with restarting=$restarting)" in {
+        ejea = helper.buildEJEA(restarting = restarting)
+        ejea ! Execute
+
+        val tokenRequest = helper.jobTokenDispenserProbe.expectMsgClass(max = awaitTimeout, classOf[JobExecutionTokenRequest])
+        // 1 is the default hog-factor value defined in reference.conf
+        tokenRequest.jobExecutionTokenType.hogFactor should be(1)
+      }
+
+      s"should use hog-factor defined in backend configuration in token request (with restarting=$restarting)" in {
+        val expectedHogFactorValue = 123
+        val overriddenHogFactorAttributeString = s"hog-factor: $expectedHogFactorValue"
+        val backendWithOverriddenHogFactorConfigDescriptor = BackendConfigurationDescriptor(backendConfig = ConfigFactory.parseString(overriddenHogFactorAttributeString), globalConfig)
+        ejea = helper.buildEJEA(restarting = restarting, backendConfigurationDescriptor = backendWithOverriddenHogFactorConfigDescriptor)
+        ejea ! Execute
+
+        val tokenRequest = helper.jobTokenDispenserProbe.expectMsgClass(max = awaitTimeout, classOf[JobExecutionTokenRequest])
+        tokenRequest.jobExecutionTokenType.hogFactor should be(expectedHogFactorValue)
       }
     }
   }

8de456ea6eb85e079df26aad8054e5a49e4506d0
Scala Steward <me@scala-steward.org>
2020-10-17 02:31:39 +0200

    Scala-Steward: Update guava from 29.0-jre to 30.0-jre

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -52,7 +52,7 @@ object Dependencies {
   private val googleOauthClientV = "1.31.0"
   private val googleCloudResourceManagerV = "0.87.0-alpha"
   private val grpcV = "1.32.2"
-  private val guavaV = "29.0-jre"
+  private val guavaV = "30.0-jre"
   private val heterodonV = "1.0.0-beta3"
   private val hsqldbV = "2.5.1"
   private val http4sVersion = "0.21.7"

55eb24b6320eb69f1a162d2d7c42aad0c48c6abb
Chris Llanwarne <cjllanwarne@gmail.com>
2020-10-19 18:42:49 -0400

    Being a dummy isn't always dumb

--- backend/src/main/scala/cromwell/backend/dummy/DummyAsyncExecutionActor.scala
+++ backend/src/main/scala/cromwell/backend/dummy/DummyAsyncExecutionActor.scala
@@ -1,11 +1,22 @@
 package cromwell.backend.dummy
 
+import java.time.OffsetDateTime
+import java.util.UUID
+
+import cats.data.Validated.{Invalid, Valid}
+import cats.implicits._
+import common.exception.AggregatedMessageException
+import common.validation.ErrorOr.ErrorOr
 import cromwell.backend.BackendJobLifecycleActor
-import cromwell.backend.async.{ExecutionHandle, SuccessfulExecutionHandle}
-import cromwell.backend.standard.{StandardAsyncExecutionActor, StandardAsyncExecutionActorParams}
+import cromwell.backend.async.{ExecutionHandle, FailedNonRetryableExecutionHandle, PendingExecutionHandle, SuccessfulExecutionHandle}
+import cromwell.backend.standard.{StandardAsyncExecutionActor, StandardAsyncExecutionActorParams, StandardAsyncJob}
 import cromwell.core.CallOutputs
 import cromwell.core.retry.SimpleExponentialBackoff
+import wom.expression.NoIoFunctionSet
+import wom.graph.GraphNodePort.{ExpressionBasedOutputPort, OutputPort}
+import wom.values.WomValue
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 class DummyAsyncExecutionActor(override val standardParams: StandardAsyncExecutionActorParams)
@@ -36,11 +47,68 @@ class DummyAsyncExecutionActor(override val standardParams: StandardAsyncExecuti
 
   override def executeOrRecoverBackOff: SimpleExponentialBackoff = SimpleExponentialBackoff(initialInterval = 1.second, maxInterval = 300.seconds, multiplier = 1.1)
 
-  override def execute(): ExecutionHandle = SuccessfulExecutionHandle(
-    outputs = CallOutputs(Map.empty),
-    returnCode = 0,
-    jobDetritusFiles = Map.empty,
-    executionEvents = Seq.empty,
-    resultsClonedFrom = None
-  )
+
+  var finishTime: Option[OffsetDateTime] = None
+
+  override def executeAsync(): Future[ExecutionHandle] = {
+    finishTime = Option(OffsetDateTime.now().plusMinutes(10))
+    Future.successful(
+      PendingExecutionHandle[StandardAsyncJob, StandardAsyncRunInfo, StandardAsyncRunState](
+        jobDescriptor = jobDescriptor,
+        pendingJob = StandardAsyncJob(UUID.randomUUID().toString),
+        runInfo = Option("pending"),
+        previousState = None
+      )
+    )
+  }
+
+  override def pollStatusAsync(handle: StandardAsyncPendingExecutionHandle): Future[String] = {
+    finishTime match {
+      case Some(ft) if (ft.isBefore(OffsetDateTime.now)) => Future.successful("done")
+      case Some(_) => Future.successful("running")
+      case None => Future.failed(new Exception("Dummy backend polling for status before finishTime is established(!!?)"))
+    }
+
+  }
+
+  override def handlePollSuccess(oldHandle: StandardAsyncPendingExecutionHandle, state: String): Future[ExecutionHandle] = {
+
+    if (state == "done") {
+      val outputsValidation: ErrorOr[Map[OutputPort, WomValue]] = jobDescriptor.taskCall.outputPorts.toList.traverse {
+        case expressionBasedOutputPort: ExpressionBasedOutputPort =>
+          expressionBasedOutputPort.expression.evaluateValue(Map.empty, NoIoFunctionSet).map(expressionBasedOutputPort -> _)
+        case other => s"Unknown output port type for Dummy backend output evaluator: ${other.getClass.getSimpleName}".invalidNel
+      }.map(_.toMap)
+
+      outputsValidation match {
+        case Valid(outputs) =>
+          Future.successful(SuccessfulExecutionHandle(
+            outputs = CallOutputs(outputs.toMap),
+            returnCode = 0,
+            jobDetritusFiles = Map.empty,
+            executionEvents = Seq.empty,
+            resultsClonedFrom = None
+          ))
+        case Invalid(errors) =>
+          Future.successful(FailedNonRetryableExecutionHandle(
+            throwable = AggregatedMessageException("Evaluate outputs from dummy job", errors.toList),
+            returnCode = None,
+            kvPairsToSave = None
+          ))
+      }
+    }
+    else if (state == "running") {
+      Future.successful(
+        PendingExecutionHandle[StandardAsyncJob, StandardAsyncRunInfo, StandardAsyncRunState](
+          jobDescriptor = jobDescriptor,
+          pendingJob = StandardAsyncJob(UUID.randomUUID().toString),
+          runInfo = Option("pending"),
+          previousState = Option(state)
+        )
+      )
+    }
+    else {
+      Future.failed(new Exception(s"Unexpected Dummy state in handlePollSuccess: $state"))
+    }
+  }
 }
--- backend/src/main/scala/cromwell/backend/dummy/DummyLifecycleActorFactory.scala
+++ backend/src/main/scala/cromwell/backend/dummy/DummyLifecycleActorFactory.scala
@@ -1,6 +1,7 @@
 package cromwell.backend.dummy
 
 import cromwell.backend.BackendConfigurationDescriptor
+import cromwell.backend.standard.callcaching.{StandardCacheHitCopyingActor, StandardFileHashingActor}
 import cromwell.backend.standard.{StandardAsyncExecutionActor, StandardLifecycleActorFactory}
 
 class DummyLifecycleActorFactory(override val name: String, override val configurationDescriptor: BackendConfigurationDescriptor) extends StandardLifecycleActorFactory {
@@ -18,5 +19,11 @@ class DummyLifecycleActorFactory(override val name: String, override val configu
     * @return the asynchronous executor class.
     */
   override def asyncExecutionActorClass: Class[_ <: StandardAsyncExecutionActor] = classOf[DummyAsyncExecutionActor]
+
+  // Don't cache-hit copy
+  override lazy val cacheHitCopyingActorClassOption: Option[Class[_ <: StandardCacheHitCopyingActor]] = None
+
+  // Don't hash files
+  override lazy val fileHashingActorClassOption: Option[Class[_ <: StandardFileHashingActor]] = None
 
 }

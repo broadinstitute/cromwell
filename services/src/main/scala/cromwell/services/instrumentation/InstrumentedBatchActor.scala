6a7fdbeefdf47a9dca26cb830947f0d810effc6d
Chris Llanwarne <cjllanwarne@gmail.com>
2020-10-28 11:40:22 -0400

    Don't divide

--- services/src/main/scala/cromwell/services/instrumentation/InstrumentedBatchActor.scala
+++ services/src/main/scala/cromwell/services/instrumentation/InstrumentedBatchActor.scala
@@ -30,7 +30,7 @@ trait InstrumentedBatchActor[C] { this: BatchActor[C] with CromwellInstrumentati
     instrumentationPath.concatNel(NonEmptyList.one(name))
 
   private val processedPath = makePath("processed")
-  private val durationPath = makePath("duration")
+  private val durationPath = makePath("time_per_batch")
   private val failurePath = makePath("failure")
   private val queueSizePath = makePath("queue")
 
@@ -59,7 +59,7 @@ trait InstrumentedBatchActor[C] { this: BatchActor[C] with CromwellInstrumentati
     action onComplete {
       case Success(numProcessed) =>
         count(processedPath, numProcessed.toLong, instrumentationPrefix)
-        if (numProcessed > 0) sendTiming(durationPath, ((OffsetDateTime.now().toEpochSecond - startTime.toEpochSecond) / numProcessed).seconds, instrumentationPrefix)
+        sendTiming(durationPath, (OffsetDateTime.now().toEpochSecond - startTime.toEpochSecond).seconds, instrumentationPrefix)
       case Failure(_) => count(failurePath, 1L, instrumentationPrefix)
     }
     action

282f657ec5e9a38f87f5aeb63e83b8202337279e
Grigoriy Sterin <gsterin@broadinstitute.org>
2020-10-29 15:54:56 -0400

    comment

--- engine/src/test/scala/cromwell/engine/io/gcs/GcsBatchFlowSpec.scala
+++ engine/src/test/scala/cromwell/engine/io/gcs/GcsBatchFlowSpec.scala
@@ -57,7 +57,7 @@ class GcsBatchFlowSpec extends TestKitSuite with AnyFlatSpecLike with CromwellTi
     val recoverCommandPrivateMethod = PrivateMethod[PartialFunction[Throwable, Future[GcsBatchResponse[_]]]]('recoverCommand)
     val partialFuncAcceptingThrowable = gcsBatchFlow invokePrivate recoverCommandPrivateMethod(gcsBatchCommandContext)
 
-    val futureRes = partialFuncAcceptingThrowable(new NullPointerException(null))
+    val futureRes = partialFuncAcceptingThrowable(new NullPointerException(null)) // no unhandled exceptions should thrown here
     futureRes.isCompleted shouldBe true
   }
 }

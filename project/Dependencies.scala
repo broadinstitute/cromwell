eae49d85020a74839cca181841d0884ea981ae01
Scala Steward <me@scala-steward.org>
2020-11-04 20:15:22 +0100

    Scala-Steward: Update google-oauth-client from 1.31.0 to 1.31.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
   private val googleHttpClientV = "1.36.0"
   private val googleLifeSciencesServicesV2BetaApiV = "v2beta-rev20200806-1.30.11"
   private val googleOauth2V = "0.22.0"
-  private val googleOauthClientV = "1.31.0"
+  private val googleOauthClientV = "1.31.2"
   private val googleCloudResourceManagerV = "0.87.0-alpha"
   private val grpcV = "1.32.2"
   private val guavaV = "30.0-jre"

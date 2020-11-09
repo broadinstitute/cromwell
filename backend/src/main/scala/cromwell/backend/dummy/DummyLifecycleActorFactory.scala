1863ba0713e33b0cb3b7e5795676dde0af249614
Chris Llanwarne <cjllanwarne@gmail.com>
2020-11-09 16:38:13 -0500

    Dummy Backend and Selective TSV instrumentation service

---
+++ backend/src/main/scala/cromwell/backend/dummy/DummyLifecycleActorFactory.scala
@@ -0,0 +1,32 @@
+package cromwell.backend.dummy
+
+import akka.actor.{ActorRef, Props}
+import cromwell.backend.BackendConfigurationDescriptor
+import cromwell.backend.standard.callcaching.{StandardCacheHitCopyingActor, StandardFileHashingActor}
+import cromwell.backend.standard.{StandardAsyncExecutionActor, StandardLifecycleActorFactory}
+
+class DummyLifecycleActorFactory(override val name: String, override val configurationDescriptor: BackendConfigurationDescriptor) extends StandardLifecycleActorFactory {
+
+  /**
+    * Returns the key to use for storing and looking up the job id.
+    *
+    * @return the key to use for storing and looking up the job id.
+    */
+  override def jobIdKey: String = "__dummy_operation_id"
+
+  /**
+    * Returns the asynchronous executor class.
+    *
+    * @return the asynchronous executor class.
+    */
+  override def asyncExecutionActorClass: Class[_ <: StandardAsyncExecutionActor] = classOf[DummyAsyncExecutionActor]
+
+  // Don't cache-hit copy
+  override lazy val cacheHitCopyingActorClassOption: Option[Class[_ <: StandardCacheHitCopyingActor]] = None
+
+  // Don't hash files
+  override lazy val fileHashingActorClassOption: Option[Class[_ <: StandardFileHashingActor]] = None
+
+  override def backendSingletonActorProps(serviceRegistryActor: ActorRef): Option[Props] = Option(Props(new DummySingletonActor()))
+
+}

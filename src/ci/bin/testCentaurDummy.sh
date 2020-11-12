0fe3516b0de9882f3b5ebb1a0d94b94ee3ae9e34
Chris Llanwarne <cjllanwarne@gmail.com>
2020-11-12 17:22:22 -0500

    Fixup dummy tests [force ci]

--- src/ci/bin/testCentaurDummy.sh
+++ src/ci/bin/testCentaurDummy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset -o pipefail
-export CROMWELL_BUILD_OPTIONAL_SECURE=true
+export CROMWELL_BUILD_REQUIRES_SECURE=true
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
@@ -12,6 +12,6 @@ cromwell::build::setup_centaur_environment
 
 cromwell::build::assemble_jars
 
-cromwell::build::run_centaur -n dummy
+cromwell::build::run_centaur -i dummy
 
 cromwell::build::generate_code_coverage

c563c24248793d6d1f10e05a40d7c9ea6e5acc28
Adam Nichols <aednichols@gmail.com>
2020-11-23 14:34:25 -0500

    src/ci/bin/testSbt.sh

--- src/ci/bin/test.sh
+++ src/ci/bin/test.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset -o pipefail
+set -x
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
-
-echo "Building in background. No output is normal."
 
 cromwell::build::exec_test_script

8beeb33daf17ed699a48c385be8ffe6768042691
Kristian Cibulskis <kcibul@broadinstitute.org>
2020-11-23 16:12:00 -0500

    trying something smaller

---
+++ src/ci/bin/testJustAssemble.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+export CROMWELL_BUILD_OPTIONAL_SECURE=true
+# import in shellcheck / CI / IntelliJ compatible ways
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/test_unit.inc.sh" || source test_unit.inc.sh
+
+cromwell::build::setup_common_environment
+
+cromwell::build::assemble_jars

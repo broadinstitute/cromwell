f3ab018b19763627ab76b12c0498bd33219f6c3b
Adam Nichols <aednichols@gmail.com>
2020-11-24 10:51:06 -0500

    Revert in favor of stability

--- src/ci/bin/test.sh
+++ src/ci/bin/test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset -o pipefail
-set -x
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh

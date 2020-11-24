e1592aade0e281adccc66292fa883e5f1ae7957e
Kristian Cibulskis <kcibul@broadinstitute.org>
2020-11-23 20:13:46 -0500

    hackery

--- src/ci/bin/testJustAssemble.sh
+++ src/ci/bin/testJustAssemble.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset -o pipefail
-export CROMWELL_BUILD_OPTIONAL_SECURE=true
+#export CROMWELL_BUILD_OPTIONAL_SECURE=true
+export CROMWELL_BUILD_OPTIONAL_SECURE=false
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh

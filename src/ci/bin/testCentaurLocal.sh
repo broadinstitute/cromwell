df03ad53463e59bd5d3f75f3e11fb389c936385e
Adam Nichols <aednichols@gmail.com>
2020-11-25 16:09:47 -0500

    64 is the answer

--- src/ci/bin/testCentaurLocal.sh
+++ src/ci/bin/testCentaurLocal.sh
@@ -12,6 +12,6 @@ cromwell::build::setup_centaur_environment
 
 cromwell::build::assemble_jars
 
-cromwell::build::run_centaur -p 128
+cromwell::build::run_centaur -p 64
 
 cromwell::build::generate_code_coverage

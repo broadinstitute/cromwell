ca442b8cd7a6963d9f8a912989666e0fa6072287
Kristian Cibulskis <kcibul@broadinstitute.org>
2020-11-25 11:59:47 -0500

    hackery

--- src/ci/docker/build_docker.sh
+++ src/ci/docker/build_docker.sh
@@ -1,2 +1,2 @@
-docker build -t us.gcr.io/broad-dsde-cromwell-dev/cromwell-cloudbuild-base .
+docker build -t us.gcr.io/broad-dsde-cromwell-dev/cromwell-cloudbuild-base:asroot .
 docker push us.gcr.io/broad-dsde-cromwell-dev/cromwell-cloudbuild-base:latest

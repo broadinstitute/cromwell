d334ac0b6eb618c5cb11b0ce285013f6d84167f1
Adam Nichols <aednichols@gmail.com>
2020-12-17 14:02:30 -0500

    2.12/2.13

--- codegen_java/project/Publishing.scala
+++ codegen_java/project/Publishing.scala
@@ -14,8 +14,8 @@ import Artifactory._
   }
 
    private val artifactoryCredentials: Credentials = {
-    val username = sys.env.getOrElse("ARTIFACTORY_USERNAME", "")
-    val password = sys.env.getOrElse("ARTIFACTORY_PASSWORD", "")
+    val username = sys.env.getOrElse("ARTIFACTORY_USERNAME", throw new Exception("Artifactory username must be set as `ARTIFACTORY_USERNAME`. See https://github.com/broadinstitute/workbench-libs/blob/develop/GETTING_STARTED.md"))
+    val password = sys.env.getOrElse("ARTIFACTORY_PASSWORD", throw new Exception("Artifactory password must be set as `ARTIFACTORY_PASSWORD`. See https://github.com/broadinstitute/workbench-libs/blob/develop/GETTING_STARTED.md"))
     Credentials("Artifactory Realm", artifactoryHost, username, password)
   }
 

dab268df70531d9f8b8144d16a2483a9e7477004
Miguel Covarrubias <mcovarr@broadinstitute.org>
2021-01-11 14:07:49 -0500

    better words

--- src/ci/bin/testCentaurPapiUpgradePapiV2alpha1.sh
+++ src/ci/bin/testCentaurPapiUpgradePapiV2alpha1.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-echo "$(tput setab 1)$(tput blink)BT-81: This sub-build currently does no useful work; it builds Cromwell and launches Centaur but finds no matching tests to run. See the linked ticket for further information.$(tput sgr 0)"
-exit 0
+echo "$(tput setab 1)$(tput blink)BT-81: This sub-build was doing no useful work; it built Cromwell and launched Centaur but never found tests to run. However, tests for functionality implied by this sub-build's name may be desirable, see BT-81 for further information.$(tput sgr 0)"exit 0

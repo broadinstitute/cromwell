b308dc487a98f3fba117595da2e797365959dcce
Miguel Covarrubias <mcovarr@broadinstitute.org>
2021-01-11 14:03:15 -0500

    wrong ticket number

--- src/ci/bin/testCentaurPapiUpgradeNewWorkflowsPapiV2alpha1.sh
+++ src/ci/bin/testCentaurPapiUpgradeNewWorkflowsPapiV2alpha1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "$(tput setab 1)$(tput blink)BT-84: This sub-build currently does no useful work; it builds Cromwell and launches Centaur but finds no matching tests to run. See the linked ticket for further information.$(tput sgr 0)"
+echo "$(tput setab 1)$(tput blink)BT-81: This sub-build currently does no useful work; it builds Cromwell and launches Centaur but finds no matching tests to run. See the linked ticket for further information.$(tput sgr 0)"
 exit 0

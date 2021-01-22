a3f0c85a86f75f0f9ed132548355d3a1efe2831c
Grigoriy Sterin <gsterin@broadinstitute.org>
2021-01-21 19:19:15 -0500

    check manifest format version

--- supportedBackends/google/pipelines/common/src/main/scala/cromwell/backend/google/pipelines/common/PipelinesApiDockerCacheMappingOperations.scala
+++ supportedBackends/google/pipelines/common/src/main/scala/cromwell/backend/google/pipelines/common/PipelinesApiDockerCacheMappingOperations.scala
@@ -8,10 +8,15 @@ import com.google.cloud.storage.{BlobId, Storage, StorageOptions}
 import cromwell.cloudsupport.gcp.auth.GoogleAuthMode
 import cromwell.filesystems.gcs.GcsPathBuilder.ValidFullGcsPath
 
+import scala.util.control.NoStackTrace
+
 case class DockerImageCacheEntry(dockerImageDigest: String, diskImageName: String)
 case class DockerImageCacheManifest(manifestFormatVersion: Int, dockerImageCacheMap: Map[String, DockerImageCacheEntry])
 
 trait PipelinesApiDockerCacheMappingOperations {
+
+  private val CURRENT_SUPPORTED_MANIFEST_FORMAT_VERSION = 2
+  private class DockerImageManifestVersionError(message: String) extends RuntimeException(message) with NoStackTrace
 
   def generateDockerImageToDiskImageMapping(auth: GoogleAuthMode,
                                             dockerImageCacheManifestFile: ValidFullGcsPath): Map[String, DockerImageCacheEntry] = {
@@ -29,7 +34,18 @@ trait PipelinesApiDockerCacheMappingOperations {
     val manifestFileBlobIo = IO { gcsClient.get(BlobId.of(gcsPath.bucket, gcsPath.path.substring(1))) }
     manifestFileBlobIo flatMap { manifestFileBlob =>
       val jsonStringIo = IO { manifestFileBlob.getContent().map(_.toChar).mkString }
-      jsonStringIo.flatMap(jsonStr => IO.fromEither(decode[DockerImageCacheManifest](jsonStr)))
+      jsonStringIo.flatMap { jsonStr =>
+        decode[DockerImageCacheManifest](jsonStr) match {
+          case Left(error) => IO.raiseError(error)
+          case Right(parsedManifest) =>
+            if (parsedManifest.manifestFormatVersion == CURRENT_SUPPORTED_MANIFEST_FORMAT_VERSION) {
+              IO.pure(parsedManifest)
+            } else {
+              IO.raiseError(new DockerImageManifestVersionError(s"Current supported docker image cache manifest format version " +
+                s"is $CURRENT_SUPPORTED_MANIFEST_FORMAT_VERSION, but got ${parsedManifest.manifestFormatVersion}"))
+            }
+        }
+      }
     }
   }
 

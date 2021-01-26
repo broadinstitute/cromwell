3f4e7b9487c2076d89f7ad97d5663b2eb8f953b4
Grigoriy Sterin <gsterin@broadinstitute.org>
2021-01-26 11:09:45 -0500

    fix

--- src/ci/docker-compose/cromwell-test/docker-setup.sh
+++ src/ci/docker-compose/cromwell-test/docker-setup.sh
@@ -16,7 +16,7 @@ apt-get install -y \
     jq \
     mysql-client \
     postgresql-client \
-    python-dev \
+    python3-dev \
     software-properties-common \
     sudo \
 
@@ -27,8 +27,8 @@ apt-get update
 apt-get install -y sbt
 
 # upgrade python dependencies
-curl https://bootstrap.pypa.io/2.7/get-pip.py -o get-pip.py
-python get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 pip install --upgrade --force-reinstall pyopenssl
 
 # install gcloud

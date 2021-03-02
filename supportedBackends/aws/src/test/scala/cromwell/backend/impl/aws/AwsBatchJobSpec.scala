c721e52610cc42d5eaa2e23c25278f17084912da
Henrique Silva <henrique.rd.silva@gmail.com>
2021-03-02 15:55:50 -0300

    add awsBatchRetryAttempts to AwsBatchJobSpec test

--- supportedBackends/aws/src/test/scala/cromwell/backend/impl/aws/AwsBatchJobSpec.scala
+++ supportedBackends/aws/src/test/scala/cromwell/backend/impl/aws/AwsBatchJobSpec.scala
@@ -113,6 +113,7 @@ class AwsBatchJobSpec extends TestKitSuite with AnyFlatSpecLike with Matchers wi
       continueOnReturnCode = ContinueOnReturnCodeFlag(false),
       noAddress = false,
       scriptS3BucketName = "script-bucket",
+      awsBatchRetryAttempts = 1,
       fileSystem = "s3")
 
   private def generateBasicJob: AwsBatchJob = {

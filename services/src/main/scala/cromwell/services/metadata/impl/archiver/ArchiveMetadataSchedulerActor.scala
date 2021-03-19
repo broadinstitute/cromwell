4e22b30dbf613ddd4134fd55400d30914936e254
Chris Llanwarne <cjllanwarne@gmail.com>
2021-03-19 16:32:15 -0400

    WIP continues...

--- services/src/main/scala/cromwell/services/metadata/impl/archiver/ArchiveMetadataSchedulerActor.scala
+++ services/src/main/scala/cromwell/services/metadata/impl/archiver/ArchiveMetadataSchedulerActor.scala
@@ -1,9 +1,22 @@
 package cromwell.services.metadata.impl.archiver
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import cats.data.NonEmptyList
+import cromwell.core.{WorkflowAborted, WorkflowFailed, WorkflowId, WorkflowSucceeded}
+import cromwell.core.retry.SimpleExponentialBackoff
 import cromwell.services.instrumentation.CromwellInstrumentation
+import cromwell.services.metadata.MetadataArchiveStatus.Unarchived
+import cromwell.services.metadata.MetadataService
+import cromwell.services.metadata.MetadataService.{QueryForWorkflowsMatchingParameters, WorkflowQueryFailure, WorkflowQuerySuccess}
+import cromwell.services.metadata.WorkflowQueryKey.{IncludeSubworkflows, MetadataArchiveStatus, MinimumSummaryEntryId, Page, PageSize, Status}
 import cromwell.util.GracefulShutdownHelper
 import cromwell.util.GracefulShutdownHelper.ShutdownCommand
+import cromwell.services.metadata.impl.archiver.ArchiveMetadataSchedulerActor._
+import cromwell.services.metadata.impl.archiver.StreamMetadataToGcsActor.ArchiveMetadataForWorkflow
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.{Duration, MILLISECONDS}
+import scala.util.{Failure, Success, Try}
 
 /*
   This class would do what CarboniteWorkerActor is doing. It would schedule workflows to archive at periodic intervals
@@ -12,17 +25,70 @@ import cromwell.util.GracefulShutdownHelper.ShutdownCommand
 class ArchiveMetadataSchedulerActor(archiveMetadataConfig: ArchiveMetadataConfig,
                                     override val serviceRegistryActor: ActorRef) extends Actor with ActorLogging with GracefulShutdownHelper with CromwellInstrumentation {
 
-  // val streamMetadataActor = ??? // instance of StreamMetadataToGcsActor
+  implicit val ec: ExecutionContext = context.dispatcher
+  val streamMetadataActor: ActorRef = context.actorOf(StreamMetadataToGcsActor.props())
+
+  // Schedule the initial workflows to archive query
+  scheduleNextWorkflowsToArchiveQuery()
 
   override def receive: Receive = {
-    case ShutdownCommand => context.stop(self) // TODO: handle this better when we are actually archiving
+    case querySuccess: WorkflowQuerySuccess =>
+      // TODO: Instrument "workflows to archive" query time?
+
+      if (querySuccess.response.results.nonEmpty)
+        archiveWorkflow(querySuccess.response.results.head.id)
+      else
+        scheduleNextWorkflowsToArchiveQuery()
+    case f: WorkflowQueryFailure =>
+      log.error(f.reason, s"Error while querying workflow to carbonite, will retry.")
+      scheduleNextWorkflowsToArchiveQuery()
+
+    case ShutdownCommand => waitForActorsAndShutdown(NonEmptyList.of(streamMetadataActor))
     case other => log.info(s"Programmer Error! The ArchiveMetadataSchedulerActor received unexpected message! ($sender sent $other})")
+  }
+
+  def archiveWorkflow(workflowId: String): Unit = {
+    Try(WorkflowId.fromString(workflowId)) match {
+      case Success(id: WorkflowId) =>
+        if (archiveMetadataConfig.debugLogging) { log.info(s"Starting to archive workflow: $workflowId") }
+        streamMetadataActor ! ArchiveMetadataForWorkflow(id)
+      case Failure(e) =>
+        log.error(e, s"Programmer Error: Cannot archive workflow $workflowId. Error while converting ${workflowId} to WorkflowId, will retry.")
+        scheduleNextWorkflowsToArchiveQuery()
+    }
+  }
+
+  val nothingToArchiveBackoff: SimpleExponentialBackoff = SimpleExponentialBackoff(
+    initialInterval = archiveMetadataConfig.initialInterval,
+    maxInterval = archiveMetadataConfig.maxInterval,
+    multiplier = archiveMetadataConfig.multiplier,
+    randomizationFactor = 0.0
+  )
+
+  def scheduleNextWorkflowsToArchiveQuery(): Unit = {
+    val duration = Duration(nothingToArchiveBackoff.backoffMillis, MILLISECONDS)
+    context.system.scheduler.scheduleOnce(duration)(queryForWorkflowToArchive())
+    ()
+  }
+
+  def queryForWorkflowToArchive(): Unit = {
+    serviceRegistryActor ! QueryForWorkflowsMatchingParameters(queryParametersForWorkflowsToArchive)
   }
 }
 
-
 object ArchiveMetadataSchedulerActor {
-
-  def props(archiveMetadataConfig: ArchiveMetadataConfig, serviceRegistryActor: ActorRef) =
+  def props(archiveMetadataConfig: ArchiveMetadataConfig, serviceRegistryActor: ActorRef): Props =
     Props(new ArchiveMetadataSchedulerActor(archiveMetadataConfig, serviceRegistryActor))
+
+  // TODO: Archive from oldest-first
+  // TODO: Allow requirements like "End timestamp not within 1y (eg)"
+  val queryParametersForWorkflowsToArchive: Seq[(String, String)] = Seq(
+    IncludeSubworkflows.name -> "true",
+    Status.name -> WorkflowSucceeded.toString,
+    Status.name -> WorkflowFailed.toString,
+    Status.name -> WorkflowAborted.toString,
+    MetadataArchiveStatus.name -> Unarchived.toString,
+    Page.name -> "1",
+    PageSize.name -> "1"
+  )
 }
--- services/src/main/scala/cromwell/services/metadata/impl/archiver/StreamMetadataToGcsActor.scala
+++ services/src/main/scala/cromwell/services/metadata/impl/archiver/StreamMetadataToGcsActor.scala
@@ -1,9 +1,40 @@
 package cromwell.services.metadata.impl.archiver
 
+import akka.actor.{Actor, ActorLogging, Props}
+import cromwell.core.WorkflowId
+import cromwell.services.metadata.impl.archiver.StreamMetadataToGcsActor.{ArchiveMetadataForWorkflow, MetadataArchiveResult}
+import cromwell.util.GracefulShutdownHelper
+import cromwell.util.GracefulShutdownHelper.ShutdownCommand
+import org.apache.commons.lang3.NotImplementedException
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.util.{Failure, Try}
+
 /*
   This actor will archive the metadata by streaming it to GCS. Almost similar to what
   cromwell.services.metadata.hybridcarbonite.CarbonitingMetadataFreezerActor does
  */
-class StreamMetadataToGcsActor {
+class StreamMetadataToGcsActor extends Actor with ActorLogging with GracefulShutdownHelper {
+
+  implicit val ec: ExecutionContext = context.dispatcher
+
+  override def receive: Receive = {
+    case ArchiveMetadataForWorkflow(_) =>
+      val s = sender()
+      context.system.scheduler.scheduleOnce(2.minutes) {
+        s ! MetadataArchiveResult(Failure(new NotImplementedException("Archive process not implemented yet!")))
+      }
+    case ShutdownCommand => context.stop(self) // TODO: Anything more graceful?
+    case other => log.info(s"Programmer Error! The ArchiveMetadataSchedulerActor received unexpected message! ($sender sent $other})")
+  }
+}
+
+object StreamMetadataToGcsActor {
+  def props(): Props = Props(new StreamMetadataToGcsActor)
+
+  sealed trait StreamMetadataToGcsActorMessage
+  final case class ArchiveMetadataForWorkflow(workflow: WorkflowId) extends StreamMetadataToGcsActorMessage
+  final case class MetadataArchiveResult(freezingResult: Try[Unit]) extends StreamMetadataToGcsActorMessage
 
 }

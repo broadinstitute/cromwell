8a19b890098b6a427e9a927a41dcae0d9c31e720
Khalid Shakir <kshakir@broadinstitute.org>
2021-03-20 11:35:22 -0400

    Always pass GCP http init to batch from builder

--- supportedBackends/google/pipelines/v1alpha2/src/main/scala/cromwell/backend/google/pipelines/v1alpha2/PipelinesApiLifecycleActorFactory.scala
+++ supportedBackends/google/pipelines/v1alpha2/src/main/scala/cromwell/backend/google/pipelines/v1alpha2/PipelinesApiLifecycleActorFactory.scala
@@ -1,6 +1,6 @@
 package cromwell.backend.google.pipelines.v1alpha2
 
-import akka.actor.ActorRef
+import akka.actor.{ActorRef, Props}
 import cromwell.backend.BackendConfigurationDescriptor
 import cromwell.backend.google.pipelines.common.{PipelinesApiBackendLifecycleActorFactory, PipelinesApiBackendSingletonActor, PipelinesApiConfiguration}
 import cromwell.backend.google.pipelines.v1alpha2.api.request.RequestHandler
@@ -8,10 +8,21 @@ import cromwell.backend.standard.{StandardFinalizationActor, StandardInitializat
 
 class PipelinesApiLifecycleActorFactory(name: String, configurationDescriptor: BackendConfigurationDescriptor)
   extends PipelinesApiBackendLifecycleActorFactory(name, configurationDescriptor) {
-  private val genomicsFactory = GenomicsFactory(googleConfig.applicationName, papiAttributes.auths.genomics, papiAttributes.endpointUrl)
-  override protected val jesConfiguration = new PipelinesApiConfiguration(configurationDescriptor, genomicsFactory, googleConfig, papiAttributes)
-  override def requiredBackendSingletonActor(serviceRegistryActor: ActorRef) = {
-    implicit val batchHandler = new RequestHandler(googleConfig.applicationName, papiAttributes.endpointUrl)
+  private val genomicsFactory =
+    GenomicsFactory(
+      googleConfig.applicationName,
+      papiAttributes.auths.genomics,
+      papiAttributes.endpointUrl,
+    )
+  override protected val jesConfiguration =
+    new PipelinesApiConfiguration(configurationDescriptor, genomicsFactory, googleConfig, papiAttributes)
+  override def requiredBackendSingletonActor(serviceRegistryActor: ActorRef): Props = {
+    implicit val batchHandler: RequestHandler =
+      new RequestHandler(
+        googleConfig.applicationName,
+        papiAttributes.endpointUrl,
+        papiAttributes.batchRequestTimeoutConfiguration,
+      )
     PipelinesApiBackendSingletonActor.props(
       jesConfiguration.papiAttributes.qps,
       jesConfiguration.papiAttributes.requestWorkers,

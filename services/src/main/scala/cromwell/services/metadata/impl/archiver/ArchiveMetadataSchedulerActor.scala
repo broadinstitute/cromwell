08ebae309724d6028bbe317626d7ea23b536509a
Rebecca Asch <rasch@broadinstitute.org>
2021-03-22 16:53:17 -0400

    wip skeleton

--- services/src/main/scala/cromwell/services/metadata/impl/archiver/ArchiveMetadataSchedulerActor.scala
+++ services/src/main/scala/cromwell/services/metadata/impl/archiver/ArchiveMetadataSchedulerActor.scala
@@ -1,19 +1,16 @@
 package cromwell.services.metadata.impl.archiver
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import cats.data.NonEmptyList
 import cromwell.core.retry.SimpleExponentialBackoff
 import cromwell.core.{WorkflowAborted, WorkflowFailed, WorkflowId, WorkflowSucceeded}
 import cromwell.services.instrumentation.CromwellInstrumentation
 import cromwell.services.metadata.MetadataArchiveStatus.Unarchived
-import cromwell.services.metadata.MetadataService.{QueryForWorkflowsMatchingParameters, WorkflowQueryFailure, WorkflowQuerySuccess}
 import cromwell.services.metadata.WorkflowQueryKey._
 import cromwell.services.metadata.impl.archiver.ArchiveMetadataSchedulerActor._
-import cromwell.services.metadata.impl.archiver.StreamMetadataToGcsActor.ArchiveMetadataForWorkflow
 import cromwell.util.GracefulShutdownHelper
 import cromwell.util.GracefulShutdownHelper.ShutdownCommand
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.{Duration, MILLISECONDS}
 import scala.util.{Failure, Success, Try}
 
@@ -22,42 +19,46 @@ class ArchiveMetadataSchedulerActor(archiveMetadataConfig: ArchiveMetadataConfig
                                     override val serviceRegistryActor: ActorRef) extends Actor with ActorLogging with GracefulShutdownHelper with CromwellInstrumentation {
 
   implicit val ec: ExecutionContext = context.dispatcher
-  val streamMetadataActor: ActorRef = context.actorOf(StreamMetadataToGcsActor.props(archiveMetadataConfig, serviceRegistryActor))
 
   // Schedule the initial workflows to archive query
-  scheduleNextWorkflowsToArchiveQuery()
+  scheduleNextWorkflowToArchive()
 
   override def receive: Receive = {
-    case querySuccess: WorkflowQuerySuccess =>
-      // TODO: Instrument "workflows to archive" query time?
-
-      if (querySuccess.response.results.nonEmpty)
-        archiveWorkflow(querySuccess.response.results.head.id)
-      else
-        scheduleNextWorkflowsToArchiveQuery()
-    case queryFailure: WorkflowQueryFailure =>
-      log.error(queryFailure.reason, s"Error while querying workflow to archive, will retry.")
-      scheduleNextWorkflowsToArchiveQuery()
-    case archiveComplete: ArchiveWorkflowComplete =>
-      // TODO: Instrument time to archive workflow?
-      if (archiveMetadataConfig.debugLogging) { log.info(s"Archiving complete for workflow ${archiveComplete.workflowId}") }
-
-      // Immediately reset the timer and check for the next workflow to archive:
+    case ArchiveNextWorkflowMessage => archiveNextWorkflow().onComplete(result => { self ! ArchiveResultMessage(result) })
+    case ArchiveResultMessage(Success(true)) =>
       nothingToArchiveBackoff.googleBackoff.reset()
-      queryForWorkflowToArchive()
-    case ShutdownCommand => waitForActorsAndShutdown(NonEmptyList.of(streamMetadataActor))
+      self ! ArchiveNextWorkflowMessage
+    case ArchiveResultMessage(Success(false)) => scheduleNextWorkflowToArchive()
+    case ArchiveResultMessage(Failure(error)) =>
+      log.error(error, s"Error while archiving, will retry.")
+      scheduleNextWorkflowToArchive()
+    case ShutdownCommand => context.stop(self)  // TODO: cancel any streaming that might be happening
     case other => log.info(s"Programmer Error! The ArchiveMetadataSchedulerActor received unexpected message! ($sender sent $other})")
   }
 
-  def archiveWorkflow(workflowId: String): Unit = {
-    Try(WorkflowId.fromString(workflowId)) match {
-      case Success(id: WorkflowId) =>
-        if (archiveMetadataConfig.debugLogging) { log.info(s"Starting to archive workflow: $workflowId") }
-        streamMetadataActor ! ArchiveMetadataForWorkflow(id)
-      case Failure(e) =>
-        log.error(e, s"Programmer Error: Cannot archive workflow $workflowId. Error while converting ${workflowId} to WorkflowId, will retry.")
-        scheduleNextWorkflowsToArchiveQuery()
-    }
+  def archiveNextWorkflow(): Future[Boolean] = {
+    for {
+      maybeWorkflowId <- getNextWorkflowId()
+      result <- maybeWorkflowId match {
+        case Some(id) => for {
+          _ <- streamMetadataToGcs(id)
+          _ <- markWorkflowAsArchived(id)
+        } yield true
+        case None => Future.successful(false)
+      }
+    } yield result
+  }
+
+  def getNextWorkflowId(): Future[Option[WorkflowId]] = {
+    ???
+  }
+
+  def streamMetadataToGcs(workflowId: WorkflowId): Future[Unit] = {
+    ???
+  }
+
+  def markWorkflowAsArchived(workflowId: WorkflowId): Future[Unit] = {
+    ???
   }
 
   val nothingToArchiveBackoff: SimpleExponentialBackoff = SimpleExponentialBackoff(
@@ -67,21 +68,16 @@ class ArchiveMetadataSchedulerActor(archiveMetadataConfig: ArchiveMetadataConfig
     randomizationFactor = 0.0
   )
 
-  def scheduleNextWorkflowsToArchiveQuery(): Unit = {
+  def scheduleNextWorkflowToArchive(): Unit = {
     val duration = Duration(nothingToArchiveBackoff.backoffMillis, MILLISECONDS)
-    context.system.scheduler.scheduleOnce(duration)(queryForWorkflowToArchive())
+    context.system.scheduler.scheduleOnce(duration)(self ! ArchiveNextWorkflowMessage)
     ()
-  }
-
-  def queryForWorkflowToArchive(): Unit = {
-    serviceRegistryActor ! QueryForWorkflowsMatchingParameters(queryParametersForWorkflowsToArchive)
   }
 }
 
 object ArchiveMetadataSchedulerActor {
-
-  sealed trait ArchiveWorkflowWorkerMessage
-  case class ArchiveWorkflowComplete(workflowId: WorkflowId, result: cromwell.services.metadata.MetadataArchiveStatus) extends ArchiveWorkflowWorkerMessage
+  case object ArchiveNextWorkflowMessage
+  case class ArchiveResultMessage(result: Try[Boolean])
 
   // TODO: Archive from oldest-first
   // TODO: Allow requirements like "End timestamp not within 1y (eg)"

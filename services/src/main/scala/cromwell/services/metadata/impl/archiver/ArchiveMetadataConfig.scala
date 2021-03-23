e249691b11d6b52419c61c7b0370d1b788d776d9
Rebecca Asch <rasch@broadinstitute.org>
2021-03-23 11:47:05 -0400

    simplified backoff into delay; reduced number of message types

--- services/src/main/scala/cromwell/services/metadata/impl/archiver/ArchiveMetadataConfig.scala
+++ services/src/main/scala/cromwell/services/metadata/impl/archiver/ArchiveMetadataConfig.scala
@@ -3,7 +3,6 @@ package cromwell.services.metadata.impl.archiver
 import akka.actor.ActorSystem
 import com.typesafe.config.Config
 import common.Checked
-import common.validation.Checked._
 import common.validation.Validation._
 import cromwell.core.filesystem.CromwellFileSystems
 import cromwell.core.path.PathFactory.PathBuilders
@@ -19,9 +18,7 @@ import scala.util.Try
 final case class ArchiveMetadataConfig(pathBuilders: PathBuilders,
                                        bucket: String,
                                        bucketReadLimit: Int,
-                                       initialInterval: FiniteDuration,
-                                       maxInterval: FiniteDuration,
-                                       multiplier: Double,
+                                       interval: FiniteDuration,
                                        debugLogging: Boolean) {
   def makePath(workflowId: WorkflowId)= PathFactory.buildPath(ArchiveMetadataConfig.pathForWorkflow(workflowId, bucket), pathBuilders)
 }
@@ -33,7 +30,6 @@ object ArchiveMetadataConfig {
 
   def parseConfig(archiveMetadataConfig: Config)(implicit system: ActorSystem): Checked[ArchiveMetadataConfig] = {
     val defaultMaxInterval: FiniteDuration = 5 minutes
-    val defaultMultiplier = 1.1
     val defaultDebugLogging = true
 
     for {
@@ -43,13 +39,9 @@ object ArchiveMetadataConfig {
         .toCheckedWithContext("construct Carboniter path builders from factories")
       bucket <- Try(archiveMetadataConfig.getString("bucket")).toCheckedWithContext("parse Carboniter 'bucket' field from config")
       bucketReadLimit <- Try(archiveMetadataConfig.getOrElse[Int]("bucket-read-limit-bytes", 150000000)).toCheckedWithContext("parse Carboniter 'bucket-read-limit-bytes' field from config")
-      initialInterval <- Try(archiveMetadataConfig.getOrElse[FiniteDuration]("initial-interval", defaultMaxInterval)).toChecked
-      maxInterval <- Try(archiveMetadataConfig.getOrElse("max-interval", defaultMaxInterval)).toChecked
-      multiplier <- Try(archiveMetadataConfig.getOrElse("multiplier", defaultMultiplier)).toChecked
-      _ <- if (initialInterval > maxInterval) s"'max-interval' $maxInterval should be greater than or equal to finite 'initial-interval' $initialInterval.".invalidNelCheck else "".validNelCheck
-      _ <- if (multiplier > 1) "".validNelCheck else "`multiplier` must be greater than 1.".invalidNelCheck
+      interval <- Try(archiveMetadataConfig.getOrElse[FiniteDuration]("interval", defaultMaxInterval)).toChecked
       debugLogging <- Try(archiveMetadataConfig.getOrElse("debug-logging", defaultDebugLogging)).toChecked
-    } yield ArchiveMetadataConfig(pathBuilders, bucket, bucketReadLimit, initialInterval, maxInterval, multiplier, debugLogging)
+    } yield ArchiveMetadataConfig(pathBuilders, bucket, bucketReadLimit, interval, debugLogging)
   }
 }
 
--- services/src/main/scala/cromwell/services/metadata/impl/archiver/ArchiveMetadataSchedulerActor.scala
+++ services/src/main/scala/cromwell/services/metadata/impl/archiver/ArchiveMetadataSchedulerActor.scala
@@ -1,7 +1,6 @@
 package cromwell.services.metadata.impl.archiver
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import cromwell.core.retry.SimpleExponentialBackoff
 import cromwell.core.{WorkflowAborted, WorkflowFailed, WorkflowId, WorkflowSucceeded}
 import cromwell.services.instrumentation.CromwellInstrumentation
 import cromwell.services.metadata.MetadataArchiveStatus.Unarchived
@@ -11,8 +10,7 @@ import cromwell.util.GracefulShutdownHelper
 import cromwell.util.GracefulShutdownHelper.ShutdownCommand
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.concurrent.duration.{Duration, MILLISECONDS}
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Success}
 
 
 class ArchiveMetadataSchedulerActor(archiveMetadataConfig: ArchiveMetadataConfig,
@@ -20,18 +18,17 @@ class ArchiveMetadataSchedulerActor(archiveMetadataConfig: ArchiveMetadataConfig
 
   implicit val ec: ExecutionContext = context.dispatcher
 
-  // Schedule the initial workflows to archive query
-  scheduleNextWorkflowToArchive()
+  // kick off archiving immediately
+  self ! ArchiveNextWorkflowMessage
 
   override def receive: Receive = {
-    case ArchiveNextWorkflowMessage => archiveNextWorkflow().onComplete(result => { self ! ArchiveResultMessage(result) })
-    case ArchiveResultMessage(Success(true)) =>
-      nothingToArchiveBackoff.googleBackoff.reset()
-      self ! ArchiveNextWorkflowMessage
-    case ArchiveResultMessage(Success(false)) => scheduleNextWorkflowToArchive()
-    case ArchiveResultMessage(Failure(error)) =>
-      log.error(error, s"Error while archiving, will retry.")
-      scheduleNextWorkflowToArchive()
+    case ArchiveNextWorkflowMessage => archiveNextWorkflow().onComplete({
+      case Success(true) => self ! ArchiveNextWorkflowMessage
+      case Success(false) => scheduleNextWorkflowToArchive()
+      case Failure(error) =>
+        log.error(error, s"Error while archiving, will retry.")
+        scheduleNextWorkflowToArchive()
+    })
     case ShutdownCommand => context.stop(self)  // TODO: cancel any streaming that might be happening
     case other => log.info(s"Programmer Error! The ArchiveMetadataSchedulerActor received unexpected message! ($sender sent $other})")
   }
@@ -61,23 +58,14 @@ class ArchiveMetadataSchedulerActor(archiveMetadataConfig: ArchiveMetadataConfig
     ???
   }
 
-  val nothingToArchiveBackoff: SimpleExponentialBackoff = SimpleExponentialBackoff(
-    initialInterval = archiveMetadataConfig.initialInterval,
-    maxInterval = archiveMetadataConfig.maxInterval,
-    multiplier = archiveMetadataConfig.multiplier,
-    randomizationFactor = 0.0
-  )
-
   def scheduleNextWorkflowToArchive(): Unit = {
-    val duration = Duration(nothingToArchiveBackoff.backoffMillis, MILLISECONDS)
-    context.system.scheduler.scheduleOnce(duration)(self ! ArchiveNextWorkflowMessage)
+    context.system.scheduler.scheduleOnce(archiveMetadataConfig.interval)(self ! ArchiveNextWorkflowMessage)
     ()
   }
 }
 
 object ArchiveMetadataSchedulerActor {
   case object ArchiveNextWorkflowMessage
-  case class ArchiveResultMessage(result: Try[Boolean])
 
   // TODO: Archive from oldest-first
   // TODO: Allow requirements like "End timestamp not within 1y (eg)"

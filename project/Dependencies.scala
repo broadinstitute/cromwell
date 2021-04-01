ca225eb8649ff8a5e69f4ca6722fdd5749bb97d0
Scala Steward <me@scala-steward.org>
2021-04-01 02:10:20 +0200

    Scala-Steward: Update google-cloud-monitoring from 2.0.8 to 2.0.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
   private val googleApiClientV = "1.30.11"
   private val googleCloudBigQueryV = "1.127.11"
   private val googleCloudKmsV = "v1-rev20201102-1.31.0"
-  private val googleCloudMonitoringV = "2.0.8"
+  private val googleCloudMonitoringV = "2.0.14"
   private val googleCloudNioV = "0.61.0-alpha" // scala-steward:off
   private val googleCloudStorageV = "1.113.4"
   private val googleGaxGrpcV = "1.60.1"

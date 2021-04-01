63dcb9a1adf252212a5e9458aef4e84b22f7bf21
Scala Steward <me@scala-steward.org>
2021-04-01 02:07:58 +0200

    Scala-Steward: Update aliyun-sdk-oss from 3.11.1 to 3.11.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private val aliyunBcsV = "6.2.4"
   private val aliyunCoreV = "4.5.16"
   private val aliyunCrV = "4.1.1"
-  private val aliyunOssV = "3.11.1"
+  private val aliyunOssV = "3.11.3"
   private val ammoniteOpsV = "2.3.8"
   private val apacheCommonNetV = "3.8.0"
   private val apacheHttpClientV = "4.5.13"

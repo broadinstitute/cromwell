ded8153ab5dd87addf7240855618ec7ebc7ba5ae
Scala Steward <me@scala-steward.org>
2021-04-01 02:14:23 +0200

    Scala-Steward: Update scalatest from 3.2.3 to 3.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -105,7 +105,7 @@ object Dependencies {
   private val scalacticV = "3.2.6"
   private val scalameterV = "0.19"
   private val scalamockV = "5.1.0"
-  private val scalatestV = "3.2.3"
+  private val scalatestV = "3.2.6"
   private val scalatestPlusMockitoV = "1.0.0-M2"
   private val scalazV = "7.3.2"
   private val scoptV = "4.0.0"

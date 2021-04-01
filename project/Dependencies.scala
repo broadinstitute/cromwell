d33334e383bff9d57261e5479e40204ebf22ec9b
Scala Steward <me@scala-steward.org>
2021-04-01 02:08:12 +0200

    Scala-Steward: Update jackson-annotations, jackson-core, ... from 2.12.0 to 2.12.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
   private val heterodonV = "1.0.0-beta3"
   private val hsqldbV = "2.5.1"
   private val http4sVersion = "0.21.7" // scala-steward:off (CROM-6678)
-  private val jacksonV = "2.12.0"
+  private val jacksonV = "2.12.2"
   private val jacksonJqV = "1.0.0-preview.20201123"
   private val janinoV = "3.1.3"
   private val javaxActivationV = "1.2.0"

458662344a899294b6c69895f61bc8f61cde71ea
Scala Steward <me@scala-steward.org>
2021-04-01 02:10:53 +0200

    Scala-Steward: Update pprint from 0.6.0 to 0.6.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -399,7 +399,7 @@ object Dependencies {
     "org.typelevel" %% "cats-effect" % catsEffectV,
     "org.apache.commons" % "commons-lang3" % commonsLang3V,
     "org.apache.commons" % "commons-text" % commonsTextV,
-    "com.lihaoyi" %% "pprint" % "0.6.0",
+    "com.lihaoyi" %% "pprint" % "0.6.3",
   ) ++ catsDependencies ++ configDependencies ++ slf4jFacadeDependencies ++ refinedTypeDependenciesList
 
   val cloudSupportDependencies = googleApiClientDependencies ++ googleCloudDependencies ++ betterFilesDependencies ++ awsCloudDependencies

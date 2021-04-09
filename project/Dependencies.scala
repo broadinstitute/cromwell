c9d8042bfaade2fcb54796e1b986798a2e476594
Scala Steward <me@scala-steward.org>
2021-04-09 04:10:18 +0200

    Scala-Steward: Update scalaz-core from 7.3.2 to 7.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -107,7 +107,7 @@ object Dependencies {
   private val scalamockV = "5.1.0"
   private val scalatestV = "3.2.3"
   private val scalatestPlusMockitoV = "1.0.0-M2"
-  private val scalazV = "7.3.2"
+  private val scalazV = "7.3.3"
   private val scoptV = "4.0.1"
   private val sentryLogbackV = "1.7.30" // scala-steward:off (BA-6640)
   private val shapelessV = "2.3.3"

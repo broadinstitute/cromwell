e88fde1423a52dbe4771bdbaebe65b16754e135a
Miguel Covarrubias <mcovarr@broadinstitute.org>
2021-04-13 20:39:16 -0400

    fixes

--- processes/release_processes/scripts/start_publish_cromwell.sh
+++ processes/release_processes/scripts/start_publish_cromwell.sh
@@ -1,7 +1,6 @@
 # Launch a Cromwell server using the Local backend and a local MySQL instance (such as one launched by
 # `start_publish_mysql_docker.sh`). Well suited for publishing new versions of Cromwell.
 #
-# To run this script the current working directory should be the root of the Cromwell git working tree.
 # Vault authentication is required to render config files, see
 # https://github.com/broadinstitute/dsde-toolbox#authenticating-to-vault.
 # Once authenticated, the Cromwell config files can be rendered with:
@@ -14,14 +13,18 @@
 # Fail loudly if things go wrong https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
 set -euo pipefail
 
-# Set up a number of variables required by the configuration files:
+# https://stackoverflow.com/questions/957928/is-there-a-way-to-get-the-git-root-directory-in-one-command
+# shellcheck disable=SC2155
+export CROMWELL_ROOT=$(git rev-parse --show-toplevel)
+
+# Set up variables required by the configuration files
 export CROMWELL_BUILD_CENTAUR_SLICK_PROFILE=slick.jdbc.MySQLProfile$
 export CROMWELL_BUILD_CENTAUR_JDBC_DRIVER=com.mysql.cj.jdbc.Driver
 export CROMWELL_BUILD_CENTAUR_JDBC_URL="jdbc:mysql://localhost:3306/cromwell_test?allowPublicKeyRetrieval=true&useSSL=false&rewriteBatchedStatements=true&serverTimezone=UTC&useInformationSchema=true"
-export CROMWELL_BUILD_RESOURCES_DIRECTORY="${PWD}/target/ci/resources"
+export CROMWELL_BUILD_RESOURCES_DIRECTORY="${CROMWELL_ROOT}/target/ci/resources"
 export CROMWELL_BUILD_PAPI_JSON_FILE="${CROMWELL_BUILD_RESOURCES_DIRECTORY}/cromwell-centaur-service-account.json"
 export CROMWELL_BUILD_CENTAUR_READ_LINES_LIMIT=128000
 export CROMWELL_BUILD_CENTAUR_256_BITS_KEY="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
 
 # Launch the Cromwell server
-sbt -Dconfig.file=${CROMWELL_BUILD_RESOURCES_DIRECTORY}/local_application.conf "server/run server"
+cd "${CROMWELL_ROOT}" && sbt -Dconfig.file="${CROMWELL_BUILD_RESOURCES_DIRECTORY}"/local_application.conf -Dcall-caching.enabled=true "server/run server"

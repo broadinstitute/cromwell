7f9cfcb29e1be7273e2b5b4858193fcc7f88127e
kshakir (Khalid Shakir) (he/him) <kshakir@broadinstitute.org>
2021-06-10 18:48:18 -0400

    Papi V2 Cascade Lake BT-298 (#6362)

--- common/src/main/scala/common/util/IntUtil.scala
+++
@@ -1,8 +0,0 @@
-package common.util
-
-object IntUtil {
-  implicit class EnhancedInt(val int: Int) extends AnyVal {
-    def isEven: Boolean = int % 2 == 0
-    def isOdd: Boolean = !isEven
-  }
-}

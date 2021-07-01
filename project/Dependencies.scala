577cce21cdf1e080a29aa6dbdea53a05b4cc8d6c
Scala Steward <me@scala-steward.org>
2021-07-01 02:10:37 +0200

    Scala-Steward: Update gax-grpc from 1.62.0 to 1.66.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   private val googleCloudMonitoringV = "2.0.14"
   private val googleCloudNioV = "0.61.0-alpha" // scala-steward:off
   private val googleCloudStorageV = "1.113.14"
-  private val googleGaxGrpcV = "1.62.0"
+  private val googleGaxGrpcV = "1.66.0"
   private val googleGenomicsServicesV2Alpha1ApiV = "v2alpha1-rev20210322-1.31.0"
   private val googleHttpClientApacheV = "2.1.2"
   private val googleHttpClientV = "1.38.0"

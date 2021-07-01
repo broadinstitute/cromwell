e4920a004d0d5d70b3f6a1023cd4d0626663baa5
Scala Steward <me@scala-steward.org>
2021-07-01 02:34:50 +0200

    Scala-Steward: Update cats-effect from 2.3.3 to 2.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   private val apacheHttpClientV = "4.5.13"
   private val awsSdkV = "2.15.41" // scala-steward:off (CROM-6776)
   private val betterFilesV = "3.9.1"
-  private val catsEffectV = "2.3.3"
+  private val catsEffectV = "2.5.1"
   private val catsV = "2.3.1"
   private val circeConfigV = "0.8.0"
   private val circeGenericExtrasV = "0.13.0"

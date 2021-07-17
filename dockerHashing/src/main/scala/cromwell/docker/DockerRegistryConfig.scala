2d34499bee24a4aa24a362eb13f3dbc2b72f7e09
Miguel Covarrubias <mcovarr@broadinstitute.org>
2021-07-17 16:01:50 -0400

    PR feedback

--- dockerHashing/src/main/scala/cromwell/docker/DockerRegistryConfig.scala
+++ dockerHashing/src/main/scala/cromwell/docker/DockerRegistryConfig.scala
@@ -1,6 +1,7 @@
 package cromwell.docker
 
 import java.util.concurrent.Executors
+
 import cats.syntax.apply._
 import common.validation.Validation._
 import net.ceedubs.ficus.Ficus._
@@ -8,14 +9,14 @@ import com.typesafe.config.Config
 import common.validation.ErrorOr.ErrorOr
 import cromwell.core.io.Throttle
 
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+import scala.concurrent.ExecutionContext
 
 case class DockerRegistryConfig(throttle: Option[Throttle], nbThreads: Int) {
-  lazy val executionContext: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(nbThreads))
+  lazy val executionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(nbThreads))
 }
 
 object DockerRegistryConfig {
-  lazy val default: DockerRegistryConfig = DockerRegistryConfig(None, 5)
+  lazy val default = DockerRegistryConfig(None, 5)
 
   def fromConfig(config: Config): ErrorOr[DockerRegistryConfig] = {
     val throttle = validate { config.getAs[Throttle]("throttle") }

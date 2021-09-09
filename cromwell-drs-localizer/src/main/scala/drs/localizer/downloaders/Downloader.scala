821d1e391b6eca8cdec4177561cfb701e33b5395
Miguel Covarrubias <mcovarr@broadinstitute.org>
2021-09-08 21:43:30 -0400

    better words

--- cromwell-drs-localizer/src/main/scala/drs/localizer/downloaders/Downloader.scala
+++ cromwell-drs-localizer/src/main/scala/drs/localizer/downloaders/Downloader.scala
@@ -9,19 +9,19 @@ case object DownloadSuccess extends DownloadResult {
   override def exitCode: ExitCode = ExitCode(0)
 }
 sealed trait DownloadFailure extends DownloadResult
-// For a `FatalDownloadFailure` Cromwell should not make any further download attempts.
+// For a `FatalDownloadFailure` Cromwell should fail the download immediately with no further download attempts.
 case class FatalDownloadFailure(override val exitCode: ExitCode) extends DownloadResult
+// For a `TransientRetryableDownloadFailure` Cromwell should retry without incrementing the retry counter.
+case class TransientRetryableDownloadFailure(override val exitCode: ExitCode) extends DownloadFailure
 sealed trait RetryableDownloadFailure extends DownloadResult
-// For a `TransientRetryableDownloadFailure` Cromwell should not increment the retry counter.
-case class TransientRetryableDownloadFailure(override val exitCode: ExitCode) extends RetryableDownloadFailure
-// A regular retryable download failure
+// A regular retryable download failure corresponding to a known set of conditions (http status, stderr content etc.).
 case class RecognizedRetryableDownloadFailure(override val exitCode: ExitCode) extends RetryableDownloadFailure
 // `UnrecognizedRetryableDownloadFailure` instances are created when the result handling code sees a set of
 // circumstances that isn't expected, shrugs and falls back to the default behavior of retrying an attempt.
-// In terms of retries this should be handled identically to a `RetryableDownloadFailure`.
 case class UnrecognizedRetryableDownloadFailure(override val exitCode: ExitCode) extends RetryableDownloadFailure
 // Checksum failures represent error-free downloads where a checksum on the downloaded data does not match
-// the associated checksum.
+// the associated checksum. Checksums failures are currently retried at most once, while `RetryableDownloadFailure`s
+// are retried up to three times.
 case object ChecksumFailure extends DownloadFailure {
   def exitCode: ExitCode = ExitCode(0)
 }

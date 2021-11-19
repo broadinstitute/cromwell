be00ff92fee362f8d074748e9b84c3062ef21983
Miguel Covarrubias <mcovarr@broadinstitute.org>
2021-11-18 20:19:41 -0500

    Delete v1 cruft and robustify aborts [CROM-6829]

--- supportedBackends/google/pipelines/common/src/main/scala/cromwell/backend/google/pipelines/common/api/clients/PipelinesApiAbortClient.scala
+++ supportedBackends/google/pipelines/common/src/main/scala/cromwell/backend/google/pipelines/common/api/clients/PipelinesApiAbortClient.scala
@@ -4,7 +4,7 @@ import akka.actor.{Actor, ActorLogging, ActorRef}
 import cromwell.backend.google.pipelines.common.PapiInstrumentation
 import cromwell.backend.google.pipelines.common.api.PipelinesApiRequestFactory
 import cromwell.backend.google.pipelines.common.api.PipelinesApiRequestManager.{PAPIAbortRequest, PipelinesApiAbortQueryFailed}
-import cromwell.backend.google.pipelines.common.api.clients.PipelinesApiAbortClient.{PAPIAbortRequestSuccessful, PAPIOperationAlreadyCancelled, PAPIOperationHasAlreadyFinished}
+import cromwell.backend.google.pipelines.common.api.clients.PipelinesApiAbortClient.{PAPIAbortRequestSuccessful, PAPIOperationHasAlreadyFinished}
 import cromwell.backend.standard.StandardAsyncJob
 import cromwell.core.WorkflowId
 import cromwell.core.logging.JobLogging
@@ -12,7 +12,6 @@ import cromwell.core.logging.JobLogging
 object PipelinesApiAbortClient {
   sealed trait PAPIAbortRequestSuccess
   case class PAPIAbortRequestSuccessful(operationId: String) extends PAPIAbortRequestSuccess
-  case class PAPIOperationAlreadyCancelled(operationId: String) extends PAPIAbortRequestSuccess
   case class PAPIOperationHasAlreadyFinished(operationId: String) extends PAPIAbortRequestSuccess
 }
 
@@ -21,20 +20,15 @@ trait PipelinesApiAbortClient { this: Actor with ActorLogging with JobLogging wi
 
   val papiApiActor: ActorRef
   val requestFactory: PipelinesApiRequestFactory
-  
+
   def abortJob(jobId: StandardAsyncJob) = {
     papiApiActor ! PAPIAbortRequest(workflowId, self, requestFactory.cancelRequest(jobId), jobId)
   }
 
   def abortActorClientReceive: Actor.Receive = {
-    /* When we deprecate PAPIv1: v2+ do not seem to distinguish between cancelled and finished, unify into "terminated" or similar [PROD-444] */
-
     case PAPIAbortRequestSuccessful(jobId) =>
       abortSuccess()
       jobLogger.info(s"Successfully requested cancellation of $jobId")
-    // In this case we could immediately return an aborted handle and spare ourselves a round of polling
-    case PAPIOperationAlreadyCancelled(jobId) =>
-      jobLogger.info(s"Operation $jobId was already cancelled")
     // In this case we could immediately return an aborted handle and spare ourselves a round of polling
     case PAPIOperationHasAlreadyFinished(jobId) =>
       jobLogger.info(s"Operation $jobId has already finished")
--- supportedBackends/google/pipelines/v2beta/src/main/scala/cromwell/backend/google/pipelines/v2beta/api/request/AbortRequestHandler.scala
+++ supportedBackends/google/pipelines/v2beta/src/main/scala/cromwell/backend/google/pipelines/v2beta/api/request/AbortRequestHandler.scala
@@ -17,7 +17,7 @@ trait AbortRequestHandler extends LazyLogging { this: RequestHandler =>
     // No need to fail the request if the job already terminated, we're all good
     // As seen in `cromwell.backend.google.pipelines.common.api.clients.PipelinesApiAbortClient` the difference between "finished" and "cancelled" only affects logging
     // Enhance to be more specific if/when Google implements https://partnerissuetracker.corp.google.com/issues/171993833
-    if (Option(e.getCode).contains(400)) {
+    if (Option(e.getCode).contains(400) || Option(e.getMessage).exists(_.contains("Precondition check failed"))) {
       logger.info(s"PAPI declined to abort job ${abortQuery.jobId.jobId} in workflow ${abortQuery.workflowId}, most likely because it is no longer running. Marking as finished. Message: ${e.getMessage}")
       abortQuery.requester ! PAPIOperationHasAlreadyFinished(abortQuery.jobId.jobId)
       Success(())
@@ -28,7 +28,7 @@ trait AbortRequestHandler extends LazyLogging { this: RequestHandler =>
   }
 
   // The Genomics batch endpoint doesn't seem to be able to handle abort requests on V2 operations at the moment
-  // For now, don't batch the request and execute it on its own 
+  // For now, don't batch the request and execute it on its own
   def handleRequest(abortQuery: PAPIAbortRequest, batch: BatchRequest, pollingManager: ActorRef)(implicit ec: ExecutionContext): Future[Try[Unit]] = {
     Future(abortQuery.httpRequest.execute()) map {
       case response if response.isSuccessStatusCode =>

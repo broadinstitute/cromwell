4d6f6fda548567c8919dedb7487d07e6aa01bbfc
Scala Steward <me@scala-steward.org>
2022-01-01 02:14:34 +0100

    Scala-Steward: Update mouse from 1.0.5 to 1.0.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -91,7 +91,7 @@ object Dependencies {
   private val metrics3StatsdV = "4.2.0"
   private val mockFtpServerV = "3.0.0"
   private val mockserverNettyV = "5.11.2"
-  private val mouseV = "1.0.5"
+  private val mouseV = "1.0.8"
   private val mysqlV = "8.0.26"
   private val nettyV = "4.1.66.Final"
   private val owlApiV = "5.1.19"

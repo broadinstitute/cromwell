2b380208c67f3d712295c28e4f0c155413d6a4b6
Scala Steward <me@scala-steward.org>
2022-01-01 02:13:19 +0100

    Scala-Steward: Update hsqldb from 2.6.0 to 2.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
   private val grpcV = "1.41.0"
   private val guavaV = "31.0.1-jre"
   private val heterodonV = "1.0.0-beta3"
-  private val hsqldbV = "2.6.0"
+  private val hsqldbV = "2.6.1"
   private val http4sV = "0.21.7" // scala-steward:off (CROM-6678)
   private val jacksonV = "2.13.0"
   private val janinoV = "3.1.6"

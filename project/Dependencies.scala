0e61a9f9c6ae7b562f3fe37b66312b8ebe7d00d2
Scala Steward <me@scala-steward.org>
2022-01-01 02:12:48 +0100

    Scala-Steward: Update mysql-connector-java from 8.0.26 to 8.0.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -92,7 +92,7 @@ object Dependencies {
   private val mockFtpServerV = "3.0.0"
   private val mockserverNettyV = "5.11.2"
   private val mouseV = "1.0.5"
-  private val mysqlV = "8.0.26"
+  private val mysqlV = "8.0.27"
   private val nettyV = "4.1.66.Final"
   private val owlApiV = "5.1.19"
   private val paradiseV = "2.1.1"

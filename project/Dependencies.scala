2ca1c4d0d1be6953fae2a17496dc0d2e4b0c6d9c
Scala Steward <me@scala-steward.org>
2022-01-01 02:12:27 +0100

    Scala-Steward: Update grpc-core from 1.41.0 to 1.41.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -68,7 +68,7 @@ object Dependencies {
   private val googleOauth2V = "1.1.0"
   private val googleOauthClientV = "1.32.1"
   private val googleCloudResourceManagerV = "1.1.2"
-  private val grpcV = "1.41.0"
+  private val grpcV = "1.41.1"
   private val guavaV = "31.0.1-jre"
   private val heterodonV = "1.0.0-beta3"
   private val hsqldbV = "2.6.0"

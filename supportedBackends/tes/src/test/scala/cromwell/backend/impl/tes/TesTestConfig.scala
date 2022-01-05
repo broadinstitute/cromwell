8b58a97a1d59bc163ccb1dfca8f020bafecc481d
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2022-01-05 16:06:23 -0500

    Pass extra runtime attributes through as TES backend parameters [BT-458] (#6597)
    * When config allows, pass extra runtime attributes through in TES backend params
    
    * Tests
    
    * Give up on generating backend params when warning about unused attrs
    
    * Prettier used key management

--- supportedBackends/tes/src/test/scala/cromwell/backend/impl/tes/TesTestConfig.scala
+++ supportedBackends/tes/src/test/scala/cromwell/backend/impl/tes/TesTestConfig.scala
@@ -24,5 +24,27 @@ object TesTestConfig {
       |""".stripMargin
 
   val backendConfig = ConfigFactory.parseString(backendConfigString)
+
+  private val backendConfigStringWithBackendParams =
+    """
+      |root = "local-cromwell-executions"
+      |dockerRoot = "/cromwell-executions"
+      |endpoint = "http://127.0.0.1:9000/v1/jobs"
+      |use_tes_11_preview_backend_parameters = true
+      |
+      |default-runtime-attributes {
+      |  cpu: 1
+      |  failOnStderr: false
+      |  continueOnReturnCode: 0
+      |  memory: "2 GB"
+      |  disk: "2 GB"
+      |  preemptible: false
+      |  # The keys below have been commented out as they are optional runtime attributes.
+      |  # dockerWorkingDir
+      |  # docker
+      |}
+      |""".stripMargin
+
+  val backendConfigWithBackendParams = ConfigFactory.parseString(backendConfigStringWithBackendParams)
 }
 

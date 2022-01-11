1918360cfccaeb32a5fa24997eaafffc49d29ccf
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-01-10 19:03:47 -0500

    VM cost WIP

---
+++ scripts/tools_metrics/cpu_hours.sql
@@ -0,0 +1,74 @@
+SELECT
+    machine_type.WORKFLOW_EXECUTION_UUID AS WORKFLOW_EXECUTION_UUID,
+    machine_type.CALL_FQN AS CALL_FQN,
+    machine_type.JOB_SCATTER_INDEX AS JOB_SCATTER_INDEX,
+    machine_type.JOB_RETRY_ATTEMPT AS JOB_RETRY_ATTEMPT,
+    start_ts.METADATA_VALUE AS start_timestamp,
+    end_ts.METADATA_VALUE AS end_timestamp,
+    machine_type.METADATA_VALUE AS machine,
+    preemptible.METADATA_VALUE AS preemptible,
+    preemptible.multiplier AS multiplier,
+    machine_type.num_cpus AS num_cpus,
+    machine_type.memory_mib AS memory_mib,
+    TIMESTAMP_DIFF(end_ts.METADATA_TIMESTAMP, start_ts.METADATA_TIMESTAMP, SECOND) AS SECONDS,
+    (machine_type.num_cpus * preemptible.multiplier * (0.033174 / 3600) * TIMESTAMP_DIFF(end_ts.METADATA_TIMESTAMP, start_ts.METADATA_TIMESTAMP, SECOND) ) AS cpu_cost,
+    (machine_type.memory_mib * preemptible.multiplier * (0.004446 / 3600) / 1024.0 * TIMESTAMP_DIFF(end_ts.METADATA_TIMESTAMP, start_ts.METADATA_TIMESTAMP, SECOND) ) AS memory_cost,
+    ( (machine_type.num_cpus * preemptible.multiplier * (0.033174 / 3600) / 1024.0 * TIMESTAMP_DIFF(end_ts.METADATA_TIMESTAMP, start_ts.METADATA_TIMESTAMP, SECOND) ) + (machine_type.memory_mib * preemptible.multiplier * (0.004446 / 3600) * TIMESTAMP_DIFF(end_ts.METADATA_TIMESTAMP, start_ts.METADATA_TIMESTAMP, SECOND) ) ) AS total_cost
+FROM (
+         SELECT
+             *,
+             CAST(SPLIT(METADATA_VALUE,'-')[
+                 OFFSET
+                     (1)] AS INT64) AS num_cpus,
+             CAST(SPLIT(METADATA_VALUE,'-')[
+                 OFFSET
+                     (2)] AS INT64) AS memory_mib
+         FROM
+             `broad-dsde-prod-analytics-dev.warehouse_dev.cromwell_metadata_2022`
+         WHERE
+                 METADATA_KEY = 'jes:machineType'
+           AND METADATA_TIMESTAMP < '2022-01-02') machine_type
+         JOIN (
+    SELECT
+        *
+    FROM
+        `broad-dsde-prod-analytics-dev.warehouse_dev.cromwell_metadata_2022`
+    WHERE
+            METADATA_KEY = 'start'
+      AND METADATA_TIMESTAMP < '2022-01-02') start_ts
+              ON
+                  (machine_type.WORKFLOW_EXECUTION_UUID = start_ts.WORKFLOW_EXECUTION_UUID
+                      AND machine_type.CALL_FQN = start_ts.CALL_FQN
+                      AND machine_type.JOB_SCATTER_INDEX = start_ts.JOB_SCATTER_INDEX
+                      AND machine_type.JOB_RETRY_ATTEMPT = start_ts.JOB_RETRY_ATTEMPT)
+         JOIN (
+    SELECT
+        *
+    FROM
+        `broad-dsde-prod-analytics-dev.warehouse_dev.cromwell_metadata_2022`
+    WHERE
+            METADATA_KEY = 'end'
+      AND METADATA_TIMESTAMP < '2022-01-02') end_ts
+              ON
+                  ( machine_type.WORKFLOW_EXECUTION_UUID = end_ts.WORKFLOW_EXECUTION_UUID
+                      AND machine_type.CALL_FQN = end_ts.CALL_FQN
+                      AND machine_type.JOB_SCATTER_INDEX = end_ts.JOB_SCATTER_INDEX
+                      AND machine_type.JOB_RETRY_ATTEMPT = end_ts.JOB_RETRY_ATTEMPT)
+         JOIN (
+    SELECT
+        *,
+        (CASE METADATA_VALUE
+             WHEN 'false' THEN 1
+             WHEN 'true' THEN 0.1
+            END
+            ) AS multiplier
+    FROM
+        `broad-dsde-prod-analytics-dev.warehouse_dev.cromwell_metadata_2022`
+    WHERE
+            METADATA_KEY = 'preemptible'
+      AND METADATA_TIMESTAMP < '2022-01-02') preemptible
+              ON
+                  ( machine_type.WORKFLOW_EXECUTION_UUID = preemptible.WORKFLOW_EXECUTION_UUID
+                      AND machine_type.CALL_FQN = preemptible.CALL_FQN
+                      AND machine_type.JOB_SCATTER_INDEX = preemptible.JOB_SCATTER_INDEX
+                      AND machine_type.JOB_RETRY_ATTEMPT = preemptible.JOB_RETRY_ATTEMPT)

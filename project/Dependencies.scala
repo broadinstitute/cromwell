a3a7b475aad23d95c1b6f591376c713d829d7e1d
Miguel Covarrubias <mcovarr@users.noreply.github.com>
2022-01-12 13:35:24 -0500

    comment for posterity
    Co-authored-by: Adam Nichols <anichols@broadinstitute.org>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
   private val guavaV = "31.0.1-jre"
   private val heterodonV = "1.0.0-beta3"
   private val hsqldbV = "2.6.0"
-  private val http4sV = "0.21.31"
+  private val http4sV = "0.21.31" // this release is EOL. We need to upgrade further for cats3. https://http4s.org/versions/
   private val jacksonV = "2.13.0"
   private val janinoV = "3.1.6"
   private val jsr305V = "3.0.2"

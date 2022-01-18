1d3dbc2a74fa54165d1ea761e52ef58655a390a6
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-01-18 13:54:32 -0500

    PR feedback

--- scripts/backpressure_report/backpressure_report/lib/backpressure_event.py
+++ scripts/backpressure_report/backpressure_report/lib/backpressure_event.py
@@ -1,6 +1,7 @@
 from backpressure_report.lib import log_utils
 from datetime import datetime
 from dateutil import parser
+import itertools
 from typing import AnyStr
 
 
@@ -38,37 +39,38 @@ def build_backpressure_events_from_log_jsons(logs: list):
     # "backpressure stop" log message for the same pod name.
     in_progress_starts_by_pod = {}
 
-    for log in logs:
-        for entry in log_utils.filter_and_sort_log_entries(log):
-            insert_id = entry['insertId']
-            # skip duplicates
-            if insert_id in seen_insert_ids:
-                continue
+    # Merge the logs so the sorting covers all log entries.
+    merged_logs = itertools.chain(*logs)
+    for entry in log_utils.filter_and_sort_log_entries(merged_logs):
+        insert_id = entry['insertId']
+        # skip duplicates
+        if insert_id in seen_insert_ids:
+            continue
 
-            seen_insert_ids.add(insert_id)
-            # Most of the pod name is the same across all pods, only the bit after the last '-' is unique.
-            pod = entry['resource']['labels']['pod_name'].split('-')[-1]
+        seen_insert_ids.add(insert_id)
+        # Most of the pod name is the same across all pods, only the bit after the last '-' is unique.
+        pod = entry['resource']['labels']['pod_name'].split('-')[-1]
 
-            if log_utils.is_end_event(entry):
-                if pod in in_progress_starts_by_pod.keys():
-                    # Make a backpressure event object
-                    start = parser.isoparse(in_progress_starts_by_pod[pod])
-                    end = parser.isoparse(entry['timestamp'])
-                    event = BackpressureEvent(pod=pod, start=start, end=end)
+        if log_utils.is_end_event(entry):
+            if pod in in_progress_starts_by_pod:
+                # Make a backpressure event object
+                start = parser.isoparse(in_progress_starts_by_pod[pod])
+                end = parser.isoparse(entry['timestamp'])
+                event = BackpressureEvent(pod=pod, start=start, end=end)
 
-                    # Add this object to complete
-                    complete.append(event)
+                # Add this object to complete
+                complete.append(event)
 
-                    # Remove the wip object from in_progress_starts_by_pod
-                    in_progress_starts_by_pod.pop(pod)
+                # Remove the wip object from in_progress_starts_by_pod
+                del in_progress_starts_by_pod[pod]
 
-            elif log_utils.is_start_event(entry):
-                # There are actually two timestamps in the JSON log entries which appear to represent different
-                # concepts: time emitted ('jsonPayload.localTimestamp') versus time added to the log ('timestamp').
-                # Time emitted would seem to be preferable but that value is not specified with a timezone and is
-                # ambiguously interpreted by the parsing code as being EST when it's actually UTC which makes the report
-                # a bit confusing. In practice the timestamps only seem to differ by a millisecond or so, so no big
-                # deal to use 'timestamp' with its explicit UTC timezone.
-                in_progress_starts_by_pod[pod] = entry['timestamp']
+        elif log_utils.is_start_event(entry):
+            # There are actually two timestamps in the JSON log entries which appear to represent different concepts:
+            # time emitted ('jsonPayload.localTimestamp') versus time added to the log ('timestamp'). Time emitted would
+            # seem to be preferable but that value is not specified with a timezone and is ambiguously interpreted by
+            # the parsing code as being EST when it's actually UTC. This can make reading the report a bit confusing or
+            # misleading. In practice the timestamps only seem to differ by small amounts, so no big deal to use
+            # 'timestamp' with its explicit UTC timezone.
+            in_progress_starts_by_pod[pod] = entry['timestamp']
 
     return complete

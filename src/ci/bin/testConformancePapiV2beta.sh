4d8f40931f7b23cf9f8960c900e6b41dde6e8842
Katrina Pierre <kpierre@broadinstitute.org>
2022-03-03 11:22:42 -0500

    Removing blink due to Travis web UI incompatibility

--- src/ci/bin/testConformancePapiV2beta.sh
+++ src/ci/bin/testConformancePapiV2beta.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "$(tput setab 1)$(tput blink)Test is temporarily disabled due to ongoing PAPI 503 flakiness.$(tput sgr 0)"
+echo "$(tput setab 1)Test is temporarily disabled due to ongoing PAPI 503 flakiness.$(tput sgr 0)"
 exit 0

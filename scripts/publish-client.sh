45ffab4b004a2068ee62ffae7b66529c957efc63
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-03-28 19:45:40 -0400

    Scala 2.13 codegen fixes

--- scripts/publish-client.sh
+++ scripts/publish-client.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
- set -e
+set -e
+set -o xtrace
 
 # sbt publish publishes libs to Artifactory for the scala version sbt is running as.
 cd codegen_java
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
-	sbt --warn -Dproject.isSnapshot=false "+ publish"
+	sbt --warn -Dproject.isSnapshot=false "+ publishLocal"
 else
-	sbt --warn -Dproject.isSnapshot=true "+ publish"
+	sbt --warn -Dproject.isSnapshot=true "+ publishLocal"
 fi

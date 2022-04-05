33e710bad7e367ef35c3f724d2877da4a70db9ac
Michael Spector <michael.l.spector@gmail.com>
2022-04-05 08:08:55 -0400

    use StorageException.getReason to detect requester pays

--- filesystems/drs/src/main/scala/cromwell/filesystems/drs/DrsReader.scala
+++ filesystems/drs/src/main/scala/cromwell/filesystems/drs/DrsReader.scala
@@ -96,7 +96,7 @@ case class GcsReader(googleAuthMode: GoogleAuthMode,
         throwable =>
           (requesterPaysProjectIdOption, throwable) match {
             case (Some(requesterPaysProjectId), storageException: StorageException)
-              if storageException.getMessage.contains("requester pays bucket but no user project") =>
+              if storageException.getReason().equals("userProjectMissing") =>
               IO(
                 storage
                   .get(bucketName, objectName, BlobGetOption.userProject(requesterPaysProjectId))

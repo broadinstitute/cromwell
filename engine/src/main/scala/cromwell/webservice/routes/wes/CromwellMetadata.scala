ded7e718e662d7dfb2f7ca6b0ae6d8c273f5c2b5
Katrina P <68349264+kpierre13@users.noreply.github.com>
2022-06-14 12:10:44 -0400

    BW-1256 /runs/{run id} endpoint_KP (#6777)
    * Removed Wes2Cromwell project, added 'runs' endpoint a necessary functions.
    
    * saving...
    
    * saving...
    
    * saving...
    
    * saving...
    
    * Fix missing JSON implicits
    
    * Added endpoint to cromwell.yaml and added api prefix
    
    * Getting rid of unnecessary implicits
    
    * Fixing and changing function name
    
    * Updated RESTAPI.md, cromwell.yaml, and RunListResponse.scala
    
    * Getting rid of comments
    
    * Omitting unexpected arguments
    
    * Removing headers, adding further details in cromwell.yaml
    
    * Updating description and RESTAPI.md
    
    * saving...
    
    * saving...
    
    * saving...
    
    * saving...
    
    * saving...
    
    * Moving functions to consolidate, updating CHANGELOG.md, deleting Wes2CromwellInterface.scala file
    
    * Updated CHANGELOG.md, passed error message to API
    
    * saving...
    
    * Update CHANGELOG.md
    
    Co-authored-by: Adam Nichols <anichols@broadinstitute.org>
    
    * Update engine/src/main/scala/cromwell/webservice/routes/wes/WesRunRoutes.scala
    
    Co-authored-by: Adam Nichols <anichols@broadinstitute.org>
    
    * Update engine/src/main/scala/cromwell/webservice/routes/wes/WesRunRoutes.scala
    
    Co-authored-by: Adam Nichols <anichols@broadinstitute.org>
    
    * Changes to CHANGELOG.md, updating routes
    
    * Unused imports
    
    * Cleanup
    
    * Cleanup
    
    * codecov
    
    * saving...
    
    * saving...
    
    * saving...
    
    * saving...
    
    * runLog method
    
    * saving...
    
    * RESTAPI.md
    
    * Code cleanup
    
    * Code cleanup + comment
    
    * Unused import correction
    
    Co-authored-by: Adam Nichols <aednichols@gmail.com>
    Co-authored-by: Adam Nichols <anichols@broadinstitute.org>
    Co-authored-by: Saloni Shah <salonipshah11@gmail.com>

---
+++ engine/src/main/scala/cromwell/webservice/routes/wes/CromwellMetadata.scala
@@ -0,0 +1,101 @@
+package cromwell.webservice.routes.wes
+
+import spray.json.{JsObject, JsonFormat, JsonParser}
+
+final case class CromwellSubmittedFiles(workflow: Option[String],
+                                        workflowType: Option[String],
+                                        workflowTypeVersion: Option[String],
+                                        options: Option[String],
+                                        inputs: Option[String],
+                                        labels: Option[String]
+                                       )
+
+final case class CromwellCallsMetadata(shardIndex: Option[Int],
+                                       commandLine: Option[String],
+                                       returnCode: Option[Int],
+                                       start: Option[String],
+                                       end: Option[String],
+                                       stdout: Option[String],
+                                       stderr: Option[String]
+                                      )
+
+final case class CromwellMetadata(workflowName: Option[String],
+                                  id: String,
+                                  status: String,
+                                  start: Option[String],
+                                  end: Option[String],
+                                  submittedFiles: CromwellSubmittedFiles,
+                                  outputs: Option[JsObject],
+                                  calls: Option[Map[String, Seq[CromwellCallsMetadata]]]
+                                 ) {
+  import CromwellMetadata._
+
+  def wesRunLog: WesRunLog = {
+    val workflowParams = submittedFiles.inputs.map(JsonParser(_).asJsObject)
+    val workflowTags = submittedFiles.labels.map(JsonParser(_).asJsObject)
+    val workflowEngineParams = submittedFiles.options.map(JsonParser(_).asJsObject)
+
+    val workflowRequest = WesRunRequest(workflow_params = workflowParams,
+      workflow_type = submittedFiles.workflowType.getOrElse("None supplied"),
+      workflow_type_version = submittedFiles.workflowTypeVersion.getOrElse("None supplied"),
+      tags = workflowTags,
+      workflow_engine_parameters = workflowEngineParams,
+      workflow_url = None
+    )
+
+    val workflowLogData = WesLog(name = workflowName,
+      cmd = None,
+      start_time = start,
+      end_time = end,
+      stdout = None,
+      stderr = None,
+      exit_code = None
+    )
+
+    val taskLogs = for {
+      callsArray <- calls.toList
+      (taskName, metadataEntries) <- callsArray
+      metadataEntry <- metadataEntries
+      logEntry = cromwellCallsMetadataEntryToLogEntry(taskName, metadataEntry)
+    } yield logEntry
+
+    WesRunLog(
+      run_id = id,
+      request = workflowRequest,
+      state = WesState.fromStatusString(Option(status)),
+      run_log = Option(workflowLogData),
+      task_logs = Option(taskLogs),
+      outputs = outputs
+    )
+  }
+}
+
+object CromwellMetadata {
+  import spray.json.DefaultJsonProtocol._
+
+  implicit val cromwellCallsMetadataFormat: JsonFormat[CromwellCallsMetadata] = jsonFormat7(CromwellCallsMetadata.apply)
+  implicit val cromwellSubmittedFilesFormat: JsonFormat[CromwellSubmittedFiles] = jsonFormat6(CromwellSubmittedFiles.apply)
+  implicit val cromwellMetadataFormat: JsonFormat[CromwellMetadata] = jsonFormat8(CromwellMetadata.apply)
+
+  def fromJson(json: String): CromwellMetadata = {
+    val jsonAst = JsonParser(json)
+    jsonAst.convertTo[CromwellMetadata]
+  }
+
+  def cromwellCallsMetadataEntryToLogEntry(taskName: String, callsMetadata: CromwellCallsMetadata): WesLog = {
+    val newTaskName = callsMetadata.shardIndex map {
+      case -1 => taskName
+      case notMinusOne => s"$taskName.$notMinusOne"
+    } getOrElse taskName
+
+    WesLog(
+      name = Option(newTaskName),
+      cmd = callsMetadata.commandLine.map(c => List(c)),
+      start_time = callsMetadata.start,
+      end_time = callsMetadata.end,
+      stdout = callsMetadata.stdout,
+      stderr = callsMetadata.stderr,
+      exit_code = callsMetadata.returnCode
+    )
+  }
+}

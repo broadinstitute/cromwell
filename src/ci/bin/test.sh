1c80524c6d5fc700b3bb5de8dde6c6b96768c3af
Adam Nichols <aednichols@gmail.com>
2022-06-29 16:03:35 -0400

    Test

--- src/ci/bin/test.sh
+++ src/ci/bin/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset -o pipefail
+set -x
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh

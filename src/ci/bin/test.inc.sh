7415dc34d0ec911ca5474f1bfd09021fd7ef2d25
Adam Nichols <aednichols@gmail.com>
2022-06-30 17:54:31 -0400

    Restore unrelated, inoffensive comment

--- src/ci/bin/test.inc.sh
+++ src/ci/bin/test.inc.sh
@@ -154,6 +154,11 @@ cromwell::private::create_build_variables() {
         CROMWELL_BUILD_GIT_HASH_SUFFIX="gUNKNOWN"
     fi
 
+    # Value of the `TRAVIS_BRANCH` variable depends on type of Travis build: if it is pull request build, the value
+    # will be the name of the branch targeted by the pull request, and for push builds it will be the name of the
+    # branch. So, in case of push builds `git diff` will always return empty result. This is why we only use this short
+    # circuiting logic for pull request builds
+
     case "${CROMWELL_BUILD_PROVIDER}" in
         "${CROMWELL_BUILD_PROVIDER_TRAVIS}")
             CROMWELL_BUILD_IS_CI=true

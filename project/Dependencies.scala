0fe333e42798e794d38fd5a4a52ceccfe8da4178
Christian Freitas <christian.j.freitas@gmail.com>
2022-07-22 08:57:50 -0400

    BT-684 Remove unecessary dependency strings

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,6 @@ object Dependencies {
   // We would like to use the BOM to manage Azure SDK versions, but SBT doesn't support it.
   // https://github.com/Azure/azure-sdk-for-java/tree/main/sdk/boms/azure-sdk-bom
   // https://github.com/sbt/sbt/issues/4531
-  private val azureStorageBlobV = "12.17.0"
   private val azureStorageBlobNioV = "12.0.0-beta.18"
   private val azureIdentitySdkV = "1.4.2"
   private val azureKeyVaultSdkV = "4.3.7"

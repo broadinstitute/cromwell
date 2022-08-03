16bdb3a6280d29d6475eea7e19f4e18fdcc685ac
Christian Freitas <christian.j.freitas@gmail.com>
2022-08-03 11:13:40 -0400

    Fix imports for brackets

--- engine/src/test/scala/cromwell/webservice/routes/wes/ServiceInfoSpec.scala
+++ engine/src/test/scala/cromwell/webservice/routes/wes/ServiceInfoSpec.scala
@@ -3,8 +3,7 @@ package cromwell.webservice.routes.wes
 import akka.actor.Props
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.util.Timeout
-import cromwell.languages.config.CromwellLanguages
-import cromwell.languages.config.LanguageConfiguration
+import cromwell.languages.config.{CromwellLanguages, LanguageConfiguration}
 import cromwell.webservice.routes.CromwellApiService
 import cromwell.webservice.routes.CromwellApiServiceSpec.MockWorkflowStoreActor
 import org.scalatest.flatspec.AsyncFlatSpec

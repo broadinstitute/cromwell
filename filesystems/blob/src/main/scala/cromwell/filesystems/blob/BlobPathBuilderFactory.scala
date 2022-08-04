df7c09c7ef0c12a10d069f40e5335c8512cbd6cb
Christian Freitas <christian.j.freitas@gmail.com>
2022-08-04 09:04:10 -0400

    BT-710 Add configs for BlobPathBuilderFactory (#6817)
    BT-710 Add configs for BlobPathBuilderFactory

--- filesystems/blob/src/main/scala/cromwell/filesystems/blob/BlobPathBuilderFactory.scala
+++ filesystems/blob/src/main/scala/cromwell/filesystems/blob/BlobPathBuilderFactory.scala
@@ -6,15 +6,20 @@ import com.typesafe.config.Config
 import cromwell.core.WorkflowOptions
 import cromwell.core.path.PathBuilderFactory
 import cromwell.filesystems.blob.BlobPathBuilder
+import net.ceedubs.ficus.Ficus._
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
-final case class BlobPathBuilderFactory(globalConfig: Config, instanceConfig: Config) extends PathBuilderFactory {
+final case class BlobFileSystemConfig(config: Config)
+final case class BlobPathBuilderFactory(globalConfig: Config, instanceConfig: Config, singletonConfig: BlobFileSystemConfig) extends PathBuilderFactory {
+  val sasToken: String = instanceConfig.as[String]("sas-token")
+  val container: String = instanceConfig.as[String]("store")
+  val endpoint: String = instanceConfig.as[String]("endpoint")
+  val workspaceId: String = instanceConfig.as[String]("workspace-id")
+  val workspaceManagerURL = singletonConfig.config.as[String]("workspace-manager-url")
+
   override def withOptions(options: WorkflowOptions)(implicit as: ActorSystem, ec: ExecutionContext): Future[BlobPathBuilder] = {
-    val sasToken: String = instanceConfig.getString("sasToken")
-    val container: String = instanceConfig.getString("store")
-    val endpoint: String = instanceConfig.getString("endpoint")
     Future {
       new BlobPathBuilder(new AzureSasCredential(sasToken), container, endpoint)
     }
---
+++ filesystems/blob/src/test/scala/cromwell/filesystems/blob/BlobPathBuilderFactorySpec.scala
@@ -0,0 +1,31 @@
+package cromwell.filesystems.blob
+
+import com.typesafe.config.ConfigFactory
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class BlobPathBuilderFactorySpec extends AnyFlatSpec with Matchers {
+
+  it should "parse configs for a functioning factory" in {
+    val endpoint = BlobPathBuilderSpec.buildEndpoint("coaexternalstorage")
+    val store = "inputs"
+    val sasToken = "{SAS TOKEN HERE}"
+    val workspaceId = "mockWorkspaceId"
+    val workspaceManagerURL = "https://test.ws.org"
+    val instanceConfig = ConfigFactory.parseString(
+      s"""
+      |sas-token = "$sasToken"
+      |store = "$store"
+      |endpoint = "$endpoint"
+      |workspace-id = "$workspaceId"
+      """.stripMargin)
+    val singletonConfig = ConfigFactory.parseString(s"""workspace-manager-url = "$workspaceManagerURL" """)
+    val globalConfig = ConfigFactory.parseString("""""")
+    val factory = BlobPathBuilderFactory(globalConfig, instanceConfig, new BlobFileSystemConfig(singletonConfig))
+    factory.container should equal(store)
+    factory.endpoint should equal(endpoint)
+    factory.sasToken should equal(sasToken)
+    factory.workspaceId should equal(workspaceId)
+    factory.workspaceManagerURL should equal(workspaceManagerURL)
+  }
+}

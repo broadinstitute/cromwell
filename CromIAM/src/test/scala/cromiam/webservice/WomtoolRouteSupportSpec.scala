34ed82b9bb72d2c3cf679ea1c4dc78aa8939cfc2
Adam Nichols <aednichols@gmail.com>
2022-08-11 18:48:47 -0400

    Test update

--- CromIAM/src/test/scala/cromiam/webservice/WomtoolRouteSupportSpec.scala
+++ CromIAM/src/test/scala/cromiam/webservice/WomtoolRouteSupportSpec.scala
@@ -2,6 +2,7 @@ package cromiam.webservice
 
 import akka.http.scaladsl.model.ContentTypes
 import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.headers.{Authorization, OAuth2BearerToken, RawHeader}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import common.assertion.CromwellTimeoutSpec
 import org.scalatest.flatspec.AnyFlatSpec
@@ -16,7 +17,11 @@ class WomtoolRouteSupportSpec extends AnyFlatSpec with CromwellTimeoutSpec with 
   behavior of "Womtool endpoint routes"
 
   it should "return 200 when we request to the right path" in {
-    Post(s"/api/womtool/v1/describe") ~> womtoolRoutes ~> check {
+    Post(
+      s"/api/womtool/v1/describe"
+    ).withHeaders(
+      List(Authorization(OAuth2BearerToken("my-token")), RawHeader("OIDC_CLAIM_user_id", "Zardoz@example.com"))
+    ) ~> womtoolRoutes ~> check {
       status shouldBe OK
       responseAs[String] shouldBe "Hey there, workflow describer"
       contentType should be(ContentTypes.`text/plain(UTF-8)`)

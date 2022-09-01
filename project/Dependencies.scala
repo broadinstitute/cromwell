f6078286db631ac3bb5fb1331eddfab5d5390f2d
Scala Steward <scala_steward@virtuslab.com>
2022-09-01 13:13:10 +0000

    Scala-Steward: Update guava from 31.0.1-jre to 31.1-jre

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
   private val googleOauthClientV = "1.33.1"
   private val googleCloudResourceManagerV = "1.2.5"
   private val grpcV = "1.45.0"
-  private val guavaV = "31.0.1-jre"
+  private val guavaV = "31.1-jre"
   private val heterodonV = "1.0.0-beta3"
   private val hsqldbV = "2.6.1"
   private val http4sV = "0.21.31" // this release is EOL. We need to upgrade further for cats3. https://http4s.org/versions/

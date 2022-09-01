0d2eb06abfcba831a229fbdd0c5bf315cb37828c
Scala Steward <scala_steward@virtuslab.com>
2022-09-01 13:14:20 +0000

    Scala-Steward: Update typesafe:config from 1.4.1 to 1.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -123,7 +123,7 @@ object Dependencies {
   private val swaggerUiV = "4.5.0"
   private val testContainersScalaV = "0.40.2"
   private val tikaV = "2.3.0"
-  private val typesafeConfigV = "1.4.1"
+  private val typesafeConfigV = "1.4.2"
   private val workbenchGoogleV = "0.21-5c9c4f6" // via: https://github.com/broadinstitute/workbench-libs/blob/develop/google/CHANGELOG.md
   private val workbenchModelV = "0.15-f9f0d4c" // via: https://github.com/broadinstitute/workbench-libs/blob/develop/model/CHANGELOG.md
   private val workbenchUtilV = "0.6-65bba14" // via: https://github.com/broadinstitute/workbench-libs/blob/develop/util/CHANGELOG.md

cfb6dc1b314234b718a62bfb527b096d07d8089b
Scala Steward <scala_steward@virtuslab.com>
2022-09-01 13:09:33 +0000

    Scala-Steward: Update azure-resourcemanager from 2.17.0 to 2.18.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -192,7 +192,7 @@ object Dependencies {
       exclude("jakarta.xml.bind", "jakarta.xml.bind-api")
       exclude("jakarta.activation", "jakarta.activation-api"),
     "com.azure" % "azure-core-management" % "1.7.0",
-    "com.azure.resourcemanager" % "azure-resourcemanager" % "2.17.0"
+    "com.azure.resourcemanager" % "azure-resourcemanager" % "2.18.0"
   )
 
   val implFtpDependencies = List(

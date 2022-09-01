f227bf868028477824cf6f766cf64e6f9df1f669
Scala Steward <scala_steward@virtuslab.com>
2022-09-01 13:28:36 +0000

    Scala-Steward: Update hsqldb from 2.6.1 to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,7 @@ object Dependencies {
   private val grpcV = "1.45.0"
   private val guavaV = "31.0.1-jre"
   private val heterodonV = "1.0.0-beta3"
-  private val hsqldbV = "2.6.1"
+  private val hsqldbV = "2.7.0"
   private val http4sV = "0.21.31" // this release is EOL. We need to upgrade further for cats3. https://http4s.org/versions/
   private val jacksonV = "2.13.3"
   private val janinoV = "3.1.6"

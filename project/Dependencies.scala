f64b2c45c38426e495a46d6630348fb8dec56e18
Christian Freitas <christian.j.freitas@gmail.com>
2022-10-24 14:09:56 -0400

    Update commons text to 1.10.0 (#6937)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   private val commonsLang3V = "3.12.0"
   private val commonsMathV = "3.6.1"
   private val commonNetV = "3.8.0" // via: https://commons.apache.org/proper/commons-net/
-  private val commonsTextV = "1.9"
+  private val commonsTextV = "1.10.0"
   private val configsV = "0.6.1"
   private val delightRhinoSandboxV = "0.0.15"
   private val diffsonSprayJsonV = "4.1.1"

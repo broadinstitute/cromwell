e305c07c9716757733846960d33553cff5aa0703
Christian Freitas <christian.j.freitas@gmail.com>
2022-11-21 14:23:36 -0500

    WX-696 Enable getting SAS token from WSM

--- filesystems/blob/src/main/scala/cromwell/filesystems/blob/BlobPathBuilderFactory.scala
+++ filesystems/blob/src/main/scala/cromwell/filesystems/blob/BlobPathBuilderFactory.scala
@@ -1,13 +1,22 @@
 package cromwell.filesystems.blob
 
 import akka.actor.ActorSystem
+import cats.syntax.validated._
+import com.azure.core.credential.TokenRequestContext
+import com.azure.core.management.AzureEnvironment
+import com.azure.core.management.profile.AzureProfile
+import com.azure.identity.DefaultAzureCredentialBuilder
 import com.typesafe.config.Config
+import common.validation.ErrorOr.ErrorOr
 import cromwell.core.WorkflowOptions
 import cromwell.core.path.PathBuilderFactory
 import cromwell.core.path.PathBuilderFactory.PriorityBlob
 import net.ceedubs.ficus.Ficus._
 
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.DurationConverters._
+import scala.util.{Failure, Success, Try}
 
 final case class BlobFileSystemConfig(config: Config)
 
@@ -24,7 +33,7 @@ final case class BlobPathBuilderFactory(globalConfig: Config, instanceConfig: Co
   val workspaceId: Option[WorkspaceId] = instanceConfig.as[Option[String]]("workspace-id").map(WorkspaceId)
   val expiryBufferMinutes: Long = instanceConfig.as[Option[Long]]("expiry-buffer-minutes").getOrElse(10)
   val workspaceManagerURL: Option[WorkspaceManagerURL] = singletonConfig.config.as[Option[String]]("workspace-manager-url").map(WorkspaceManagerURL)
-  val b2cToken: Option[String] = instanceConfig.as[Option[String]]("b2cToken")
+  val b2cToken: Option[String] = AzureCredentials(None).getAccessToken.toOption
 
   val blobTokenGenerator: BlobTokenGenerator = (workspaceManagerURL, b2cToken, workspaceId) match {
     case (Some(url), Some(token), Some(workspaceId)) =>
@@ -44,4 +53,43 @@ final case class BlobPathBuilderFactory(globalConfig: Config, instanceConfig: Co
   }
 
   override def priority: Int = PriorityBlob
+}
+
+/**
+  * Strategy for obtaining an access token in an environment with available Azure identity.
+  * If you need to disambiguate among multiple active user-assigned managed identities, pass
+  * in the client id of the identity that should be used.
+  */
+case class AzureCredentials(identityClientId: Option[String]) {
+
+  final val tokenAcquisitionTimeout = 30.seconds
+
+  val azureProfile = new AzureProfile(AzureEnvironment.AZURE)
+  val tokenScope = "https://management.azure.com/.default"
+
+  def tokenRequestContext: TokenRequestContext = {
+    val trc = new TokenRequestContext()
+    trc.addScopes(tokenScope)
+    trc
+  }
+
+  def defaultCredentialBuilder: DefaultAzureCredentialBuilder =
+    new DefaultAzureCredentialBuilder()
+      .authorityHost(azureProfile.getEnvironment.getActiveDirectoryEndpoint)
+
+  def getAccessToken: ErrorOr[String] = {
+    val credentials = identityClientId.foldLeft(defaultCredentialBuilder) {
+      (builder, clientId) => builder.managedIdentityClientId(clientId)
+    }.build()
+
+    Try(
+      credentials
+        .getToken(tokenRequestContext)
+        .block(tokenAcquisitionTimeout.toJava)
+    ) match {
+      case Success(null) => "null token value attempting to obtain access token".invalidNel
+      case Success(token) => token.getToken.validNel
+      case Failure(error) => s"Failed to refresh access token: ${error.getMessage}".invalidNel
+    }
+  }
 }

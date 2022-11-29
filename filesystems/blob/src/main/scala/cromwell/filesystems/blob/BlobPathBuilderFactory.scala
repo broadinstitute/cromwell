38cd10307ce249272f97561489ee3d7985247c0f
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2022-11-29 11:00:28 -0500

    Move AzureCredentials to separate file

--- filesystems/blob/src/main/scala/cromwell/filesystems/blob/BlobPathBuilderFactory.scala
+++ filesystems/blob/src/main/scala/cromwell/filesystems/blob/BlobPathBuilderFactory.scala
@@ -1,22 +1,13 @@
 package cromwell.filesystems.blob
 
 import akka.actor.ActorSystem
-import cats.syntax.validated._
-import com.azure.core.credential.TokenRequestContext
-import com.azure.core.management.AzureEnvironment
-import com.azure.core.management.profile.AzureProfile
-import com.azure.identity.DefaultAzureCredentialBuilder
 import com.typesafe.config.Config
-import common.validation.ErrorOr.ErrorOr
 import cromwell.core.WorkflowOptions
 import cromwell.core.path.PathBuilderFactory
 import cromwell.core.path.PathBuilderFactory.PriorityBlob
 import net.ceedubs.ficus.Ficus._
 
-import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.jdk.DurationConverters._
-import scala.util.{Failure, Success, Try}
 
 final case class BlobFileSystemConfig(config: Config)
 
@@ -74,43 +65,4 @@ final case class BlobPathBuilderFactory(globalConfig: Config, instanceConfig: Co
   }
 
   override def priority: Int = PriorityBlob
-}
-
-/**
-  * Strategy for obtaining an access token in an environment with available Azure identity.
-  * If you need to disambiguate among multiple active user-assigned managed identities, pass
-  * in the client id of the identity that should be used.
-  */
-case class AzureCredentials(identityClientId: Option[String]) {
-
-  final val tokenAcquisitionTimeout = 5.seconds
-
-  val azureProfile = new AzureProfile(AzureEnvironment.AZURE)
-  val tokenScope = "https://management.azure.com/.default"
-
-  def tokenRequestContext: TokenRequestContext = {
-    val trc = new TokenRequestContext()
-    trc.addScopes(tokenScope)
-    trc
-  }
-
-  def defaultCredentialBuilder: DefaultAzureCredentialBuilder =
-    new DefaultAzureCredentialBuilder()
-      .authorityHost(azureProfile.getEnvironment.getActiveDirectoryEndpoint)
-
-  def getAccessToken: ErrorOr[String] = {
-    val credentials = identityClientId.foldLeft(defaultCredentialBuilder) {
-      (builder, clientId) => builder.managedIdentityClientId(clientId)
-    }.build()
-
-    Try(
-      credentials
-        .getToken(tokenRequestContext)
-        .block(tokenAcquisitionTimeout.toJava)
-    ) match {
-      case Success(null) => "null token value attempting to obtain access token".invalidNel
-      case Success(token) => token.getToken.validNel
-      case Failure(error) => s"Failed to refresh access token: ${error.getMessage}".invalidNel
-    }
-  }
 }

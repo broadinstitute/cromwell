2242b749e248840fa2f8d94535f3cea6171a0f58
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2022-12-16 10:08:45 -0500

    WX-867 Translate crc32c hashes to b64 for getm (#6970)
    * Translate crc32c hashes to b64 for getm
    
    * Update tests
    
    * Remove obsolete b64 handling for md5, centralize hex validation
    
    * Restore old test, fix other test

--- cromwell-drs-localizer/src/test/scala/drs/localizer/downloaders/GetmChecksumSpec.scala
+++ cromwell-drs-localizer/src/test/scala/drs/localizer/downloaders/GetmChecksumSpec.scala
@@ -31,9 +31,10 @@ class GetmChecksumSpec extends AnyFlatSpec with CromwellTimeoutSpec with Matcher
     val results = Table(
       ("description", "algorithm", "expected"),
       ("md5 hex", Md5("abcdef"), "--checksum-algorithm 'md5' --checksum abcdef".validNel),
-      ("md5 base64", Md5("cR84lXY1y17c3q7/7riLEA=="), "--checksum-algorithm 'md5' --checksum 711f38957635cb5edcdeaeffeeb88b10".validNel),
-      ("md5 gibberish", Md5("what is this???"), "Invalid md5 checksum value is neither hex nor base64: what is this???".invalidNel),
-      ("crc32c", Crc32c("012345"), "--checksum-algorithm 'gs_crc32c' --checksum 012345".validNel),
+      ("md5 base64", Md5("cR84lXY1y17c3q7/7riLEA=="), "Invalid checksum value, expected hex but got: cR84lXY1y17c3q7/7riLEA==".invalidNel),
+      ("md5 gibberish", Md5("what is this???"), "Invalid checksum value, expected hex but got: what is this???".invalidNel),
+      ("crc32c", Crc32c("012345"), "--checksum-algorithm 'gs_crc32c' --checksum ASNF".validNel),
+      ("crc32c gibberish", Crc32c("????"), "Invalid checksum value, expected hex but got: ????".invalidNel),
       ("AWS ETag", AwsEtag("012345"), "--checksum-algorithm 's3_etag' --checksum 012345".validNel),
       // Escape checksum values constructed from unvalidated data returned by DRS servers.
       ("Unsupported", Unsupported("Robert'); DROP TABLE Students;\n --\\"), raw"--checksum-algorithm 'null' --checksum Robert\'\)\;\ DROP\ TABLE\ Students\;\ --\\".validNel),
@@ -44,6 +45,21 @@ class GetmChecksumSpec extends AnyFlatSpec with CromwellTimeoutSpec with Matcher
       it should s"produce the expected checksum arguments for $description" in {
         algorithm.args shouldBe expected
       }
+    }
+  }
+
+  it should "correctly validate hex strings" in {
+    val results = Table(
+      ("test string", "expected"),
+      ("", "Invalid checksum value, expected hex but got: ".invalidNel),
+      (" ", "Invalid checksum value, expected hex but got: ".invalidNel),
+      ("myfavoritestring", "Invalid checksum value, expected hex but got: myfavoritestring".invalidNel),
+      (" AbC123 ", "AbC123".validNel),
+      ("456", "456".validNel),
+    )
+
+    forAll(results) { (testString, expected) =>
+      GetmChecksum.validateHex(testString) shouldBe expected
     }
   }
 }

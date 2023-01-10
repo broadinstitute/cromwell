8f0c786f07a0b3e276d7906db61077028394e3d2
jakejennings <jjennings@burwood.com>
2023-01-10 14:24:38 -0600

    add batch configuration

---
+++ supportedBackends/google/pipelines/batch/src/main/scala/cromwell/backend/google/pipelines/batch/GcpBatchConfiguration.scala
@@ -0,0 +1,36 @@
+package cromwell.backend.google.pipelines.batch
+
+import com.typesafe.config.Config
+import cromwell.backend.BackendConfigurationDescriptor
+import cromwell.backend.google.pipelines.common.api.PipelinesApiFactoryInterface
+import cromwell.backend.google.pipelines.common.authentication.{PipelinesApiAuths, PipelinesApiDockerCredentials}
+import cromwell.cloudsupport.gcp.GoogleConfiguration
+import cromwell.core.BackendDockerConfiguration
+import net.ceedubs.ficus.Ficus._
+import spray.json._
+
+import scala.concurrent.duration.FiniteDuration
+
+class GcpBatchConfiguration(val configurationDescriptor: BackendConfigurationDescriptor,
+                                //val batchFactory: GcpBatchFactoryInterface,
+                                val googleConfig: GoogleConfiguration,
+                                val batchAttributes: GcpBatchConfigurationAttributes) extends DefaultJsonProtocol {
+
+  // val jesAuths: PipelinesApiAuths = papiAttributes.auths
+  val root: String = configurationDescriptor.backendConfig.getString("root")
+  val pipelineTimeout: FiniteDuration = batchAttributes.pipelineTimeout
+  val runtimeConfig: Option[Config] = configurationDescriptor.backendRuntimeAttributesConfig
+
+  //val dockerCredentials: Option[PipelinesApiDockerCredentials] = {
+  //  BackendDockerConfiguration.build(configurationDescriptor.backendConfig).dockerCredentials map { creds =>
+  //    PipelinesApiDockerCredentials.apply(creds, googleConfig)
+  //  }
+  //}
+
+  // val dockerEncryptionKeyName: Option[String] = dockerCredentials flatMap { _.keyName }
+  // val dockerEncryptionAuthName: Option[String] = dockerCredentials flatMap { _.authName }
+  // val dockerToken: Option[String] = dockerCredentials map { _.token }
+
+  val jobShell: String = configurationDescriptor.backendConfig.as[Option[String]]("job-shell").getOrElse(
+    configurationDescriptor.globalConfig.getOrElse("system.job-shell", "/bin/bash"))
+}

8c605fa5bf586447f49c14af0a5430c69d1d96ba
jarroyo28 <johnarroyo45@gmail.com>
2023-01-11 11:29:31 -0600

    Added GCPBatchJob case class

---
+++ supportedBackends/google/pipelines/batch/src/main/scala/cromwell/backend/google/pipelines/batch/GCPBatchJob.scala
@@ -0,0 +1,44 @@
+package cromwell.backend.google.pipelines.batch
+import com.google.cloud.batch.v1.AllocationPolicy.{InstancePolicy, InstancePolicyOrTemplate}
+import com.google.cloud.batch.v1.Runnable.Container
+import com.google.cloud.batch.v1.{AllocationPolicy, BatchServiceClient, ComputeResource, CreateJobRequest, Job, LogsPolicy, Runnable, TaskGroup, TaskSpec}
+import cromwell.backend.google.pipelines.batch.GcpBatchBackendSingletonActor.BatchRequest
+import com.google.protobuf.Duration
+import com.google.cloud.batch.v1.LogsPolicy.Destination
+import java.util.concurrent.TimeUnit
+
+final case class GCPBatchJob(
+                             jobSubmission: BatchRequest,
+                             cpu: Long,
+                             memory: Long,
+                             machineType: String,
+                             dockerImage: String,
+                            ) {
+
+  // VALUES HERE
+  val entryPoint = "/bin/sh"
+  val retryCount = 2
+  val durationInSeconds: Long = 3600
+  val taskCount: Long = 1
+  val parentPath = "projects/%s/locations/%s"
+
+  def submitJob(): Unit = {
+    try {
+      val batchServiceClient = BatchServiceClient.create
+      val runnable = Runnable.newBuilder.setContainer((Container.newBuilder.setImageUri(dockerImage).setEntrypoint(entryPoint).addCommands("-c").addCommands("echo Hello World!").build)).build
+      val computeResource = ComputeResource.newBuilder.setCpuMilli(cpu).setMemoryMib(memory).build
+      val task = TaskSpec.newBuilder.addRunnables(runnable).setComputeResource(computeResource).setMaxRetryCount(retryCount).setMaxRunDuration(Duration.newBuilder.setSeconds(durationInSeconds).build)
+      val taskGroup = TaskGroup.newBuilder.setTaskCount(taskCount).setTaskSpec(task).build
+      val instancePolicy = InstancePolicy.newBuilder.setMachineType(machineType).build
+      val allocationPolicy = AllocationPolicy.newBuilder.addInstances(InstancePolicyOrTemplate.newBuilder.setPolicy(instancePolicy).build).build
+      val job = Job.newBuilder.addTaskGroups(taskGroup).setAllocationPolicy(allocationPolicy).putLabels("env", "testing").putLabels("type", "script").setLogsPolicy(LogsPolicy.newBuilder.setDestination(Destination.CLOUD_LOGGING).build)
+      val parent = (String.format(parentPath, jobSubmission.projectId, jobSubmission.region))
+      val createJobRequest = CreateJobRequest.newBuilder.setParent(parent).setJob(job).setJobId(jobSubmission.jobName).build()
+      val result = batchServiceClient.createJobCallable.futureCall(createJobRequest).get(3, TimeUnit.MINUTES)
+      println("RESULT HERE: " + result.getName)
+    } catch {
+      case _: Throwable => println("OOPS")
+    }
+
+  }
+}
--- supportedBackends/google/pipelines/batch/src/main/scala/cromwell/backend/google/pipelines/batch/GcpBatchBackendSingletonActor.scala
+++ supportedBackends/google/pipelines/batch/src/main/scala/cromwell/backend/google/pipelines/batch/GcpBatchBackendSingletonActor.scala
@@ -1,26 +1,9 @@
 package cromwell.backend.google.pipelines.batch
 
 import akka.actor.{Actor, ActorLogging, Props}
-
-//import scala.concurrent.Promise
-//import scala.util.Try
-
-//import scala.concurrent.Future
-//import scala.util.Try
-
-//import scala.concurrent.duration.DurationInt
-//import akka.actor.{Actor, ActorSystem, ActorLogging, Props}
-//import akka.actor.{Actor, ActorSystem, ActorLogging, ActorRef, Props}
-import com.google.cloud.batch.v1.AllocationPolicy.{InstancePolicy, InstancePolicyOrTemplate}
-import com.google.cloud.batch.v1.LogsPolicy.Destination
-import com.google.cloud.batch.v1.Runnable.Container
-import com.google.cloud.batch.v1.{AllocationPolicy, BatchServiceClient, ComputeResource, CreateJobRequest, Job, LogsPolicy, Runnable, TaskGroup, TaskSpec}
-
-import com.google.protobuf.Duration
 import scala.concurrent.ExecutionContext
 
 
-import java.util.concurrent.TimeUnit
 
 object GcpBatchBackendSingletonActor {
   def props(name: String) = Props(new GcpBatchBackendSingletonActor(name))
@@ -37,22 +20,10 @@ final class GcpBatchBackendSingletonActor (name: String) extends Actor with Acto
 
 
   def receive: Receive = {
-
     case jobSubmission: BatchRequest =>
-      val batchServiceClient = BatchServiceClient.create
-      val runnable = Runnable.newBuilder.setContainer((Container.newBuilder.setImageUri("gcr.io/google-containers/busybox").setEntrypoint("/bin/sh").addCommands("-c").addCommands("echo Hello World!").build)).build
-      val computeResource = ComputeResource.newBuilder.setCpuMilli(2000).setMemoryMib(200).build
-      val task = TaskSpec.newBuilder.addRunnables(runnable).setComputeResource(computeResource).setMaxRetryCount(2).setMaxRunDuration(Duration.newBuilder.setSeconds(3600).build)
-      val taskGroup = TaskGroup.newBuilder.setTaskCount(1).setTaskSpec(task).build
-      val instancePolicy = InstancePolicy.newBuilder.setMachineType("e2-standard-4").build
-      val allocationPolicy = AllocationPolicy.newBuilder.addInstances(InstancePolicyOrTemplate.newBuilder.setPolicy(instancePolicy).build).build
-      val job = Job.newBuilder.addTaskGroups(taskGroup).setAllocationPolicy(allocationPolicy).putLabels("env", "testing").putLabels("type", "script").setLogsPolicy(LogsPolicy.newBuilder.setDestination(Destination.CLOUD_LOGGING).build)
-      val parent = (String.format("projects/%s/locations/%s", jobSubmission.projectId, jobSubmission.region))
-      val createJobRequest = CreateJobRequest.newBuilder.setParent(parent).setJob(job).setJobId(jobSubmission.jobName).build()
-      val result = batchServiceClient.createJobCallable.futureCall(createJobRequest).get(3, TimeUnit.MINUTES)
-      println(result.getName)
-
-
+      val job = GCPBatchJob(jobSubmission, 2000, 200, "e2-standard-4", "gcr.io/google-containers/busybox")
+      job.submitJob()
+      println("WE ARE INSIDE THE CASE JOB THAT HAS JUST BEEN UPDATED *************************")
     case other =>
       log.error("Unknown message to GCP Batch Singleton Actor: {}. Dropping it.", other)
 

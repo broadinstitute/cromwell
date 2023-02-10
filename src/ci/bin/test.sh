6b25da0d19b2f885be9e3ece7bd71d7e7f50d60f
Tom Wiseman <twiseman@broadinstitute.org>
2023-02-10 15:24:21 -0500

    whitespace

--- src/ci/bin/test.sh
+++ src/ci/bin/test.sh
@@ -4,4 +4,5 @@ set -o errexit -o nounset -o pipefail
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
+
 cromwell::build::exec_test_script

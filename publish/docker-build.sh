adb44c5f6fb20d2ce4cd7b0f1a109b4c405347ef
Adam Nichols <aednichols@gmail.com>
2023-02-16 11:03:13 -0500

    Hack

--- publish/docker-build.sh
+++ publish/docker-build.sh
@@ -4,6 +4,10 @@
 
 set -euo pipefail
 
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+docker buildx create --use --name multi-arch-builder3
+
 build_root="$( dirname "${BASH_SOURCE[0]}" )"
-docker build "${build_root}" -t broadinstitute/cromwell-publish
-docker push broadinstitute/cromwell-publish
+docker buildx build "${build_root}" --platform linux/amd64,linux/arm64 -t broadinstitute/cromwell-publish:latest --push
+#docker push broadinstitute/cromwell-publish

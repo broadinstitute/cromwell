3de1fc23491796972d240408c4974aeda7120205
dspeck1 <dspeck@burwood.com>
2023-02-24 09:50:15 -0600

    Added job not found exceptions.  Removed future from poll async.  Added logging to supported classes and code cleanpu

--- supportedBackends/google/pipelines/batch/src/main/scala/cromwell/backend/google/pipelines/batch/GcpBatchBackendLifecycleActorFactory.scala
+++ supportedBackends/google/pipelines/batch/src/main/scala/cromwell/backend/google/pipelines/batch/GcpBatchBackendLifecycleActorFactory.scala
@@ -1,21 +1,12 @@
 package cromwell.backend.google.pipelines.batch
 
 import akka.actor.{ActorRef, Props}
-//import com.google.api.client.util.ExponentialBackOff
 import com.typesafe.scalalogging.StrictLogging
-//import cromwell.backend.google.pipelines.common.PipelinesApiBackendLifecycleActorFactory.logger
-//import cromwell.backend.google.pipelines.common.{PipelinesApiConfigurationAttributes, PipelinesApiFinalizationActorParams, PipelinesApiInitializationActorParams}
-
-//import scala.util.{Failure, Try}
-//import cromwell.backend.google.pipelines.common.PipelinesApiBackendLifecycleActorFactory.robustBuildAttributes
-//import cromwell.backend.google.pipelines.common.{PipelinesApiConfiguration, PipelinesApiConfigurationAttributes}
 import cromwell.backend.BackendWorkflowDescriptor
 import cromwell.backend.{BackendInitializationData, JobExecutionMap}
-//import cromwell.backend.{BackendInitializationData, BackendWorkflowDescriptor, JobExecutionMap}
 import cromwell.cloudsupport.gcp.GoogleConfiguration
 import cromwell.core.CallOutputs
 import wom.graph.CommandCallNode
-//import cromwell.backend.{BackendInitializationData, BackendJobDescriptor}
 import cromwell.backend.BackendConfigurationDescriptor
 import cromwell.backend.standard._
 
@@ -28,9 +19,6 @@ class GcpBatchBackendLifecycleActorFactory(name: String, override val configurat
   override def jobIdKey: String = "__gcp_batch"
   protected val googleConfig: GoogleConfiguration = GoogleConfiguration(configurationDescriptor.globalConfig)
 
-
-  //protected def requiredBackendSingletonActor(serviceRegistryActor: ActorRef): Props
-
   override lazy val initializationActorClass: Class[_ <: StandardInitializationActor] = classOf[GcpBatchInitializationActor]
 
   override def asyncExecutionActorClass: Class[_ <: StandardAsyncExecutionActor] =
@@ -39,9 +27,6 @@ class GcpBatchBackendLifecycleActorFactory(name: String, override val configurat
   override lazy val finalizationActorClassOption: Option[Class[_ <: StandardFinalizationActor]] =
     Option(classOf[GcpBatchFinalizationActor])
 
-  //override def backendSingletonActorProps(serviceRegistryActor: ActorRef): Option[Props] = Option(Props(new GcpBatchBackendSingletonActor("batch")()))
-  //protected def requiredBackendSingletonActor(serviceRegistryActor: ActorRef): Props
-  //override def backendSingletonActorProps(serviceRegistryActor: ActorRef): Option[Props] = Option(requiredBackendSingletonActor(serviceRegistryActor))
 
   val batchConfiguration = new GcpBatchConfiguration(configurationDescriptor, googleConfig)
 
@@ -65,7 +50,6 @@ class GcpBatchBackendLifecycleActorFactory(name: String, override val configurat
                                            workflowOutputs: CallOutputs,
                                            initializationDataOption: Option[BackendInitializationData]): StandardFinalizationActorParams = {
     GcpBatchFinalizationActorParams(workflowDescriptor, ioActor, batchConfiguration, calls, jobExecutionMap, workflowOutputs, initializationDataOption)
-    //GcpBatchInitializationActorParams(workflowDescriptor, ioActor, calls, batchConfiguration, jobExecutionMap, workflowOutputs, initializationDataOption)
   }
 
   override def backendSingletonActorProps(serviceRegistryActor: ActorRef): Option[Props] = {
--- supportedBackends/google/pipelines/batch/src/main/scala/cromwell/backend/google/pipelines/batch/GcpBatchJobGetRequest.scala
+++ supportedBackends/google/pipelines/batch/src/main/scala/cromwell/backend/google/pipelines/batch/GcpBatchJobGetRequest.scala
@@ -1,28 +1,6 @@
 package cromwell.backend.google.pipelines.batch
 
-//import cats.instances.unit
-//import com.google.api.core.ApiFuture
-//import cromwell.backend.google.pipelines.batch.GcpBatchBackendSingletonActor.BatchGetJob
-
-//import scala.concurrent.Promise
-import scala.util.Try
 import com.google.cloud.batch.v1.{BatchServiceClient, GetJobRequest, JobName}
-//import com.google.cloud.batch.v1.{BatchServiceClient, GetJobRequest, Job, JobName}
-//import com.google.api.gax.rpc.NotFoundException
-
-//import java.util.concurrent.ExecutionException
-//import com.google.cloud.batch.v1.{BatchServiceClient, GetJobRequest, Job, JobName}
-
-//import scala.concurrent.Await
-//import scala.concurrent.duration.DurationInt
-//import scala.util.{Failure, Success, Try}
-//import org.http4s.Response.timeout
-
-//import scala.concurrent.Await
-//import scala.concurrent.duration.Duration
-//import cromwell.backend.google.pipelines.batch.GcpBatchBackendSingletonActor.GcpBatchJobSuccess
-
-//import scala.concurrent.Future
 
 class GcpBatchJobGetRequest {
 
@@ -38,73 +16,7 @@ class GcpBatchJobGetRequest {
 
     batchServiceClient.close()
     job.getStatus.getState
-
-
-    //print(response.getStatus.getState)
-    //print(response.getStatus.toString)
-    //batchServiceClient.close()
-
-    //response
-
-
-    /*
-        val batchServiceClient = BatchServiceClient.create()
-
-        val job = batchServiceClient
-          .getJob(JobName
-            .newBuilder()
-            .setProject(projectId)
-            .setLocation(region)
-            .setJob(jobName)
-            .build())
-        */
-
-
-    /*
-    val batchServiceClient = BatchServiceClient.create
-    val request = GetJobRequest.newBuilder.setName(JobName.of(projectId, region, jobName).toString())
-                               .build
-    println(request.toString)
-    val future: ApiFuture[Job] = batchServiceClient.getJobCallable.futureCall(request)
-    //Await.ready(future.get, Duration.Inf)
-    //future.get
-
-    val response = future.get()
-    //println(response)
-    //response.getStatus.getState
-    batchServiceClient.close()
-    //println(response.getStatus.getState)
-    */
-    //BatchGetJob(jobName)
-
-    /*
-    Await.result(future, 1.second) match {
-      case Success(res) => println("succeeded")
-      case Failure(e) => println("failed")
-    }*/
-
-
-    //val response: Job = future.get()
-
-
-    //val status = job.getStatus.getState
-    //val jobResult = job
-    //jobResult
-
   }
-
-  def jobGetRequest(jobId: String) = {
-    val gcpBatchPoll = new GcpBatchJobGetRequest
-    gcpBatchPoll.GetJob(jobId)
-    //jobDetail
-  }
-
-  def status(jobId: String): Try[RunStatus] = for {
-    _ <- Try(jobGetRequest(jobId).toString)
-    //runStatus <- RunStatus.fromJobStatus(jobId)
-    runStatus <- RunStatus.testJobStatus(jobId)
-  } yield runStatus
-
 
 }
 

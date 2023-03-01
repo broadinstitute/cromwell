df6cd41cba410a8027995a893de512f81b95eb44
Scala Steward <scala_steward@virtuslab.com>
2023-03-01 15:08:53 +0000

    Scala-Steward: Update scala-library from 2.13.9 to 2.13.10

--- project/Settings.scala
+++ project/Settings.scala
@@ -70,7 +70,7 @@ object Settings {
     assembly / assemblyMergeStrategy := customMergeStrategy.value,
   )
 
-  val Scala2_13Version = "2.13.9"
+  val Scala2_13Version = "2.13.10"
   private val ScalaVersion: String = Scala2_13Version
   private val sharedSettings: Seq[Setting[_]] =
     cromwellVersionWithGit ++ publishingSettings ++ List(

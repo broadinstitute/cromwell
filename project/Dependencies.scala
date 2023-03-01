7775dd82310d5276bbfdadeb1becc6a0579c7461
Scala Steward <scala_steward@virtuslab.com>
2023-03-01 15:04:10 +0000

    Scala-Steward: Update jakarta.xml.bind-api from 2.3.2 to 2.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
   private val jakartaActivationV = "1.2.1"
   private val jakartaAnnotationV = "1.3.5"
   private val jakartaInjectV = "2.6.1"
-  private val jakartaXmlBindApiV = "2.3.2"
+  private val jakartaXmlBindApiV = "2.3.3"
   private val janinoV = "3.1.7"
   private val jerseyV = "2.32" // Use a jersey compatible with WSM. See notes in wsmDependencies below.
   private val jsr305V = "3.0.2"

f5333aa6debc68aca204fd46e2f597f074c9e5be
Scala Steward <scala_steward@virtuslab.com>
2023-03-01 15:04:22 +0000

    Scala-Steward: Update mysql-connector-java from 8.0.28 to 8.0.32

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -87,7 +87,7 @@ object Dependencies {
   Newer version 8.0.29 fails `Control characters should work with metadata` Centaur tests, has charset changes mentioned in release notes
   https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-29.html#mysqld-8-0-29-charset
    */
-  private val mysqlV = "8.0.28"
+  private val mysqlV = "8.0.32"
   private val nettyV = "4.1.72.Final"
   private val owlApiV = "5.1.19"
   private val postgresV = "42.4.1"

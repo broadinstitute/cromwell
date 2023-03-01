e1fe758057ce53607134024e9d6ef186f6f39a9c
Scala Steward <scala_steward@virtuslab.com>
2023-03-01 15:04:36 +0000

    Scala-Steward: Update commons-csv from 1.9.0 to 1.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   private val circeV = "0.14.1"
   private val circeYamlV = "0.14.1"
   private val commonsCodecV = "1.15" // via: https://commons.apache.org/proper/commons-codec/
-  private val commonsCsvV = "1.9.0"
+  private val commonsCsvV = "1.10.0"
   private val commonsIoV = "2.11.0" // via: https://commons.apache.org/proper/commons-io/
   private val commonsLang3V = "3.12.0"
   private val commonsMathV = "3.6.1"

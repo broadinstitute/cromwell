ce68a47f58ac713c54dbf6bee367f2e37a23ac15
Tom Wiseman <twiseman@broadinstitute.org>
2023-03-03 09:50:34 -0500

    Centaur slurm (#7083)

--- src/ci/bin/test_slurm.inc.sh
+++ src/ci/bin/test_slurm.inc.sh
@@ -21,21 +21,23 @@ cromwell::build::slurm::setup_slurm_environment() {
     # https://slurm.schedmd.com/
     sudo apt-get update
 
-    # Try the Lawrence Livermore National Laboratory (LLNL) version first
-    sudo apt-get install -y slurm-llnl || sudo apt-get install -y slurm-wlm
+    sudo apt-get install -y slurm-wlm
+    # As an alternative to 'slurm-wlm', you may also try 'slurm-llnl'.
+    # If you use 'slurm-llnl', change the below tee path to '/etc/slurm-llnl/slurm.conf'
+    # For reasons that are unclear, slurm-llnl doesn't work in Github Actions, and slurm-wlm doesn't work in Travis.
+    # We transitioned from slurm-llnl to slurm-wlm when moving from Travis to Github Actions.
+    # See the PR associated with ticket WX-888.  
 
     # Create various directories used by slurm
-    sudo mkdir -p /etc/slurm-llnl
-    sudo mkdir -p /var/run/slurm-llnl
     sudo mkdir -p /var/run/munge
     sudo mkdir -p /var/spool/slurmd
 
-    # A mash of configure-until-it-runs. Feel free to PR suggestions/fixes
+    # A mash of configure-until-it-runs. Feel free to PR suggestions/fixes.
     # https://slurm.schedmd.com/tutorials.html
     # https://slurm.schedmd.com/configurator.html
     # https://slurm.schedmd.com/slurm.conf.html
     # https://slurm.schedmd.com/quickstart_admin.html
-    cat <<SLURM_CONF | sudo tee /etc/slurm-llnl/slurm.conf >/dev/null
+    cat <<SLURM_CONF | sudo tee /etc/slurm/slurm.conf >/dev/null
 ClusterName=localhost
 ControlMachine=localhost
 NodeName=localhost

b7dd69e928631e2423fbc3f0cc91a15353effa49
Tom Wiseman <twiseman@broadinstitute.org>
2023-03-10 13:58:48 -0500

    denied

---
+++ src/ci/bin/testCentaurBlob.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+# import in shellcheck / CI / IntelliJ compatible ways
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
+
+cromwell::build::setup_common_environment
+
+cromwell::build::setup_centaur_environment
+
+cromwell::build::assemble_jars
+
+cromwell::build::run_centaur
+
+cromwell::build::generate_code_coverage

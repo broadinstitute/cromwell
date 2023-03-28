7fe11e9a3c8247f397ad3d5d47fc3232d002318b
Tom Wiseman <twiseman@broadinstitute.org>
2023-03-28 10:43:10 -0400

    test

--- src/ci/bin/testSingleWorkflowRunner.sh
+++ src/ci/bin/testSingleWorkflowRunner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit -o nounset -o pipefail +x
+set -o errexit -o nounset -o pipefail
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh

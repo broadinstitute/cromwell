49188d8928ee81826dfca0c8ba295abf88220633
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2023-04-19 16:08:10 -0400

    Make TES request backoff behavior configurable

--- supportedBackends/tes/src/main/scala/cromwell/backend/impl/tes/TesConfiguration.scala
+++ supportedBackends/tes/src/main/scala/cromwell/backend/impl/tes/TesConfiguration.scala
@@ -1,8 +1,12 @@
 package cromwell.backend.impl.tes
 
+import com.typesafe.config.Config
 import cromwell.backend.BackendConfigurationDescriptor
-
+import cromwell.core.retry.SimpleExponentialBackoff
 import net.ceedubs.ficus.Ficus._
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class TesConfiguration(val configurationDescriptor: BackendConfigurationDescriptor) {
 
@@ -13,8 +17,35 @@ class TesConfiguration(val configurationDescriptor: BackendConfigurationDescript
       .backendConfig
       .as[Option[Boolean]](TesConfiguration.useBackendParametersKey)
       .getOrElse(false)
+
+  val pollBackoff =
+    configurationDescriptor
+      .backendConfig
+      .as[Option[Config]]("poll-backoff")
+      .map(SimpleExponentialBackoff(_))
+      .getOrElse(TesConfiguration.defaultPollBackoff)
+
+  val executeOrRecoverBackoff =
+    configurationDescriptor
+      .backendConfig
+      .as[Option[Config]]("execute-or-recover-backoff")
+      .map(SimpleExponentialBackoff(_))
+      .getOrElse(TesConfiguration.defaultExecOrRecoverBackoff)
+
 }
 
 object TesConfiguration {
   final val useBackendParametersKey = "use_tes_11_preview_backend_parameters"
+
+  final val defaultPollBackoff = SimpleExponentialBackoff(
+    initialInterval = 1 seconds,
+    maxInterval = 5 minutes,
+    multiplier = 1.1
+  )
+
+  final val defaultExecOrRecoverBackoff = SimpleExponentialBackoff(
+    initialInterval = 3 seconds,
+    maxInterval = 30 seconds,
+    multiplier = 1.1
+  )
 }

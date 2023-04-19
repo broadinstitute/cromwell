96e7a4d6d37a39baa31647b45d4a396e27a87840
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2023-04-19 16:08:32 -0400

    Unit tests for backoff config behavior

--- supportedBackends/tes/src/test/scala/cromwell/backend/impl/tes/TesTestConfig.scala
+++ supportedBackends/tes/src/test/scala/cromwell/backend/impl/tes/TesTestConfig.scala
@@ -46,5 +46,70 @@ object TesTestConfig {
       |""".stripMargin
 
   val backendConfigWithBackendParams = ConfigFactory.parseString(backendConfigStringWithBackendParams)
+
+  private val backendConfigStringWithBackoffs =
+    """
+      |root = "local-cromwell-executions"
+      |dockerRoot = "/cromwell-executions"
+      |endpoint = "http://127.0.0.1:9000/v1/jobs"
+      |
+      |default-runtime-attributes {
+      |  cpu: 1
+      |  failOnStderr: false
+      |  continueOnReturnCode: 0
+      |  memory: "2 GB"
+      |  disk: "2 GB"
+      |  preemptible: false
+      |  # The keys below have been commented out as they are optional runtime attributes.
+      |  # dockerWorkingDir
+      |  # docker
+      |}
+      |
+      |poll-backoff {
+      |  min: "5 seconds"
+      |  max: "1 minute"
+      |  multiplier: 2.5
+      |  randomization-factor: .7
+      |}
+      |
+      |execute-or-recover-backoff {
+      |  min: "3 minutes"
+      |  max: "1 hours"
+      |  multiplier: 5
+      |  randomization-factor: .1
+      |}
+      |
+      |""".stripMargin
+
+  val backendConfigWithBackoffs = ConfigFactory.parseString(backendConfigStringWithBackoffs)
+
+  private val backendConfigStringWithInvalidBackoffs =
+    """
+      |root = "local-cromwell-executions"
+      |dockerRoot = "/cromwell-executions"
+      |endpoint = "http://127.0.0.1:9000/v1/jobs"
+      |
+      |default-runtime-attributes {
+      |  cpu: 1
+      |  failOnStderr: false
+      |  continueOnReturnCode: 0
+      |  memory: "2 GB"
+      |  disk: "2 GB"
+      |  preemptible: false
+      |  # The keys below have been commented out as they are optional runtime attributes.
+      |  # dockerWorkingDir
+      |  # docker
+      |}
+      |
+      |poll-backoff {
+      |  min: "5 seconds"
+      |  max: "1 minute"
+      |  multiplier: 2.5
+      |  # missing randomization-factor
+      |}
+      |
+      |""".stripMargin
+
+  val backendConfigWithInvalidBackoffs = ConfigFactory.parseString(backendConfigStringWithInvalidBackoffs)
 }
 

a301d8d3fb26da8d1f70e238d22bdec157a8eace
Tom Wiseman <twiseman@broadinstitute.org>
2023-05-05 12:09:42 -0400

    cleanup + comment

--- src/ci/bin/test-deadlock.sh
+++ src/ci/bin/test-deadlock.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 pip3 install docker-py
+# Workaround for issue described https://github.com/docker/docker-py/issues/3113. 
+# This is a temporary fix until the next release of docker-py
 pip3 install --force-reinstall -v "requests<2.2.29"
 
 cromwell_hosts_file="${CROMWELL_BUILD_RESOURCES_DIRECTORY}/cromwell_hosts.txt"

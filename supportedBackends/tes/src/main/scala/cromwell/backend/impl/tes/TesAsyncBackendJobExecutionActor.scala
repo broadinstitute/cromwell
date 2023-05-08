4bd556da2ad79b4c6799f1aa7a10ff1b7b2f12d3
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2023-05-08 17:07:39 -0400

    Diagnostic logging

--- supportedBackends/tes/src/main/scala/cromwell/backend/impl/tes/TesAsyncBackendJobExecutionActor.scala
+++ supportedBackends/tes/src/main/scala/cromwell/backend/impl/tes/TesAsyncBackendJobExecutionActor.scala
@@ -144,7 +144,7 @@ class TesAsyncBackendJobExecutionActor(override val standardParams: StandardAsyn
         jobShell,
         mode)
     })
-
+    jobLogger.info(s"Submitting task, will poll starting each ${pollBackOff.googleBackoff.getInitialIntervalMillis} millis")
     tesTask.map(TesTask.makeTask)
   }
 

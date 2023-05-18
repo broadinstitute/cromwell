6f060574c52bc1e889da32a01dcdc721f0a70069
Tom Wiseman <twiseman@broadinstitute.org>
2023-05-18 11:08:34 -0400

    testing

--- project/Settings.scala
+++ project/Settings.scala
@@ -129,7 +129,7 @@ object Settings {
                          | && /usr/local/gcloud/google-cloud-sdk/install.sh""".stripMargin),
       // instructions to install `getm`. Pin to version 0.0.4 as the behaviors of future versions with respect to
       // messages or exit codes may change.
-      Instructions.Run("pip3 install getm==0.0.4")
+      Instructions.Run("pip3 install -i https://test.pypi.org/simple/ getm")
     )
   )
 

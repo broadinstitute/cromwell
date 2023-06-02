9c1a204fb157d9fb7bb3278cc4cb1641971010fb
Adam Nichols <aednichols@gmail.com>
2023-06-02 15:54:53 -0400

    Version trim fix

---
+++ languageFactories/language-factory-core/src/test/scala/cromwell/languages/util/LooksParseableSpec.scala
@@ -0,0 +1,55 @@
+package cromwell.languages.util
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class LooksParseableSpec extends AnyFlatSpec with Matchers {
+  behavior of "simpleLooksParseable"
+
+  it should "work with `version 1.0` followed by whitespace" in {
+
+    val source =
+      """
+        |version 1.0              
+        |# Programmer warning: make sure your editor does not trim the whitespace in the line above.
+        |""".stripMargin
+
+    LanguageFactoryUtil.simpleLooksParseable(List("version 1.0"), List("#"))(source) shouldBe true
+  }
+
+  it should "work with `version 1.0` preceded by whitespace" in {
+
+    val source =
+      """
+        |              version 1.0
+        |# Programmer warning: make sure your editor does not trim the whitespace in the line above.
+        |""".stripMargin
+
+    LanguageFactoryUtil.simpleLooksParseable(List("version 1.0"), List("#"))(source) shouldBe true
+  }
+
+  it should "work with `version 1.0` surrounded by whitespace" in {
+
+    val source =
+      """
+        |              version 1.0              
+        |# Programmer warning: make sure your editor does not trim the whitespace in the line above.
+        |""".stripMargin
+
+    LanguageFactoryUtil.simpleLooksParseable(List("version 1.0"), List("#"))(source) shouldBe true
+  }
+
+  it should "work with `version 1.0` surrounded by whitespace on all sides" in {
+
+    val source =
+      """
+        |              
+        |              version 1.0              
+        |              
+        |# Programmer warning: make sure your editor does not trim the whitespace in the line above.
+        |""".stripMargin
+
+    LanguageFactoryUtil.simpleLooksParseable(List("version 1.0"), List("#"))(source) shouldBe true
+  }
+
+}

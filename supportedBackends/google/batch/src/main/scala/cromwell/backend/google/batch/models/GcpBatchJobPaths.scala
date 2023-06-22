5777b01104825e61b4bb247c9efd5214a5962c94
dspeck <dspeck@burwood.com>
2023-06-22 17:46:29 +0000

    Remove SRA as it is no longer used.  Removed unused comments.

--- supportedBackends/google/batch/src/main/scala/cromwell/backend/google/batch/models/GcpBatchJobPaths.scala
+++ supportedBackends/google/batch/src/main/scala/cromwell/backend/google/batch/models/GcpBatchJobPaths.scala
@@ -39,10 +39,5 @@ case class GcpBatchJobPaths(override val workflowPaths: GcpBatchWorkflowPaths, j
   val batchMonitoringScriptFilename: String = s"${GcpBatchJobPaths.BatchMonitoringKey}.sh"
   val batchMonitoringImageScriptFilename: String = s"${GcpBatchJobPaths.BatchMonitoringImageKey}.sh"
 
-
-  //override val returnCodeFilename: String = s"$batchLogBasename-rc.txt"
-  //override def defaultStdoutFilename: String = s"$batchLogBasename-stdout.log"
-  //override def defaultStderrFilename: String = s"$batchLogBasename-stderr.log"
-
   override def forCallCacheCopyAttempts: JobPaths = this.copy(isCallCacheCopyAttempt = true)
 }

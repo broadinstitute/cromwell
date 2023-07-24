f3a1b2a048bf862b4faac31c45838f6e03f3a984
dspeck <dspeck@burwood.com>
2023-07-24 19:36:25 +0000

    Remove tests that are not valid because directory/file input behavior changes in PAPIv2.  Fixed metadata keys issue identified by test.

--- supportedBackends/google/batch/src/main/scala/cromwell/backend/google/batch/actors/GcpBatchJobCachingActorHelper.scala
+++ supportedBackends/google/batch/src/main/scala/cromwell/backend/google/batch/actors/GcpBatchJobCachingActorHelper.scala
@@ -1,11 +1,15 @@
 package cromwell.backend.google.batch.actors
 
 import cromwell.backend.google.batch.io.{GcpBatchAttachedDisk, GcpBatchWorkingDisk}
+import cromwell.backend.google.batch.runnable.{GcpBatchMetadataKeys, WorkflowOptionKeys}
 import cromwell.backend.google.batch.models._
 import cromwell.backend.standard.StandardCachingActorHelper
 import cromwell.core.labels.Labels
 import cromwell.core.logging.JobLogging
 import cromwell.core.path.Path
+import cromwell.services.metadata.CallMetadataKeys
+
+import scala.language.postfixOps
 
 trait GcpBatchJobCachingActorHelper extends StandardCachingActorHelper {
   this: GcpBatchAsyncBackendJobExecutionActor with JobLogging =>
@@ -62,5 +66,22 @@ trait GcpBatchJobCachingActorHelper extends StandardCachingActorHelper {
   lazy val originalLabels: Labels = defaultLabels
 
   lazy val backendLabels: Seq[GcpLabel] = GcpLabel.safeLabels(originalLabels.asTuple: _*)
+
+  lazy val originalLabelEvents: Map[String, String] = originalLabels.value map { l => s"${CallMetadataKeys.Labels}:${l.key}" -> l.value } toMap
+
+  override protected def nonStandardMetadata: Map[String, Any] = {
+    val googleProject = initializationData
+      .workflowPaths
+      .workflowDescriptor
+      .workflowOptions
+      .get(WorkflowOptionKeys.GoogleProject)
+      .getOrElse(batchAttributes.project)
+
+    Map[String, Any](
+      GcpBatchMetadataKeys.GoogleProject -> googleProject,
+      GcpBatchMetadataKeys.ExecutionBucket -> initializationData.workflowPaths.executionRootString,
+      "preemptible" -> preemptible
+    ) ++ originalLabelEvents
+  }
 
 }

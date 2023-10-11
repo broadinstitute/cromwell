ee6758c4903f98014db2d4ee27a0393ab485a77e
Harris Mendell <harrismendell@gmail.com>
2023-10-11 11:46:13 -0400

    Remove override on isEmpty

--- cloud-nio/cloud-nio-spi/src/main/scala/cloud/nio/spi/UnixPath.scala
+++ cloud-nio/cloud-nio-spi/src/main/scala/cloud/nio/spi/UnixPath.scala
@@ -69,7 +69,7 @@ final private[spi] case class UnixPath(path: String) extends CharSequence {
 
   def isAbsolute: Boolean = UnixPath.isAbsolute(path)
 
-  override def isEmpty: Boolean = path.isEmpty
+  def isEmpty: Boolean = path.isEmpty
 
   def hasTrailingSeparator: Boolean = UnixPath.hasTrailingSeparator(path)
 

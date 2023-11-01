3859c76bc957622b4b4f47d60cd3802afde5ad86
Adam Nichols <aednichols@gmail.com>
2023-11-01 15:47:19 -0400

    Make `throwExceptionOnExecuteError` false for PAPI aborts

--- supportedBackends/google/pipelines/v2beta/src/main/scala/cromwell/backend/google/pipelines/v2beta/api/request/AbortRequestHandler.scala
+++ supportedBackends/google/pipelines/v2beta/src/main/scala/cromwell/backend/google/pipelines/v2beta/api/request/AbortRequestHandler.scala
@@ -3,7 +3,7 @@ package cromwell.backend.google.pipelines.v2beta.api.request
 import akka.actor.ActorRef
 import com.google.api.client.googleapis.batch.BatchRequest
 import com.google.api.client.googleapis.json.GoogleJsonError
-import com.google.api.client.http.HttpHeaders
+import com.google.api.client.http.{ByteArrayContent, HttpHeaders}
 import com.typesafe.scalalogging.LazyLogging
 import cromwell.backend.google.pipelines.common.api.PipelinesApiRequestManager._
 import cromwell.backend.google.pipelines.common.api.clients.PipelinesApiAbortClient.{PAPIAbortRequestSuccessful, PAPIOperationIsAlreadyTerminal}
@@ -35,7 +35,7 @@ trait AbortRequestHandler extends LazyLogging { this: RequestHandler =>
   // The Genomics batch endpoint doesn't seem to be able to handle abort requests on V2 operations at the moment
   // For now, don't batch the request and execute it on its own
   def handleRequest(abortQuery: PAPIAbortRequest, batch: BatchRequest, pollingManager: ActorRef)(implicit ec: ExecutionContext): Future[Try[Unit]] = {
-    Future(abortQuery.httpRequest.execute()) map {
+    Future(abortQuery.httpRequest.setThrowExceptionOnExecuteError(false).execute()) map {
       case response if response.isSuccessStatusCode =>
         abortQuery.requester ! PAPIAbortRequestSuccessful(abortQuery.jobId.jobId)
         Success(())

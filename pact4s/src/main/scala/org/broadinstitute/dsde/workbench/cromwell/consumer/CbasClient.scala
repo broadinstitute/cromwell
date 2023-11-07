3cc0708c18c2a1fefb89e18f546c0fbb04102bf7
Maria Yazykova <95451512+zykonda@users.noreply.github.com>
2023-11-07 13:55:01 -0800

    Contract Pact tests for CBAS provider

--- pact4s/src/main/scala/org/broadinstitute/dsde/workbench/cromwell/consumer/CbasClient.scala
+++ pact4s/src/main/scala/org/broadinstitute/dsde/workbench/cromwell/consumer/CbasClient.scala
@@ -9,19 +9,14 @@ import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.client.Client
 import org.http4s.headers.Authorization
 
-import java.util.UUID
+final case class WorkflowCallbackMessage(workflowId: String,
+                                 state: String,
+                                 outputs: Object,
+                                 failures: List[String])
 
-case class WorkflowResultMetadata(workflowId: UUID,
-                                  state: String,
-                                  outputs: Object,
-                                  failures: List[String]
-)
 trait CbasClient[F[_]] extends LazyLogging {
-  def postWorkflowResults(bearerToken: String,
-                          workflowId: UUID,
-                          state: String,
-                          outputs: Object,
-                          failures: List[String]): F[Boolean]
+  def postWorkflowResults(authHeader: Authorization,
+                          callbackMessage: WorkflowCallbackMessage): F[Boolean]
 }
 
 /*
@@ -30,33 +25,30 @@ trait CbasClient[F[_]] extends LazyLogging {
  */
 class CbasClientImpl[F[_]: Concurrent](client: Client[F], baseUrl: Uri) extends CbasClient[F] {
   val apiVersion = "v1"
- implicit val workflowResultMetadataEncoder: Encoder[WorkflowResultMetadata] = Encoder.forProduct4(
+
+  implicit val workflowCallbackMessageEncoder: Encoder[WorkflowCallbackMessage] = Encoder.forProduct4(
     "workflowId",
     "state",
     "outputs",
     "failures"
   )(x => (x.workflowId, x.state, x.outputs.toString, x.failures))
-  implicit val resourceMetadataRequestEntityEncoder: EntityEncoder[F, WorkflowResultMetadata] = circeEntityEncoder[F, WorkflowResultMetadata]
-  override def postWorkflowResults(bearerToken: String,
-                                    workflowId: UUID,
-                                   state: String,
-                                   outputs: Object,
-                                   failures: List[String]): F[Boolean] = {
-    val body = WorkflowResultMetadata(workflowId = workflowId, state = state, outputs = outputs, failures = failures)
-    val entityBody: EntityBody[F] = EntityEncoder[F, WorkflowResultMetadata].toEntity(body).body
-    val request = Request[F](uri = baseUrl / "api" / "batch"/ apiVersion / "runs" / "results", method=Method.POST, body=entityBody)
-      .putHeaders(
-        Authorization(Credentials.Token(AuthScheme.Bearer, bearerToken)),
-        org.http4s.headers.`Content-Type`(MediaType.application.json)
-    )//org.http4s.headers.`Authorization`(Credentials.Token(AuthScheme.Bearer, bearerToken))
 
-        client.run(request).use { resp =>
-        resp.status match {
-          case Status.Ok => true.pure[F]
-          case Status.InternalServerError => false.pure[F]
-          case _ => UnknownError.raiseError
-        }
+  override def postWorkflowResults(authHeader: Authorization,
+                                   callbackMessage: WorkflowCallbackMessage): F[Boolean] = {
+    val body = callbackMessage
+    val entityBody: EntityBody[F] = EntityEncoder[F, WorkflowCallbackMessage].toEntity(body).body
+    val request = Request[F](uri = baseUrl / "api" / "batch" / apiVersion / "runs" / "results", method = Method.POST, body = entityBody)
+      .withHeaders(
+        org.http4s.headers.`Content-Type`(MediaType.application.json),
+        authHeader
+      )
+    client.run(request).use { resp =>
+      resp.status match {
+        case Status.Ok => true.pure[F]
+        case Status.InternalServerError => false.pure[F]
+        case _ => UnknownError.raiseError
       }
+    }
   }
 }
 

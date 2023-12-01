e3c9bb536ef1786d8f0dfcab36eaf70546f23a3f
Scala Steward <scala_steward@virtuslab.com>
2023-12-01 14:19:11 +0000

    Scala-Steward: Update azure-storage-blob from 12.23.0-beta.1 to 12.23.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -214,7 +214,7 @@ object Dependencies {
    */
   val azureBlobNioDependencies = List(
     "com.azure" % "azure-core" % "1.40.0",
-    "com.azure" % "azure-storage-blob" % "12.23.0-beta.1",
+    "com.azure" % "azure-storage-blob" % "12.23.1",
     "com.azure" % "azure-storage-common" % "12.22.0-beta.1",
     "com.azure" % "azure-core-test" % "1.18.0",
     "org.junit.jupiter" % "junit-jupiter-params" % "5.9.3",

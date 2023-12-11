80eabf1d4a5af088e208db80f9f20922681fcee6
Adam Nichols <aednichols@gmail.com>
2023-12-11 12:23:57 -0500

    Test

--- project/Settings.scala
+++ project/Settings.scala
@@ -77,8 +77,8 @@ object Settings {
     organization := "org.broadinstitute",
     scalaVersion := ScalaVersion,
     resolvers ++= additionalResolvers,
-    Global / parallelExecution := true,
-    Test / parallelExecution := false,
+    Compile / parallelExecution := true,
+    Default / parallelExecution := false,
     Global / concurrentRestrictions ++= List(
       // Don't run any other tasks while running tests, especially helps in low CPU environments like Travis
       Tags.exclusive(Tags.Test),
@@ -95,7 +95,8 @@ object Settings {
     Compile / console / scalacOptions --= consoleHostileSettings,
     excludeDependencies ++= List(
       "org.typelevel" % "simulacrum-scalafix-annotations_2.13"
-    )
+    ),
+    // Global / lintUnusedKeysOnLoad := false
   )
 
   val pact4sSettings = sharedSettings ++ List(

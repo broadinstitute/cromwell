7ec33706de030fa9f7171f08c036ea6f8f72605c
Adam Nichols <aednichols@gmail.com>
2023-12-12 12:48:35 -0500

    Javaism

--- cloud-nio/cloud-nio-spi/src/main/scala/cloud/nio/spi/UnixPath.scala
+++ cloud-nio/cloud-nio-spi/src/main/scala/cloud/nio/spi/UnixPath.scala
@@ -67,7 +67,7 @@ final private[spi] case class UnixPath(path: String) extends CharSequence {
 
   def isAbsolute: Boolean = UnixPath.isAbsolute(path)
 
-  override def isEmpty: Boolean = path.isEmpty
+  def izEmpty: Boolean = path.isEmpty
 
   def hasTrailingSeparator: Boolean = UnixPath.hasTrailingSeparator(path)
 

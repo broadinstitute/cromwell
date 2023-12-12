5dfdeb4b96f98d70c563e6d6f86224bcda2bada5
Maria Yazykova <95451512+zykonda@users.noreply.github.com>
2023-12-12 14:20:09 -0800

    Attempt to fix compilation error

--- cloud-nio/cloud-nio-spi/src/main/scala/cloud/nio/spi/UnixPath.scala
+++ cloud-nio/cloud-nio-spi/src/main/scala/cloud/nio/spi/UnixPath.scala
@@ -67,7 +67,7 @@ final private[spi] case class UnixPath(path: String) extends CharSequence {
 
   def isAbsolute: Boolean = UnixPath.isAbsolute(path)
 
-  def isEmpty: Boolean = path.isEmpty
+  final def isEmpty: Boolean = path.isEmpty
 
   def hasTrailingSeparator: Boolean = UnixPath.hasTrailingSeparator(path)
 

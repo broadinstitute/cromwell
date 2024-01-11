2bd9e6bba00548ea6796f6e6626a28ff1af2d7b6
Beibei Chen <beibei@formbio.com>
2024-01-11 14:17:31 -0600

    remove test case that is valid after update

--- supportedBackends/google/batch/src/test/scala/cromwell/backend/google/batch/models/GcpBatchLabelSpec.scala
+++ supportedBackends/google/batch/src/test/scala/cromwell/backend/google/batch/models/GcpBatchLabelSpec.scala
@@ -18,7 +18,6 @@ class GcpBatchLabelSpec extends AnyFlatSpec with CromwellTimeoutSpec with Matche
     "cromwell-root-workflow-id-" -> "cromwell-root-workflow-id---x",
     "0-cromwell-root-workflow-id-" -> "x--0-cromwell-root-workflow-id---x",
     "Cromwell-root-workflow-id" -> "cromwell-root-workflow-id",
-   // "cromwell_root_workflow_id" -> "cromwell-root-workflow-id",
     "too-long-too-long-too-long-too-long-too-long-too-long-too-long-t" -> "too-long-too-long-too-long-too---g-too-long-too-long-too-long-t",
     "0-too-long-and-invalid-too-long-and-invalid-too-long-and-invali+" -> "x--0-too-long-and-invalid-too----nvalid-too-long-and-invali---x"
   )

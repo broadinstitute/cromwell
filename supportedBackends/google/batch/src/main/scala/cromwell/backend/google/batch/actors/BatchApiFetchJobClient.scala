b6003c257d4be813fc659e1a3402252766ea9bdd
Alexis Hernandez <alexis22229@gmail.com>
2024-02-05 00:55:49 +0100

    Clean up unnecessary code + batch abort request bugfix
    Abort request handler was returning the wrong message, also, keep the old
    behavior where abort request is handled once only.

--- supportedBackends/google/batch/src/main/scala/cromwell/backend/google/batch/actors/BatchApiFetchJobClient.scala
+++ supportedBackends/google/batch/src/main/scala/cromwell/backend/google/batch/actors/BatchApiFetchJobClient.scala
@@ -27,29 +27,17 @@ trait BatchApiFetchJobClient { this: Actor with ActorLogging with BatchInstrumen
       log.debug("JES poll failed!")
       completePromise(Failure(e))
   }
-  // handles messages produced from GcpBatchBackendSingletonActor
-//  def pollingActorClientReceive: Actor.Receive = {
-//    case GcpBatchBackendSingletonActor.Event.JobStatusRetrieved(job) =>
-//      log.info(s"Job retrieved from GCP: ${job.getName}: ${job.getStatus}")
-//      pollSuccess()
-//      completePromise(Success(job))
-//
-//    case GcpBatchBackendSingletonActor.Event.ActionFailed(jobName, cause) =>
-//      val msg = s"Failed to query job ($jobName) from GCP"
-//      log.error(cause, msg)
-//      pollFailed()
-//      completePromise(Failure(cause))
-//  }
 
   private def completePromise(result: Try[Job]): Unit = {
     pollingActorClientPromise foreach { _.complete(result) }
     pollingActorClientPromise = None
   }
 
-  def fetchJob(workflowId: WorkflowId,
-               jobName: JobName,
-               backendSingletonActor: ActorRef,
-               requestFactory: GcpBatchRequestFactory
+  def fetchJob(
+    workflowId: WorkflowId,
+    jobName: JobName,
+    backendSingletonActor: ActorRef,
+    requestFactory: GcpBatchRequestFactory
   ): Future[Job] =
     pollingActorClientPromise match {
       case Some(p) => p.future
@@ -60,7 +48,6 @@ trait BatchApiFetchJobClient { this: Actor with ActorLogging with BatchInstrumen
           requestFactory.queryRequest(jobName),
           StandardAsyncJob(jobName.toString)
         )
-//        backendSingletonActor ! GcpBatchBackendSingletonActor.Action.QueryJob(jobName)
 
         val newPromise = Promise[Job]()
         pollingActorClientPromise = Option(newPromise)

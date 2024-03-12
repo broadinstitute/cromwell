6fdb9e5e108ecf03093b8ffb47c7028742c03c5a
Adam Nichols <aednichols@gmail.com>
2024-03-12 13:26:10 -0400

    Simplify

--- wdl/transforms/new-base/src/main/scala/wdl/transforms/base/linking/expression/values/LiteralEvaluators.scala
+++ wdl/transforms/new-base/src/main/scala/wdl/transforms/base/linking/expression/values/LiteralEvaluators.scala
@@ -4,16 +4,14 @@ import cats.syntax.apply._
 import cats.syntax.traverse._
 import cats.syntax.validated._
 import cats.instances.list._
+import common.validation.ErrorOr
 import common.validation.ErrorOr.ErrorOr
-import common.validation.Validation.TryValidation
 import wdl.model.draft3.elements.ExpressionElement
 import wdl.model.draft3.elements.ExpressionElement._
 import wdl.model.draft3.graph.expression.{EvaluatedValue, ForCommandInstantiationOptions, ValueEvaluator}
 import wdl.model.draft3.graph.expression.ValueEvaluator.ops._
 import wom.expression.IoFunctionSet
 import wom.values.{WomArray, WomMap, WomObject, WomPair, WomString, WomValue}
-
-import scala.util.Try
 
 object LiteralEvaluators {
 
@@ -87,16 +85,13 @@ object LiteralEvaluators {
           ) mapN { (key, value) => key -> value }
       }
 
-      // Expose `flatten` to handle the `ErrorOr[ErrorOr[]]`
+      // Expose `flatten` to handle the `ErrorOr[ErrorOr[]]` [WX-757]
       import common.validation.ErrorOr.NestedErrorOr
 
       (evaluated map { kvps =>
-        val tryResult: Try[EvaluatedValue[_ <: WomValue]] = Try {
-          val value = kvps.map(entry => entry._1.value -> entry._2.value).toMap
-          val sideEffectFiles = kvps.flatMap(entry => entry._1.sideEffectFiles ++ entry._2.sideEffectFiles)
-          EvaluatedValue(WomMap.apply(value.toMap), sideEffectFiles)
-        }
-        tryResult.toErrorOr
+        val value = kvps.map(entry => entry._1.value -> entry._2.value).toMap
+        val sideEffectFiles = kvps.flatMap(entry => entry._1.sideEffectFiles ++ entry._2.sideEffectFiles)
+        ErrorOr(EvaluatedValue(WomMap.apply(value.toMap), sideEffectFiles))
       }).flatten
     }
   }

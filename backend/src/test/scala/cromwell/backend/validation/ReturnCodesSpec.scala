2c4d8b36fa8194570e90386a8aa8d520d95f24b0
Ryan Saperstein <rsaperst@wmd55-b47.broadinstitute.org>
2024-03-21 09:57:08 -0400

    scalafmt

--- backend/src/test/scala/cromwell/backend/validation/ReturnCodesSpec.scala
+++ backend/src/test/scala/cromwell/backend/validation/ReturnCodesSpec.scala
@@ -9,10 +9,7 @@ import org.scalatest.wordspec.AnyWordSpecLike
 class ReturnCodesSpec extends AnyWordSpecLike with CromwellTimeoutSpec with Matchers with BeforeAndAfterAll {
   "Checking for return codes" should {
     "continue on expected return code flags" in {
-      val flagTests = Table(("flag", "returnCode", "expectedContinue"),
-        ("*", 0, true),
-        ("*", 1, true),
-      )
+      val flagTests = Table(("flag", "returnCode", "expectedContinue"), ("*", 0, true), ("*", 1, true))
 
       forAll(flagTests) { (flag, returnCode, expectedContinue) =>
         ReturnCodesString(flag).continueFor(returnCode) should be(expectedContinue)

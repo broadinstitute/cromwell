ef1106f544f666b58ea1408b6a354647c3532d5a
Tom Wiseman <twiseman@broadinstitute.org>
2024-05-02 11:05:29 -0400

    try github action

--- src/ci/bin/test.inc.sh
+++ src/ci/bin/test.inc.sh
@@ -133,7 +133,7 @@ cromwell::private::create_build_variables() {
             CROMWELL_BUILD_GIT_USER_EMAIL=""
             CROMWELL_BUILD_GIT_USER_NAME="${GITHUB_ACTOR}"
             CROMWELL_BUILD_HEARTBEAT_PATTERN="…"
-            CROMWELL_BUILD_GENERATE_COVERAGE=true
+            CROMWELL_BUILD_GENERATE_COVERAGE=false
             CROMWELL_BUILD_RUN_TESTS=true
             ;;
         *)

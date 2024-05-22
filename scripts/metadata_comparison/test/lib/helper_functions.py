1e3df380a0b1c82c6e34fc9304eaca2e8b45962c
Adam Nichols <aednichols@gmail.com>
2024-05-22 18:34:11 -0400

    Remove PAPI v1 vs. v2 comparison scripts

--- scripts/metadata_comparison/test/lib/helper_functions.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from pathlib import Path
-
-"""
-Can be used to read files from the resources directory, like:
- (RESOURCES / filename).read_text()
-"""
-RESOURCES = Path(__file__).parent.parent / Path("resources")

9f10bd1f4e2a719b853cdb28110a1302c773981a
Ryan Saperstein <rsaperst@broadinstitute.org>
2024-05-24 13:45:54 -0400

    Don't run lots_of_inputs on Papi

--- src/ci/bin/testCentaurHoricromtalPapiV2beta.sh
+++ src/ci/bin/testCentaurHoricromtalPapiV2beta.sh
@@ -26,6 +26,7 @@ cromwell::build::run_centaur \
     -e standard_output_paths_colliding_prevented \
     -e papi_v2alpha1_gcsa \
     -e restart \
+    -e lots_of_inputs_papiv2 \
     -e lots_of_inputs \
     # `lots_of_inputs` is redundant to, and a less rigorous version of, `lots_of_inputs_papiv2`
 

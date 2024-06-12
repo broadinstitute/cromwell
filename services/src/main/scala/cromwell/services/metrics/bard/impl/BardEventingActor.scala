01ca57f70c82f8468ecf0f11ae78e4a34785299c
Tristan Garwood <tristan.garwood@gmail.com>
2024-06-12 12:40:48 -0400

    Add instrumentation for bard success and failure

--- services/src/main/scala/cromwell/services/metrics/bard/impl/BardEventingActor.scala
+++ services/src/main/scala/cromwell/services/metrics/bard/impl/BardEventingActor.scala
@@ -1,19 +1,24 @@
 package cromwell.services.metrics.bard.impl
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import cats.data.NonEmptyList
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import common.util.StringUtil.EnhancedToStringable
 import cromwell.core.Dispatcher.ServiceDispatcher
+import cromwell.services.instrumentation.CromwellInstrumentation
 import cromwell.services.metrics.bard.BardEventing.BardEventRequest
 import cromwell.services.metrics.bard.{BardConfig, BardService}
 import cromwell.util.GracefulShutdownHelper.ShutdownCommand
 
 import scala.concurrent.ExecutionContext
 
-class BardEventingActor(serviceConfig: Config, globalConfig: Config, serviceRegistryActor: ActorRef)
+class BardEventingActor(serviceConfig: Config, globalConfig: Config, serviceRegistry: ActorRef)
     extends Actor
-    with LazyLogging {
+    with LazyLogging
+    with CromwellInstrumentation {
+
+  override lazy val serviceRegistryActor: ActorRef = serviceRegistry
 
   implicit val system: ActorSystem = context.system
   implicit val ec: ExecutionContext = context.dispatcher
@@ -23,7 +28,14 @@ class BardEventingActor(serviceConfig: Config, globalConfig: Config, serviceRegi
 
   override def receive: Receive = {
     case BardEventRequest(event) if bardConfig.enabled =>
-      bardService.sendEvent(event)
+      try {
+        bardService.sendEvent(event)
+        increment(NonEmptyList.of("send_event", "success"), Some("bard"))
+      } catch {
+        case e: Exception =>
+          increment(NonEmptyList.of("send_event", "failure"), Some("bard"))
+          throw e
+      }
     // This service currently doesn't do any work on shutdown but the service registry pattern requires it
     // (see https://github.com/broadinstitute/cromwell/issues/2575)
     case ShutdownCommand => context stop self

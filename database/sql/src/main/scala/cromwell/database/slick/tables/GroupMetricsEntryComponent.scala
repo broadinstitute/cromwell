a9989766d77e0d6f05dc193552addad1a83dc0a7
Saloni Shah <salonipshah11@gmail.com>
2024-08-07 12:05:49 -0400

    comment about Slick's .?

--- database/sql/src/main/scala/cromwell/database/slick/tables/GroupMetricsEntryComponent.scala
+++ database/sql/src/main/scala/cromwell/database/slick/tables/GroupMetricsEntryComponent.scala
@@ -19,6 +19,13 @@ trait GroupMetricsEntryComponent {
 
     def quotaExhaustionDetected = column[Timestamp]("QUOTA_EXHAUSTION_DETECTED")
 
+    /*
+       In Slick, adding '?' at end of column lifts it into an Option column. The reason the auto-incremented primary key
+       in Slick is represented as Option[T], for example 'groupMetricsEntryId' in this case, is because when a row is
+       inserted into the table the value for the primary key is unknown (since it's auto-incremented by DB) and hence
+       in this case the value for that column is represented as 'None'. After insertion, database assigns it an
+       auto-incremented value and at this point the value for that column is represented as 'Some(value)'.
+     */
     override def * : ProvenShape[GroupMetricsEntry] = (groupId,
                                                        quotaExhaustionDetected,
                                                        groupMetricsEntryId.?

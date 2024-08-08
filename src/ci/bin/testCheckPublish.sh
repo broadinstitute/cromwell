0a38d296d2255a5cbf43eacc2472a8cb7ab2b53b
Adam Nichols <aednichols@gmail.com>
2024-08-08 14:53:16 -0400

    Remove discontinued `swagger2markup` library

--- src/ci/bin/testCheckPublish.sh
+++ src/ci/bin/testCheckPublish.sh
@@ -10,6 +10,6 @@ cromwell::build::setup_common_environment
 cromwell::build::pip_install mkdocs
 mkdocs build -s
 
-sbt -Dsbt.supershell=false --warn checkRestApiDocs +package assembly dockerPushCheck +doc
+sbt -Dsbt.supershell=false --warn +package assembly dockerPushCheck +doc
 
 git secrets --scan-history

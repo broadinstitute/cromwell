b1d661bb48f1a90116fa62b190745c0d8a944606
Adam Nichols <aednichols@gmail.com>
2024-08-20 21:42:14 -0400

    WX-1712 WOM cleanup (#7499)

--- wom/src/main/scala/wom/WomFileMapper.scala
+++ wom/src/main/scala/wom/WomFileMapper.scala
@@ -56,7 +56,6 @@ object WomFileMapper {
             o map Option.apply recover { case _: FileNotFoundException => None } map buildWomOptionalValue
           case None => Success(buildWomOptionalValue(None))
         }
-      case coproduct: WomCoproductValue => mapWomFiles(mapper)(coproduct.womValue)
       case other => Success(other)
     }
 }
--- wom/src/main/scala/wom/values/WomEnumerationValue.scala
+++
@@ -1,8 +0,0 @@
-package wom.values
-
-import wom.types.WomEnumerationType
-
-case class WomEnumerationValue(womType: WomEnumerationType, value: String) extends WomPrimitive {
-
-  override def toWomString: String = value
-}

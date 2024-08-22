a574f9b5d9601a3339c0265b345e33c19eab3c99
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2024-08-22 10:44:54 -0400

    draft test

--- engine/src/test/scala/cromwell/webservice/MetadataBuilderActorSpec.scala
+++ engine/src/test/scala/cromwell/webservice/MetadataBuilderActorSpec.scala
@@ -181,6 +181,32 @@ class MetadataBuilderActorSpec
     assertMetadataResponse(queryAction, mdQuery, events, expectedRes, metadataBuilderActorName)
   }
 
+  it should "build workflow cost response" in {
+    // Note that this is testing dummy behavior and should be updated when we add real logic
+    def makeEvent(workflow: WorkflowId, key: Option[MetadataJobKey]) =
+      MetadataEvent(MetadataKey(workflow, key, "NOT_CHECKED"), MetadataValue("NOT_CHECKED"))
+
+    val workflowId = WorkflowId.randomId()
+    val events = Seq(
+      MetadataEvent(MetadataKey(workflowId, Option(MetadataJobKey("callA", None, 1)), "NOT_CHECKED"),
+                    MetadataValue("NOT_CHECKED")
+      ),
+      MetadataEvent(MetadataKey(workflowId, None, "status"), MetadataValue("Succeeded"))
+    )
+
+    val expectedRes =
+      s"""{
+         |"cost": 3.5,
+         |"currency": "USD",
+         |"id": "${workflowId}",
+         |"status": "Succeeded"
+         |}""".stripMargin
+
+    val action = GetCost(workflowId, false, false)
+
+
+  }
+
   it should "build the call list for failed tasks when prompted" in {
 
     def makeEvent(workflow: WorkflowId, key: Option[MetadataJobKey]) =

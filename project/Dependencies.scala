515b4ecd11e6cbd0caae3375e1cd6804487e2488
Scala Steward <scala_steward@virtuslab.com>
2024-12-01 15:40:47 +0000

    Scala-Steward: Update rdf4j-model, rdf4j-rio-api, ... from 3.7.1 to 3.7.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -102,7 +102,7 @@ object Dependencies {
   private val pact4sV = "0.9.0"
   private val postgresV = "42.4.4"
   private val pprintV = "0.7.3"
-  private val rdf4jV = "3.7.1"
+  private val rdf4jV = "3.7.7"
   private val re2jV = "1.6"
   private val refinedV = "0.10.1"
   private val rhinoV = "1.7.14"

98f43935a36012d6a847ead598ded240e06e7fe9
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2025-07-29 18:15:53 -0400

    initial ci setup

--- src/ci/bin/testCentaurAws.sh
+++ src/ci/bin/testCentaurAws.sh
@@ -6,6 +6,9 @@ export CROMWELL_BUILD_REQUIRES_SECURE=true
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
 
+source "${BASH_SOURCE%/*}/test_aws.inc.sh" || source test_aws.inc.sh
+
+
 cromwell::build::setup_common_environment
 
 cromwell::build::setup_centaur_environment
@@ -26,30 +29,29 @@ export AWS_CONFIG_FILE="${CROMWELL_BUILD_RESOURCES_DIRECTORY}"/aws_config
 # !!!!!!!!!!                WITH DOCKER REPOS                  !!!!!!!!!!
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 cromwell::build::run_centaur \
-     -i hello \
-     -i mutect2.aws
-
-#    -p 100 \
-#    -e smartseq2singlesample \
-#    -e arrays \
-#    -e haplotypecaller \
-#    -e jointdiscovery \
-#    -e mutect2 \
-#    -e singlesample \
-#    -e singlesample_production \
-#    -e cnv_somatic_pair \
-#    -e cnv_somatic_panel \
-#    -e localdockertest \
-#    -e non_root_default_user \
-#    -e non_root_specified_user \
-#    -e abort.scheduled_abort \
-#    -e relative_output_paths \
-#    -e relative_output_paths_colliding \
-#    -e standard_output_paths_colliding_prevented \
-#    -e space \
-#    -e scatter \
-#    -e long_cmd \
-#    -e runtwiceexpectingcallcaching \
-#    -e cachewithinwf
+#     -i hello \
+#     -i mutect2.aws
+    -p 100 \
+    -e smartseq2singlesample \
+    -e arrays \
+    -e haplotypecaller \
+    -e jointdiscovery \
+    -e mutect2 \
+    -e singlesample \
+    -e singlesample_production \
+    -e cnv_somatic_pair \
+    -e cnv_somatic_panel \
+    -e localdockertest \
+    -e non_root_default_user \
+    -e non_root_specified_user \
+    -e abort.scheduled_abort \
+    -e relative_output_paths \
+    -e relative_output_paths_colliding \
+    -e standard_output_paths_colliding_prevented \
+    -e space \
+    -e scatter \
+    -e long_cmd \
+    -e runtwiceexpectingcallcaching \
+    -e cachewithinwf
 
 cromwell::build::generate_code_coverage
---
+++ src/ci/bin/testCentaurAwsRestart.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+export CROMWELL_BUILD_REQUIRES_SECURE=true
+# import in shellcheck / CI / IntelliJ compatible ways
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/test.inc.sh" || source test.inc.sh
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/test_aws.inc.sh" || source test_aws.inc.sh
+
+cromwell::build::setup_common_environment
+
+cromwell::build::setup_centaur_environment
+
+#cromwell::build::batch::setup_aws_centaur_environment
+
+cromwell::build::assemble_jars
+
+cromwell::build::run_centaur \
+    -p 100 \
+    -i restart \
+
+cromwell::build::generate_code_coverage

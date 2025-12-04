13e4544b294183a85c015b6c6e027b384722ee19
Liz Baldo <lizbaldo@Liz-Baldo-H5267Y2XDX.local>
2025-12-04 16:39:40 -0500

    forgot the import

--- engine/src/main/scala/cromwell/webservice/SwaggerUiHttpService.scala
+++ engine/src/main/scala/cromwell/webservice/SwaggerUiHttpService.scala
@@ -1,6 +1,6 @@
 package cromwell.webservice
 
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route

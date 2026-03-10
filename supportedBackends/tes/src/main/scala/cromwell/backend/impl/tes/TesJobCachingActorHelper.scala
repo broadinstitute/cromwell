1dd51a09e140a169e279c72b9a463d0076b582d9
geertvandeweyer <geertvandeweyer@gmail.com>
2026-03-10 09:38:27 +0100

    tes: add memory-retry, local-root, backoff_limit & improved hashing

--- supportedBackends/tes/src/main/scala/cromwell/backend/impl/tes/TesJobCachingActorHelper.scala
+++ supportedBackends/tes/src/main/scala/cromwell/backend/impl/tes/TesJobCachingActorHelper.scala
@@ -16,8 +16,15 @@ trait TesJobCachingActorHelper extends StandardCachingActorHelper {
 
   lazy val tesConfiguration: TesConfiguration = initializationData.tesConfiguration
 
-  lazy val runtimeAttributes =
-    TesRuntimeAttributes(validatedRuntimeAttributes, jobDescriptor.runtimeAttributes, tesConfiguration)
+  lazy val runtimeAttributes = {
+    val raw = jobDescriptor.runtimeAttributes
+    jobLogger.debug(s"[backoff_limit] raw runtimeAttributes keys: ${raw.keys.mkString(", ")}")
+    jobLogger.debug(s"[backoff_limit] raw runtimeAttributes values: ${raw.map { case (k, v) => s"$k=${v.toWomString}" }.mkString(", ")}")
+    jobLogger.debug(s"[backoff_limit] useBackendParameters=${tesConfiguration.useBackendParameters}")
+    val attrs = TesRuntimeAttributes(validatedRuntimeAttributes, raw, tesConfiguration)
+    jobLogger.debug(s"[backoff_limit] resulting backendParameters: ${attrs.backendParameters}")
+    attrs
+  }
   override protected def nonStandardMetadata: Map[String, Any] =
     super.nonStandardMetadata ++ tesJobPaths.azureLogPathsForMetadata
 

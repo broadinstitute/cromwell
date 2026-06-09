ae022272c4852c22be91957db92995c92362a32f
Adam Nichols <aednichols@gmail.com>
2026-06-09 18:23:21 -0400

    Update logback version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,7 @@ object Dependencies {
   private val kindProjectorV = "0.13.2"
   private val kittensV = "2.3.2"
   private val liquibaseV = "4.8.0"
-  private val logbackV = "1.2.13"
+  private val logbackV = "1.5.34"
   private val lz4JavaV = "1.11.0"
   private val mariadbV = "2.7.4"
   /*

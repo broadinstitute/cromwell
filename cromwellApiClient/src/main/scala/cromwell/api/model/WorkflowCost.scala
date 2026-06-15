b4c060c2e4b0f4a27a9eb9a0e6cc6cb0cfa53610
Adam Nichols <aednichols@gmail.com>
2026-06-15 13:11:35 -0400

    Fix some implicits

--- cromwellApiClient/src/main/scala/cromwell/api/model/WorkflowCost.scala
+++ cromwellApiClient/src/main/scala/cromwell/api/model/WorkflowCost.scala
@@ -1,9 +1,10 @@
 package cromwell.api.model
 
 import spray.json.DefaultJsonProtocol
+import spray.json.RootJsonFormat
 
 object WorkflowCostJsonSupport extends DefaultJsonProtocol {
-  implicit val WorkflowCostJsonFormat = jsonFormat5(WorkflowCost)
+  implicit val WorkflowCostJsonFormat: RootJsonFormat[WorkflowCost] = jsonFormat5(WorkflowCost)
 }
 
 final case class WorkflowCost(errors: List[String], id: String, cost: BigDecimal, status: String, currency: String)
--- cromwellApiClient/src/main/scala/cromwell/api/model/WorkflowOutputs.scala
+++ cromwellApiClient/src/main/scala/cromwell/api/model/WorkflowOutputs.scala
@@ -1,9 +1,10 @@
 package cromwell.api.model
 
 import spray.json.{DefaultJsonProtocol, JsValue}
+import spray.json.RootJsonFormat
 
 object WorkflowOutputsJsonSupport extends DefaultJsonProtocol {
-  implicit val OutputResponseFormat = jsonFormat2(WorkflowOutputs)
+  implicit val OutputResponseFormat: RootJsonFormat[WorkflowOutputs] = jsonFormat2(WorkflowOutputs)
 }
 
 case class WorkflowOutputs(id: String, outputs: JsValue)

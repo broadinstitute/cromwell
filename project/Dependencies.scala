afe7aa97bf04369f5439332151fa1443cad0222b
Adam Nichols <aednichols@gmail.com>
2026-07-16 09:45:01 -0400

    Very latest Jackson

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
   private val guavaV = "33.2.1-jre"
   private val hsqldbV = "2.6.1"
   private val http4sV = "0.21.31" // this release is EOL. We need to upgrade further for cats3. https://http4s.org/versions/
-  private val jacksonV = "2.18.9"
+  private val jacksonV = "3.2.1"
   private val jakartaActivationV = "1.2.2"
   private val jakartaAnnotationV = "1.3.5"
   private val jakartaInjectV = "2.6.1"
@@ -649,7 +649,7 @@ object Dependencies {
   )
 
   val jacksonDependencyOverrides: List[ModuleID] = List(
-    "com.fasterxml.jackson.core" % "jackson-core" % jacksonV
+    "tools.jackson.core" % "jackson-core" % jacksonV
   )
 
   val servicesDependencies: List[ModuleID] = List(

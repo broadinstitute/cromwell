36afd77291b5ccc6d15984cd90bae1946ac56976
Adam Nichols <aednichols@gmail.com>
2026-09-11 20:10:47 -0400

    Skip building DRS localizer when not needed

--- src/ci/bin/testCentaurGcpBatchRestart.sh
+++ src/ci/bin/testCentaurGcpBatchRestart.sh
@@ -12,6 +12,9 @@ cromwell::build::setup_common_environment
 
 cromwell::build::setup_centaur_environment
 
+# No DRS tests in this suite. Set a placeholder image so we do not spend time building the DRS localizer.
+export CROMWELL_BUILD_BATCH_DOCKER_IMAGE_DRS="mirror.gcr.io/almalinux:latest"
+
 cromwell::build::batch::setup_batch_centaur_environment
 
 cromwell::build::assemble_jars
